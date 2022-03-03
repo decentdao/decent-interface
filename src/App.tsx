56ac93aa1e6d8945b78a7e415cfeae6a4697b168
Adam Gall <adam@decentlabs.io>
2022-03-03 16:32:28 -0500

    Allow user to connect wallet on main App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,29 @@
+import { useWeb3 } from './web3';
+import { connect } from './web3/providers';
+import useDisplayName from './hooks/useDisplayName';
+
 function App() {
+  const { account } = useWeb3();
+  const accountDisplayName = useDisplayName(account);
+
   return (
     <div className="container my-2">
       <header>
         <p>
           Interface
         </p>
+        <div>
+          {!account && (
+            <button onClick={connect}>
+              connect wallet
+            </button>
+          )}
+          {account && (
+            <div>
+              <div>{accountDisplayName}</div>
+            </div>
+          )}
+        </div>
       </header>
     </div>
   );
