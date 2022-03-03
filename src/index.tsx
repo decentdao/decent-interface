229cb11d38c7d88dff29d29fc4ae3b4283fcb969
Adam Gall <adam@decentlabs.io>
2022-03-03 16:23:19 -0500

    Install and prepare react-toastify

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { ToastContainer } from 'react-toastify';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
+    <ToastContainer />
     <App />
   </React.StrictMode>,
   document.getElementById('root')
