b90bed73c750898529b97eee2af1bfa1b23c6bd6
Christopher Dancy <chris.dancy@decentlabs.io>
2022-04-01 15:18:32 -0400

    Created DAO directory for tracking DAO REST

---
+++ src/components/DAOs/DAO.tsx
@@ -0,0 +1,97 @@
+import { useState, useEffect } from 'react';
+import { useParams, useLocation } from 'react-router-dom';
+
+import EtherscanLink from '../ui/EtherscanLink';
+import useAddress from '../../hooks/useAddress';
+import useIsDAO from '../../hooks/useIsDAO';
+import SearchingDAO from './SearchingDAO';
+import { useDAOData } from '../../daoData';
+
+function ValidDAO({
+  address,
+}: {
+  address: string,
+}) {
+  const [{ name, accessControlAddress }, setDAOAddress] = useDAOData();
+
+  useEffect(() => {
+    setDAOAddress(address);
+  }, [address, setDAOAddress]);
+
+  return (
+    <div>
+      <div>
+        <EtherscanLink address={address}>
+          <span className="break-all">{address}</span>
+        </EtherscanLink> is a valid dao!
+      </div>
+      <div>
+        <div>name: {name}</div>
+        <div>access control address: {accessControlAddress}</div>
+      </div>
+    </div>
+  );
+}
+
+function FoundValidDAO({
+  address,
+}: {
+  address: string | undefined,
+}) {
+  if (address !== undefined) {
+    return (
+      <ValidDAO address={address} />
+    );
+  }
+
+  return <></>;
+}
+
+function Search() {
+  const params = useParams();
+  const [address, validAddress, addressLoading] = useAddress(params.address);
+  const [addressIsDAO, isDAOLoading] = useIsDAO(address);
+
+  const [loading, setLoading] = useState(false);
+  useEffect(() => {
+    setLoading(addressLoading || isDAOLoading);
+  }, [addressLoading, isDAOLoading]);
+
+  return (
+    <SearchingDAO
+      searchAddress={params.address}
+      loading={loading}
+      validAddress={validAddress}
+      address={address}
+      addressIsDAO={addressIsDAO}
+      validDAOComponent={<FoundValidDAO address={address} />}
+    />
+  )
+}
+
+function DAO() {
+  const location = useLocation();
+
+  const [validatedAddress, setValidatedAddress] = useState((location.state as { validatedAddress: string } | null)?.validatedAddress);
+  useEffect(() => {
+    if (!location || !location.state) {
+      setValidatedAddress(undefined);
+      return;
+    }
+
+    const locationState = location.state as { validatedAddress: string };
+    setValidatedAddress(locationState.validatedAddress);
+  }, [location]);
+
+  if (validatedAddress) {
+    return (
+      <ValidDAO address={validatedAddress} />
+    );
+  }
+
+  return (
+    <Search />
+  );
+}
+
+export default DAO;
---
+++ src/components/DAOs/New.tsx
@@ -0,0 +1,9 @@
+function New() {
+    return (
+      <div>
+        make a new DAO
+      </div>
+    );
+  }
+  
+  export default New;
