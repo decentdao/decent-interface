218d5a766b6a639a85ef523b7ab21a35b6931f9b
Christopher Dancy <chris.dancy@decentlabs.io>
2022-04-06 13:58:49 -0400

    Utilize DAO input component for DAOdetails

--- src/components/DAODetails.tsx
+++ src/components/DAODetails.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import CreateDAOInput from './ui/CreateDAOInput'
 
 const DAODetails = ({formData, setFormData}:
   {
@@ -6,22 +7,22 @@ const DAODetails = ({formData, setFormData}:
       DAOName: string,
     },
     setFormData:any,
-  }
-  
-  
+  }  
   ) => {
+  
+  const handleChange = (event:any) => {
+    setFormData({...formData, DAOName: event.target.value})
+  }
   return (
     <div>
-      <div className="text-sm">Fractal Name</div>
-      <div className = "grid grid-cols-3 gap-4">
-        <input 
-        className="col-span-2 w-full border rounded py-1 px-2 shadow-inner"
-        type ="text" 
-        value= {formData.DAOName} 
-        onChange = {(event) => setFormData({...formData, DAOName: event.target.value})}
+        <CreateDAOInput
+          dataType = "text"
+          value = {formData.DAOName}
+          onChange = {handleChange}
+          label= "Fractal Name"
+          helperText = "What is your Fractal called?"
+          disabled= {false}
         />
-        <div className="text-sm text-center">What is your Fractal called?</div>
-      </div>
     </div>
   )
 }
