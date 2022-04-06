b7655839314a2d9a4542a407f828b3f39afdc1d5
Christopher Dancy <chris.dancy@decentlabs.io>
2022-04-06 13:58:58 -0400

    DAO component for gov details

--- src/components/GovernanceDetails.tsx
+++ src/components/GovernanceDetails.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import CreateDAOInput from './ui/CreateDAOInput'
 
 const GovernanceDetails = ({formData, setFormData}:
   {
@@ -12,41 +13,33 @@ const GovernanceDetails = ({formData, setFormData}:
   
   
   ) => {
+
   return (
     <div>
-      <div className="text-sm">Proposal Threshold</div>
-      <div className = "grid grid-cols-3 gap-4">
-        <input 
-        disabled={true}
-        className="col-span-2 w-full border rounded py-1 px-2 shadow-inner"
-        type ="number" 
-        value= {formData.proposalThreshold} 
-        onChange = {(event) => setFormData({...formData, proposalThreshold: event.target.value})}
+              <CreateDAOInput
+          dataType = "text"
+          value = {formData.proposalThreshold}
+          onChange = {true}
+          label= "Proposal Threshold"
+          helperText = "How many tokens does it take to create a proposal?"
+          disabled= {true}
         />
-        <div className="text-sm text-center">How many tokens does it take to create a proposal?</div>
-      </div>
-      <div className="text-sm pt-4">Quorum</div>
-      <div className = "grid grid-cols-3 gap-4">
-        <input 
-        disabled={true}
-          className="col-span-2 w-full border rounded py-1 px-2 shadow-inner"
-          type ="number" 
-          value= {formData.quorum} 
-          onChange = {(event) => setFormData({...formData, quorum: event.target.value})}
-          />
-        <div className="text-sm text-center">What percentage of token votes are required in order for a proposal to pass</div>
-      </div>
-      <div className="text-sm pt-4">Execution Delay</div>
-      <div className = "grid grid-cols-3 gap-4"> 
-        <input 
-        disabled={true}
-          className="col-span-2 w-full border rounded py-1 px-2 shadow-inner"
-          type ="number" 
-          value= {formData.executionDelay === 0 ? "" : formData.executionDelay} 
-          onChange = {(event) => setFormData({...formData, executionDelay: event.target.value})}
+                <CreateDAOInput
+          dataType = "text"
+          value = {formData.quorum}
+          onChange = {true}
+          label= "Quorum"
+          helperText = "What percentage of token votes are required in order for a proposal to pass"
+          disabled= {true}
         />
-        <div className="text-sm text-center">How long after a proposal passes must people wait until it can be executed</div>
-      </div>
+                <CreateDAOInput
+          dataType = "text"
+          value = {formData.executionDelay}
+          onChange = {true}
+          label= "Execution Delay"
+          helperText = "How long after a proposal passes must people wait until it can be executed?"
+          disabled= {true}
+        />
     </div>
   )
 }
