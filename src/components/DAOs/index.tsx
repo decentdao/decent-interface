9c6ca6d91e7a1a7a85ec6cf68fdf9fd36911cc62
Christopher Dancy <chris.dancy@decentlabs.io>
2022-04-07 12:08:04 -0400

    Home page + routing

---
+++ src/components/DAOs/index.tsx
@@ -0,0 +1,19 @@
+import { Routes, Route } from 'react-router-dom';
+
+import DAOSearch from './DAOSearch';
+import DAO from './DAO';
+import New from './New';
+
+function DAOs() {
+  return (
+    <div>
+      <Routes>
+      <Route index element={<DAOSearch />} />
+        <Route path="new" element={<New />} />
+        <Route path=":address" element={<DAO />} />
+      </Routes>
+    </div>
+  )
+}
+
+export default DAOs;
