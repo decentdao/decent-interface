e4402bf2b6c379f29628f720bec310a7fa5c7d0d
Adam Gall <adam@decentlabs.io>
2022-04-07 13:47:05 -0400

    Re-add New component

--- src/components/DAOs/index.tsx
+++ src/components/DAOs/index.tsx
@@ -2,13 +2,14 @@ import { Routes, Route } from 'react-router-dom';
 
 import DAOSearch from './DAOSearch';
 import DAO from './DAO';
+import New from './New';
 
 function DAOs() {
   return (
     <div>
       <Routes>
         <Route index element={<DAOSearch />} />
-        <Route path="new" element={<div>placeholder</div>} />
+        <Route path="new" element={<New />} />
         <Route path=":address" element={<DAO />} />
       </Routes>
     </div>
