4125b7fa6c2c900d43f341ba64bd325361f3aa53
Adam Gall <adam@decentlabs.io>
2022-04-07 13:25:44 -0400

    No build errors, make "daos" resource a placeholder

--- src/components/DAOs/index.tsx
+++ src/components/DAOs/index.tsx
@@ -1,17 +1,7 @@
-import { Routes, Route } from 'react-router-dom';
-
-import DAOSearch from './DAOSearch';
-import DAO from './DAO';
-import New from './New';
-
 function DAOs() {
   return (
     <div>
-      <Routes>
-      <Route index element={<DAOSearch />} />
-        <Route path="new" element={<New />} />
-        <Route path=":address" element={<DAO />} />
-      </Routes>
+      placeholder
     </div>
   )
 }
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,24 +1,22 @@
-import React from 'react'
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const Home = () => {
-    return (
-        <div >
-            <p className= "text-center pt-24 pb-8 text-xl">Welcome to Fractal?</p>                
-                <div className="container mx-auto bg-slate-100 content-center px-32 pt-8">
-                    <p className= "text-center">Where would you like to start?</p>
-                    <div className="grid grid-cols-2 gap-2 py-8">
-                    <div className="bg-black">
-                        <Link to="/daos/new" className="col-span-1 btn"><div className="p-8 text-white text-center">Create a new Fractal</div></Link>
-                    </div>
-                    <div className="bg-black">
-                        <Link to="/daos" className="btn btn-primary"> <div className="p-8 text-white text-center">Find an existing Fractal</div></Link>
-                    </div>
-                    </div>
-                </div>  
-
-            </div>
-      );
+  return (
+    <div >
+      <p className="text-center pt-24 pb-8 text-xl">Welcome to Fractal?</p>
+      <div className="container mx-auto bg-slate-100 content-center px-32 pt-8">
+        <p className="text-center">Where would you like to start?</p>
+        <div className="grid grid-cols-2 gap-2 py-8">
+          <div className="bg-black">
+            <Link to="/daos/new" className="col-span-1 btn"><div className="p-8 text-white text-center">Create a new Fractal</div></Link>
+          </div>
+          <div className="bg-black">
+            <Link to="/daos" className="btn btn-primary"> <div className="p-8 text-white text-center">Find an existing Fractal</div></Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 
