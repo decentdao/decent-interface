5044fd6d75ee4a477272b838f9e15b4b832b252a
Christopher Dancy <chris.dancy@decentlabs.io>
2022-04-08 15:20:18 -0400

    Added gov details page to index w/ correct params

--- src/components/DAOs/New/index.tsx
+++ src/components/DAOs/New/index.tsx
@@ -1,41 +1,40 @@
 import { useEffect, useState } from 'react'
 import DAODetails from './DAODetails';
 import TokenDetails from './TokenDetails';
-// import GovernanceDetails from './GovernanceDetails';
-// import { useWeb3 } from '../../../web3';
-// import { useNavigate } from 'react-router';
+import GovernanceDetails from './GovernanceDetails';
 import Button from '../../ui/Button';
 import ConnectModal from '../../ConnectModal';
-// import DeployDAO from '../../transactions/DeployDAO';
 
 const StepDisplay = ({
   step,
   setPrevEnabled,
   setNextEnabled,
-  
   daoName,
   setDAOName,
-
   tokenName,
   setTokenName,
   tokenSymbol,
   setTokenSymbol,
   tokenSupply,
   setTokenSupply,
+  proposalThreshold,
+  quorum,
+  executionDelay,
 }: {
   step: number,
   setPrevEnabled: React.Dispatch<React.SetStateAction<boolean>>,
   setNextEnabled: React.Dispatch<React.SetStateAction<boolean>>,
-  
   daoName: string | undefined,
   setDAOName: React.Dispatch<React.SetStateAction<string | undefined>>
-
   tokenName: string | undefined,
   setTokenName: React.Dispatch<React.SetStateAction<string | undefined>>,
   tokenSymbol: string | undefined,
   setTokenSymbol: React.Dispatch<React.SetStateAction<string | undefined>>,
   tokenSupply: number | undefined,
   setTokenSupply: React.Dispatch<React.SetStateAction<number | undefined>>,
+  proposalThreshold: number | undefined,
+  quorum: number | undefined,
+  executionDelay: number | undefined,
 }) => {
   if (step === 0) {
     return (
@@ -59,19 +58,20 @@ const StepDisplay = ({
         setSupply={setTokenSupply}
       />
     );
+  } else if (step === 2)  {
+      return (
+        <GovernanceDetails 
+        setPrevEnabled={setPrevEnabled}
+        proposalThreshold= {proposalThreshold}
+        quorum= {quorum}
+        executionDelay = {executionDelay}
+        />
+      );
+    }
+    return <></>
   }
-  return <></>
-  // } else if (step === 1) {
-  //   return <TokenDetails formData={formData} setFormData={setFormData} />;
-  // } else {
-  //   return <GovernanceDetails formData={formData} setFormData={setFormData} />;
-  // }
-}
 
 const New = () => {
-  // const navigate = useNavigate();
-  // const { signerOrProvider } = useWeb3();
-
   const [step, setStep] = useState(0);
 
   const [prevEnabled, setPrevEnabled] = useState(false);
@@ -81,9 +81,9 @@ const New = () => {
   const [tokenName, setTokenName] = useState<string>();
   const [tokenSymbol, setTokenSymbol] = useState<string>();
   const [tokenSupply, setTokenSupply] = useState<number>();
-  const [proposalThreshold, setProposalThreshold] = useState<number>(0);
-  const [quorum, setQuorum] = useState<number>(4);
-  const [executionDelay, setExecutionDelay] = useState<number>(24);
+  const [proposalThreshold] = useState<number>(0);
+  const [quorum] = useState<number>(4);
+  const [executionDelay] = useState<number>(24);
 
   const decrement = () => {
     setStep((currPage) => currPage - 1);
@@ -93,10 +93,6 @@ const New = () => {
     setStep((currPage) => currPage + 1);
   }
 
-  // Able to send transaction / checks for web3 connection - if not - it has a popup and asks for connection
-  // todo: connect to real metafactory not dao creator
-  // todo: move creat dao components to its own folder
-  // todo: create Pending page during transction
   return (
     <div>
       <ConnectModal />
@@ -108,16 +104,17 @@ const New = () => {
               step={step}
               setPrevEnabled={setPrevEnabled}
               setNextEnabled={setNextEnabled}
-              
               daoName={daoName}
               setDAOName={setDAOName}
-
               tokenName={tokenName}
               setTokenName={setTokenName}
               tokenSymbol={tokenSymbol}
               setTokenSymbol={setTokenSymbol}
               tokenSupply={tokenSupply}
               setTokenSupply={setTokenSupply}
+              proposalThreshold= {proposalThreshold}
+              quorum= {quorum}
+              executionDelay = {executionDelay}
             />
           </form>
         </div>
@@ -141,7 +138,7 @@ const New = () => {
           )}
           {step > 1 && (
             <Button onClick={() => {
-              // DeployDAO(formData.daoName, formData.tokenName, formData.tokenSymbol, formData.tokenSupply, signerOrProvider, navigate)
+              console.log(`Deploy DAO ${daoName} ${tokenName} ${tokenSymbol} ${tokenSupply}`)
             }}>
               Create DAO
             </Button>
