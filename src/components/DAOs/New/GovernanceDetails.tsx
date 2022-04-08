0f0115388a4becf823340a6c9f40731d7351538b
Christopher Dancy <chris.dancy@decentlabs.io>
2022-04-08 15:19:59 -0400

    Created Gov Details and aligned style with other steos

--- src/components/DAOs/New/GovernanceDetails.tsx
+++ src/components/DAOs/New/GovernanceDetails.tsx
@@ -1,42 +1,49 @@
+import { useEffect } from 'react';
 import CreateDAOInput from '../../ui/CreateDAOInput';
 
-const GovernanceDetails = ({ formData }:
-  {
-    formData: {
-      proposalThreshold: number,
-      quorum: number,
-      executionDelay: number,
-    },
-    setFormData: any,
-  }
-) => {
+const GovernanceDetails = ({
+  setPrevEnabled,
+  proposalThreshold,
+  quorum,
+  executionDelay,
+}: {
+    setPrevEnabled: React.Dispatch<React.SetStateAction<boolean>>,
+    proposalThreshold: number | undefined,
+    quorum: number | undefined,
+    executionDelay: number | undefined,
+  }) => {
+    
+  useEffect(() => {
+    setPrevEnabled(true);
+  }, [setPrevEnabled]);
+    
   return (
     <div>
       <div className="pb-8 text-lg">Governance Setup</div>
-      {/* <CreateDAOInput
+      <CreateDAOInput
         dataType="text"
-        value={formData.proposalThreshold}
-        onChange={true}
+        value={proposalThreshold?.toString()}
+        onChange={()=> {}}
         label="Proposal Threshold"
         helperText="How many tokens does it take to create a proposal?"
         disabled={true}
       />
       <CreateDAOInput
         dataType="text"
-        value={formData.quorum}
-        onChange={true}
+        value={quorum?.toString()}
+        onChange={()=> {}}
         label="Quorum"
         helperText="What percentage of token votes are required in order for a proposal to pass"
         disabled={true}
       />
       <CreateDAOInput
         dataType="text"
-        value={formData.executionDelay}
-        onChange={true}
+        value={executionDelay?.toString()}
+        onChange={()=> {}}
         label="Execution Delay"
         helperText="How long after a proposal passes must people wait until it can be executed?"
         disabled={true}
-      /> */}
+      />
     </div>
   );
 }
