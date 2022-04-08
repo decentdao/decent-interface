24dae8c240c0fe3f521ae28b6b765eb0cbdd31be
Christopher Dancy <chris.dancy@decentlabs.io>
2022-04-08 18:03:33 -0400

    Created DeplyDao w/ metaFactory

--- src/components/DAOs/New/index.tsx
+++ src/components/DAOs/New/index.tsx
@@ -4,6 +4,12 @@ import TokenDetails from './TokenDetails';
 import GovernanceDetails from './GovernanceDetails';
 import Button from '../../ui/Button';
 import ConnectModal from '../../ConnectModal';
+import { useTransaction } from '../../transactions';
+import { MetaFactory, MetaFactory__factory } from '../../../typechain-types';
+import { useNavigate } from 'react-router';
+import { useWeb3 } from '../../../web3';
+import { ContractReceipt } from '@ethersproject/contracts';
+import { BigNumber, ethers } from 'ethers';
 
 const StepDisplay = ({
   step,
@@ -72,6 +78,10 @@ const StepDisplay = ({
 }
 
 const New = () => {
+  const { signerOrProvider, provider } = useWeb3();
+  let navigate = useNavigate();
+  const { contractCall: contractCallDeploy } = useTransaction();
+
   const [step, setStep] = useState(0);
 
   const [prevEnabled, setPrevEnabled] = useState(false);
@@ -91,6 +101,148 @@ const New = () => {
 
   const increment = () => {
     setStep((currPage) => currPage + 1);
+  }
+
+  const deployDAO = async (daoName: string, tokenName: string, tokenSymbol: string, tokenSupply: number) => {
+
+
+    if (!signerOrProvider || !provider || !daoName || !tokenName || !tokenSymbol || !tokenSupply) {
+      return;
+    }
+    const factory: MetaFactory = MetaFactory__factory.connect("0x5FbDB2315678afecb367f032d93F642f64180aa3", signerOrProvider)
+    const abiCoder = new ethers.utils.AbiCoder();
+
+    const createDAOParams = {
+      daoImplementation: "0x5FC8d32690cc91D4c39d9d3abcBD16989F875707",
+      accessControlImplementation: "0x0165878A594ca255338adfa4d48449f69242Eb8F",
+      daoName: daoName,
+      roles: [
+        "EXECUTE_ROLE",
+        "UPGRADE_ROLE",
+        "WITHDRAWER_ROLE",
+        "GOVERNOR_ROLE",
+      ],
+      rolesAdmins: ["DAO_ROLE", "DAO_ROLE", "DAO_ROLE", "DAO_ROLE"],
+      members: [
+        [],
+        [],
+        [],
+        [],
+      ],
+      daoFunctionDescs: [
+        "execute(address[],uint256[],bytes[])",
+        "upgradeTo(address)",
+      ],
+      daoActionRoles: [["EXECUTE_ROLE"], ["UPGRADE_ROLE"]],
+    };
+
+    const moduleFactoriesCalldata = [
+      {
+        factory: "0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0",
+        data: [abiCoder.encode(["address"], ["0xa513E6E4b8f2a923D98304ec87F64353C4D5C853"])],
+        value: 0,
+        newContractAddressesToPass: [1],
+        addressesReturned: 1,
+      },
+      {
+        factory: "0xCf7Ed3AccA5a467e9e704C703E8D87F634fB0Fc9",
+        data: [
+          abiCoder.encode(["string"], [tokenName]),
+          abiCoder.encode(["string"], [tokenSymbol]),
+          abiCoder.encode(["address[]"], [[]]),
+          abiCoder.encode(
+            ["uint256[]"],
+            [
+              [],
+            ]
+          ),
+          abiCoder.encode(["uint256"], [ethers.utils.parseUnits(tokenSupply.toString(), 18)]),
+        ],
+        value: 0,
+        newContractAddressesToPass: [2],
+        addressesReturned: 1,
+      },
+      {
+        factory: "0xDc64a140Aa3E981100a9becA4E685f962f0cF6C9",
+        data: [
+          abiCoder.encode(["address"], ["0x2279B7A0a67DB372996a5FaB50D91eAA73d2eBe6"]),
+          abiCoder.encode(["address"], ["0x8A791620dd6260079BF849Dc5567aDC3F2FdC318"]),
+          abiCoder.encode(["string"], [""]),
+          abiCoder.encode(["uint64"], [BigNumber.from("0")]),
+          abiCoder.encode(["uint256"], [BigNumber.from("1")]),
+          abiCoder.encode(["uint256"], [BigNumber.from("5")]),
+          abiCoder.encode(["uint256"], [BigNumber.from("0")]),
+          abiCoder.encode(["uint256"], [BigNumber.from("4")]),
+          abiCoder.encode(["uint256"], [BigNumber.from("1")]),
+        ],
+        value: 0,
+        newContractAddressesToPass: [0, 1, 3],
+        addressesReturned: 2,
+      },
+    ];
+
+    const moduleActionCalldata = {
+      contractIndexes: [2, 2, 2, 2, 2, 2, 4, 5, 5, 5, 5, 5],
+      functionDescs: [
+        "withdrawEth(address[],uint256[])",
+        "depositERC20Tokens(address[],address[],uint256[])",
+        "withdrawERC20Tokens(address[],address[],uint256[])",
+        "depositERC721Tokens(address[],address[],uint256[])",
+        "withdrawERC721Tokens(address[],address[],uint256[])",
+        "upgradeTo(address)",
+        "upgradeTo(address)",
+        "upgradeTo(address)",
+        "updateDelay(uint256)",
+        "scheduleBatch(address[],uint256[],bytes[],bytes32,bytes32,uint256)",
+        "cancel(bytes32)",
+        "executeBatch(address[],uint256[],bytes[],bytes32,bytes32)",
+      ],
+      roles: [
+        ["WITHDRAWER_ROLE"],
+        ["WITHDRAWER_ROLE"],
+        ["WITHDRAWER_ROLE"],
+        ["WITHDRAWER_ROLE"],
+        ["WITHDRAWER_ROLE"],
+        ["UPGRADE_ROLE"],
+        ["UPGRADE_ROLE"],
+        ["UPGRADE_ROLE"],
+        ["GOVERNOR_ROLE"],
+        ["GOVERNOR_ROLE"],
+        ["GOVERNOR_ROLE"],
+        ["GOVERNOR_ROLE"],
+      ],
+    };
+
+    contractCallDeploy(
+      () => factory.createDAOAndModules(
+        "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512",
+        0,
+        createDAOParams,
+        moduleFactoriesCalldata,
+        moduleActionCalldata,
+        [[5], [0], [0], [4]]
+      ),
+      'Deploying',
+      'Deploy failed',
+      'Deploy succeeded',
+      undefined,
+      undefined,
+      async (receipt: ContractReceipt) => {
+        const event = receipt.events?.filter((x) => {
+          return x.address === "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512";
+        });
+        console.log(event)
+        if (
+          event === undefined ||
+          event[0].topics[1] === undefined
+        ) {
+          return "";
+        } else {
+          const daoAddress = abiCoder.decode(["address"], event[0].topics[1]);
+          navigate(`/daos/${daoAddress}`,)
+        }
+      }
+    );
   }
 
   return (
@@ -137,9 +289,11 @@ const New = () => {
             </Button>
           )}
           {step > 1 && (
-            <Button onClick={() => {
-              console.log(`Deploy DAO ${daoName} ${tokenName} ${tokenSymbol} ${tokenSupply}`)
-            }}>
+            <Button onClick={async () => {
+              if (!daoName || !tokenName || !tokenSymbol || !tokenSupply) { return }
+              deployDAO(daoName, tokenName, tokenSymbol, tokenSupply)
+            }
+            }>
               Create DAO
             </Button>
           )}
