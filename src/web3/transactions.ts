3759fdacc283bed292ac68576e8c8f24a31d9af7
Christopher Dancy <chris.dancy@decentlabs.io>
2022-04-11 16:05:39 -0400

    cleaned up trnasaction page

--- src/web3/transactions.ts
+++ src/web3/transactions.ts
@@ -1,75 +1,75 @@
-import { ContractReceipt, ethers } from 'ethers';
+import { ContractReceipt, ContractTransaction } from 'ethers';
 import React, { useCallback } from 'react';
 import { toast } from 'react-toastify';
 
 interface ProviderRpcError extends Error {
-    message: string;
-    code: number;
-    data?: unknown;
+  message: string;
+  code: number;
+  data?: unknown;
 }
 
 export const useTransaction = () => {
-    const contractCall = useCallback(
-        (
-            contractFn: () => Promise<ethers.ContractTransaction>,
-            pendingMessage: string,
-            failedMessage: string,
-            successMessage: string,
-            stopedCallback?: () => void,
-            waitingCallback?: () => void,
-            completedCallback?: (txReceipt: ContractReceipt) => void,
-            txnHashCallback?: (hash: string) => void
-        ) => {
-            let toastId: React.ReactText;
-            contractFn()
-                .then((txResponse: ethers.ContractTransaction) => {
-                    toast(
-                        pendingMessage,
-                    );
-                    if (stopedCallback) stopedCallback();
-                    return Promise.all([txResponse.wait(), toastId]);
-                })
-                .then(([txReceipt, toastId]) => {
-                    toast.dismiss(toastId);
-                    if (txReceipt.status === 0) {
-                        toast(failedMessage);
-                    } else if (txReceipt.status === 1) {
-                        toast(successMessage);
-                        if (waitingCallback) waitingCallback();
-                    } else {
-                        toast(failedMessage);
-                    }
-                    if (completedCallback) completedCallback(txReceipt);
+  const contractCall = useCallback(
+    (
+      contractFn: () => Promise<ContractTransaction>,
+      pendingMessage: string,
+      failedMessage: string,
+      successMessage: string,
+      stoppedCallback?: () => void,
+      waitingCallback?: () => void,
+      completedCallback?: (txReceipt: ContractReceipt) => void,
+      txnHashCallback?: (hash: string) => void
+    ) => {
+      let toastId: React.ReactText;
+      contractFn()
+        .then((txResponse: ContractTransaction) => {
+          toast(
+            pendingMessage,
+          );
+          if (stoppedCallback) stoppedCallback();
+          return Promise.all([txResponse.wait(), toastId]);
+        })
+        .then(([txReceipt, toastId]) => {
+          toast.dismiss(toastId);
+          if (txReceipt.status === 0) {
+            toast(failedMessage);
+          } else if (txReceipt.status === 1) {
+            toast(successMessage);
+            if (waitingCallback) waitingCallback();
+          } else {
+            toast(failedMessage);
+          }
+          if (completedCallback) completedCallback(txReceipt);
 
-                    if (txnHashCallback) txnHashCallback(txReceipt.transactionHash);
-                    return txReceipt;
-                })
-                .catch((error: ProviderRpcError) => {
-                    if (stopedCallback) stopedCallback();
-                    toast.dismiss(toastId);
-                    console.error(error);
-                    if (error.code !== 4001) {
-                        switch (error.code) {
-                            case 4100:
-                                toast(failedMessage);
-                                break;
-                            case 4200:
-                                toast(failedMessage);
-                                break;
-                            case 4900:
-                                toast(failedMessage);
-                                break;
-                            case 4901:
-                                toast(failedMessage);
-                                break;
-                        }
-                    } else {
-                        toast(failedMessage);
-                    }
-                });
-        },
-        []
-    );
+          if (txnHashCallback) txnHashCallback(txReceipt.transactionHash);
+          return txReceipt;
+        })
+        .catch((error: ProviderRpcError) => {
+          if (stoppedCallback) stoppedCallback();
+          toast.dismiss(toastId);
+          console.error(error);
+          if (error.code !== 4001) {
+            switch (error.code) {
+              case 4100:
+                toast(failedMessage);
+                break;
+              case 4200:
+                toast(failedMessage);
+                break;
+              case 4900:
+                toast(failedMessage);
+                break;
+              case 4901:
+                toast(failedMessage);
+                break;
+            }
+          } else {
+            toast(failedMessage);
+          }
+        });
+    },
+    []
+  );
 
-    return { contractCall };
+  return { contractCall };
 };
