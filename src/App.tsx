cdcb45a513efeb4470eda6ae05279f36a3847df7
Adam Gall <adam@decentlabs.io>
2022-04-11 15:14:08 -0400

    Explicitly set the "main" site font's weight to "medium" (500)

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen font-medium">
       <Header />
       <main className="py-4 container flex-grow">
         <Body />
