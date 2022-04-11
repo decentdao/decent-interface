1bada828decc7b1e8a61d30eb12a5d221778a3b1
tbwebb22 <tbwebb22@gmail.com>
2022-04-11 17:48:44 -0400

    Update DAOData to include module addresses

--- src/daoData/daoData.ts
+++ src/daoData/daoData.ts
@@ -3,10 +3,13 @@ import { useState } from 'react';
 import useDAOContract from './useDAOContract';
 import useDAOName from './useDAOName';
 import useAccessControlAddress from './useAccessControlAddress';
+import useAccessControlContract from './useAccessControlContract';
+import useModuleAddresses from './useModuleAddresses';
 
 export interface DAOData {
   name: string | undefined,
   accessControlAddress: string | undefined,
+  moduleAddresses: string[] | undefined,
 };
 
 export const useDAODatas = () => {
@@ -15,10 +18,13 @@ export const useDAODatas = () => {
   const daoContract = useDAOContract(daoAddress);
   const name = useDAOName(daoContract);
   const accessControlAddress = useAccessControlAddress(daoContract);
+  const accessControlContract = useAccessControlContract(accessControlAddress);
+  const moduleAddresses = useModuleAddresses(daoContract, accessControlContract);
 
   const daoData: DAOData = {
     name,
     accessControlAddress,
+    moduleAddresses
   };
 
   return [daoData, setDAOAddress] as const;
