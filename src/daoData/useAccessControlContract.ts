ba81bc7c8cf58a3220eddfe4698cb0e9ce8ea0e1
tbwebb22 <tbwebb22@gmail.com>
2022-04-11 17:48:07 -0400

    Create useAccessControlContract.ts

---
+++ src/daoData/useAccessControlContract.ts
@@ -0,0 +1,21 @@
+import { useState, useEffect } from 'react';
+import { AccessControl, AccessControl__factory } from '../typechain-types';
+import { useWeb3 } from '../web3';
+
+const useAccessControlContract = (accessControlAddress: string | undefined) => {
+  const [accessControlContract, setAccessControlContract] = useState<AccessControl>();
+  const { signerOrProvider } = useWeb3();
+
+  useEffect(() => {
+    if(!accessControlAddress || !signerOrProvider) {
+      setAccessControlContract(undefined);
+      return;
+    }
+
+    setAccessControlContract(AccessControl__factory.connect(accessControlAddress, signerOrProvider));
+  }, [accessControlAddress, signerOrProvider]);
+
+  return accessControlContract;
+}
+
+export default useAccessControlContract;
