eee30d4e07e4294bb966a86b9befaa81fa61ce2b
Christopher Dancy <chris.dancy@decentlabs.io>
2022-04-12 17:15:45 -0400

    Transactions working

--- src/components/DAOs/New/index.tsx
+++ src/components/DAOs/New/index.tsx
@@ -4,14 +4,8 @@ import TokenDetails from './TokenDetails';
 import GovernanceDetails from './GovernanceDetails';
 import Button from '../../ui/Button';
 import ConnectModal from '../../ConnectModal';
-import { useTransaction } from '../../../web3/transactions';
-import { MetaFactory, MetaFactory__factory } from '../../../typechain-types';
-import { useNavigate } from 'react-router';
-import { useWeb3 } from '../../../web3';
-import { ContractReceipt } from '@ethersproject/contracts';
-import { BigNumber, ethers } from 'ethers';
-import { useAddresses } from '../../../web3/chains';
 import Pending from '../../Pending';
+import useDeployDAO from '../../../daoData/useDeployDAO';
 
 const StepDisplay = ({
   step,
@@ -80,12 +74,6 @@ const StepDisplay = ({
 }
 
 const New = () => {
-  const { signerOrProvider, chainId } = useWeb3();
-  const addresses = useAddresses(chainId);
-
-  const { contractCall: contractCallDeploy } = useTransaction();
-  const navigate = useNavigate();
-
   const [step, setStep] = useState(0);
   const [prevEnabled, setPrevEnabled] = useState(false);
   const [nextEnabled, setNextEnabled] = useState(false);
@@ -108,226 +96,74 @@ const New = () => {
     setStep((currPage) => currPage + 1);
   }
 
-  const deployDAO = (daoName: string, tokenName: string, tokenSymbol: string, tokenSupply: number) => {
-    if (
-      !signerOrProvider ||
-      !daoName ||
-      !tokenName ||
-      !tokenSymbol ||
-      !tokenSupply ||
-      (!proposalThreshold && proposalThreshold !== 0) ||
-      !quorum ||
-      !executionDelay ||
-      !addresses.metaFactory?.address ||
-      !addresses.daoFactory?.address ||
-      !addresses.dao?.address ||
-      !addresses.accessControl?.address ||
-      !addresses.treasuryModuleFactory?.address ||
-      !addresses.treasuryModule?.address ||
-      !addresses.tokenFactory?.address ||
-      !addresses.governorFactory?.address ||
-      !addresses.governorModule?.address ||
-      !addresses.timelockUpgradeable?.address
-    ) {
-      return;
-    }
-    const factory: MetaFactory = MetaFactory__factory.connect(addresses.metaFactory?.address, signerOrProvider)
-    const abiCoder = new ethers.utils.AbiCoder();
+  const deploy = useDeployDAO(
+    daoName, 
+    tokenName, 
+    tokenSymbol, 
+    tokenSupply,
+    proposalThreshold,
+    quorum,
+    executionDelay
+    )
+return (
+  <div>
+    <Pending
+      pending={pending}
+    />
+    <ConnectModal />
+    <div className="mx-24">
+      <div className="mt-24 text-xl">{!daoName || daoName.trim() === "" ? "New Fractal Configuration" : daoName + " - Configuration"}</div>
+      <div className="mx-auto bg-slate-100 px-8 mt-4 mb-8 pt-8 pb-8 content-center">
+        <form onSubmit={e => e.preventDefault()}>
+          <StepDisplay
+            step={step}
+            setPrevEnabled={setPrevEnabled}
+            setNextEnabled={setNextEnabled}
+            daoName={daoName}
+            setDAOName={setDAOName}
+            tokenName={tokenName}
+            setTokenName={setTokenName}
+            tokenSymbol={tokenSymbol}
+            setTokenSymbol={setTokenSymbol}
+            tokenSupply={tokenSupply}
+            setTokenSupply={setTokenSupply}
+            proposalThreshold={proposalThreshold}
+            quorum={quorum}
+            executionDelay={executionDelay}
+          />
+        </form>
+      </div>
 
-    const createDAOParams = {
-      daoImplementation: addresses.dao?.address,
-      accessControlImplementation: addresses.accessControl?.address,
-      daoName: daoName,
-      roles: [
-        "EXECUTE_ROLE",
-        "UPGRADE_ROLE",
-        "WITHDRAWER_ROLE",
-        "GOVERNOR_ROLE",
-      ],
-      rolesAdmins: ["DAO_ROLE", "DAO_ROLE", "DAO_ROLE", "DAO_ROLE"],
-      members: [
-        [],
-        [],
-        [],
-        [],
-      ],
-      daoFunctionDescs: [
-        "execute(address[],uint256[],bytes[])",
-        "upgradeTo(address)",
-      ],
-      daoActionRoles: [["EXECUTE_ROLE"], ["UPGRADE_ROLE"]],
-    };
-
-    const moduleFactoriesCalldata = [
-      {
-        factory: addresses.treasuryModuleFactory?.address, // Treasury Factory
-        data: [abiCoder.encode(["address"], [addresses.treasuryModule?.address])],
-        value: 0,
-        newContractAddressesToPass: [1],
-        addressesReturned: 1,
-      },
-      {
-        factory: addresses.tokenFactory?.address, // Token Factory
-        data: [
-          abiCoder.encode(["string"], [tokenName]),
-          abiCoder.encode(["string"], [tokenSymbol]),
-          abiCoder.encode(["address[]"], [[]]),
-          abiCoder.encode(
-            ["uint256[]"],
-            [
-              [],
-            ]
-          ),
-          abiCoder.encode(["uint256"], [ethers.utils.parseUnits(tokenSupply.toString(), 18)]),
-        ],
-        value: 0,
-        newContractAddressesToPass: [2],
-        addressesReturned: 1,
-      },
-      {
-        factory: addresses.governorFactory?.address, // Governor Factory
-        data: [
-          abiCoder.encode(["address"], [addresses.governorModule?.address]), // Governor Impl
-          abiCoder.encode(["address"], [addresses.timelockUpgradeable?.address]), // Timelock Impl
-          abiCoder.encode(["string"], [""]),
-          abiCoder.encode(["uint64"], [BigNumber.from("0")]),
-          abiCoder.encode(["uint256"], [BigNumber.from("1")]),
-          abiCoder.encode(["uint256"], [BigNumber.from("5")]),
-          abiCoder.encode(["uint256"], [BigNumber.from("0")]),
-          abiCoder.encode(["uint256"], [BigNumber.from("4")]),
-          abiCoder.encode(["uint256"], [BigNumber.from("1")]),
-        ],
-        value: 0,
-        newContractAddressesToPass: [0, 1, 3],
-        addressesReturned: 2,
-      },
-    ];
-
-    const moduleActionCalldata = {
-      contractIndexes: [2, 2, 2, 2, 2, 2, 4, 5, 5, 5, 5, 5],
-      functionDescs: [
-        "withdrawEth(address[],uint256[])",
-        "depositERC20Tokens(address[],address[],uint256[])",
-        "withdrawERC20Tokens(address[],address[],uint256[])",
-        "depositERC721Tokens(address[],address[],uint256[])",
-        "withdrawERC721Tokens(address[],address[],uint256[])",
-        "upgradeTo(address)",
-        "upgradeTo(address)",
-        "upgradeTo(address)",
-        "updateDelay(uint256)",
-        "scheduleBatch(address[],uint256[],bytes[],bytes32,bytes32,uint256)",
-        "cancel(bytes32)",
-        "executeBatch(address[],uint256[],bytes[],bytes32,bytes32)",
-      ],
-      roles: [
-        ["WITHDRAWER_ROLE"],
-        ["WITHDRAWER_ROLE"],
-        ["WITHDRAWER_ROLE"],
-        ["WITHDRAWER_ROLE"],
-        ["WITHDRAWER_ROLE"],
-        ["UPGRADE_ROLE"],
-        ["UPGRADE_ROLE"],
-        ["UPGRADE_ROLE"],
-        ["GOVERNOR_ROLE"],
-        ["GOVERNOR_ROLE"],
-        ["GOVERNOR_ROLE"],
-        ["GOVERNOR_ROLE"],
-      ],
-    };
-
-    contractCallDeploy(
-      () => factory.createDAOAndModules(
-        addresses.daoFactory?.address!,
-        0,
-        createDAOParams,
-        moduleFactoriesCalldata,
-        moduleActionCalldata,
-        [[5], [0], [0], [4]]
-      ),
-      'Deploying',
-      'Deploy failed',
-      'Deploy succeeded',
-      () => {
-        setPending(false);
-      },
-      () => {
-        setPending(true);
-      },
-      (receipt: ContractReceipt) => {
-        const event = receipt.events?.filter((x) => {
-          return x.address === addresses.daoFactory?.address;
-        });
-        if (
-          event === undefined ||
-          event[0].topics[1] === undefined
-        ) {
-          return "";
-        } else {
-          const daoAddress = abiCoder.decode(["address"], event[0].topics[1]);
-          navigate(`/daos/${daoAddress}`,)
-        }
-      }
-    );
-  }
-  return (
-    <div>
-      <Pending
-        pending={pending}
-      />
-      <ConnectModal />
-      <div className="mx-24">
-        <div className="mt-24 text-xl">{!daoName || daoName.trim() === "" ? "New Fractal Configuration" : daoName + " - Configuration"}</div>
-        <div className="mx-auto bg-slate-100 px-8 mt-4 mb-8 pt-8 pb-8 content-center">
-          <form onSubmit={e => e.preventDefault()}>
-            <StepDisplay
-              step={step}
-              setPrevEnabled={setPrevEnabled}
-              setNextEnabled={setNextEnabled}
-              daoName={daoName}
-              setDAOName={setDAOName}
-              tokenName={tokenName}
-              setTokenName={setTokenName}
-              tokenSymbol={tokenSymbol}
-              setTokenSymbol={setTokenSymbol}
-              tokenSupply={tokenSupply}
-              setTokenSupply={setTokenSupply}
-              proposalThreshold={proposalThreshold}
-              quorum={quorum}
-              executionDelay={executionDelay}
-            />
-          </form>
-        </div>
-
-        <div className="flex items-center justify-center">
-          {step > 0 && (
-            <Button
-              onClick={decrement}
-              disabled={!prevEnabled}
-            >
-              Prev
-            </Button>
-          )}
-          {step < 2 && (
-            <Button
-              onClick={increment}
-              disabled={!nextEnabled}
-            >
-              Next
-            </Button>
-          )}
-          {step > 1 && (
-            <Button onClick={() => {
-              if (!daoName || !tokenName || !tokenSymbol || !tokenSupply) { return }
-              deployDAO(daoName, tokenName, tokenSymbol, tokenSupply)
-            }
-            }>
-              Create DAO
-            </Button>
-          )}
-        </div>
+      <div className="flex items-center justify-center">
+        {step > 0 && (
+          <Button
+            onClick={decrement}
+            disabled={!prevEnabled}
+          >
+            Prev
+          </Button>
+        )}
+        {step < 2 && (
+          <Button
+            onClick={increment}
+            disabled={!nextEnabled}
+          >
+            Next
+          </Button>
+        )}
+        {step > 1 && (
+          <Button onClick={() => {
+            if (!daoName || !tokenName || !tokenSymbol || !tokenSupply) { return }
+            deploy();
+          }
+          }>
+            Create DAO
+          </Button>
+        )}
       </div>
     </div>
-  );
+  </div>
+);
 }
 
 export default New;
