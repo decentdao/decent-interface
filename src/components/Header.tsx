d933b4c1aab033f0e21aac33e63a161ab833b00e
Christopher Dancy <chris.dancy@decentlabs.io>
2022-04-13 16:39:21 -0400

    added dropdown to disconnet account and updated colors for account

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,10 @@
-import { Suspense } from 'react';
+import { Suspense, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { Jazzicon } from '@ukstv/jazzicon-react';
 import { useImage } from 'react-image';
 
 import { useWeb3 } from '../web3';
 import { connect, disconnect } from '../web3/providers';
-import Button from './ui/Button';
 import useDisplayName from '../hooks/useDisplayName';
 import useAvatar from '../hooks/useAvatar';
 import EtherscanLink from './ui/EtherscanLink';
@@ -16,7 +15,7 @@ function JazziconAvatar({
   address: string,
 }) {
   return (
-    <div className="ml-2 h-10 w-10">
+    <div className="ml-2 h-8 w-8">
       <Jazzicon address={address} />
     </div>
   );
@@ -59,9 +58,14 @@ function Avatar({
 }
 
 function Header() {
+  const [dropDown, setDropDown] = useState<boolean>(false);
   const { account } = useWeb3();
   const accountDisplayName = useDisplayName(account);
   const avatarURL = useAvatar(account);
+
+  const toggleDropDown = () => {
+    setDropDown((curr) => !curr)
+  }
 
   return (
     <header className="py-4 border-b bg-header-black">
@@ -73,33 +77,55 @@ function Header() {
             </Link>
           </div>
         </div>
-        <div className="sm:text-right text-white">
+        <div className="sm:text-right text-sm text-header-gold">
           {!account && (
-            <Button
+            <button
               onClick={connect}
               disabled={false}
             >
               Connect Wallet
-            </Button>
+            </button>
           )}
           {account && (
             <div className="flex items-center">
-              <div className="flex flex-col items-end">
-                <div className="text-xl">
+              <EtherscanLink address={account}>
+                <Avatar address={account} url={avatarURL} />
+              </EtherscanLink>
+              <div className="pl-2 flex flex-col items-end">
+                <div className="sm:text-right text-sm">
                   <EtherscanLink address={account}>
                     {accountDisplayName}
                   </EtherscanLink>
                 </div>
-                <button
-                  className="px-0.5 border rounded shadow text-xs block"
-                  onClick={disconnect}
-                >
-                  disconnect
-                </button>
               </div>
-              <EtherscanLink address={account}>
-                <Avatar address={account} url={avatarURL} />
-              </EtherscanLink>
+
+              <div className="relative">
+                <button
+                  id="dropdownInformationButton"
+                  data-dropdown-toggle="dropdownInformation"
+                  className="text-white"
+                  onClick={toggleDropDown}
+                  type="button">
+                  <svg className="ml-2 w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7"></path>
+                  </svg>
+                </button>
+
+                {
+                  dropDown &&
+                  <div id="dropdownInformation"
+                    className="bg-white absolute right-0 text-base z-50 list-none divide-y divide-gray-100 rounded shadow my-4">
+                    <div className="py-1">
+                      <button
+                        className="block py-2 px-4 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white"
+                        onClick={disconnect}
+                      >
+                        Disconnect
+                      </button>
+
+                    </div>
+                  </div>
+                }
+              </div>
             </div>
           )}
         </div>
