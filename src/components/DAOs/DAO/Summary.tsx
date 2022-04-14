1bb43ab8efbf8d1e536ffe236d6e1cfd880804a7
Adam Gall <adam@decentlabs.io>
2022-04-14 12:13:27 -0400

    Move the DAO Details component out of Summary and into own file

--- src/components/DAOs/DAO/Summary.tsx
+++ src/components/DAOs/DAO/Summary.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useParams, useLocation } from "react-router-dom";
+import { useParams, useLocation, Link } from "react-router-dom";
 
 import EtherscanLink from "../../ui/EtherscanLink";
 import useAddress from "../../../hooks/useAddress";
@@ -9,26 +9,10 @@ import { useDAOData } from "../../../daoData";
 import H1 from "../../ui/H1";
 import Button from "../../ui/Button";
 
-function ValidDAO({ address }: { address: string }) {
-  const [showDetails, setShowDetails] = useState(false);
-
-  if (showDetails) {
-    return (
-      <ValidDAODetails address={address} setShowDetails={setShowDetails} />
-    );
-  } else {
-    return (
-      <ValidDAOSummary address={address} setShowDetails={setShowDetails} />
-    );
-  }
-}
-
-function ValidDAOSummary({
+function ValidDAO({
   address,
-  setShowDetails,
 }: {
   address: string;
-  setShowDetails: React.Dispatch<React.SetStateAction<boolean>>;
 }) {
   const [{ name, accessControlAddress }, setDAOAddress] =
     useDAOData();
@@ -46,49 +30,9 @@ function ValidDAOSummary({
         is a valid dao!
       </H1>
       <div>
-        <Button disabled={false} onClick={() => setShowDetails(true)}>
-          DAO Details
-        </Button>
+        <Link to="details">DAO Details</Link>
         <div>name: {name}</div>
         <div>access control address: {accessControlAddress}</div>
-      </div>
-    </div>
-  );
-}
-
-function ValidDAODetails({
-  address,
-  setShowDetails,
-}: {
-  address: string;
-  setShowDetails: React.Dispatch<React.SetStateAction<boolean>>;
-}) {
-  const [{ name, accessControlAddress, moduleAddresses }, setDAOAddress] =
-    useDAOData();
-
-  useEffect(() => {
-    setDAOAddress(address);
-  }, [address, setDAOAddress]);
-
-  return (
-    <div>
-      <H1>
-        <EtherscanLink address={address}>
-          <span className="break-all">{address}</span>
-        </EtherscanLink>{" "}
-        Details
-      </H1>
-      <div>
-        <Button disabled={false} onClick={() => setShowDetails(false)}>
-          Back
-        </Button>
-        <div>name: {name}</div>
-        <div>access control address: {accessControlAddress}</div>
-        {moduleAddresses?.map((address, index) => (
-          <div key={address}>
-            Module {index}: {address}
-          </div>
-        ))}
       </div>
     </div>
   );
