cffeb91b5a833a9c002f52f7a8d0af4b9dc600b6
Adam Gall <adam@decentlabs.io>
2022-04-14 12:14:40 -0400

    Create route to show the DAO details component at /address/details

--- src/components/DAOs/DAO/index.tsx
+++ src/components/DAOs/DAO/index.tsx
@@ -1,10 +1,19 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useParams } from 'react-router-dom';
+
 import Summary from './Summary';
+import Details from './Details';
 
 function DAOs() {
+  const params = useParams();
+
+  if (!params.address) {
+    return <div>if you see this, it's a bug</div>
+  }
+
   return (
     <Routes>
       <Route index element={<Summary />} />
+      <Route path="details" element={<Details address={params.address} />} />
     </Routes>
   );
 }
