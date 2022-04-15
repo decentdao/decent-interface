374070143beab6630ff311a901bfc0ffec74a5b6
tbwebb22 <tbwebb22@gmail.com>
2022-04-15 15:05:18 -0400

    Refactor useCreateProposal hook

--- src/daoData/useCreateProposal.ts
+++ src/daoData/useCreateProposal.ts
@@ -2,6 +2,7 @@ import { useCallback, useEffect } from 'react'
 import { useTransaction } from '../web3/transactions';
 import { useWeb3 } from '../web3';
 import { BigNumber } from 'ethers';
+import { useNavigate } from 'react-router';
 import { GovernorModule, GovernorModule__factory } from '../typechain-types';
 
 type ProposalData = {
@@ -12,16 +13,19 @@ type ProposalData = {
 }
 
 const useCreateProposal = ({
+  daoAddress,
   governorAddress,
   proposalData,
   setPending,
 }: {
-  governorAddress: string,
-  proposalData: ProposalData,
+  daoAddress: string | undefined,
+  governorAddress: string | undefined,
+  proposalData: ProposalData | undefined,
   setPending: React.Dispatch<React.SetStateAction<boolean>>
 }
 ) => {
   const { signerOrProvider } = useWeb3();
+  const navigate = useNavigate();
 
   const [contractCallDeploy, contractCallPending] = useTransaction();
 
@@ -33,7 +37,8 @@ const useCreateProposal = ({
     if (
       !signerOrProvider ||
       !proposalData ||
-      !setPending
+      !setPending ||
+      !governorAddress
     ) {
       return;
     }
@@ -46,13 +51,13 @@ const useCreateProposal = ({
       failedMessage: "Proposal Creation Failed",
       successMessage: "Proposal Created",
       successCallback: (receipt) => {
-        return("Proposal Created");
+        navigate(`/daos/${daoAddress}`,);
       },
       rpcErrorCallback: (error: any) => {
         console.error(error)
       },
     });
-  }, [contractCallDeploy, governorAddress, proposalData, setPending, signerOrProvider])
+  }, [daoAddress, navigate, contractCallDeploy, governorAddress, proposalData, setPending, signerOrProvider])
   return createProposal;
 }
 
