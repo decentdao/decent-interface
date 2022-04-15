5520878fcfa8d5f8d1b56227982a8da9cb1a3b6f
Christopher Dancy <chris.dancy@decentlabs.io>
2022-04-15 13:37:06 -0400

    removed footer and added background image and linear

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,17 @@
 import Header from './components/Header';
 import Body from './components/Body'
 import Footer from './components/Footer';
+import image from "./assets/images/bg-glow-top-left.png";
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen font-medium">
       <Header />
-      <main className="py-4 container flex-grow">
+      <main className="py-4 flex-grow h-1"
+        style={{ backgroundImage: `url(${image}), linear-gradient(to bottom, #272520, #1b1a18)` }}
+      >
         <Body />
       </main>
-      <Footer />
     </div>
   );
 }
