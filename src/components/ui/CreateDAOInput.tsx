2a829a71635f2485456e2d2112a1d8121a782692
Christopher Dancy <chris.dancy@decentlabs.io>
2022-04-18 17:55:54 -0400

    conditional styling input layout for inputs

--- src/components/ui/CreateDAOInput.tsx
+++ src/components/ui/CreateDAOInput.tsx
@@ -11,15 +11,15 @@ const CreateDAOInput = ({ dataType, value, onChange, label, helperText, disabled
     <div className="bg-lighterBlack rounded-lg my-4">
       <div className= "px-4 py-4">
       <div className="text-sm text-mediumGray pb-2">{label}</div>
-      <div className="grid grid-cols-3 gap-4">
+      <div className="md:grid md:grid-cols-3 md:gap-4 flex flex-col items-center">
         <input
-          className="col-span-2 w-full border border-lightBlack bg-medBlack rounded py-1 text-mediumGray pl-2"
+          className="md:col-span-2 w-full border border-lightBlack bg-medBlack rounded py-1 px-2 text-mediumGray"
           type={dataType}
           value={value || ""}
           onChange={(event) => onChange(event.target.value)}
           disabled={disabled}
         />
-        <div className="text-sm text-mediumGray text-center">{helperText}</div>
+        <div className="md:pt-0 pt-2 text-sm text-mediumGray text-center">{helperText}</div>
         </div>
       </div>
     </div>
