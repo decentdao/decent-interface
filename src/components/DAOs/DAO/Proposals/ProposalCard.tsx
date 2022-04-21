d829e4ff3b81c4274927fb187db6a13e0e3311be
tbwebb22 <tbwebb22@gmail.com>
2022-04-21 11:52:46 -0400

    Remove votes percentages and add description to proposal card

--- src/components/DAOs/DAO/Proposals/ProposalCard.tsx
+++ src/components/DAOs/DAO/Proposals/ProposalCard.tsx
@@ -4,17 +4,12 @@ import { Link } from "react-router-dom";
 function ProposalCard({ proposal }: { proposal: ProposalData }) {
 
   return (
+    <Link to={`proposals/${proposal.number}`} className="underline">
     <div className="flex flex-col bg-gray-300 m-2 max-w-lg">
       <div>Proposal #{proposal.number}</div>
-      <div>For: {proposal.forVotesPercent}%</div>
-      <div>Against: {proposal.againstVotesPercent}%</div>
-      <div>Abstain: {proposal.abstainVotesPercent}%</div>
-      <div className="m-4">
-        <Link to={`proposals/${proposal.number}`} className="underline">
-          View Proposal
-        </Link>
-      </div>
+      <div>{proposal.description}</div>
     </div>
+    </Link>
   );
 }
 
