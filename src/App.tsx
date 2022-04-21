e7873c20616acd29f4b27648700ec4b4c2c3609c
Adam Gall <adam@decentlabs.io>
2022-04-21 11:33:05 -0400

    Move some container/spacing divs up from Body to App, remove x padding

--- src/App.tsx
+++ src/App.tsx
@@ -5,9 +5,10 @@ function App() {
   return (
     <div className="flex flex-col min-h-screen font-medium">
       <Header />
-      <main className="flex-grow bg-image-pattern"
-      >
-        <Body />
+      <main className="flex-grow bg-image-pattern">
+        <div className="container pt-20">
+          <Body />
+        </div>
       </main>
     </div>
   );
--- src/components/Body.tsx
+++ src/components/Body.tsx
@@ -5,14 +5,10 @@ import DAOs from './DAOs/index';
 
 function Body() {
   return (
-    <div className="container">
-      <div className= "lg:px-36 py-20">
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="daos/*" element={<DAOs />} />
-        </Routes>
-      </div>
-    </div>
+    <Routes>
+      <Route index element={<Home />} />
+      <Route path="daos/*" element={<DAOs />} />
+    </Routes>
   )
 }
 
