4d871d22453a1fa62990c6e210a0f06e465c2b6e
Adam Gall <adam@decentlabs.io>
2022-04-21 12:56:51 -0400

    Rename a property to be more descriptive

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -4,17 +4,17 @@ import H1 from "./ui/H1";
 
 const ColoredButton = ({
   to,
-  color,
+  backgroundColor,
   textColor,
   children,
 }: {
   to: string,
-  color: string,
+  backgroundColor: string,
   textColor: string,
   children: React.ReactNode,
 }) => {
   return (
-    <div className={`border border-gold-500 ${color} rounded-lg md:my-0 mb-4 md:w-full w-96`}>
+    <div className={`border border-gold-500 ${backgroundColor} rounded-lg md:my-0 mb-4 md:w-full w-96`}>
       <Link to={to}>
         <p className={`${textColor} text-center py-4 px-2`}>
           {children}
@@ -30,8 +30,8 @@ const Home = () => {
       <H1>Welcome to Fractal</H1>
       <ContentBox title="What path will you take?">
         <div className="md:grid md:grid-cols-2 md:gap-6 flex flex-col items-center py-4">
-          <ColoredButton to="/daos/new" color='bg-gold-500' textColor="text-gray-900">Create a Fractal</ColoredButton>
-          <ColoredButton to="/daos" color="bg-chocolate-400" textColor="text-gray-500">Find a Fractal</ColoredButton>
+          <ColoredButton to="/daos/new" backgroundColor='bg-gold-500' textColor="text-gray-900">Create a Fractal</ColoredButton>
+          <ColoredButton to="/daos" backgroundColor="bg-chocolate-400" textColor="text-gray-500">Find a Fractal</ColoredButton>
         </div>
       </ContentBox>
     </div>
