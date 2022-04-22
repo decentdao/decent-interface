23e4b215cd26c40534033a2fd788fb435d740739
tbwebb22 <tbwebb22@gmail.com>
2022-04-22 16:54:35 -0400

    Add start and end dates to proposal card

--- src/components/DAOs/DAO/Proposals/ProposalCard.tsx
+++ src/components/DAOs/DAO/Proposals/ProposalCard.tsx
@@ -1,14 +1,26 @@
 import { ProposalData } from "../../../../daoData/useProposals";
+import useDisplayName from "../../../../hooks/useDisplayName";
 import { Link } from "react-router-dom";
+import { useEffect } from "react";
 
 function ProposalCard({ proposal }: { proposal: ProposalData }) {
+  const proposerDisplayName = useDisplayName(proposal.proposer);
+
+  useEffect(() => {
+    console.log(proposal);
+  }, [proposal]);
 
   return (
-    <Link to={`proposals/${proposal.number}`} className="underline">
-    <div className="flex flex-col bg-gray-300 m-2 max-w-lg">
-      <div>Proposal #{proposal.number}</div>
-      <div>{proposal.description}</div>
-    </div>
+    <Link to={`proposals/${proposal.number}`}>
+      <div className="flex flex-col bg-gray-300 mx-2 max-w-lg py-2">
+        <div className="flex flex-row mx-1">
+          <div className="mx-1">{proposal.stateString}</div>
+          <div className="mx-1">#{proposal.number}</div>
+          <div className="mx-1">{proposal.startTimeString} - {proposal.endTimeString}</div>
+        </div>
+        <div className="mx-2">{proposal.description}</div>
+        <div className="mx-2">Created by {proposerDisplayName}</div>
+      </div>
     </Link>
   );
 }
