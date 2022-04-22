8f7de20d11d5e2f3c30a095c3bc3a08e2f04ef25
tbwebb22 <tbwebb22@gmail.com>
2022-04-22 19:17:14 -0400

    Make ProposalsList component reusable within DAO Summary component

--- src/components/DAOs/DAO/Proposals/ProposalsList.tsx
+++ src/components/DAOs/DAO/Proposals/ProposalsList.tsx
@@ -1,24 +1,14 @@
-import EtherscanLink from "../../../ui/EtherscanLink";
 import { useDAOData } from "../../../../daoData";
-import H1 from "../../../ui/H1";
 import ProposalCard from "./ProposalCard";
 
 function ProposalsList({ address }: { address: string }) {
   const [{ proposals }] = useDAOData();
 
   return (
-    <div>
-      <H1>
-        <EtherscanLink address={address}>
-          <span className="break-all">{address}</span>
-        </EtherscanLink>{" "}
-        Proposal List
-        <div className="grid grid-cols-3 gap-4">
-        {proposals?.map((proposal) => (
-          <ProposalCard key={proposal.number} proposal={proposal} />
-        ))}
-        </div>
-      </H1>
+    <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+      {proposals?.map((proposal) => (
+        <ProposalCard key={proposal.number} proposal={proposal} />
+      ))}
     </div>
   );
 }
