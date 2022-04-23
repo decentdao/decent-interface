fe0cbc0270f32c054f4555e050b65694e6e5c546
tbwebb22 <tbwebb22@gmail.com>
2022-04-22 22:58:11 -0400

    Style ProposalVotes

--- src/components/DAOs/DAO/Proposals/ProposalVotes.tsx
+++ src/components/DAOs/DAO/Proposals/ProposalVotes.tsx
@@ -2,22 +2,22 @@ import { ProposalData } from "../../../../daoData/useProposals";
 
 function ProposalVotes({ proposal }: { proposal: ProposalData }) {
   return (
-    <div className="flex flex-col bg-gray-300 m-2 max-w-xs py-2 rounded-md">
-      <div className="flex mx-2 my-1">Results</div>
-      <hr className="mx-2 my-1" />
+    <div className="flex flex-col bg-gray-600 m-2 max-w-xs py-2 rounded-md">
+      <div className="flex mx-2 my-1 text-gray-25">Results</div>
+      <hr className="mx-2 my-1 border-gray-200" />
       <div className="flex flex-row mx-2 my-1">
-        <div className="flex flex-grow mr-4">Yes</div>
-        <div className="flex ml-2">{proposal.forVotesPercent}%</div>
+        <div className="flex flex-grow mr-4 text-gray-50">Yes</div>
+        <div className="flex ml-2 text-gray-25">{proposal.forVotesPercent}%</div>
       </div>
-      <hr className="mx-2 my-1" />
+      <hr className="mx-2 my-1 border-gray-200" />
       <div className="flex flex-row mx-2 my-1">
-        <div className="flex flex-grow mr-4">No</div>
-        <div className="flex ml-2">{proposal.againstVotesPercent}%</div>
+        <div className="flex flex-grow mr-4 text-gray-50">No</div>
+        <div className="flex ml-2 text-gray-25">{proposal.againstVotesPercent}%</div>
       </div>
-      <hr className="mx-2 my-1" />
+      <hr className="mx-2 my-1 border-gray-200" />
       <div className="flex flex-row mx-2 my-1">
-        <div className="flex flex-grow mr-4">Abstain</div>
-        <div className="flex ml-4">{proposal.abstainVotesPercent}%</div>
+        <div className="flex flex-grow mr-4 text-gray-50">Abstain</div>
+        <div className="flex ml-4 text-gray-25">{proposal.abstainVotesPercent}%</div>
       </div>
     </div>
   );
