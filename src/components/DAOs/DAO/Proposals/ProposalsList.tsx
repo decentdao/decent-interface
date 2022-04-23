f97b02101aa88a72f88b5d6cfa22957e636267c6
tbwebb22 <tbwebb22@gmail.com>
2022-04-22 20:25:04 -0400

    Add showId prop to ProposalCard

--- src/components/DAOs/DAO/Proposals/ProposalsList.tsx
+++ src/components/DAOs/DAO/Proposals/ProposalsList.tsx
@@ -7,7 +7,7 @@ function ProposalsList() {
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
       {proposals?.map((proposal) => (
-        <ProposalCard key={proposal.number} proposal={proposal} />
+        <ProposalCard key={proposal.number} proposal={proposal} showId={false} />
       ))}
     </div>
   );
