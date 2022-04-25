e6a97a239cd6d10f9ece35b41a8460edd1e3e7b7
tbwebb22 <tbwebb22@gmail.com>
2022-04-25 17:38:20 -0400

    Create useGovernorModuleContract.ts

---
+++ src/daoData/useGovernorModuleContract.ts
@@ -0,0 +1,27 @@
+import { useState, useEffect } from "react";
+import { GovernorModule, GovernorModule__factory } from "../typechain-types";
+import { useWeb3 } from "../web3";
+
+const useGovernorModuleContract = (moduleAddresses: string[] | undefined) => {
+  const [governorModule, setGovernorModule] = useState<GovernorModule>();
+  const { signerOrProvider } = useWeb3();
+
+  useEffect(() => {
+    if (
+      moduleAddresses === undefined ||
+      moduleAddresses[1] === undefined ||
+      signerOrProvider === undefined
+    ) {
+      setGovernorModule(undefined);
+      return;
+    }
+
+    setGovernorModule(
+      GovernorModule__factory.connect(moduleAddresses[1], signerOrProvider)
+    );
+  }, [moduleAddresses, signerOrProvider]);
+
+  return governorModule;
+};
+
+export default useGovernorModuleContract;
