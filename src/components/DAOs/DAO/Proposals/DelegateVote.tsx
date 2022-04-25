1285b54a867bb62b90bb84981596c10abfe5b2c9
tbwebb22 <tbwebb22@gmail.com>
2022-04-25 17:37:01 -0400

    Add DelegateVote component

---
+++ src/components/DAOs/DAO/Proposals/DelegateVote.tsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+import Button from "../../../ui/Button";
+import useDelegateVote from "../../../../daoData/useDelegateVote";
+import CreateDAOInput from "../../../ui/CreateDAOInput";
+import ConnectModal from "../../../ConnectModal";
+import Pending from "../../../Pending";
+
+function DelegateVote() {
+  const [delegatee, setDelegatee] = useState<string>();
+  const [pending, setPending] = useState<boolean>(false);
+
+  const delegateVote = useDelegateVote({
+    delegatee: delegatee,
+    setPending,
+  });
+
+  return (
+    <>
+      <Pending message="Delegating Vote..." pending={pending} />
+      <ConnectModal />
+      <div className="flex flex-col bg-gray-600 m-2 p-2 max-w-xs py-2 rounded-md">
+        <div className="flex mx-2 my-1 text-gray-25">Delegate Vote</div>
+        <hr className="mx-2 my-1 border-gray-200" />
+        <CreateDAOInput
+          dataType="text"
+          value={delegatee}
+          onChange={(e) => setDelegatee(e)}
+          label="Delegatee Address"
+          helperText="The address to delegate your votes to"
+          disabled={false}
+        />
+        <Button onClick={() => delegateVote()}>Delegate</Button>
+      </div>
+    </>
+  );
+}
+
+export default DelegateVote;
