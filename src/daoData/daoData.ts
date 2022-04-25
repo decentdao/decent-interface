0c3c42aa06cb274b5ecf42955733625f5812e9f7
tbwebb22 <tbwebb22@gmail.com>
2022-04-25 17:38:09 -0400

    Add governorModuleContract and tokenContract to DAOData object

--- src/daoData/daoData.ts
+++ src/daoData/daoData.ts
@@ -5,14 +5,19 @@ import useDAOName from './useDAOName';
 import useAccessControlAddress from './useAccessControlAddress';
 import useAccessControlContract from './useAccessControlContract';
 import useModuleAddresses from './useModuleAddresses';
+import useGovernorModuleContract from './useGovernorModuleContract';
+import useTokenContract from './useTokenContract';
 import useProposals from './useProposals';
 import { ProposalData } from './useProposals';
+import { GovernorModule, VotesTokenWithSupply } from '../typechain-types';
 
 export interface DAOData {
   name: string | undefined,
   accessControlAddress: string | undefined,
   moduleAddresses: string[] | undefined,
   proposals: ProposalData[] | undefined,
+  governorModuleContract: GovernorModule | undefined,
+  tokenContract: VotesTokenWithSupply | undefined
 };
 
 export const useDAODatas = () => {
@@ -23,13 +28,17 @@ export const useDAODatas = () => {
   const accessControlAddress = useAccessControlAddress(daoContract);
   const accessControlContract = useAccessControlContract(accessControlAddress);
   const moduleAddresses = useModuleAddresses(daoContract, accessControlContract);
+  const governorModuleContract = useGovernorModuleContract(moduleAddresses);
+  const tokenContract = useTokenContract(governorModuleContract);
   const proposals = useProposals(moduleAddresses);
 
   const daoData: DAOData = {
     name,
     accessControlAddress,
     moduleAddresses,
-    proposals
+    proposals,
+    governorModuleContract,
+    tokenContract
   };
 
   return [daoData, setDAOAddress] as const;
