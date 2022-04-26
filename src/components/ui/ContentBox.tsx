284aa5b0816bd9e13a9618557287d1675a698608
Christopher Dancy <chris.dancy@decentlabs.io>
2022-04-27 00:48:41 +0200

    added on hover attribute for proposal component

--- src/components/ui/ContentBox.tsx
+++ src/components/ui/ContentBox.tsx
@@ -2,13 +2,15 @@ import ContentBoxTitle from "./ContentBoxTitle";
 
 const ContentBox = ({
   title,
+  onHover,
   children,
 }: {
   title?: string,
+  onHover?: string,
   children: React.ReactNode,
 }) => {
   return (
-    <div className="rounded-lg bg-gray-600 px-4 py-4 my-4">
+    <div className={`rounded-lg bg-gray-600 px-4 py-4 my-4 ${onHover}`}>
       {title && <ContentBoxTitle>
         {title}
       </ContentBoxTitle>}
