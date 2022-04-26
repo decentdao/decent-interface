3f288c13211aeb05dc574de343f79d9d16da74e1
Christopher Dancy <chris.dancy@decentlabs.io>
2022-04-27 00:56:43 +0200

    spacing between proposals

--- src/components/DAOs/DAO/Proposals/ProposalsList.tsx
+++ src/components/DAOs/DAO/Proposals/ProposalsList.tsx
@@ -5,7 +5,7 @@ function ProposalsList() {
   const [{ proposals }] = useDAOData();
 
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 gap-4">
+    <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 lg:gap-4">
       {proposals?.slice(0).reverse().map((proposal) => (
         <ProposalCard key={proposal.number} proposal={proposal} />
       ))}
