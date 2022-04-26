b47174765355f5c1f6639120a1fa354786db9be5
Christopher Dancy <chris.dancy@decentlabs.io>
2022-04-27 00:48:51 +0200

    style componenet

--- src/components/DAOs/DAO/Proposals/ProposalCard.tsx
+++ src/components/DAOs/DAO/Proposals/ProposalCard.tsx
@@ -1,6 +1,8 @@
 import { ProposalData } from "../../../../daoData/useProposals";
 import useDisplayName from "../../../../hooks/useDisplayName";
 import { Link } from "react-router-dom";
+import ContentBox from "../../../ui/ContentBox";
+import Clock from "../../../ui/svg/Clock";
 
 function ProposalCard({
   proposal
@@ -11,17 +13,20 @@ function ProposalCard({
 
   return (
     <Link to={`proposals/${proposal.number}`}>
-      <div className="flex flex-col bg-gray-300 m-2 max-w-lg py-2 rounded-md">
-        <div className="flex flex-row mx-1">
-          <div className="mx-1">{proposal.stateString}</div>
-          <div className="mx-1">#{proposal.number}</div>
-          <div className="mx-1">
+      <ContentBox onHover="hover:border-2 hover:border-gold-300 hover:shadow-sm hover:shadow-gold-300">
+        <div className="flex flex-row">
+          <div className="px-2 py-1 bg-chocolate-400 border border-gold-500 rounded text-gold-500 text-sm">{proposal.stateString}</div>
+          <div className="px-4 py-1 text-gray-25 text-sm">#{proposal.number}</div>
+          <div className="py-1">
+            <Clock />
+          </div>
+          <div className="px-2 py-1 text-gray-50 text-sm">
             {proposal.startTimeString} - {proposal.endTimeString}
           </div>
         </div>
-        <div className="mx-2">{proposal.description}</div>
-        <div className="mx-2">Created By: {proposerDisplayName}</div>
-      </div>
+        <div className="py-4 text-white">{proposal.description}</div>
+        <div className="text-gray-50 text-sm">Created By <span className="text-gold-500 text-sm">{proposerDisplayName}</span></div>
+      </ContentBox>
     </Link>
   );
 }
