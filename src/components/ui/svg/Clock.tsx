c88aaf18b43bdea9cf448dc4483b5251d9cf25ae
Christopher Dancy <chris.dancy@decentlabs.io>
2022-04-27 00:43:33 +0200

    clock styling for component

--- src/components/ui/svg/Clock.tsx
+++ src/components/ui/svg/Clock.tsx
@@ -1,9 +1,9 @@
 const Clock = () => {
   return (
     <svg
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
+      width="17"
+      height="17"
+      viewBox="0 0 24 20"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
