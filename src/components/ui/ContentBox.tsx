12140f64b4f294eba9ba955a8999fd48b5c89cff
Christopher Dancy <chris.dancy@decentlabs.io>
2022-04-27 17:12:55 +0200

    uplifted hover state to the proposals list component and removed div classname

--- src/components/ui/ContentBox.tsx
+++ src/components/ui/ContentBox.tsx
@@ -2,15 +2,13 @@ import ContentBoxTitle from "./ContentBoxTitle";
 
 const ContentBox = ({
   title,
-  onHover,
   children,
 }: {
   title?: string,
-  onHover?: string,
   children: React.ReactNode,
 }) => {
   return (
-    <div className={`rounded-lg bg-gray-600 px-4 py-4 my-2 ${onHover}`}>
+    <div>
       {title && <ContentBoxTitle>
         {title}
       </ContentBoxTitle>}
