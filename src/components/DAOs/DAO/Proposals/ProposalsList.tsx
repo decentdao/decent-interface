ade338a1a293c0278fec316763882d28ddde578f
Adam Gall <adam@decentlabs.io>
2022-04-27 12:31:48 -0400

    Show janky "proposals loading" message when proposals are loading

--- src/components/DAOs/DAO/Proposals/ProposalsList.tsx
+++ src/components/DAOs/DAO/Proposals/ProposalsList.tsx
@@ -4,6 +4,12 @@ import ProposalCard from "./ProposalCard";
 function ProposalsList() {
   const [{ proposals }] = useDAOData();
 
+  if (!proposals.length) {
+    return (
+      <div className="text-white">Proposals loading...</div>
+    )
+  }
+
   return (
     <div className="grid grid-cols-1 lg:grid-cols-2 2xl:grid-cols-3 lg:gap-4">
       {[...proposals].reverse().map((proposal) => (
