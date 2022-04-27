4e8781d704d66c0c342e9e08073e900407ca518c
Christopher Dancy <chris.dancy@decentlabs.io>
2022-04-27 17:13:27 +0200

    added hover state to the proposal component

--- src/components/DAOs/DAO/Proposals/ProposalCard.tsx
+++ src/components/DAOs/DAO/Proposals/ProposalCard.tsx
@@ -25,23 +25,23 @@ function ProposalCard({
   const proposerDisplayName = useDisplayName(proposal.proposer);
   return (
     <Link to={`proposals/${proposal.number}`}>
-      <ContentBox onHover="border border-chocolate-400 hover:border-gold-300 hover:shadow-sm hover:shadow-gold-300">
-        <div className="flex flex-row">
-          {proposal.stateString === "Open" ?
-            <TextBox borderTextColor="border-gold-500">{proposal.stateString}</TextBox> :
-            <TextBox borderTextColor="border-gray-50 text-gray-50">{proposal.stateString}</TextBox>
-          }
-          <div className="px-4 py-1 text-gray-25 text-sm">#{proposal.number}</div>
-          <div className="py-1">
+      <div className={`rounded-lg bg-gray-600 p-4 border border-chocolate-400 hover:border-gold-300 hover:shadow-sm hover:shadow-gold-300`}>
+        <ContentBox>
+          <div className="flex flex-row">
+            {proposal.stateString === "Open" ?
+              <TextBox borderTextColor="border-gold-500">{proposal.stateString}</TextBox> :
+              <TextBox borderTextColor="border-gray-50 text-gray-50">{proposal.stateString}</TextBox>
+            }
+            <div className="px-4 py-1 text-gray-25 text-sm">#{proposal.number}</div>
             <Clock />
+            <div className="px-2 py-1 text-gray-50 text-sm">
+              {proposal.startTimeString} - {proposal.endTimeString}
+            </div>
           </div>
-          <div className="px-2 py-1 text-gray-50 text-sm">
-            {proposal.startTimeString} - {proposal.endTimeString}
-          </div>
-        </div>
-        <div className="py-4 text-white">{proposal.description}</div>
-        <div className="text-gray-50 text-sm">Created By <span className="text-gold-500 text-sm">{proposerDisplayName}</span></div>
-      </ContentBox>
+          <div className="py-4 text-white">{proposal.description}</div>
+          <div className="text-gray-50 text-sm">Created By <span className="text-gold-500 text-sm">{proposerDisplayName}</span></div>
+        </ContentBox>
+      </div>
     </Link>
   );
 }
