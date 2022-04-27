96e2dbc953cd68fe0c97fb2df762e02b3f2789ac
tbwebb22 <tbwebb22@gmail.com>
2022-04-27 15:24:33 -0400

    Refactor DelegateVote component

--- src/components/DAOs/DAO/DelegateVote.tsx
+++ src/components/DAOs/DAO/DelegateVote.tsx
@@ -1,22 +1,34 @@
 import { useState } from "react";
 import Button from "../../ui/Button";
 import useDelegateVote from "../../../daoData/useDelegateVote";
-import CreateDAOInput from "../../ui/CreateDAOInput";
+import useDisplayName from "../../../hooks/useDisplayName";
+import { InputAddress } from "../../ui/Input";
 import ConnectModal from "../../ConnectModal";
 import Pending from "../../Pending";
 import { useWeb3 } from "../../../web3";
+import { useDAOData } from "../../../daoData";
 
 function DelegateVote() {
-  const [delegatee, setDelegatee] = useState<string>();
+  const [newDelegatee, setNewDelegatee] = useState<string>("");
   const [pending, setPending] = useState<boolean>(false);
   const { account } = useWeb3();
+  const [
+    {
+      tokenData: { symbol, userBalance, delegatee },
+    },
+  ] = useDAOData();
+  const delegateeDisplayName = useDisplayName(delegatee);
 
   const delegateSelf = () => {
-    setDelegatee(account);
-  }
+    if(account === undefined) {
+      return;
+    }
+
+    setNewDelegatee(account);
+  };
 
   const delegateVote = useDelegateVote({
-    delegatee: delegatee,
+    delegatee: newDelegatee,
     setPending,
   });
 
@@ -28,14 +40,28 @@ function DelegateVote() {
         <div className="flex mx-2 my-1 text-gray-25">Delegate Vote</div>
         <hr className="mx-2 my-1 border-gray-200" />
         <Button onClick={() => delegateSelf()}>Self</Button>
-        <CreateDAOInput
+        {/* <CreateDAOInput
           dataType="text"
-          value={delegatee}
-          onChange={(e) => setDelegatee(e)}
+          value={newDelegatee}
+          onChange={(e) => setNewDelegatee(e)}
           label="Delegatee Address"
           helperText="The address to delegate your votes to"
           disabled={false}
-        />
+        /> */}
+          <InputAddress
+            value={newDelegatee}
+            disabled={false}
+            placeholder={""}
+            error={false}
+            onChange={(e) => setNewDelegatee(e)}
+          />
+        )
+        <div className="flex mx-2 my-1 text-gray-25">
+          {`Balance: ${userBalance} ${symbol}`}
+        </div>
+        <div className="flex mx-2 my-1 text-gray-25">
+          {`Delegatee: ${delegateeDisplayName}`}
+        </div>
         <Button onClick={() => delegateVote()}>Delegate</Button>
       </div>
     </>
