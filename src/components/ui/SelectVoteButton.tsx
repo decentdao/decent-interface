5a31ea098bdb829440540b405a04354a683ed781
tbwebb22 <tbwebb22@gmail.com>
2022-04-28 17:39:11 -0400

    Refactor SelectVoteButton

--- src/components/ui/SelectVoteButton.tsx
+++ src/components/ui/SelectVoteButton.tsx
@@ -1,4 +1,5 @@
 import Check from "./svg/Check";
+import Button from "./Button";
 
 function SelectVoteButton({
   disabled = false,
@@ -14,20 +15,18 @@ function SelectVoteButton({
   children: React.ReactNode;
 } & React.ButtonHTMLAttributes<HTMLButtonElement>) {
   return (
-    <button
-      className={`flex px-8 py-2 mx-2 border rounded ${
+    <Button
+      onClick={onClick}
+      disabled={disabled}
+      addedClassNames={`flex px-8 py-2 mx-2 ${
         selected
           ? "border-gold-500 bg-black-500 text-gold-500"
           : "border-gray-25 bg-chocolate-500 text-gray-25"
-      } ${disabled ? "opacity-50" : ""}`}
-      disabled={disabled}
-      onClick={onClick}
-      type={type}
-      {...rest}
+      }`}
     >
       {children}
-      {selected ? <Check /> : null}
-    </button>
+      {selected && <Check />}
+    </Button>
   );
 }
 
