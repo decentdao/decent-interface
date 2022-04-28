777800ab69a612d8072fac444ed69f3dae90d8bc
David Colon <dcolonantonio@gmail.com>
2022-04-28 16:36:17 -0400

    updates logo to match figma - adds Logo Svg

--- src/components/ui/Header/index.tsx
+++ src/components/ui/Header/index.tsx
@@ -1,13 +1,14 @@
 import { Link } from "react-router-dom";
 import WalletAndMenu from "./WalletAndMenu";
+import FractalLogo from '../svg/Logo';
 
 function Header() {
   return (
     <header className="py-4 bg-gray-600">
       <div className="container flex flex-col sm:flex-row sm:justify-between sm:items-center">
         <div className="mr-2 mb-4 sm:mb-0">
-          <div className="mr-2 text-2xl uppercase text-white tracking-widest">
-            <Link to="/">fractal</Link>
+          <div className="mr-2 text-3xl uppercase text-white font-bold font-mono tracking-widest">
+            <Link to="/"><FractalLogo /></Link>
           </div>
         </div>
         <WalletAndMenu />
