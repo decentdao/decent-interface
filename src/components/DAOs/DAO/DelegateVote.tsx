19f7cb333fc16024ac56cd4641ce49d5252eb6aa
tbwebb22 <tbwebb22@gmail.com>
2022-04-28 10:40:45 -0400

    Utilize ContentBox component

--- src/components/DAOs/DAO/DelegateVote.tsx
+++ src/components/DAOs/DAO/DelegateVote.tsx
@@ -3,6 +3,7 @@ import Button from "../../ui/Button";
 import useDelegateVote from "../../../daoData/useDelegateVote";
 import useDisplayName from "../../../hooks/useDisplayName";
 import { InputAddress } from "../../ui/Input";
+import ContentBox from "../../ui/ContentBox";
 import EtherscanLink from "../../ui/EtherscanLink";
 import ConnectModal from "../../ConnectModal";
 import Pending from "../../Pending";
@@ -48,32 +49,43 @@ function DelegateVote() {
       <Pending message="Delegating Vote..." pending={pending} />
       <ConnectModal />
       <div className="flex flex-col bg-gray-600 my-4 p-2 py-2 rounded-md">
-        <div className="flex mx-2 my-1 text-gray-25">Delegate Vote</div>
-        <hr className="mx-2 my-1 border-gray-200" />
-        <div className="flex mx-2 my-1 text-gray-50">
-          New Delegate Address
-        </div>
-        <div className="flex flex-row m-2">
-        <InputAddress
-          value={newDelegatee}
-          disabled={false}
-          placeholder={""}
-          error={invalidAddress}
-          onChange={(e) => setNewDelegatee(e)}
-        />
-        <Button onClick={() => delegateSelf()} addedClassNames="mx-2 px-2">Self</Button>
-        </div>
-        <div className="flex mx-2 my-1 text-gray-50">
-          {`Balance: ${userBalance} ${symbol}`}
-        </div>
-        <div className="flex mx-2 my-1 text-gray-50">
-          Current Delegatee: &nbsp;
-          <EtherscanLink address={delegatee}><p className="text-gold-500">{delegateeDisplayName}</p></EtherscanLink>
-        </div>
-        {invalidAddress && (
-          <div className="flex mx-2 my-1 text-red">Invalid Address</div>
-        )}
-        <Button disabled={invalidAddress || newDelegatee === ""} onClick={() => delegateVote()} addedClassNames="py-1 m-2">Delegate</Button>
+        <ContentBox title="Delegate Vote">
+          <hr className="mx-2 my-1 border-gray-200" />
+          <div className="flex mx-2 my-1 text-gray-50">
+            New Delegate Address
+          </div>
+          <div className="flex flex-row m-2">
+            <InputAddress
+              value={newDelegatee}
+              disabled={false}
+              placeholder={""}
+              error={invalidAddress}
+              onChange={(e) => setNewDelegatee(e)}
+            />
+            <Button onClick={() => delegateSelf()} addedClassNames="mx-2 px-2">
+              Self
+            </Button>
+          </div>
+          <div className="flex mx-2 my-1 text-gray-50">
+            {`Balance: ${userBalance} ${symbol}`}
+          </div>
+          <div className="flex mx-2 my-1 text-gray-50">
+            Current Delegatee: &nbsp;
+            <EtherscanLink address={delegatee}>
+              <p className="text-gold-500">{delegateeDisplayName}</p>
+            </EtherscanLink>
+          </div>
+          {invalidAddress && (
+            <div className="flex mx-2 my-1 text-red">Invalid Address</div>
+          )}
+          <Button
+            disabled={invalidAddress || newDelegatee === ""}
+            onClick={() => delegateVote()}
+            addedClassNames="m-2 px-4 py-1"
+          >
+            Delegate
+          </Button>
+        </ContentBox>
       </div>
     </>
   );
