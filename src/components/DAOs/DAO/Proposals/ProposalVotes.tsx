c869aef4d2e501937e0877a742a6ae0bdb35a2c4
tbwebb22 <tbwebb22@gmail.com>
2022-04-28 22:30:26 -0400

    Update height of ProposalVotes

--- src/components/DAOs/DAO/Proposals/ProposalVotes.tsx
+++ src/components/DAOs/DAO/Proposals/ProposalVotes.tsx
@@ -2,7 +2,7 @@ import { ProposalData } from "../../../../daoData/useProposals";
 
 function ProposalVotes({ proposal }: { proposal: ProposalData }) {
   return (
-    <div className="flex flex-grow flex-col bg-gray-600 m-2 p-2 py-2 rounded-md">
+    <div className="flex flex-grow flex-col h-full bg-gray-600 m-2 p-2 py-2 rounded-md">
       <div className="flex mx-2 my-1 text-gray-25">Results</div>
       <hr className="mx-2 my-1 border-gray-200" />
       <div className="flex flex-row mx-2 my-1">
