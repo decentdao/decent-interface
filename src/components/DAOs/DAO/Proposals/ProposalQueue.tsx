992a7ab64c3914124a9bedaef1f95e3431493607
tbwebb22 <tbwebb22@gmail.com>
2022-04-29 11:35:58 -0400

    Update styling

--- src/components/DAOs/DAO/Proposals/ProposalQueue.tsx
+++ src/components/DAOs/DAO/Proposals/ProposalQueue.tsx
@@ -1,29 +1,29 @@
-
 import { ProposalData } from "../../../../daoData/useProposals";
 import useQueueTransaction from "../../../../daoData/useQueueTransaction";
-import Button from "../../../ui/Button";
+import PrimaryButton from "../../../ui/PrimaryButton";
 import ContentBox from "../../../ui/ContentBox";
 
 function ProposalQueue({ proposal }: { proposal: ProposalData }) {
   const queueTransaction = useQueueTransaction({
-    proposalData: proposal
+    proposalData: proposal,
   });
+
+  if (proposal.stateString !== "Succeeded") {
+    return null;
+  }
+
   return (
-    <div>
-      {
-        proposal.stateString === "Succeeded" &&
-        <ContentBox>
-          <div className="flex flex-col">
-            <Button
-              onClick={queueTransaction}
-              addedClassNames="px-2 py-2 mx-2 min-w-full border-gold-300 bg-chocolate-500 text-gold-300"
-            >
-              Queue Proposal
-            </Button>
-            <div className="text-sm text-gray-25 text-center py-2">Voting has closed and the vote has passed. Anyone can now queue the contract.</div>
-          </div>
-        </ContentBox>
-      }
+    <div className="flex flex-shrink m-2">
+      <ContentBox title="Queue Proposal">
+        <div className="flex justify-center">
+          <PrimaryButton onClick={queueTransaction}>
+            Queue Proposal
+          </PrimaryButton>
+        </div>
+        <div className="text-sm text-gray-50 text-center py-2">
+          Proposal has succeeded and ready to queue
+        </div>
+      </ContentBox>
     </div>
   );
 }
