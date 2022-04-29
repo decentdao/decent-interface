5e93d7179721a4fe38d82da18cf2686f54b6825a
Christopher Dancy <chris.dancy@decentlabs.io>
2022-04-29 03:34:50 +0200

    created proposal execute component for proposal details

---
+++ src/components/DAOs/DAO/Proposals/ProposalExecute.tsx
@@ -0,0 +1,31 @@
+
+import { ProposalData } from "../../../../daoData/useProposals";
+import useExecuteTransaction from "../../../../daoData/useExecuteTransaction";
+import Button from "../../../ui/Button";
+import ContentBox from "../../../ui/ContentBox";
+
+function ProposalExecute({ proposal }: { proposal: ProposalData }) {
+  const queueTransaction = useExecuteTransaction({
+    proposalData: proposal
+  });
+  return (
+    <div>
+      {
+        // proposal.stateString === "Succeeded" &&
+        <ContentBox>
+          <div className="flex flex-col">
+            <Button
+              onClick={queueTransaction}
+              addedClassNames="px-2 py-2 min-w-full border-gold-300 bg-chocolate-500 text-gold-300"
+            >
+              Execute Proposal
+            </Button>
+            <div className="text-sm text-gray-25 text-center py-2">Queuing has completed. Anyone can now Execute the contract.</div>
+          </div>
+        </ContentBox>
+      }
+    </div>
+  );
+}
+
+export default ProposalExecute;
