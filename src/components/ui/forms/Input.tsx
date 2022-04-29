d7bae3e4c916e02e8de878aba137ae24eb0ae2dd
David Colon <dcolonantonio@gmail.com>
2022-04-28 23:19:22 -0400

    create Text/Number/TextArea Component - handles labels, subLabels, and errors - also handles most normal input attributes

---
+++ src/components/ui/forms/Input.tsx
@@ -0,0 +1,66 @@
+import { ChangeEvent, FormEvent } from "react";
+
+interface InputProps {
+  type: "text" | "number" | "textarea";
+  value: string;
+  label?: string;
+  subLabel?: string;
+  disabled?: boolean;
+  errorMessage?: string;
+  placeholder?: string;
+  min?: string | number;
+  onChange(event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>): void;
+  onKeyDown?: React.KeyboardEventHandler<HTMLInputElement | HTMLTextAreaElement>;
+}
+
+/**
+ * FormField builds and renders Input fields based on given parameters
+ * includes: surrounding wrapper, label, input and error handling
+ *
+ */
+const Input = ({ value, min, placeholder, type, label, subLabel, errorMessage, disabled, onChange, onKeyDown }: InputProps) => {
+  const FieldType = type === "textarea" ? "textarea" : "input";
+  const hasError = !!errorMessage;
+  const isDisabled = disabled;
+
+  const Label = () =>
+    !!label ? (
+      <label htmlFor="form-field" className="text-gray-25 text-xs font-medium mb-1">
+        {label}
+      </label>
+    ) : null;
+
+  const SubLabel = () => (!!subLabel && !hasError ? <div className="text-gray-50 text-xs font-medium mt-1">{subLabel}</div> : null);
+  const ErrorMessage = () => (!!hasError ? <div className="text-red text-xs mt-1">{errorMessage}</div> : null);
+
+  const INPUT_BASE_STYLES = "w-full border border-gray-20 bg-gray-400 rounded py-1 px-2 shadow-inner text-gray-50 focus:outline-none";
+  const borderColor = hasError ? "border border-red" : "";
+  const inputTextColor = hasError ? "text-red" : "text-gray-25";
+
+  const _type = type !== "textarea" ? type : undefined;
+
+  return (
+    <div className="flex flex-col">
+      <Label />
+      <FieldType
+        id="form-field"
+        type={_type}
+        placeholder={placeholder}
+        className={` h-f${INPUT_BASE_STYLES} ${borderColor} ${inputTextColor}`}
+        disabled={isDisabled}
+        value={value}
+        min={min}
+        onChange={onChange}
+        onKeyDown={onKeyDown}
+        onWheel={(e: FormEvent<HTMLInputElement | HTMLTextAreaElement>) => (e.target as HTMLInputElement).blur()}
+        autoCorrect="off"
+        autoCapitalize="none"
+        spellCheck="false"
+      />
+      <SubLabel />
+      <ErrorMessage />
+    </div>
+  );
+};
+
+export default Input;
