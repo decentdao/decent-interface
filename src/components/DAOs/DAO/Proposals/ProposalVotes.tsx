99a3252829f0de363f42a8d6ce6648745814297d
David Colon <dcolonantonio@gmail.com>
2022-04-30 18:32:38 -0400

    updates ProposalVotes - updates styles and fonts

--- src/components/DAOs/DAO/Proposals/ProposalVotes.tsx
+++ src/components/DAOs/DAO/Proposals/ProposalVotes.tsx
@@ -1,30 +1,23 @@
 import { ProposalData } from "../../../../daoData/useProposals";
 
+const VotesPercentage = ({ label, percentage }: { label: string; percentage?: number }) => {
+  return (
+    <div className="flex flex-row mx-2 py-3 border-t border-gray-200">
+      <div className="flex flex-grow mr-4 text-gray-50 font-medium">{label}</div>
+      <div className="flex ml-2 text-gray-25 text-lg font-semibold font-mono">{percentage}%</div>
+    </div>
+  );
+};
+
 function ProposalVotes({ proposal }: { proposal: ProposalData }) {
   return (
-    <div className="flex flex-grow flex-col h-full bg-gray-600 m-2 p-2 py-2 rounded-md">
-      <div className="flex mx-2 my-1 text-gray-25">Results</div>
-      <hr className="mx-2 my-1 border-gray-200" />
-      <div className="flex flex-row mx-2 my-1">
-        <div className="flex flex-grow mr-4 text-gray-50">Yes</div>
-        <div className="flex ml-2 text-gray-25">
-          {proposal.forVotesPercent}%
-        </div>
-      </div>
-      <hr className="mx-2 my-1 border-gray-200" />
-      <div className="flex flex-row mx-2 my-1">
-        <div className="flex flex-grow mr-4 text-gray-50">No</div>
-        <div className="flex ml-2 text-gray-25">
-          {proposal.againstVotesPercent}%
-        </div>
-      </div>
-      <hr className="mx-2 my-1 border-gray-200" />
-      <div className="flex flex-row mx-2 my-1">
-        <div className="flex flex-grow mr-4 text-gray-50">Abstain</div>
-        <div className="flex ml-4 text-gray-25">
-          {proposal.abstainVotesPercent}%
-        </div>
-      </div>
+    <div className="flex flex-grow flex-col h-full bg-gray-600 m-2 p-2 pb-4 rounded-md">
+      <div className="flex mx-2 my-2 text-gray-25 mb-3 text-lg font-semibold">Results</div>
+      <VotesPercentage label="Yes" percentage={proposal.forVotesPercent} />
+
+      <VotesPercentage label="No" percentage={proposal.againstVotesPercent} />
+
+      <VotesPercentage label="Abstain" percentage={proposal.abstainVotesPercent} />
     </div>
   );
 }
