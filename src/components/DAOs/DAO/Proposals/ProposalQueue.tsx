f95eef8943cab6e7539c237aa5a69b98d141186b
David Colon <dcolonantonio@gmail.com>
2022-05-02 08:57:37 -0400

    updates ProposalQueue with updated button

--- src/components/DAOs/DAO/Proposals/ProposalQueue.tsx
+++ src/components/DAOs/DAO/Proposals/ProposalQueue.tsx
@@ -1,6 +1,6 @@
 import { ProposalData } from "../../../../daoData/useProposals";
 import useQueueTransaction from "../../../../daoData/useQueueTransaction";
-import PrimaryButton from "../../../ui/PrimaryButton";
+import { PrimaryButton } from "../../../ui/forms/Button";
 
 function ProposalQueue({ proposal }: { proposal: ProposalData }) {
   const queueTransaction = useQueueTransaction({
@@ -14,11 +14,9 @@ function ProposalQueue({ proposal }: { proposal: ProposalData }) {
   return (
     <div className="flex border-1 items-center m-2 bg-gray-600 py-2 rounded-md">
       <div className="align-middle text-gray-25 mx-4">Proposal has succeeded and ready to queue</div>
-        <div className="flex flex-grow justify-end mx-4">
-          <PrimaryButton onClick={queueTransaction}>
-            Queue Proposal
-          </PrimaryButton>
-        </div>
+      <div className="flex flex-grow justify-end mx-4">
+        <PrimaryButton label="Queue Proposal" onClick={queueTransaction} />
+      </div>
     </div>
   );
 }
