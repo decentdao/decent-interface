b8cc4c84aee1345fa4a860c5c5b6bc0620bcf534
Adam Gall <adam@decentlabs.io>
2022-05-02 15:16:35 -0400

    When DAOs component is unloaded, set the DAO address back to undefined

--- src/components/DAOs/DAO/index.tsx
+++ src/components/DAOs/DAO/index.tsx
@@ -1,11 +1,17 @@
+import { useEffect } from "react";
 import { Routes, Route, useParams } from "react-router-dom";
 
 import Summary from "./Summary";
 import Details from "./Details";
 import Proposals from "./Proposals";
+import { useDAOData } from "../../../daoData";
 
 function DAOs() {
   const params = useParams();
+  const [, setAddress] = useDAOData();
+
+  // when this component unloads, setAddress back to undefined to clear app state
+  useEffect(() => () => setAddress(undefined), [setAddress]);
 
   if (!params.address) {
     return <div>if you see this, it's a bug</div>;
