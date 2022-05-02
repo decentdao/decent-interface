46d86ee85605de86c0503352ae572bf5733ff4ea
tbwebb22 <tbwebb22@gmail.com>
2022-05-02 15:54:29 -0400

    Update useCurrentTimestamp to increment time every second

--- src/hooks/useCurrentTimestamp.ts
+++ src/hooks/useCurrentTimestamp.ts
@@ -1,27 +1,35 @@
-import useBlockNumber from "./useCurrentBlockNumber";
 import { useEffect, useState } from "react";
 import { useWeb3 } from "../web3";
 
-const useCurrentTimestamp = () => {
-  const [blockTimestamp, setBlockTimestamp] = useState<number>(Math.floor(Date.now() / 1000));
+const useCurrentTimestamp = (blockNumber: number | undefined) => {
+  const [timestamp, setTimestamp] = useState<number>(
+    Math.floor(Date.now() / 1000)
+  );
   const { provider } = useWeb3();
-  const blockNumber = useBlockNumber();
+
+  useEffect(() => {
+    const timer = setInterval(() => setTimestamp((oldTimestamp) => oldTimestamp + 1), 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
 
   useEffect(() => {
     if (provider === undefined || blockNumber === undefined) {
-      setBlockTimestamp(Math.floor(Date.now() / 1000));
+      setTimestamp(Math.floor(Date.now() / 1000));
       return;
     }
 
     provider
       .getBlock(blockNumber)
       .then((block) => {
-        setBlockTimestamp(block.timestamp);
+        setTimestamp(block.timestamp);
       })
       .catch(console.error);
   }, [provider, blockNumber]);
 
-  return blockTimestamp;
+  return timestamp;
 };
 
 export default useCurrentTimestamp;
