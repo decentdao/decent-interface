a7286a9acb983525ed83ea28842c23ca27e2cc58
Adam Gall <adam@decentlabs.io>
2022-05-03 09:02:46 -0400

    Remove unnecessary `useParams` from Proposals index

--- src/components/DAOs/DAO/Proposals/index.tsx
+++ src/components/DAOs/DAO/Proposals/index.tsx
@@ -1,15 +1,9 @@
-import { Routes, Route, useParams } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 
 import New from "./New";
 import ProposalDetails from "./ProposalDetails";
 
 function Proposals({ address }: { address: string }) {
-  const params = useParams();
-
-  if (!params.address) {
-    return <div>if you see this, it's a bug</div>;
-  }
-
   return (
     <Routes>
       <Route path="new" element={<New address={address} />} />
