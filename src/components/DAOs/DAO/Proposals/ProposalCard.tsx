669e70bd78bb5f6a7bd0e3c0a0d4e5ee4388f737
David Colon <dcolonantonio@gmail.com>
2022-05-03 14:28:50 -0400

    fixes proposal details display on mobile

--- src/components/DAOs/DAO/Proposals/ProposalCard.tsx
+++ src/components/DAOs/DAO/Proposals/ProposalCard.tsx
@@ -3,28 +3,26 @@ import useDisplayName from "../../../../hooks/useDisplayName";
 import { Link } from "react-router-dom";
 import ContentBox from "../../../ui/ContentBox";
 import Clock from "../../../ui/svg/Clock";
-import StatusBox from '../../../ui/StatusBox';
+import StatusBox from "../../../ui/StatusBox";
 
-function ProposalCard({
-  proposal
-}: {
-  proposal: ProposalData
-}) {
+function ProposalCard({ proposal }: { proposal: ProposalData }) {
   const proposerDisplayName = useDisplayName(proposal.proposer);
   return (
     <Link to={`proposals/${proposal.number}`}>
       <div>
         <ContentBox isLightBackground>
-          <div className="flex flex-row">
+          <div className="flex items-center">
             <StatusBox status={proposal.stateString} />
-            <div className="px-4 py-1 text-gray-25 text-sm">#{proposal.number}</div>
+            <div className="px-4 text-gray-25 text-sm">#{proposal.number}</div>
             <Clock />
-            <div className="px-2 py-1 text-gray-50 text-sm">
-              {proposal.startTimeString} - {proposal.endTimeString}
+            <div className="px-2 text-gray-50 text-sm flex whitespace-nowrap flex-wrap gap-1 items-start">
+              <span>{proposal.startTimeString} -</span> <span>{proposal.endTimeString}</span>
             </div>
           </div>
           <div className="py-4 text-white text-lg font-mono">{proposal.description}</div>
-          <div className="text-gray-50 text-sm">Created By<span className="text-gold-500 text-sm ml-2">{proposerDisplayName}</span></div>
+          <div className="text-gray-50 text-sm">
+            Created By<span className="text-gold-500 text-sm ml-2">{proposerDisplayName}</span>
+          </div>
         </ContentBox>
       </div>
     </Link>
