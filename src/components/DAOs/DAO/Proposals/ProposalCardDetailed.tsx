68d916f855e782f22ee6719f1b92040615645fd9
Christopher Dancy <chris.dancy@decentlabs.io>
2022-05-04 14:35:35 -0400

    individual components for proposalCardDetailed

--- src/components/DAOs/DAO/Proposals/ProposalCardDetailed.tsx
+++ src/components/DAOs/DAO/Proposals/ProposalCardDetailed.tsx
@@ -1,17 +1,40 @@
 import { ProposalData } from "../../../../daoData/useProposals";
 import ContentBox from "../../../ui/ContentBox";
-import StatusInfo from "../../../ui/StatusInfo";
+import ProposalCreatedBy from "../../../ui/ProposalCreatedBy";
+import ProposalDescription from "../../../ui/ProposalDescription";
+import ProposalId from "../../../ui/ProposalId";
+import ProposalNumber from "../../../ui/ProposalNumber";
+import ProposalTime from "../../../ui/ProposalTime";
+import StatusBox from "../../../ui/StatusBox";
 
 function ProposalCardDetailed({ proposal }: { proposal: ProposalData }) {
+  if (!proposal || !proposal.startTimeString || !proposal.endTimeString || !proposal.idSubstring) return (<div></div>)
   return (
-    <div className = "m-2">
-      <ContentBox>
-        <StatusInfo
-          proposal={proposal}
-          includeHR
-          includeProposalId
-          includeClipboard
-        />
+    <div className="mx-2">
+      <ContentBox >
+        <div className="flex items-center">
+          <StatusBox status={proposal.stateString} />
+          <ProposalNumber
+            proposalNumber={proposal.number}
+          />
+          <ProposalTime
+            proposalStartString={proposal.startTimeString}
+            proposalEndString={proposal.endTimeString}
+          />
+        </div>
+        <ProposalDescription proposalDesc={proposal.description} />
+        <div className="pt-4 border-t border-gray-200">
+          <ProposalCreatedBy
+            proposalProposer={proposal.proposer}
+            addedClasses={"justify-between items-center"}
+            includeClipboard
+          />
+          <ProposalId
+            proposalId={proposal.idSubstring}
+            addedClasses={"justify-between items-center"}
+            includeClipboard
+          />
+        </div>
       </ContentBox>
     </div>
   );
