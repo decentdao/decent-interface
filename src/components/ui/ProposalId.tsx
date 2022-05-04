82d42bdca26d0e9bde3aeaf8b104067b52ddc096
Christopher Dancy <chris.dancy@decentlabs.io>
2022-05-04 16:39:52 -0400

    fixed clipboard - copy entire proposalId

--- src/components/ui/ProposalId.tsx
+++ src/components/ui/ProposalId.tsx
@@ -1,12 +1,13 @@
+import { BigNumber } from "@ethersproject/bignumber";
 import CopyToClipboard from "./CopyToClipboard";
 
-function ProposalCreatedBy({ proposalId, includeClipboard, addedClasses }: { proposalId: string, includeClipboard?: boolean, addedClasses?: string }) {
-
+function ProposalCreatedBy({ proposalId, proposalIdSub, includeClipboard, addedClasses }: { proposalId: BigNumber | undefined ,proposalIdSub: string | undefined, includeClipboard?: boolean, addedClasses?: string }) {
+  
   return (
     <div className={`flex text-gray-50 max-w-xs ${addedClasses}`}>
       <div>Proposal ID:</div>
-      <div className="ml-1">{proposalId}</div>
-      {includeClipboard && <CopyToClipboard textToCopy={proposalId} />}
+      <div className="ml-1">{proposalIdSub}</div>
+      {includeClipboard && <CopyToClipboard textToCopy={proposalId?.toString()} />}
     </div>
   );
 }
