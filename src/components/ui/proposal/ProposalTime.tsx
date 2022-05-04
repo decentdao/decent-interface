72f0c1abae528ce51400d86ae54024c253232250
Christopher Dancy <chris.dancy@decentlabs.io>
2022-05-04 16:57:55 -0400

    added dataloading compoennt

--- src/components/ui/proposal/ProposalTime.tsx
+++ src/components/ui/proposal/ProposalTime.tsx
@@ -1,13 +1,16 @@
+import DataLoadingWrapper from "../loaders/DataLoadingWrapper";
 import Clock from "../svg/Clock";
 
 function ProposalTime({ proposalStartString, proposalEndString, textSize = "" }: { proposalStartString: string | undefined, proposalEndString: string | undefined, textSize?: string }) {
   return (
-    <div className="flex">
-      <Clock />
-      <div className={`px-2 text-gray-50 ${textSize} flex whitespace-nowrap flex-wrap gap-1 items-start`}>
-        <span>{proposalStartString} -</span> <span>{proposalEndString}</span>
+    <DataLoadingWrapper isLoading={!proposalStartString || !proposalEndString}>
+      <div className="flex">
+        <Clock />
+        <div className={`px-2 text-gray-50 ${textSize} flex whitespace-nowrap flex-wrap gap-1 items-start`}>
+          <span>{proposalStartString} -</span> <span>{proposalEndString}</span>
+        </div>
       </div>
-    </div>
+    </DataLoadingWrapper>
   );
 }
 
