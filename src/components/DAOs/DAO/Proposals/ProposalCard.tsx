5792419481ff1cfa5a2314927f154e2e7610b19d
Christopher Dancy <chris.dancy@decentlabs.io>
2022-05-04 09:01:08 -0400

    Using statusInfo for proposalCard

--- src/components/DAOs/DAO/Proposals/ProposalCard.tsx
+++ src/components/DAOs/DAO/Proposals/ProposalCard.tsx
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import ContentBox from "../../../ui/ContentBox";
 import Clock from "../../../ui/svg/Clock";
 import StatusBox from "../../../ui/StatusBox";
+import StatusInfo from "../../../ui/StatusInfo";
 
 function ProposalCard({ proposal }: { proposal: ProposalData }) {
   const proposerDisplayName = useDisplayName(proposal.proposer);
@@ -11,14 +12,7 @@ function ProposalCard({ proposal }: { proposal: ProposalData }) {
     <Link to={`proposals/${proposal.number}`}>
       <div>
         <ContentBox isLightBackground>
-          <div className="flex sm:items-center">
-            <StatusBox status={proposal.stateString} />
-            <div className="px-4 text-gray-25 text-sm">#{proposal.number}</div>
-            <Clock />
-            <div className="px-2 text-gray-50 text-sm flex whitespace-nowrap flex-wrap gap-1 items-start">
-              <span>{proposal.startTimeString} -</span> <span>{proposal.endTimeString}</span>
-            </div>
-          </div>
+          <StatusInfo proposal={proposal} />
           <div className="py-4 text-white text-lg font-mono">{proposal.description}</div>
           <div className="text-gray-50 text-sm">
             Created By<span className="text-gold-500 text-sm ml-2">{proposerDisplayName}</span>
