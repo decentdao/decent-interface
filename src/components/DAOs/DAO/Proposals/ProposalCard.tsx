3583ee927bb6b9c8d32a2cc92a0589a3250d3a21
Christopher Dancy <chris.dancy@decentlabs.io>
2022-05-04 10:15:57 -0400

    included everything in statusInfo screen

--- src/components/DAOs/DAO/Proposals/ProposalCard.tsx
+++ src/components/DAOs/DAO/Proposals/ProposalCard.tsx
@@ -1,21 +1,16 @@
 import { ProposalData } from "../../../../daoData/useProposals";
-import useDisplayName from "../../../../hooks/useDisplayName";
 import { Link } from "react-router-dom";
 import ContentBox from "../../../ui/ContentBox";
-import Clock from "../../../ui/svg/Clock";
-import StatusBox from "../../../ui/StatusBox";
 import StatusInfo from "../../../ui/StatusInfo";
 
 function ProposalCard({ proposal }: { proposal: ProposalData }) {
-  const proposerDisplayName = useDisplayName(proposal.proposer);
   return (
     <Link to={`proposals/${proposal.number}`}>
       <div>
         <ContentBox isLightBackground>
-          <StatusInfo proposal={proposal} />
-          <div className="text-gray-50 text-sm">
-            Created By<span className="text-gold-500 text-sm ml-2">{proposerDisplayName}</span>
-          </div>
+          <StatusInfo 
+          proposal={proposal} 
+          />
         </ContentBox>
       </div>
     </Link>
