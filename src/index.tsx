2ed9745be912488b550191b70fd3aeb6df468718
Adam Gall <adam@decentlabs.io>
2022-05-06 11:54:42 -0400

    test

--- src/index.tsx
+++ src/index.tsx
@@ -14,7 +14,7 @@ import App from './App';
 ReactDOM.render(
   <React.StrictMode>
     <Helmet>
-      <title>Fractal</title>
+      <title>Test Fractal</title>
     </Helmet>
     <HashRouter>
       <Web3Provider>
