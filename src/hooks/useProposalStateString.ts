210f6a204f8637391c0ed6777d0ac16cf1ce251d
Adam Gall <adam@decentlabs.io>
2022-05-10 14:43:37 -0400

    Create a hook for turning ProposalState into a readable string

---
+++ src/hooks/useProposalStateString.ts
@@ -0,0 +1,17 @@
+import { ProposalState } from "../contexts/daoData/useProposals";
+
+const useProposalStateString = (state: ProposalState | undefined) => {
+  switch (state) {
+    case ProposalState.Pending: return "Pending";
+    case ProposalState.Active: return "Active";
+    case ProposalState.Canceled: return "Canceled";
+    case ProposalState.Defeated: return "Defeated";
+    case ProposalState.Succeeded: return "Succeeded";
+    case ProposalState.Queued: return "Queued";
+    case ProposalState.Expired: return "Expired";
+    case ProposalState.Executed: return "Executed";
+    default: return "Unknown";
+  }
+}
+
+export default useProposalStateString;
