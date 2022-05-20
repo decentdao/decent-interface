fc3904eda3156f4a1414309010ca097cf67b5b00
David Colon <dcolonantonio@gmail.com>
2022-05-20 19:02:08 -0400

    updates breadcrumbs to show tool tip on DAO Summary

--- src/contexts/daoData/daoData.ts
+++ src/contexts/daoData/daoData.ts
@@ -1,37 +1,38 @@
-import { useState } from 'react';
-import { BigNumber } from 'ethers';
+import { useMemo, useState } from "react";
+import { BigNumber } from "ethers";
 
-import useDAOContract from './useDAOContract';
-import useDAOName from './useDAOName';
-import useAccessControlAddress from './useAccessControlAddress';
-import useAccessControlContract from './useAccessControlContract';
-import useModuleAddresses from './useModuleAddresses';
-import useGovernorModuleContract from './useGovernorModuleContract';
-import useTokenContract from './useTokenContract';
-import useTokenData from './useTokenData';
-import useProposals from './useProposals';
-import { ProposalData } from './useProposals';
-import { GovernorModule, VotesTokenWithSupply } from '../../typechain-types';
-import { useBlockchainData } from '../blockchainData';
+import useDAOContract from "./useDAOContract";
+import useDAOName from "./useDAOName";
+import useAccessControlAddress from "./useAccessControlAddress";
+import useAccessControlContract from "./useAccessControlContract";
+import useModuleAddresses from "./useModuleAddresses";
+import useGovernorModuleContract from "./useGovernorModuleContract";
+import useTokenContract from "./useTokenContract";
+import useTokenData from "./useTokenData";
+import useProposals from "./useProposals";
+import { ProposalData } from "./useProposals";
+import { GovernorModule, VotesTokenWithSupply } from "../../typechain-types";
+import { useBlockchainData } from "../blockchainData";
 
 export interface DAOData {
-  daoAddress: string | undefined,
-  name: string | undefined,
-  accessControlAddress: string | undefined,
-  moduleAddresses: string[] | undefined,
-  proposals: ProposalData[] | undefined,
-  governorModuleContract: GovernorModule | undefined,
-  tokenContract: VotesTokenWithSupply | undefined,
+  daoAddress: string | undefined;
+  name: string | undefined;
+  accessControlAddress: string | undefined;
+  moduleAddresses: string[] | undefined;
+  proposals: ProposalData[] | undefined;
+  governorModuleContract: GovernorModule | undefined;
+  tokenContract: VotesTokenWithSupply | undefined;
   tokenData: {
-    name: string | undefined,
-    symbol: string | undefined,
-    decimals: number | undefined
-    userBalance: BigNumber | undefined,
-    delegatee: string | undefined,
-    votingWeight: BigNumber | undefined,
-    address: string | undefined,
-  },
-};
+    name: string | undefined;
+    symbol: string | undefined;
+    decimals: number | undefined;
+    userBalance: BigNumber | undefined;
+    delegatee: string | undefined;
+    votingWeight: BigNumber | undefined;
+    address: string | undefined;
+  };
+  isDaoLoaded: boolean;
+}
 
 type SetDAOAddressFn = React.Dispatch<React.SetStateAction<string | undefined>>;
 export type DAODataContext = readonly [DAOData, SetDAOAddressFn];
@@ -48,9 +49,13 @@ const useDAODatas = () => {
   const governorModuleContract = useGovernorModuleContract(moduleAddresses);
   const tokenContract = useTokenContract(governorModuleContract);
   const tokenData = useTokenData(tokenContract);
-  const { currentBlockNumber } = useBlockchainData()
+  const { currentBlockNumber } = useBlockchainData();
   const proposals = useProposals(governorModuleContract, currentBlockNumber);
-
+  // variable to track when dao contracts have finished loading
+  const isDaoLoaded = useMemo(
+    () => !!(daoAddress && accessControlAddress && accessControlContract && governorModuleContract && tokenContract),
+    [daoAddress, accessControlAddress, accessControlContract, governorModuleContract, tokenContract]
+  );
   const daoData: DAOData = {
     daoAddress,
     name,
@@ -60,6 +65,7 @@ const useDAODatas = () => {
     governorModuleContract,
     tokenContract,
     tokenData,
+    isDaoLoaded,
   };
 
   return [daoData, setDAOAddress] as const;
