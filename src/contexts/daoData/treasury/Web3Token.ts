47f3cceebcf3cb5f819e75424d8587d31feb7b62
David Colon <dcolonantonio@gmail.com>
2022-05-24 12:58:28 -0400

    adds ABI to web3Token file - removes added code to generated type file

--- src/contexts/daoData/treasury/Web3Token.ts
+++ src/contexts/daoData/treasury/Web3Token.ts
@@ -1,10 +1,52 @@
 import { Contract, ethers } from "ethers";
-import { ERC20_TOKEN_ABI } from "../../../typechain-types/factories/@openzeppelin/contracts/token/ERC20/ERC20__factory";
 
+const ABI = [
+  {
+    constant: true,
+    inputs: [],
+    name: 'name',
+    outputs: [{ name: '', type: 'string' }],
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'symbol',
+    outputs: [{ name: '', type: 'string' }],
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [{ name: '_owner', type: 'address' }],
+    name: 'balanceOf',
+    outputs: [{ name: 'balance', type: 'uint256' }],
+    type: 'function',
+  },
+  {
+    constant: false,
+    inputs: [
+      { name: '_to', type: 'address' },
+      { name: '_value', type: 'uint256' },
+    ],
+    name: 'transfer',
+    outputs: [{ name: '', type: 'bool' }],
+    payable: false,
+    type: 'function',
+  },
+  {
+    constant: true,
+    inputs: [],
+    name: 'decimals',
+    outputs: [{ name: '', type: 'uint8' }],
+    payable: false,
+    stateMutability: 'view',
+    type: 'function',
+  },
+];
 export default class Web3Token {
   public contract: Contract;
   constructor(public readonly tokenAddress: string, provider: ethers.providers.BaseProvider) {
-    this.contract = new Contract(this.tokenAddress, ERC20_TOKEN_ABI, provider);
+    this.contract = new Contract(this.tokenAddress, ABI, provider);
   }
 
   public async tokenName(): Promise<string> {
