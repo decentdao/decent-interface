03b1268c17b9b8dae1b9da1f1f63990bc0b7f971
Adam Gall <adam@decentlabs.io>
2022-05-26 16:16:32 -0400

    FRACTAL logo link goes back to DAO home, not webapp home

--- src/components/ui/Header/index.tsx
+++ src/components/ui/Header/index.tsx
@@ -1,13 +1,28 @@
-import { Link } from "react-router-dom";
+import { useState, useEffect } from "react";
+import { Link, useMatch } from "react-router-dom";
 import FractalLogo from "../svg/Logo";
 import HeaderMenu from "./HeaderMenu";
 
 function Header() {
+  const daoHomeMatch = useMatch("/daos/:id/*");
+  const [daoHome, setDaoHome] = useState("/");
+  const [validatedAddress, setValidatedAddress] = useState<string>();
+  
+  useEffect(() => {
+    if (daoHomeMatch) {
+      setDaoHome(daoHomeMatch.pathnameBase);
+      setValidatedAddress(daoHomeMatch.params.id);
+    } else {
+      setDaoHome("/");
+      setValidatedAddress(undefined);
+    }
+  }, [daoHomeMatch]);
+
   return (
     <header className="py-4 bg-gray-600">
       <div className="container flex justify-between items-center">
         <div className="mr-4">
-          <Link to="/">
+          <Link to={daoHome} state={{ validatedAddress }}>
             <FractalLogo />
           </Link>
         </div>
