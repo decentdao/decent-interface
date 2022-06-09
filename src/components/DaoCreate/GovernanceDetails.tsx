27021dbf711b28b000449145d3c183e3699c7b00
Christopher Dancy <chris.dancy@decentlabs.io>
2022-06-09 16:06:03 -0400

    possible to udate gov state

--- src/components/DaoCreate/GovernanceDetails.tsx
+++ src/components/DaoCreate/GovernanceDetails.tsx
@@ -5,17 +5,25 @@ import Input from '../ui/forms/Input';
 import InputBox from '../ui/forms/InputBox';
 import Lock from '../ui/svg/Lock';
 
+interface GovernanceDetailsProps {
+  proposalThreshold: string;
+  quorum: string;
+  executionDelay: string;
+  setProposalThreshold: React.Dispatch<React.SetStateAction<string>>;
+  setQuorum: React.Dispatch<React.SetStateAction<string>>;
+  setExecutionDelay: React.Dispatch<React.SetStateAction<string>>;
+  setPrevEnabled: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
 function GovernanceDetails({
-  setPrevEnabled,
   proposalThreshold,
   quorum,
   executionDelay,
-}: {
-  setPrevEnabled: React.Dispatch<React.SetStateAction<boolean>>;
-  proposalThreshold: number | undefined;
-  quorum: number | undefined;
-  executionDelay: number | undefined;
-}) {
+  setProposalThreshold,
+  setQuorum,
+  setExecutionDelay,
+  setPrevEnabled,
+} : GovernanceDetailsProps) {
   useEffect(() => {
     setPrevEnabled(true);
   }, [setPrevEnabled]);
@@ -28,36 +36,33 @@ function GovernanceDetails({
       </div>
       <InputBox>
         <Input
-          type="text"
-          value={proposalThreshold?.toString()}
+          type="number"
+          value={proposalThreshold}
           unit="Tokens"
-          onChange={() => {}}
+          onChange={e => setProposalThreshold(e.target.value)}
           label="Proposal Creation (Token Required)"
           helperText="How many tokens does a member need to have in order to create a new proposal. Recommend: 0 Tokens"
-          disabled={true}
         />
       </InputBox>
       <InputBox>
         <Input
-          type="text"
-          value={quorum?.toString()}
-          onChange={() => {}}
+          type="number"
+          value={quorum}
+          onChange={e => setQuorum(e.target.value)}
           label="Quorum"
           unit="%"
           helperText="The percentage of total votes required in order for a proposal to PASS. Recommend: 4%"
-          disabled={true}
         />
       </InputBox>
       <InputBox>
         <Input
-          type="text"
-          value={executionDelay?.toString()}
-          onChange={() => {}}
+          type="number"
+          value={executionDelay}
+          onChange={e => setExecutionDelay(e.target.value)}
           label="Execution Delay"
           unit="Hours"
           helperText="How many hours after a proposal PASSES, must it wait until it can be executed?
           Recommend: 24 Hours"
-          disabled={true}
         />
       </InputBox>
       <ContentBanner description="The Governance Setup values are not editable at this time. To change these values, a new proposal will need to be created and passed by your members." />
