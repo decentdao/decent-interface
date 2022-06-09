cbc6419e598cddbf4d54bfe1066f39ae4cc028dc
Christopher Dancy <chris.dancy@decentlabs.io>
2022-06-09 18:32:44 -0400

    verbage cleanup

--- src/components/DaoCreate/GovernanceDetails.tsx
+++ src/components/DaoCreate/GovernanceDetails.tsx
@@ -63,7 +63,7 @@ function GovernanceDetails({
           onChange={e => setVoteStartDelay(e.target.value)}
           label="Vote Start Delay"
           unit="Blocks"
-          helperText="How many blocks after a proposal is created before DAO members may vote on the proposal
+          helperText="How many blocks after a proposal is created, before DAO members may vote on the proposal
           Recommend: 24 Hours / ~6545 Blocks"
         />
       </InputBox>
@@ -74,7 +74,7 @@ function GovernanceDetails({
           onChange={e => setVotingPeriod(e.target.value)}
           label="Voting Period"
           unit="Blocks"
-          helperText="How many Blocks is the proposals voting period
+          helperText="The length of time (in blocks) between a vote's starting and ending point
           Recommend: 1 Week / ~45818 Blocks"
         />
       </InputBox>
@@ -85,7 +85,7 @@ function GovernanceDetails({
           onChange={e => setQuorum(e.target.value)}
           label="Quorum"
           unit="%"
-          helperText="The percentage of total votes / total tokens required in order for a proposal to PASS. Recommend: 4%"
+          helperText="The percentage of total votes to total tokens required in order for a proposal to PASS. Recommend: 4%"
         />
       </InputBox>
       <InputBox>
@@ -96,8 +96,7 @@ function GovernanceDetails({
           label="Late Quorum Delay"
           unit="Blocks"
           helperText="Minimum voting period after quorum is reached. 
-          This prevents a large voter from swaying a vote and triggering quorum at the last minute, 
-          by ensuring there is always time for other voters to be reached and try to oppose the decision.
+          This prevents a large voter from swaying a vote and triggering quorum at the last minute.
           Recommend: 0 Blocks"
         />
       </InputBox>
@@ -106,13 +105,13 @@ function GovernanceDetails({
           type="text"
           value={executionDelay}
           onChange={e => setExecutionDelay(e.target.value)}
-          label="Execution Delay"
+          label="Proposal Execution Delay"
           unit="Blocks"
           helperText="How many hours after a proposal PASSES / QUEUED, must it wait until it can be executed?
           Recommend: 24 Hours / ~6545 Blocks"
         />
       </InputBox>
-      <ContentBanner description="The Governance Setup values are editable at this time. A default value has been placed input box. To change these values, a new proposal will need to be created and passed by your members." />
+      <ContentBanner description="The Governance Setup values are editable at this time. A default value has been placed in each input box. To change these values, a new proposal will need to be created and passed by your members." />
     </div>
   );
 }
