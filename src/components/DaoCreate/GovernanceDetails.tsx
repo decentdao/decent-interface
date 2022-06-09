86a49c5002d18e0589e27022428917fc9945a39b
Christopher Dancy <chris.dancy@decentlabs.io>
2022-06-09 18:27:18 -0400

    added editable input boxes for state

--- src/components/DaoCreate/GovernanceDetails.tsx
+++ src/components/DaoCreate/GovernanceDetails.tsx
@@ -9,9 +9,15 @@ interface GovernanceDetailsProps {
   proposalThreshold: string;
   quorum: string;
   executionDelay: string;
+  lateQuorumExecution: string;
+  voteStartDelay: string;
+  votingPeriod: string;
   setProposalThreshold: React.Dispatch<React.SetStateAction<string>>;
   setQuorum: React.Dispatch<React.SetStateAction<string>>;
   setExecutionDelay: React.Dispatch<React.SetStateAction<string>>;
+  setLateQuorumExecution: React.Dispatch<React.SetStateAction<string>>;
+  setVoteStartDelay: React.Dispatch<React.SetStateAction<string>>;
+  setVotingPeriod: React.Dispatch<React.SetStateAction<string>>;
   setPrevEnabled: React.Dispatch<React.SetStateAction<boolean>>;
 }
 
@@ -19,11 +25,17 @@ function GovernanceDetails({
   proposalThreshold,
   quorum,
   executionDelay,
+  lateQuorumExecution,
+  voteStartDelay,
+  votingPeriod,
   setProposalThreshold,
   setQuorum,
   setExecutionDelay,
+  setLateQuorumExecution,
+  setVoteStartDelay,
+  setVotingPeriod,
   setPrevEnabled,
-} : GovernanceDetailsProps) {
+}: GovernanceDetailsProps) {
   useEffect(() => {
     setPrevEnabled(true);
   }, [setPrevEnabled]);
@@ -36,36 +48,71 @@ function GovernanceDetails({
       </div>
       <InputBox>
         <Input
-          type="number"
+          type="text"
           value={proposalThreshold}
           unit="Tokens"
           onChange={e => setProposalThreshold(e.target.value)}
-          label="Proposal Creation (Token Required)"
+          label="Proposal Creation (# of Tokens Required)"
           helperText="How many tokens does a member need to have in order to create a new proposal. Recommend: 0 Tokens"
         />
       </InputBox>
       <InputBox>
         <Input
-          type="number"
-          value={quorum}
-          onChange={e => setQuorum(e.target.value)}
-          label="Quorum"
-          unit="%"
-          helperText="The percentage of total votes required in order for a proposal to PASS. Recommend: 4%"
+          type="text"
+          value={voteStartDelay}
+          onChange={e => setVoteStartDelay(e.target.value)}
+          label="Vote Start Delay"
+          unit="Blocks"
+          helperText="How many blocks after a proposal is created before DAO members may vote on the proposal
+          Recommend: 24 Hours / ~6545 Blocks"
         />
       </InputBox>
       <InputBox>
         <Input
-          type="number"
+          type="text"
+          value={votingPeriod}
+          onChange={e => setVotingPeriod(e.target.value)}
+          label="Voting Period"
+          unit="Blocks"
+          helperText="How many Blocks is the proposals voting period
+          Recommend: 1 Week / ~45818 Blocks"
+        />
+      </InputBox>
+      <InputBox>
+        <Input
+          type="text"
+          value={quorum}
+          onChange={e => setQuorum(e.target.value)}
+          label="Quorum"
+          unit="%"
+          helperText="The percentage of total votes / total tokens required in order for a proposal to PASS. Recommend: 4%"
+        />
+      </InputBox>
+      <InputBox>
+        <Input
+          type="text"
+          value={lateQuorumExecution}
+          onChange={e => setLateQuorumExecution(e.target.value)}
+          label="Late Quorum Delay"
+          unit="Blocks"
+          helperText="Minimum voting period after quorum is reached. 
+          This prevents a large voter from swaying a vote and triggering quorum at the last minute, 
+          by ensuring there is always time for other voters to be reached and try to oppose the decision.
+          Recommend: 0 Blocks"
+        />
+      </InputBox>
+      <InputBox>
+        <Input
+          type="text"
           value={executionDelay}
           onChange={e => setExecutionDelay(e.target.value)}
           label="Execution Delay"
-          unit="Hours"
+          unit="Blocks"
           helperText="How many hours after a proposal PASSES / QUEUED, must it wait until it can be executed?
           Recommend: 24 Hours / ~6545 Blocks"
         />
       </InputBox>
-      <ContentBanner description="The Governance Setup values are not editable at this time. To change these values, a new proposal will need to be created and passed by your members." />
+      <ContentBanner description="The Governance Setup values are editable at this time. A default value has been placed input box. To change these values, a new proposal will need to be created and passed by your members." />
     </div>
   );
 }
