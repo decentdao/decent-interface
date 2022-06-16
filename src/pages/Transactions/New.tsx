8d63a6bef55f40e8d83091cb9590bd2734c9cac3
Adam Gall <adam@decentlabs.io>
2022-06-16 17:03:00 -0400

    Lil layer of UIUX to separate "create a transaction" from "create a dao"

--- src/pages/Transactions/New.tsx
+++ src/pages/Transactions/New.tsx
@@ -1,7 +1,60 @@
+import React, { useState } from 'react';
 import DaoCreator from '../../components/DaoCreator';
+import { PrimaryButton, SecondaryButton } from '../../components/ui/forms/Button';
+
+enum View {
+  Cards,
+  CreateDAO,
+}
+
+function CreateDAOCard({ onClick }: { onClick: () => void }) {
+  return (
+    <PrimaryButton
+      label="Create a SubDAO"
+      onClick={onClick}
+    />
+  );
+}
+
+function CardDetails({
+  setView,
+  children,
+}: {
+  setView: (value: React.SetStateAction<View>) => void;
+  children: React.ReactNode;
+}) {
+  return (
+    <div>
+      <SecondaryButton
+        label="back"
+        className="mb-4"
+        onClick={() => setView(View.Cards)}
+      />
+      {children}
+    </div>
+  );
+}
 
 function New() {
-  return <DaoCreator />;
+  const [view, setView] = useState(View.Cards);
+
+  switch (view) {
+    case View.CreateDAO: {
+      return (
+        <CardDetails setView={setView}>
+          <DaoCreator />
+        </CardDetails>
+      );
+    }
+    case View.Cards:
+    default: {
+      return (
+        <div className="flex flex-wrap">
+          <CreateDAOCard onClick={() => setView(View.CreateDAO)} />
+        </div>
+      );
+    }
+  }
 }
 
 export default New;
