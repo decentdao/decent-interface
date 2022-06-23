e0ccea8fa4de3e2b1f66edf8065aae67061bf8a8
Adam Gall <adam@decentlabs.io>
2022-06-22 22:06:42 -0400

    Use interface introspection to find Treasury and Governor contracts

--- src/contexts/daoData/treasury/useTreasuryModuleContract.ts
+++ src/contexts/daoData/treasury/useTreasuryModuleContract.ts
@@ -2,27 +2,42 @@ import { useEffect, useState } from 'react';
 import {
   TreasuryModule,
   TreasuryModule__factory,
+  ITreasuryModule__factory,
 } from '../../../assets/typechain-types/module-treasury';
 import { useWeb3Provider } from '../../web3Data/hooks/useWeb3Provider';
+import use165Contracts from '../../../hooks/use165Contracts';
+import useSupportsInterfaces from '../../../hooks/useSupportsInterfaces';
+import { IModuleBase__factory } from '@fractal-framework/core-contracts';
 
-const useTreasuryModuleContract = (moduleAddresses?: string[]) => {
-  const {
-    state: { provider },
-  } = useWeb3Provider();
+const useTreasuryModuleContract = (moduleAddresses: string[] | undefined) => {
   const [treasuryModule, setTreasuryModule] = useState<TreasuryModule>();
+  const {
+    state: { signerOrProvider },
+  } = useWeb3Provider();
+
+  const [contracts] = use165Contracts(moduleAddresses);
+  const [interfaces] = useState([
+    IModuleBase__factory.createInterface(),
+    ITreasuryModule__factory.createInterface(),
+  ]);
+
+  const [potentialTreasuries] = useSupportsInterfaces(contracts, interfaces);
 
   useEffect(() => {
-    if (
-      moduleAddresses === undefined ||
-      !moduleAddresses.length ||
-      !moduleAddresses[0] ||
-      !provider
-    ) {
+    if (potentialTreasuries === undefined || !signerOrProvider) {
       setTreasuryModule(undefined);
       return;
     }
-    setTreasuryModule(TreasuryModule__factory.connect(moduleAddresses[0], provider));
-  }, [moduleAddresses, provider]);
+
+    const treasury = potentialTreasuries.find(t => t.match === true);
+    if (treasury === undefined) {
+      setTreasuryModule(undefined);
+      return;
+    }
+
+    setTreasuryModule(TreasuryModule__factory.connect(treasury.address, signerOrProvider));
+  }, [potentialTreasuries, signerOrProvider, moduleAddresses]);
+
   return treasuryModule;
 };
 
