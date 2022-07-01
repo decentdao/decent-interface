c9a5d5f603b32c381cbdc267381c8409b453236c
Christopher Dancy <chris.dancy@decentlabs.io>
2022-07-01 16:09:21 -0400

    seperate treasuries for nfts vs eth and tokens

--- src/pages/Treasury/index.tsx
+++ src/pages/Treasury/index.tsx
@@ -28,7 +28,7 @@ function Treasury() {
     <div>
       <H1>{name} Treasury</H1>
       <div className="rounded-lg p-4 shadow-2xl my-4 bg-gray-600">
-        <ContentBoxTitle>Treasury</ContentBoxTitle>
+        <ContentBoxTitle>ETH / Tokens</ContentBoxTitle>
         <div className="my-2">
           <div className="flex justify-between items-end bg-gray-400 px-4 pb-2 h-10">
             <div className="flex">
@@ -56,29 +56,30 @@ function Treasury() {
               </div>
             </TableRowWrapper>
           )}
-          {treasuryAssets.map(asset => (
-            <TableRowWrapper key={asset.contractAddress}>
-              <div className="flex">
-                <EtherscanLink address={asset.contractAddress}>
-                  <div className="text-gold-500 w-16 sm:w-28">{asset.symbol}</div>
-                </EtherscanLink>
-                <div className="text-gray-25 font-medium">{asset.name}</div>
-              </div>
-              <div className="text-gray-25 font-mono font-semibold tracking-wider">
-                {asset.formatedTotal}
-              </div>
-            </TableRowWrapper>
-          ))}
+          {treasuryAssets
+            .filter(asset => asset.type.includes('erc20') || asset.type.includes('native'))
+            .map(asset => (
+              <TableRowWrapper key={asset.contractAddress}>
+                <div className="flex">
+                  <EtherscanLink address={asset.contractAddress}>
+                    <div className="text-gold-500 w-16 sm:w-28">{asset.symbol}</div>
+                  </EtherscanLink>
+                  <div className="text-gray-25 font-medium">{asset.name}</div>
+                </div>
+                <div className="text-gray-25 font-mono font-semibold tracking-wider">
+                  {asset.formatedTotal}
+                </div>
+              </TableRowWrapper>
+            ))}
         </div>
       </div>
       <div className="rounded-lg p-4 shadow-2xl my-4 bg-gray-600">
-        <ContentBoxTitle>NFT Treasury</ContentBoxTitle>
+        <ContentBoxTitle>NFTs</ContentBoxTitle>
         <div className="my-2">
           <div className="flex justify-between items-end bg-gray-400 px-4 pb-2 h-10">
             <div className="flex">
               <div className="text-gray-50 text-xs font-medium w-16 sm:w-28">Symbol</div>
               <div className="text-gray-50 text-xs font-medium">Name</div>
-              <div className="text-gray-50 text-xs font-medium">Token URI</div>
             </div>
             <div className="text-gray-50 text-xs font-medium">Token Id</div>
           </div>
