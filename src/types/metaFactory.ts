bb74ed9c55e492f2da23ec470b19335e80d4e9d7
tbwebb22 <tbwebb22@gmail.com>
2022-07-11 15:48:26 -0400

    Update MetaFactoryCreateDAOData type

--- src/types/metaFactory.ts
+++ src/types/metaFactory.ts
@@ -1,9 +1,11 @@
-import { BigNumberish, Bytes } from 'ethers';
+import { BigNumberish } from 'ethers';
 import { IDAOFactory } from '@fractal-framework/core-contracts';
 
 export interface MetaFactoryCreateDAOData {
   daoFactory: string;
   createDAOParams: IDAOFactory.CreateDAOParamsStruct;
+  moduleFactories: string[];
+  moduleFactoriesBytes: string[][];
   targets: string[];
   values: BigNumberish[];
   calldatas: string[];
