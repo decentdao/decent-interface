24fd6b85482f54fbc5695f37e7fb2eb360c5b4f8
David Colon <dcolonantonio@gmail.com>
2022-07-18 04:45:49 -0400

    adds provider types

--- src/providers/govenor/hooks/useGovenorModule.ts
+++ src/providers/govenor/hooks/useGovenorModule.ts
@@ -1,6 +1,26 @@
+import { BigNumber } from 'ethers';
 import { Context, createContext, useContext } from 'react';
+import { GovernorModule, Timelock } from '../../../assets/typechain-types/metafactory';
+import { VotesTokenWithSupply } from '../../../assets/typechain-types/votes-token';
+import { ProposalData } from './useProposals';
 
-export interface IGovernorModule {}
+export interface IGovernorModule {
+  governorModuleContract: GovernorModule | undefined;
+  timelockModuleContract: Timelock | undefined;
+  proposals: ProposalData[] | undefined;
+  votingToken: {
+    votingTokenContract: VotesTokenWithSupply | undefined;
+    votingTokenData: {
+      name: string | undefined;
+      symbol: string | undefined;
+      decimals: number | undefined;
+      userBalance: BigNumber | undefined;
+      delegatee: string | undefined;
+      votingWeight: BigNumber | undefined;
+      address: string | undefined;
+    };
+  };
+}
 
 export const GovernorContext = createContext<IGovernorModule | null>(null);
 
