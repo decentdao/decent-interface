a6bbca5bd34a5d24795abaf61907fa5ca404ef12
David Colon <dcolonantonio@gmail.com>
2022-07-22 11:37:37 -0400

    fixed spelling

--- src/components/DaoCreator/provider/constants/index.ts
+++ src/components/DaoCreator/provider/constants/index.ts
@@ -3,6 +3,6 @@ import { CreatorSteps } from '../types';
 export const CREATOR_STEP_TITLES = {
   [CreatorSteps.ESSENTIALS]: 'Configure Essentials',
   [CreatorSteps.TREASURY_GOV_TOKEN]: 'Treasury and Governace Token',
-  [CreatorSteps.GOV_CONFIG]: 'Govenance',
+  [CreatorSteps.GOV_CONFIG]: 'Governance',
   [CreatorSteps.FUNDING]: 'Funding',
 };
