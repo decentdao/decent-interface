ab8086d884f6f311b3969fcc51dcf6b9170fc902
tbwebb22 <tbwebb22@gmail.com>
2022-07-27 11:27:14 -0400

    Refactor ClaimToken component

--- src/components/Dao/ClaimToken.tsx
+++ src/components/Dao/ClaimToken.tsx
@@ -2,37 +2,45 @@ import { useState } from 'react';
 import ContentBox from '../ui/ContentBox';
 import { PrimaryButton } from '../ui/forms/Button';
 import { useDAOData } from '../../contexts/daoData';
+import useClaimToken from '../../hooks/useClaimToken';
+import { ethers } from 'ethers';
 
 function ClaimToken() {
   const [pending, setPending] = useState<boolean>(false);
-  const [claimAmount, setClaimAmount] = useState<string>('1000.0');
 
   const [
     {
       modules: {
         governor: {
           votingToken: {
-            votingTokenData: { decimals, symbol, userBalance, delegatee, votingWeight },
+            votingTokenData: { decimals, symbol, userClaimAmount },
           },
         },
       },
     },
   ] = useDAOData();
 
-  // const claimToken = useClaimToken({
-  //   setPending,
-  // });
+  const claimToken = useClaimToken({
+    setPending,
+  });
+
+  if (userClaimAmount === undefined || userClaimAmount.eq(0)) {
+    return null;
+  }
 
   return (
     <ContentBox isLightBackground>
       <div className="flex justify-between items-center">
         <div className="text-gray-25">
-          {`You have ${claimAmount} ${symbol} available to claim!`}
+          {`You have ${ethers.utils.formatUnits(
+            userClaimAmount.toString(),
+            decimals
+          )} ${symbol} available to claim!`}
         </div>
         <PrimaryButton
           label="Claim"
           className="mr-0"
-          onClick={() => console.log('Claiming')}
+          onClick={claimToken}
           disabled={pending}
         />
       </div>
