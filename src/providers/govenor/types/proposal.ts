a951bf86ef9d7e5f97645d6159e5df4372456433
David Colon <dcolonantonio@gmail.com>
2022-08-04 14:10:04 -0400

    breaks useProposal to serpeate hooks and files - moves types and utils to own files - removes conditional preventing loading of proposals

---
+++ src/providers/govenor/types/proposal.ts
@@ -0,0 +1,53 @@
+import { BigNumber } from 'ethers';
+
+export enum ProposalState {
+  Pending = 0,
+  Active = 1,
+  Canceled = 2,
+  Defeated = 3,
+  Succeeded = 4,
+  Queued = 5,
+  Expired = 6,
+  Executed = 7,
+}
+
+export type ProposalDataWithoutUserData = {
+  number: number;
+  id: BigNumber;
+  idSubstring: string | undefined;
+  startBlock: BigNumber;
+  endBlock: BigNumber;
+  startTime: Date | undefined;
+  endTime: Date | undefined;
+  startTimeString: string | undefined;
+  endTimeString: string | undefined;
+  proposer: string;
+  targets: string[];
+  signatures: string[];
+  calldatas: string[];
+  values: BigNumber[];
+  description: string;
+  state: ProposalState | undefined;
+  forVotesCount: BigNumber | undefined;
+  againstVotesCount: BigNumber | undefined;
+  abstainVotesCount: BigNumber | undefined;
+  forVotesPercent: number | undefined;
+  againstVotesPercent: number | undefined;
+  abstainVotesPercent: number | undefined;
+  eta: number | undefined;
+};
+export interface ProposalData extends ProposalDataWithoutUserData {
+  userVote: number | undefined;
+  userVoteString: 'For' | 'Against' | 'Abstain' | undefined;
+  userVotePower: BigNumber | undefined;
+}
+
+export type UserVote = {
+  proposalId: BigNumber;
+  vote: number | undefined;
+};
+
+export type UserVotePower = {
+  proposalId: BigNumber;
+  votePower: BigNumber | undefined;
+};
