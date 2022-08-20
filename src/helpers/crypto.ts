ca031cbe05bb995dd23b3cca25c7596255193bfc
Adam Gall <adam@decentlabs.io>
2022-08-20 13:20:39 -0400

    Update random byte generation code to be way more random

--- src/helpers/crypto.ts
+++ src/helpers/crypto.ts
@@ -1,9 +1,10 @@
 import { ethers } from 'ethers';
 
 export function getRandomSalt() {
-  return ethers.utils.formatBytes32String(
-    self.crypto.getRandomValues(new BigUint64Array(1))[0].toString()
-  );
+  const bytes8Array = new Uint8Array(32);
+  self.crypto.getRandomValues(bytes8Array);
+  const bytes32 = '0x' + bytes8Array.reduce((o, v) => o + ('00' + v.toString(16)).slice(-2), '');
+  return bytes32;
 }
 
 /**
