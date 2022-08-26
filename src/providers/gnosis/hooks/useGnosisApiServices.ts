01f1f42c6d9322857af92feae6fd1d61b2634891
David Colon <dcolonantonio@gmail.com>
2022-08-25 22:27:50 -0400

    hook to handle gnosis safe requests

---
+++ src/providers/gnosis/hooks/useGnosisApiServices.ts
@@ -0,0 +1,43 @@
+import { buildGnosisApiUrl } from '../helpers';
+import { useWeb3Provider } from '../../../contexts/web3Data/hooks/useWeb3Provider';
+import { useCallback, useEffect } from 'react';
+import { GnosisActions, GnosisActionTypes } from '../types';
+import axios from 'axios';
+import { GnosisSafeStatusResponse } from '../types/gnosis';
+
+export function useGnosisApiServices(
+  safeAddress: string | undefined,
+  dispatch: React.Dispatch<GnosisActionTypes>
+) {
+  const {
+    state: { chainId, account },
+  } = useWeb3Provider();
+
+  const getGnosisSafeStatus = useCallback(async () => {
+    if (!safeAddress || !account) {
+      return;
+    }
+    try {
+      const safeStatusResponse = await axios.get<GnosisSafeStatusResponse>(
+        buildGnosisApiUrl(chainId, `/safes/${safeAddress}/`, {})
+      );
+      dispatch({
+        type: GnosisActions.UPDATE_GNOSIS_SAFE_INFORMATION,
+        payload: {
+          name: ' ',
+          owners: safeStatusResponse.data.owners,
+          threshold: safeStatusResponse.data.threshold,
+          isSigner: safeStatusResponse.data.owners.includes(account),
+        },
+      });
+    } catch (e) {
+      console.log(e);
+    }
+  }, [chainId, safeAddress, dispatch, account]);
+
+  useEffect(() => {
+    getGnosisSafeStatus();
+  }, [getGnosisSafeStatus]);
+
+  return;
+}
