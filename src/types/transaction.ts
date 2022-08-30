d576e059ba3707853aa242580745ad53817e5ac1
tbwebb22 <tbwebb22@gmail.com>
2022-08-30 16:58:32 -0400

    Add GnosisTransactionData type

--- src/types/transaction.ts
+++ src/types/transaction.ts
@@ -1,3 +1,4 @@
+import { BigNumber } from 'ethers';
 export interface TransactionData {
   targetAddress: string;
   functionName: string;
@@ -5,4 +6,20 @@ export interface TransactionData {
   parameters: string;
   addressError?: string;
   fragmentError?: string;
+}
+export interface GnosisTransactionData {
+  to: string;
+  value: BigNumber;
+  data: string;
+  operation: number;
+  gasToken: string;
+  safeTxGas: BigNumber;
+  baseGas: BigNumber;
+  gasPrice: BigNumber;
+  refundReceiver: string;
+  nonce: number;
+  // contractTransactionHash: string;
+  // sender: string;
+  // signature: string;
+  // origin: string;
 }
