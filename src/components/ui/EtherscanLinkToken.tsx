6e49e2dbbd985a9c8bae9ab9726fe71a2dab6126
Kirill Klimenko <klimenkomud@gmail.com>
2022-09-01 14:25:06 +0300

    Remove /token if address equals to zero addess

--- src/components/ui/EtherscanLinkToken.tsx
+++ src/components/ui/EtherscanLinkToken.tsx
@@ -1,10 +1,13 @@
 import useSubDomain from '../../hooks/useSubDomain';
+import { ethers } from 'ethers';
 
 function EtherscanLinkToken({ address, children }: { address: string; children: React.ReactNode }) {
   const subdomain = useSubDomain();
   return (
     <a
-      href={`https://${subdomain}etherscan.io/token/${address}`}
+      href={`https://${subdomain}etherscan.io/${
+        address === ethers.constants.AddressZero ? '' : 'token/'
+      }${address}`}
       target="_blank"
       rel="noreferrer"
     >
