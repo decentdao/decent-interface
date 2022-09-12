79360d500cc52cfb2432e7f905360d6cc5d39abc
Brian Fryer <brianfryer@gmail.com>
2022-09-12 09:46:28 -0500

    Adding `react-query` dependency.

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { HashRouter } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import { HelmetProvider, Helmet } from 'react-helmet-async';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 import './index.css';
 import reportWebVitals from './reportWebVitals';
@@ -13,31 +14,34 @@ import { FractalProvider } from './providers/fractal/FractalProvider';
 
 const container = document.getElementById('root');
 const root = createRoot(container!);
+const queryClient = new QueryClient();
 
 root.render(
   <React.StrictMode>
-    <HelmetProvider>
-      <Helmet>
-        <title>Fractal</title>
-      </Helmet>
-      <HashRouter>
-        <Web3Provider>
-          <BlockchainDataProvider>
-            <FractalProvider>
-              <ToastContainer
-                position="bottom-center"
-                closeButton={false}
-                newestOnTop={false}
-                pauseOnFocusLoss={false}
-                toastClassName="mt-2 bottom-0 mb-0 font-sans font-medium shadow bg-gray-400 text-gray-25 text-center cursor-pointer"
-                progressClassName="bg-none bg-gold-500"
-              />
-              <App />
-            </FractalProvider>
-          </BlockchainDataProvider>
-        </Web3Provider>
-      </HashRouter>
-    </HelmetProvider>
+    <QueryClientProvider client={queryClient}>
+      <HelmetProvider>
+        <Helmet>
+          <title>Fractal</title>
+        </Helmet>
+        <HashRouter>
+          <Web3Provider>
+            <BlockchainDataProvider>
+              <FractalProvider>
+                <ToastContainer
+                  position="bottom-center"
+                  closeButton={false}
+                  newestOnTop={false}
+                  pauseOnFocusLoss={false}
+                  toastClassName="mt-2 bottom-0 mb-0 font-sans font-medium shadow bg-gray-400 text-gray-25 text-center cursor-pointer"
+                  progressClassName="bg-none bg-gold-500"
+                />
+                <App />
+              </FractalProvider>
+            </BlockchainDataProvider>
+          </Web3Provider>
+        </HashRouter>
+      </HelmetProvider>
+    </QueryClientProvider>
   </React.StrictMode>
 );
 
