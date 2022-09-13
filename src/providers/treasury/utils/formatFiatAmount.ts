b49ee13c895c37a672a65cf4da53bd40d67af713
Brian Fryer <brianfryer@gmail.com>
2022-09-13 14:09:34 -0500

    Update formatFiatAmount.ts

--- src/providers/treasury/utils/formatFiatAmount.ts
+++ src/providers/treasury/utils/formatFiatAmount.ts
@@ -6,15 +6,17 @@ const numberWithCommas = (number: number, decimals: number) => {
 };
 
 const formatFiatAmount = (currencyId: string, amount: number) => {
-  // 1234.567 -> 1,234.56
-  const currency = numberWithCommas(amount, 2);
+  // 1234.567 -> 1,234.57
+  const roundedAmount = Math.round((amount + Number.EPSILON) * 100) / 100;
+  const currency = numberWithCommas(roundedAmount, 2);
 
   // on occasion, we'll need to display the `amount` with
   // at least 2 decimal places, unless the amount is a
   // whole number. doing so will prevent oddly formatted
-  // values (e.g. "$0.5", "$1234.00").
+  // values (e.g. "0.5" -> "0.50").
   const decimals = countDecimals(amount);
   const maxDecimals = decimals ? Math.max(2, decimals) : 0;
+
   // 1234.567 -> 1,234.567, 1234 -> 1,234
   const formattedAmount = numberWithCommas(amount, maxDecimals);
 
