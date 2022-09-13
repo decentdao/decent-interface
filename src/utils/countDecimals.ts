990c9433603298d9f79f73871c07727a4b110802
Brian Fryer <brianfryer@gmail.com>
2022-09-13 11:57:30 -0500

    Adding clarity to `countDecimals` utility function.

--- src/utils/countDecimals.ts
+++ src/utils/countDecimals.ts
@@ -1,7 +1,9 @@
 const countDecimals = (value: number) => {
-  if (!value || value % 1 === 0) return 0;
-  const count = value?.toString().split('.').pop();
-  return count?.length || 0;
+  const isWholeNumber = value % 1 === 0;
+  if (isWholeNumber) return 0;
+
+  const count = value.toString().split('.').pop();
+  return count.length || 0;
 };
 
 export default countDecimals;
