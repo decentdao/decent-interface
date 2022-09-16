a0d4055ada75431db7cb50a36af48cd220ff5066
Brian Fryer <brianfryer@gmail.com>
2022-09-16 10:29:14 -0500

    Updating variable name for clarity.

--- src/utils/countDecimals.ts
+++ src/utils/countDecimals.ts
@@ -2,8 +2,8 @@ const countDecimals = (value: number) => {
   const isWholeNumber = value % 1 === 0;
   if (isWholeNumber) return 0;
 
-  const count = value.toString().split('.').pop();
-  return count?.length || 0;
+  const decimals = value.toString().split('.').pop();
+  return decimals?.length || 0;
 };
 
 export default countDecimals;
