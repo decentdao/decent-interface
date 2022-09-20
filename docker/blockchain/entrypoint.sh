ea11baa1cdc5b7ef02106c469ff8d9097b1d9e96
David Colon <dcolonantonio@gmail.com>
2022-09-20 13:02:37 -0400

    remove unneeded code

--- docker/blockchain/entrypoint.sh
+++ docker/blockchain/entrypoint.sh
@@ -4,9 +4,3 @@ cd /usr/src/fractal-test;
 
 # Run hardhat
 npm run start;
-# Keep node alive
-set -e
-if [ "${1#-}" != "${1}" ] || [ -z "$(command -v "${1}")" ]; then
-  set -- node "$@"
-fi
-exec "$@"
