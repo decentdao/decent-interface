fd8775d0b633bd656aa22198d61c9fbedb08c184
David Colon <dcolonantonio@gmail.com>
2022-09-21 15:31:28 -0400

    correctly forks goerli

--- docker/blockchain/entrypoint.sh
+++ docker/blockchain/entrypoint.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
-# Change to the correct directory
-cd /usr/src/fractal-test;
 
 # Run hardhat
 npm run start;
--- docker/blockchain/hardhat.config.js
+++ docker/blockchain/hardhat.config.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
 task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
@@ -18,10 +19,10 @@ task('accounts', 'Prints the list of accounts', async (taskArgs, hre) => {
   solidity: "0.8.4",
   networks: { 
     hardhat: {
-      chainId: 31337
+      chainId: 31337,
+      forking: {
+        url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
+      }
     },
-    forking: {
-      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_API_KEY}`
-    }
   },
 };
