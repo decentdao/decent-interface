320e8f59d5ff5d8ec7ba2c4b362eec0a3422680b
Adam Gall <adam@decentlabs.io>
2022-09-22 17:52:19 -0400

    Update environment variable name for alchemy api

--- docker/blockchain/hardhat.config.js
+++ docker/blockchain/hardhat.config.js
@@ -9,7 +9,7 @@ require('dotenv').config()
     hardhat: {
       chainId: 31337,
       forking: {
-        url: `https://eth-goerli.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`
+        url: `https://eth-goerli.g.alchemy.com/v2/${process.env.TESTING_ALCHEMY_API_KEY}`
       }
     },
   },
