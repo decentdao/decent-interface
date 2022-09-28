0cf518257dafb2542ccc5f67a27047ed37c40c4e
David Colon <dcolonantonio@gmail.com>
2022-09-28 12:35:34 -0400

    add timeout

--- tests/001navigateToFractal.spec.ts
+++ tests/001navigateToFractal.spec.ts
@@ -2,7 +2,7 @@ import { test, expect } from '@playwright/test';
 
 test('Go to the Fractal App', async ({ page, browserName }) => {
   // For CI use when modded wallet is present
-  await page.goto('http://localhost:3000');
+  await page.goto('http://localhost:3000', { timeout: 10000 });
 
   // Go to dev.app
   //await page.goto('https://app.dev.fractalframework.xyz/');
