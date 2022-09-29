45475f7ab5dd5872e7d27f8bba2bb37f76ec65be
Vanessa Kelly <vkelly@cannabiz.media>
2022-09-29 13:19:38 -0400

    removed custom.ts folder - PR Review

--- page-objects/Helpers/custom.ts
+++
@@ -1,5 +0,0 @@
-//import { Browser, BrowserContext, test, expect } from '@playwright/test';
-
-export async function delay(ms: number) {
-  return new Promise(resolve => setTimeout(resolve, ms));
-}
