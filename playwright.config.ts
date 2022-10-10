30eb62b1d65f1c7aa8d54ec49a9be2d19972fa40
Vanessa Kelly <vkelly@cannabiz.media>
2022-10-10 16:55:33 -0400

    Cleaned up comments + Increased viewport size
    Added another helper file for DOA safe selectors

--- playwright.config.ts
+++ playwright.config.ts
@@ -33,7 +33,7 @@ const config: PlaywrightTestConfig = {
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     headless: true,
-		viewport: { width: 1280, height: 920 },
+		viewport: { width: 1280, height: 1020 },
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 25000,
     ignoreHTTPSErrors: true,
--- tests/003createParentDao.spec.ts
+++ tests/003createParentDao.spec.ts
@@ -1,40 +1,35 @@
+import { DaoSafe } from '../page-objects/DaoSafe';
+import { delay } from '../page-objects/Helpers/helpers';
 import { expect, test } from '@playwright/test';
 import { HomePage } from '../page-objects/HomePage';
+import { InputFields } from '../page-objects/InputFields';
 import { Navbuttons } from '../page-objects/components/Navbuttons';
 import { Notifications } from '../page-objects/Notifications';
-import { InputFields } from '../page-objects/InputFields';
-import { delay } from '../page-objects/Helpers/helpers';
 
 test.describe('DAO Creation', () => {
+  let daoSafe: DaoSafe;
   let homePage: HomePage;
+  let inputFields: InputFields;
   let navButtons: Navbuttons;
   let notifications: Notifications;
-  let inputFields: InputFields;
 
   test.beforeEach(async ({ page }) => {
+    daoSafe = new DaoSafe(page);
     homePage = new HomePage(page);
     navButtons = new Navbuttons(page);
     notifications = new Notifications(page);
     inputFields = new InputFields(page);
     homePage.visit();
-    await delay(5000);
-    //await page.waitForLoadState('load');
-    //await page.waitForTimeout(1000);
+    await delay(3000);
     notifications.closeButton('Close Audit Message');
     await page.waitForLoadState();
-    await delay(2000);
-    //await delay(3000);
-    // await page.waitForTimeout(1000);
-
+    await delay(1500);
     navButtons.clickOnButton('Connect to Wallet on Header');
-    //await page.waitForTimeout(1000);
     await page.waitForLoadState('load');
     await delay(2000);
     await page.click('[data-testid="menu:connect"]');
-    //await page.waitForTimeout(1500);
     await page.waitForLoadState('domcontentloaded');
     navButtons.clickOnButton('Select Local Wallet - Web3');
-    //await page.waitForTimeout(4000);
     page.waitForSelector('#connected');
     notifications.assertConnected();
     await page.waitForLoadState('networkidle');
@@ -46,21 +41,27 @@ test.describe('DAO Creation', () => {
     /* Check URL to make sure navigation is correct. */
     await expect(page).toHaveURL('http://localhost:3000/#/daos/new');
 
+    /* Input a Fractal name */
     inputFields.fillField('Insert Fractal Name');
     navButtons.clickOnButton('Next Button');
 
     /* Select Gnosis Safe */
-    await page.locator('text=Gnosis Safe').click();
-
+    daoSafe.selectSafe('Select Gnosis 1:1 Safe');
+    await delay(1000);
     navButtons.clickOnButton('Next Button');
+
+    /* Add Wallet address */
     inputFields.fillField('Insert Local Node Wallet');
     navButtons.clickOnButton('Deploy Button');
+
+    /* Check toaster message for 'Deploying' text */
     notifications.assertDeployed();
-    await page.waitForLoadState();
+    await page.waitForLoadState('domcontentloaded');
 
     /* Check URL to make sure wallet is connected. */
     await expect(page).not.toHaveURL('http://localhost:3000/#/daos/new');
 
+    /* Check header for created DAO title */
     const parentDAO = page.locator('text=Playwright Parent | Home');
     await expect(parentDAO).toContainText('Playwright Parent | Home');
   });
