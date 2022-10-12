675812dbb5f1323b908a0146e2d88f9245032aeb
David Colon <dcolonantonio@gmail.com>
2022-10-12 01:10:12 -0400

    replaces components with chakra components

--- src/App.tsx
+++ src/App.tsx
@@ -1,65 +1,25 @@
-import { useEffect } from 'react';
-import { toast } from 'react-toastify';
-
-import Body from './components/Body';
-import Breadcrumbs from './components/Breadcrumbs';
-import Header from './components/ui/Header';
-import { TextButton } from './components/ui/forms/Button';
 import { useLocalStorage } from './hooks/useLocalStorage';
 import './i18n';
-import { useTranslation } from 'react-i18next';
+import { Flex } from '@chakra-ui/react';
+import { ActionToast } from './components/ui/toasts/ActionToast';
 
-function NotAuditedToast() {
+function App() {
   const [notAuditedAcceptance, setNotAuditedAcceptance] = useLocalStorage(
     'not_audited_acceptance',
     false
   );
-
-  const { t } = useTranslation();
-
-  useEffect(() => {
-    if (notAuditedAcceptance) {
-      return;
-    }
-
-    const toastId = toast(
-      <div className="flex flex-col items-center">
-        <div>{t('auditDisclaimer')}</div>
-        <TextButton
-          label={t('accept')}
-          onClick={() => setNotAuditedAcceptance(true)}
-        />
-      </div>,
-      {
-        autoClose: false,
-        closeOnClick: false,
-        draggable: false,
-        progress: 1,
-      }
-    );
-
-    return () => toast.dismiss(toastId);
-  }, [notAuditedAcceptance, setNotAuditedAcceptance, t]);
-
-  return null;
-}
-
-function App() {
   return (
-    <>
-      <NotAuditedToast />
-      <div className="flex flex-col min-h-screen font-medium text-gray-25">
-        <Header />
-        <main className="flex-grow bg-fixed bg-image-pattern bg-cover">
-          <Breadcrumbs />
-          <div className="container pt-20 pb-10">
-            <div className="max-w-4xl mx-auto">
-              <Body />
-            </div>
-          </div>
-        </main>
-      </div>
-    </>
+    <Flex
+      flexDirection="column"
+      minHeight="h-screen"
+    >
+      <ActionToast
+        isVisible={!notAuditedAcceptance}
+        titleTranslationKey="auditDisclaimer"
+        buttonTranslationKey="accept"
+        buttonOnClick={() => setNotAuditedAcceptance(true)}
+      />
+    </Flex>
   );
 }
 
