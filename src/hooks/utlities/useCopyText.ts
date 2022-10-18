a90a6814a30c427dab1800063d3b6446aecd881c
David Colon <dcolonantonio@gmail.com>
2022-10-17 21:37:12 -0400

    updates copy toast to use translation

--- src/hooks/utlities/useCopyText.ts
+++ src/hooks/utlities/useCopyText.ts
@@ -1,9 +1,11 @@
+import { useTranslation } from 'react-i18next';
 import { toast } from 'react-toastify';
 
 export function useCopyText() {
+  const { t } = useTranslation('common');
   const copyTextToClipboard = (textToCopy?: string) => {
     if (!textToCopy) return;
-    toast('Copied to clipboard', {
+    toast(t('toastClipboardCopy'), {
       onOpen: () => navigator.clipboard.writeText(textToCopy),
       autoClose: 1000,
     });
