74e34b8c49bded3ba24d4f736128b089439ce85e
Kirill Klimenko <klimenkomud@gmail.com>
2022-10-25 21:40:50 +0300

    Refactor useAddresses - re-use types

--- src/hooks/useAddresses.ts
+++ src/hooks/useAddresses.ts
@@ -1,29 +1,31 @@
 import { useState, useEffect } from 'react';
 import { logError } from '../helpers/errorLogging';
 
+export type ContractAddress = { address: string };
+export type ContractAddressesChainMap = { [chaindId: number]: ContractAddress };
 export function useAddresses(chainId: number | undefined) {
   const [addresses, setAddresses] = useState<{
-    metaFactory?: { address: string };
-    daoFactory?: { address: string };
-    treasuryModuleFactory?: { address: string };
-    tokenFactory?: { address: string };
-    votesMasterCopy?: { address: string };
-    governorFactory?: { address: string };
-    claimFactory?: { address: string };
-    gnosisWrapperFactory?: { address: string };
-    gnosisSafeFactory?: { address: string };
-    usulMastercopy?: { address: string };
-    linearVotingMastercopy?: { address: string };
-    zodiacModuleProxyFactory?: { address: string };
-    dao?: { address: string };
-    accessControl?: { address: string };
-    treasuryModule?: { address: string };
-    governorModule?: { address: string };
-    timelock?: { address: string };
-    claimModule?: { address: string };
-    gnosisWrapper?: { address: string };
-    gnosisSafe?: { address: string };
-    multiSend?: { address: string };
+    metaFactory?: ContractAddress;
+    daoFactory?: ContractAddress;
+    treasuryModuleFactory?: ContractAddress;
+    tokenFactory?: ContractAddress;
+    votesMasterCopy?: ContractAddress;
+    governorFactory?: ContractAddress;
+    claimFactory?: ContractAddress;
+    gnosisWrapperFactory?: ContractAddress;
+    gnosisSafeFactory?: ContractAddress;
+    usulMastercopy?: ContractAddress;
+    linearVotingMastercopy?: ContractAddress;
+    zodiacModuleProxyFactory?: ContractAddress;
+    dao?: ContractAddress;
+    accessControl?: ContractAddress;
+    treasuryModule?: ContractAddress;
+    governorModule?: ContractAddress;
+    timelock?: ContractAddress;
+    claimModule?: ContractAddress;
+    gnosisWrapper?: ContractAddress;
+    gnosisSafe?: ContractAddress;
+    multiSend?: ContractAddress;
   }>({});
 
   useEffect(() => {
@@ -56,90 +58,91 @@ export function useAddresses(chainId: number | undefined) {
       return;
     }
 
-    const metaFactoryNetworksAddresses: { [chaindId: number]: { address: string } } = JSON.parse(
+    const metaFactoryNetworksAddresses: ContractAddressesChainMap = JSON.parse(
       process.env.REACT_APP_METAFACTORY_ADDRESSES
     );
-    const daoFactoryNetworksAddresses: { [chaindId: number]: { address: string } } = JSON.parse(
+    const daoFactoryNetworksAddresses: ContractAddressesChainMap = JSON.parse(
       process.env.REACT_APP_DAOFACTORY_ADDRESSES
     );
-    const treasuryModuleFactoryNetworksAddresses: { [chaindId: number]: { address: string } } =
-      JSON.parse(process.env.REACT_APP_TREASURYMODULEFACTORY_ADDRESSES);
-    const tokenFactoryNetworksAddresses: { [chaindId: number]: { address: string } } = JSON.parse(
+    const treasuryModuleFactoryNetworksAddresses: ContractAddressesChainMap = JSON.parse(
+      process.env.REACT_APP_TREASURYMODULEFACTORY_ADDRESSES
+    );
+    const tokenFactoryNetworksAddresses: ContractAddressesChainMap = JSON.parse(
       process.env.REACT_APP_TOKENFACTORY_ADDRESSES
     );
-    const governorFactoryNetworksAddresses: { [chaindId: number]: { address: string } } =
-      JSON.parse(process.env.REACT_APP_GOVERNORFACTORY_ADDRESSES);
-    const claimFactoryNetworksAddresses: { [chaindId: number]: { address: string } } = JSON.parse(
+    const governorFactoryNetworksAddresses: ContractAddressesChainMap = JSON.parse(
+      process.env.REACT_APP_GOVERNORFACTORY_ADDRESSES
+    );
+    const claimFactoryNetworksAddresses: ContractAddressesChainMap = JSON.parse(
       process.env.REACT_APP_CLAIMFACTORY_ADDRESSES
     );
-    const gnosisWrapperFactoryNetworksAddresses: { [chaindId: number]: { address: string } } =
-      JSON.parse(process.env.REACT_APP_GNOSISWRAPPERFACTORY_ADDRESSES);
-    const gnosisSafeFactoryNetworksAddresses: { [chaindId: number]: { address: string } } =
-      JSON.parse(process.env.REACT_APP_GNOSISSAFEFACTORY_ADDRESSES);
-    const daoNetworksAddresses: { [chaindId: number]: { address: string } } = JSON.parse(
+    const gnosisWrapperFactoryNetworksAddresses: ContractAddressesChainMap = JSON.parse(
+      process.env.REACT_APP_GNOSISWRAPPERFACTORY_ADDRESSES
+    );
+    const gnosisSafeFactoryNetworksAddresses: ContractAddressesChainMap = JSON.parse(
+      process.env.REACT_APP_GNOSISSAFEFACTORY_ADDRESSES
+    );
+    const daoNetworksAddresses: ContractAddressesChainMap = JSON.parse(
       process.env.REACT_APP_DAO_ADDRESSES
     );
-    const accessControlNetworksAddresses: { [chaindId: number]: { address: string } } = JSON.parse(
+    const accessControlNetworksAddresses: ContractAddressesChainMap = JSON.parse(
       process.env.REACT_APP_ACCESSCONTROL_ADDRESSES
     );
-    const treasuryModuleNetworksAddresses: { [chaindId: number]: { address: string } } = JSON.parse(
+    const treasuryModuleNetworksAddresses: ContractAddressesChainMap = JSON.parse(
       process.env.REACT_APP_TREASURYMODULE_ADDRESSES
     );
-    const governorModuleNetworksAddresses: { [chaindId: number]: { address: string } } = JSON.parse(
+    const governorModuleNetworksAddresses: ContractAddressesChainMap = JSON.parse(
       process.env.REACT_APP_GOVERNORMODULE_ADDRESSES
     );
-    const timelockNetworksAddresses: { [chaindId: number]: { address: string } } = JSON.parse(
+    const timelockNetworksAddresses: ContractAddressesChainMap = JSON.parse(
       process.env.REACT_APP_TIMELOCK_ADDRESSES
     );
-    const claimModuleNetworksAddresses: { [chaindId: number]: { address: string } } = JSON.parse(
+    const claimModuleNetworksAddresses: ContractAddressesChainMap = JSON.parse(
       process.env.REACT_APP_CLAIM_ADDRESSES
     );
-    const gnosisWrapperNetworksAddresses: { [chaindId: number]: { address: string } } = JSON.parse(
+    const gnosisWrapperNetworksAddresses: ContractAddressesChainMap = JSON.parse(
       process.env.REACT_APP_GNOSISWRAPPER_ADDRESSES
     );
-    const gnosisSafeNetworksAddresses: { [chaindId: number]: { address: string } } = JSON.parse(
+    const gnosisSafeNetworksAddresses: ContractAddressesChainMap = JSON.parse(
       process.env.REACT_APP_GNOSISSAFE_ADDRESSES
     );
-    const zodiacProxyFactoryAddresses: { [chainId: number]: { address: string } } = JSON.parse(
+    const zodiacProxyFactoryAddresses: ContractAddressesChainMap = JSON.parse(
       process.env.REACT_APP_ZODIAC_PROXY_FACTORY_ADDRESSES
     );
-    const usulMastercopyAddresses: { [chainId: number]: { address: string } } = JSON.parse(
+    const usulMastercopyAddresses: ContractAddressesChainMap = JSON.parse(
       process.env.REACT_APP_USUL_MASTERCOPY_ADDRESSES
     );
-    const linearVotingMastercopyAddresses: { [chainId: number]: { address: string } } = JSON.parse(
+    const linearVotingMastercopyAddresses: ContractAddressesChainMap = JSON.parse(
       process.env.REACT_APP_ONE_TO_ONE_TOKEN_VOTING_MASTERCOPY_ADDRESSES
     );
-    const multiSendAddresses: { [chainId: number]: { address: string } } = JSON.parse(
+    const multiSendAddresses: ContractAddressesChainMap = JSON.parse(
       process.env.REACT_APP_GNOSIS_MULTISEND_ADDRESSES
     );
-    const votesMasterCopyAddresses: { [chainId: number]: { address: string } } = JSON.parse(
+    const votesMasterCopyAddresses: ContractAddressesChainMap = JSON.parse(
       process.env.REACT_APP_VOTES_TOKEN_MASTERCOPY_ADDRESSES
     );
 
-    const metaFactoryAddress: { address: string } = metaFactoryNetworksAddresses[chainId];
-    const daoFactoryAddress: { address: string } = daoFactoryNetworksAddresses[chainId];
-    const treasuryModuleFactoryAddress: { address: string } =
-      treasuryModuleFactoryNetworksAddresses[chainId];
-    const tokenFactoryAddress: { address: string } = tokenFactoryNetworksAddresses[chainId];
-    const governorFactoryAddress: { address: string } = governorFactoryNetworksAddresses[chainId];
-    const claimFactoryAddress: { address: string } = claimFactoryNetworksAddresses[chainId];
-    const gnosisWrapperFactoryAddress: { address: string } =
-      gnosisWrapperFactoryNetworksAddresses[chainId];
-    const gnosisSafeFactoryAddress: { address: string } =
-      gnosisSafeFactoryNetworksAddresses[chainId];
-    const daoAddress: { address: string } = daoNetworksAddresses[chainId];
-    const accessControlAddress: { address: string } = accessControlNetworksAddresses[chainId];
-    const treasuryModuleAddress: { address: string } = treasuryModuleNetworksAddresses[chainId];
-    const governorModuleAddress: { address: string } = governorModuleNetworksAddresses[chainId];
-    const timelockAddress: { address: string } = timelockNetworksAddresses[chainId];
-    const claimModuleAddress: { address: string } = claimModuleNetworksAddresses[chainId];
-    const gnosisWrapperAddress: { address: string } = gnosisWrapperNetworksAddresses[chainId];
-    const gnosisSafeAddress: { address: string } = gnosisSafeNetworksAddresses[chainId];
-    const usulMastercopy: { address: string } = usulMastercopyAddresses[chainId];
-    const zodiacModuleProxyFactory: { address: string } = zodiacProxyFactoryAddresses[chainId];
-    const linearVotingMastercopy: { address: string } = linearVotingMastercopyAddresses[chainId];
-    const multiSend: { address: string } = multiSendAddresses[chainId];
-    const votesMasterCopy: { address: string } = votesMasterCopyAddresses[chainId];
+    const metaFactoryAddress = metaFactoryNetworksAddresses[chainId];
+    const daoFactoryAddress = daoFactoryNetworksAddresses[chainId];
+    const treasuryModuleFactoryAddress = treasuryModuleFactoryNetworksAddresses[chainId];
+    const tokenFactoryAddress = tokenFactoryNetworksAddresses[chainId];
+    const governorFactoryAddress = governorFactoryNetworksAddresses[chainId];
+    const claimFactoryAddress = claimFactoryNetworksAddresses[chainId];
+    const gnosisWrapperFactoryAddress = gnosisWrapperFactoryNetworksAddresses[chainId];
+    const gnosisSafeFactoryAddress = gnosisSafeFactoryNetworksAddresses[chainId];
+    const daoAddress = daoNetworksAddresses[chainId];
+    const accessControlAddress = accessControlNetworksAddresses[chainId];
+    const treasuryModuleAddress = treasuryModuleNetworksAddresses[chainId];
+    const governorModuleAddress = governorModuleNetworksAddresses[chainId];
+    const timelockAddress = timelockNetworksAddresses[chainId];
+    const claimModuleAddress = claimModuleNetworksAddresses[chainId];
+    const gnosisWrapperAddress = gnosisWrapperNetworksAddresses[chainId];
+    const gnosisSafeAddress = gnosisSafeNetworksAddresses[chainId];
+    const usulMastercopy = usulMastercopyAddresses[chainId];
+    const zodiacModuleProxyFactory = zodiacProxyFactoryAddresses[chainId];
+    const linearVotingMastercopy = linearVotingMastercopyAddresses[chainId];
+    const multiSend = multiSendAddresses[chainId];
+    const votesMasterCopy = votesMasterCopyAddresses[chainId];
 
     if (
       !metaFactoryAddress ||
