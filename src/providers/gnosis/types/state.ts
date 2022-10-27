6349c7e6b930971fa8c293e1e6822c9b231b3233
Christopher Dancy <chris.dancy@decentlabs.io>
2022-10-27 16:19:51 +0100

    proper state types

--- src/providers/gnosis/types/state.ts
+++ src/providers/gnosis/types/state.ts
@@ -1,8 +1,15 @@
-export interface Gnosis extends GnosisInformation {
+import { GnosisAssetFungible, GnosisAssetNonFungible } from '../../treasury/types';
+
+export interface Gnosis extends GnosisInformation, GnosisAssets {
   isLoading: boolean;
   safeAddress?: string;
   name?: string;
   isSigner: boolean;
+}
+
+export interface GnosisAssets {
+  treasuryAssetsFungible: GnosisAssetFungible[];
+  treasuryAssetsNonFungible: GnosisAssetNonFungible[];
 }
 
 export interface GnosisInformation {
