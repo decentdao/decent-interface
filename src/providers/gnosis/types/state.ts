49e77ef6471a498d5507ac9e8b14c78c5f19db0d
Christopher Dancy <chris.dancy@decentlabs.io>
2022-10-28 16:06:07 +0100

    seperate state for assets

--- src/providers/gnosis/types/state.ts
+++ src/providers/gnosis/types/state.ts
@@ -1,14 +1,17 @@
 import { GnosisAssetFungible, GnosisAssetNonFungible } from '../../treasury/types';
 
-export interface Gnosis extends GnosisInformation, GnosisAssets {
+export interface Gnosis extends GnosisInformation, FungibleAssets, NonFungibleAssets {
   isLoading: boolean;
   safeAddress?: string;
   name?: string;
   isSigner: boolean;
 }
 
-export interface GnosisAssets {
+export interface FungibleAssets {
   treasuryAssetsFungible: GnosisAssetFungible[];
+}
+
+export interface NonFungibleAssets {
   treasuryAssetsNonFungible: GnosisAssetNonFungible[];
 }
 
