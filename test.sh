06f3341aca3d9d3ab326d679fd307e039dd7898d
herbig <mikeherbig@gmail.com>
2022-10-28 17:11:32 -0400

    start containers

--- test.sh
+++ test.sh
@@ -9,15 +9,28 @@
 # adapted from https://stackoverflow.com/a/48843074
 if (! docker stats --no-stream ); then
   # open Mac OS Docker app
-  open /Applications/Docker.app
+  open /Applications/Docker.app &>/dev/null & disown;
 
-echo "Waiting for Docker to launch..."
-while (! docker stats --no-stream ); do
-  # Docker takes a few seconds to initialize
-  sleep 1
-done
+  echo "Waiting for Docker to launch..."
+  while (! docker stats --no-stream ); do
+    echo "Waiting for Docker to start..."
+    sleep 5
+  done
+  echo "Docker is running!"
 fi
-echo "Docker is running!"
+
+# if the Fractal containers aren't running, start those as well (in the background)
+if [ -z `docker ps -q --no-trunc | grep $(docker-compose ps -q blockchain)` ]; then
+  echo "Running Fractal Docker containers."
+  docker compose up --build &>/dev/null & disown;
+
+  # wait for Fractal Docker containers to finish loading before proceeding with the tests
+  while [ -z `docker ps -q --no-trunc | grep $(docker-compose ps -q blockchain)` ]; do
+    echo "Waiting for containers..."
+    sleep 5
+  done
+  echo "Fractal containers are running!"
+fi
 
 if [ $# -eq 0 ]
 then
