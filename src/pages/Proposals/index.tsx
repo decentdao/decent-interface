dd9bbc6559567be6df27c6f6d740c761afa449b6
Kirill Klimenko <klimenkomud@gmail.com>
2022-11-03 12:03:10 +0200

    Restructure Proposals components

--- src/pages/Proposals/index.tsx
+++ src/pages/Proposals/index.tsx
@@ -1,14 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { Link } from 'react-router-dom';
+import ProposalsList from '../../components/Proposals/ProposalsList';
+import { TextButton, SecondaryButton } from '../../components/ui/forms/Button';
+import H1 from '../../components/ui/H1';
 
-import ProposalDetails from '../../components/Proposals/ProposalDetails';
-
-export default function Proposals() {
+export function Governance() {
+  const { t } = useTranslation(['common', 'proposal']);
   return (
-    <Routes>
-      <Route
-        path=":proposalNumber/*"
-        element={<ProposalDetails />}
-      />
-    </Routes>
+    <div>
+      <div className="flex flex-col sm:flex-row sm:justify-between">
+        <H1>Proposals</H1>
+        <div className="flex ml-auto mb-2 sm:mb-0 items-center sm:items-start">
+          <Link to="delegate">
+            <TextButton label={t('delegate')} />
+          </Link>
+          <Link to="new">
+            <SecondaryButton label={t('createProposal', { ns: 'proposal' })} />
+          </Link>
+        </div>
+      </div>
+      <ProposalsList />
+    </div>
   );
 }
