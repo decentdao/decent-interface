f3aa4acc30de2d2546e4bfe0f74397a1f5d1bbf9
tbwebb22 <tbwebb22@gmail.com>
2022-11-07 10:28:57 -0500

    Add temporary route to Delegate page for testing purposes

--- src/routes/DAOSubRoutes.tsx
+++ src/routes/DAOSubRoutes.tsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import { DAOController } from '../controller/DAOs/DAOController';
 import { DaoDashboard } from '../pages/DaoDashboard';
+import { Delegate } from '../pages/Delegate';
 import { DAO_ROUTES } from './constants';
 
 function DAOSubRoutes() {
@@ -30,6 +31,10 @@ function DAOSubRoutes() {
         <Route
           path={DAO_ROUTES.proposalNew.path}
           element={<div />}
+        />
+        <Route
+          path={DAO_ROUTES.delegate.path}
+          element={<Delegate />}
         />
       </Routes>
     </DAOController>
