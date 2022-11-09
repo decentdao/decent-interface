97538772937f114e48532a31d3203d582cefe89c
herbig <mikeherbig@gmail.com>
2022-11-08 21:20:44 -0500

    more refactoring

--- src/pages/Treasury/Transactions.tsx
+++ src/pages/Treasury/Transactions.tsx
@@ -1,63 +1,28 @@
-import { Box, Divider, HStack, Image, Spacer, Text } from '@chakra-ui/react';
+import { Box, Divider, HStack, Image, Spacer, Text, Tooltip } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
 import coinDefault from '../../assets/images/coin-icon-default.svg';
-import ethDefault from '../../assets/images/coin-icon-eth.svg';
 import nftDefault from '../../assets/images/nft-image-default.svg';
 import received from '../../assets/images/transfer-received.svg';
 import sent from '../../assets/images/transfer-sent.svg';
 import EtherscanLinkAddress from '../../components/ui/EtherscanLinkAddress';
 import EtherscanTransactionLink from '../../components/ui/EtherscanTransactionLink';
 import { ShortenedAddressLink } from '../../components/ui/ShortenedAddressLink';
-import { formatDatesDiffReadable } from '../../helpers/dateTime';
 import { useFractal } from '../../providers/fractal/hooks/useFractal';
-import { TokenInfo, TransferType } from '../../providers/fractal/types';
-import { formatCoin } from '../../utils/numberFormats';
+import { TransferType } from '../../providers/fractal/types';
+import { TokenEventType, TransferDisplayData, useFormatTransfers } from './utils';
 
-function TransferRow({
-  isSent,
-  type,
-  date,
-  displayAmount,
-  transferAddress,
-  isLast,
-  transactionHash,
-  tokenId,
-  tokenInfo,
-}: {
-  isSent: boolean;
-  type: TransferType;
-  date: string;
-  displayAmount?: string;
-  transferAddress: string;
-  isLast: boolean;
-  transactionHash: string;
-  tokenId?: string;
-  tokenInfo?: TokenInfo;
-}) {
+function TransferRow({ displayData }: { displayData: TransferDisplayData }) {
   const { t } = useTranslation(['treasury', 'common']);
-  const dateFormatted = formatDatesDiffReadable(new Date(date), new Date(), t);
-  let imageSrc;
-  switch (type) {
-    case TransferType.ERC20_TRANSFER:
-    case TransferType.ERC721_TRANSFER:
-      imageSrc = tokenInfo?.logoUri;
-      break;
-    case TransferType.ETHER_TRANSFER:
-      imageSrc = ethDefault;
-      break;
-    default:
-      imageSrc = coinDefault;
-  }
   return (
     <Box>
       <HStack
         align="center"
         marginTop="1rem"
-        marginBottom={isLast ? '0rem' : '1rem'}
+        marginBottom={displayData.isLast ? '0rem' : '1rem'}
       >
         <HStack w="33%">
           <Image
-            src={isSent ? sent : received}
+            src={displayData.eventType == TokenEventType.WITHDRAW ? sent : received}
             w="1.5rem"
             h="1.5rem"
           />
@@ -66,44 +31,61 @@ function TransferRow({
               textStyle="text-sm-sans-regular"
               color="grayscale.100"
             >
-              {t(isSent ? 'labelSent' : 'labelReceived')}
+              {t(displayData.eventType == TokenEventType.WITHDRAW ? 'labelSent' : 'labelReceived')}
             </Text>
             <Text
               textStyle="text-base-sans-regular"
               color="chocolate.200"
             >
-              {dateFormatted}
+              {displayData.dateTimeDisplay}
             </Text>
           </Box>
         </HStack>
         <HStack w="33%">
           <Image
-            src={imageSrc}
-            fallbackSrc={tokenId ? nftDefault : coinDefault}
-            alt={tokenId ? tokenInfo?.name : displayAmount}
+            src={displayData.image}
+            fallbackSrc={
+              displayData.transferType === TransferType.ERC721_TRANSFER ? nftDefault : coinDefault
+            }
+            alt={displayData.assetDisplay}
             w="1.25rem"
             h="1.25rem"
           />
-          <EtherscanTransactionLink txHash={transactionHash}>
-            <Text
-              textStyle="text-base-sans-regular"
-              color={isSent ? 'grayscale.100' : '#60B55E'}
-              data-testid={tokenId ? 'link-token-name' : 'link-token-amount'}
+          <EtherscanTransactionLink txHash={displayData.transactionHash}>
+            <Tooltip
+              label={
+                displayData.transferType === TransferType.ERC721_TRANSFER
+                  ? undefined
+                  : displayData.fullCoinTotal
+              }
+              placement="top-start"
             >
-              {(isSent ? '- ' : '+ ') +
-                (tokenId ? tokenInfo?.name + ' #' + tokenId : displayAmount)}
-            </Text>
+              <Text
+                textStyle="text-base-sans-regular"
+                color={
+                  displayData.eventType === TokenEventType.WITHDRAW ? 'grayscale.100' : '#60B55E'
+                }
+                data-testid={
+                  displayData.transferType === TransferType.ERC721_TRANSFER
+                    ? 'link-token-name'
+                    : 'link-token-amount'
+                }
+              >
+                {(displayData.eventType == TokenEventType.WITHDRAW ? '- ' : '+ ') +
+                  displayData.assetDisplay}
+              </Text>
+            </Tooltip>
           </EtherscanTransactionLink>
         </HStack>
         <HStack w="33%">
           <Spacer />
           <ShortenedAddressLink
             data-testid="link-transfer-address"
-            address={transferAddress}
+            address={displayData.transferAddress}
           />
         </HStack>
       </HStack>
-      {!isLast && <Divider color="chocolate.700" />}
+      {!displayData.isLast && <Divider color="chocolate.700" />}
     </Box>
   );
 }
@@ -146,33 +128,17 @@ export function Transactions() {
     treasury: { transfers },
   } = useFractal();
 
+  const displayData = useFormatTransfers(transfers ? transfers.results : [], safe.address!);
+
   if (!transfers || transfers.results.length === 0) return <EmptyTransactions />;
 
-  const results = transfers.results;
   return (
     <Box>
-      {results.map(transfer => {
+      {displayData.map(transfer => {
         return (
           <TransferRow
             key={transfer.transactionHash}
-            type={transfer.type}
-            isSent={safe.address === transfer.from}
-            date={transfer.executionDate}
-            displayAmount={
-              transfer?.tokenId
-                ? undefined
-                : formatCoin(
-                    transfer.value,
-                    true,
-                    transfer?.tokenInfo?.decimals,
-                    transfer?.tokenInfo?.symbol
-                  )
-            }
-            transferAddress={safe.address === transfer.from ? transfer.to : transfer.from}
-            isLast={results[results.length - 1] === transfer}
-            transactionHash={transfer.transactionHash}
-            tokenId={transfer?.tokenId}
-            tokenInfo={transfer?.tokenInfo}
+            displayData={transfer}
           />
         );
       })}
--- src/pages/Treasury/utils.ts
+++ src/pages/Treasury/utils.ts
@@ -1,6 +1,14 @@
 import { ethers } from 'ethers';
+import { useTranslation } from 'react-i18next';
+import coinDefault from '../../assets/images/coin-icon-default.svg';
 import ethDefault from '../../assets/images/coin-icon-eth.svg';
-import { GnosisAssetFungible } from '../../providers/fractal/types';
+import { formatDatesDiffReadable } from '../../helpers/dateTime';
+import {
+  AssetTransfer,
+  GnosisAssetFungible,
+  TokenInfo,
+  TransferType,
+} from '../../providers/fractal/types';
 import { formatCoin, formatUSD } from '../../utils/numberFormats';
 
 export interface TokenDisplayData {
@@ -43,4 +51,80 @@ export function formatCoins(assets: GnosisAssetFungible[]) {
     totalFiatValue: totalFiatValue,
     displayData: displayData,
   };
+}
+
+export enum TokenEventType {
+  DEPOSIT,
+  WITHDRAW,
+}
+
+export interface TransferDisplayData {
+  eventType: TokenEventType;
+  transferType: TransferType;
+  dateTimeDisplay: string;
+  image: string;
+  assetDisplay: string;
+  fullCoinTotal: string | undefined;
+  transferAddress: string;
+  isLast: boolean;
+  transactionHash: string;
+  tokenId: string;
+  tokenInfo: TokenInfo;
+}
+
+export function useFormatTransfers(
+  transfers: AssetTransfer[],
+  safeAddress: string
+): TransferDisplayData[] {
+  let displayData: TransferDisplayData[] = new Array(transfers.length);
+  const { t } = useTranslation('common');
+
+  for (let i = 0; i < transfers.length; i++) {
+    let transfer = transfers[i];
+
+    let imageSrc;
+    switch (transfer.type) {
+      case TransferType.ERC20_TRANSFER:
+      case TransferType.ERC721_TRANSFER:
+        imageSrc = transfer.tokenInfo?.logoUri;
+        break;
+      case TransferType.ETHER_TRANSFER:
+        imageSrc = ethDefault;
+        break;
+      default:
+        imageSrc = coinDefault;
+    }
+
+    const formatted: TransferDisplayData = {
+      eventType: safeAddress === transfer.from ? TokenEventType.WITHDRAW : TokenEventType.DEPOSIT,
+      transferType: transfer.type,
+      dateTimeDisplay: formatDatesDiffReadable(new Date(transfer.executionDate), new Date(), t),
+      image: imageSrc,
+      assetDisplay:
+        transfer.type === TransferType.ERC721_TRANSFER
+          ? transfer.tokenInfo.name + ' #' + transfer.tokenId
+          : formatCoin(
+              transfer.value,
+              true,
+              transfer?.tokenInfo?.decimals,
+              transfer?.tokenInfo?.symbol
+            ),
+      fullCoinTotal:
+        transfer.type === TransferType.ERC721_TRANSFER
+          ? undefined
+          : formatCoin(
+              transfer.value,
+              false,
+              transfer?.tokenInfo?.decimals,
+              transfer?.tokenInfo?.symbol
+            ),
+      transferAddress: safeAddress === transfer.from ? transfer.to : transfer.from,
+      isLast: transfers[transfers.length - 1] === transfer,
+      transactionHash: transfer.transactionHash,
+      tokenId: transfer?.tokenId,
+      tokenInfo: transfer.tokenInfo,
+    };
+    displayData[i] = formatted;
+  }
+  return displayData;
 }
