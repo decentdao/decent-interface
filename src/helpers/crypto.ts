daf404f35dd50c175a5105506de02c2121431558
Christopher Dancy <chris.dancy@decentlabs.io>
2022-11-09 11:46:05 -0500

    manually calculate Safe Transaction hash to limit contracts calls

--- src/helpers/crypto.ts
+++ src/helpers/crypto.ts
@@ -1,4 +1,4 @@
-import { BigNumber, Contract, constants, utils } from 'ethers';
+import { BigNumber, Contract, constants, utils, ethers } from 'ethers';
 import { MetaTransaction, SafeTransaction } from '../types/transaction';
 
 export function getRandomBytes() {
@@ -7,6 +7,33 @@ export function getRandomBytes() {
   const bytes32 = '0x' + bytes8Array.reduce((o, v) => o + ('00' + v.toString(16)).slice(-2), '');
   return bytes32;
 }
+
+const EIP712_SAFE_TX_TYPE = {
+  // "SafeTx(address to,uint256 value,bytes data,uint8 operation,uint256 safeTxGas,uint256 baseGas,uint256 gasPrice,address gasToken,address refundReceiver,uint256 nonce)"
+  SafeTx: [
+    { type: 'address', name: 'to' },
+    { type: 'uint256', name: 'value' },
+    { type: 'bytes', name: 'data' },
+    { type: 'uint8', name: 'operation' },
+    { type: 'uint256', name: 'safeTxGas' },
+    { type: 'uint256', name: 'baseGas' },
+    { type: 'uint256', name: 'gasPrice' },
+    { type: 'address', name: 'gasToken' },
+    { type: 'address', name: 'refundReceiver' },
+    { type: 'uint256', name: 'nonce' },
+  ],
+};
+export const calculateSafeTransactionHash = (
+  safe: string,
+  chainId: number,
+  safeTx: SafeTransaction
+): string => {
+  return ethers.utils._TypedDataEncoder.hash(
+    { verifyingContract: safe, chainId },
+    EIP712_SAFE_TX_TYPE,
+    safeTx
+  );
+};
 
 export const buildSafeTransaction = (template: {
   to: string;
