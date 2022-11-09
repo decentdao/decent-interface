08e10e47c9222d93e82689cbaa7df349b4dd49c6
Christopher Dancy <chris.dancy@decentlabs.io>
2022-11-09 17:29:31 -0500

    added helpers for gnosis txs

--- src/helpers/crypto.ts
+++ src/helpers/crypto.ts
@@ -1,5 +1,28 @@
-import { BigNumber, Contract, constants, utils } from 'ethers';
-import { MetaTransaction, SafeTransaction } from '../types/transaction';
+import { TypedDataSigner } from '@ethersproject/abstract-signer';
+import { BigNumber, Contract, constants, utils, BigNumberish, Signer, ethers } from 'ethers';
+import { GnosisSafe__factory } from '../assets/typechain-types/fractal-contracts';
+import { MetaTransaction, SafePostTransaction, SafeTransaction } from '../types/transaction';
+
+export interface SafeSignature {
+  signer: string;
+  data: string;
+}
+
+export const EIP712_SAFE_TX_TYPE = {
+  // "SafeTx(address to,uint256 value,bytes data,uint8 operation,uint256 safeTxGas,uint256 baseGas,uint256 gasPrice,address gasToken,address refundReceiver,uint256 nonce)"
+  SafeTx: [
+    { type: 'address', name: 'to' },
+    { type: 'uint256', name: 'value' },
+    { type: 'bytes', name: 'data' },
+    { type: 'uint8', name: 'operation' },
+    { type: 'uint256', name: 'safeTxGas' },
+    { type: 'uint256', name: 'baseGas' },
+    { type: 'uint256', name: 'gasPrice' },
+    { type: 'address', name: 'gasToken' },
+    { type: 'address', name: 'refundReceiver' },
+    { type: 'uint256', name: 'nonce' },
+  ],
+};
 
 export function getRandomBytes() {
   const bytes8Array = new Uint8Array(32);
@@ -7,6 +30,108 @@ export function getRandomBytes() {
   const bytes32 = '0x' + bytes8Array.reduce((o, v) => o + ('00' + v.toString(16)).slice(-2), '');
   return bytes32;
 }
+
+export const calculateSafeTransactionHash = (
+  safe: Contract,
+  safeTx: SafeTransaction,
+  chainId: BigNumberish
+): string => {
+  return utils._TypedDataEncoder.hash(
+    { verifyingContract: safe.address, chainId },
+    EIP712_SAFE_TX_TYPE,
+    safeTx
+  );
+};
+
+export const buildSignatureBytes = (signatures: SafeSignature[]): string => {
+  signatures.sort((left, right) =>
+    left.signer.toLowerCase().localeCompare(right.signer.toLowerCase())
+  );
+  let signatureBytes = '0x';
+  for (const sig of signatures) {
+    signatureBytes += sig.data.slice(2);
+  }
+  return signatureBytes;
+};
+
+export const safeSignTypedData = async (
+  signer: Signer & TypedDataSigner,
+  safe: Contract,
+  safeTx: SafeTransaction,
+  chainId?: BigNumberish
+): Promise<SafeSignature> => {
+  if (!chainId && !signer.provider) throw Error('Provider required to retrieve chainId');
+  const cid = chainId || (await signer.provider!.getNetwork()).chainId;
+  const signerAddress = await signer.getAddress();
+  return {
+    signer: signerAddress,
+    data: await signer._signTypedData(
+      { verifyingContract: safe.address, chainId: cid },
+      EIP712_SAFE_TX_TYPE,
+      safeTx
+    ),
+  };
+};
+
+export const buildSafeAPIPost = async (
+  safeAddress: string,
+  signerOrProvider: Signer & TypedDataSigner,
+  chainId: number,
+  template: {
+    to: string;
+    value?: BigNumber | number | string;
+    data?: string;
+    operation?: number;
+    safeTxGas?: number | string;
+    baseGas?: number | string;
+    gasPrice?: number | string;
+    gasToken?: string;
+    refundReceiver?: string;
+    nonce?: number;
+  }
+): Promise<SafePostTransaction> => {
+  const gnosisContract = await GnosisSafe__factory.connect(safeAddress, signerOrProvider);
+  const nonce = !template.nonce ? await (await gnosisContract.nonce()).toString() : template.nonce;
+  const safeTx = {
+    to: template.to,
+    value: template.value || 0,
+    data: template.data || '0x',
+    operation: template.operation || 0,
+    safeTxGas: template.safeTxGas || 0,
+    baseGas: template.baseGas || 0,
+    gasPrice: template.gasPrice || 0,
+    gasToken: template.gasToken || ethers.constants.AddressZero,
+    refundReceiver: template.refundReceiver || ethers.constants.AddressZero,
+    nonce: nonce,
+  };
+
+  const txHash = calculateSafeTransactionHash(gnosisContract, safeTx, chainId);
+  const sig = [
+    await safeSignTypedData(
+      signerOrProvider as Signer & TypedDataSigner,
+      gnosisContract,
+      safeTx,
+      chainId
+    ),
+  ];
+  const signatureBytes = buildSignatureBytes(sig);
+  return {
+    safe: safeAddress,
+    to: safeTx.to,
+    value: safeTx.value,
+    data: safeTx.data,
+    operation: safeTx.operation,
+    safeTxGas: safeTx.safeTxGas,
+    baseGas: safeTx.baseGas,
+    gasPrice: safeTx.gasPrice,
+    gasToken: safeTx.gasToken,
+    refundReceiver: safeTx.refundReceiver,
+    nonce: safeTx.nonce,
+    contractTransactionHash: txHash,
+    sender: await signerOrProvider.getAddress(),
+    signature: signatureBytes,
+  };
+};
 
 export const buildSafeTransaction = (template: {
   to: string;
