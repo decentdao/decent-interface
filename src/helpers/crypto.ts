f57dae6a93f21ae9af7d5ebe4c75389745ea8351
Christopher Dancy <chris.dancy@decentlabs.io>
2022-11-09 13:10:45 -0500

    remove unused const from crypto helpers

--- src/helpers/crypto.ts
+++ src/helpers/crypto.ts
@@ -8,33 +8,6 @@ export function getRandomBytes() {
   return bytes32;
 }
 
-const EIP712_SAFE_TX_TYPE = {
-  // "SafeTx(address to,uint256 value,bytes data,uint8 operation,uint256 safeTxGas,uint256 baseGas,uint256 gasPrice,address gasToken,address refundReceiver,uint256 nonce)"
-  SafeTx: [
-    { type: 'address', name: 'to' },
-    { type: 'uint256', name: 'value' },
-    { type: 'bytes', name: 'data' },
-    { type: 'uint8', name: 'operation' },
-    { type: 'uint256', name: 'safeTxGas' },
-    { type: 'uint256', name: 'baseGas' },
-    { type: 'uint256', name: 'gasPrice' },
-    { type: 'address', name: 'gasToken' },
-    { type: 'address', name: 'refundReceiver' },
-    { type: 'uint256', name: 'nonce' },
-  ],
-};
-export const calculateSafeTransactionHash = (
-  safe: string,
-  chainId: number,
-  safeTx: SafeTransaction
-): string => {
-  return ethers.utils._TypedDataEncoder.hash(
-    { verifyingContract: safe, chainId },
-    EIP712_SAFE_TX_TYPE,
-    safeTx
-  );
-};
-
 export const buildSafeTransaction = (template: {
   to: string;
   value?: BigNumber | number | string;
