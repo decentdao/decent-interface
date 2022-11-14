de35dc2d130e7f59238888c0083909799e287968
herbig <mikeherbig@gmail.com>
2022-11-13 22:02:49 -0500

    remove DAO route reference

--- src/pages/Proposals/index.tsx
+++ src/pages/Proposals/index.tsx
@@ -3,25 +3,17 @@ import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
 import ProposalsList from '../../components/Proposals/ProposalsList';
 import H1 from '../../components/ui/H1';
-import { TextButton, SecondaryButton } from '../../components/ui/forms/Button';
+import { SecondaryButton } from '../../components/ui/forms/Button';
 import { ModalType } from '../../modals/ModalProvider';
 import { useFractalModal } from '../../modals/useFractalModal';
-import { useFractal } from '../../providers/fractal/hooks/useFractal';
-import { DAO_ROUTES } from '../../routes/constants';
 
 export function Governance() {
   const { t } = useTranslation(['common', 'proposal']);
-  const {
-    gnosis: { safe },
-  } = useFractal();
   return (
     <div>
       <div className="flex flex-col sm:flex-row sm:justify-between">
         <H1>Proposals</H1>
         <div className="flex ml-auto mb-2 sm:mb-0 items-center sm:items-start">
-          <Link to={DAO_ROUTES.delegate.relative(safe.address)}>
-            <TextButton label={t('delegate')} />
-          </Link>
           <Button onClick={useFractalModal(ModalType.DELEGATE)}>{t('delegate')}</Button>
           <Link to="new">
             <SecondaryButton label={t('createProposal', { ns: 'proposal' })} />
