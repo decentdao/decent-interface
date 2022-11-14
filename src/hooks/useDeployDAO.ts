51788e79a637bcc09f765bbb013f63bf8135cf94
Christopher Dancy <chris.dancy@decentlabs.io>
2022-11-14 17:54:32 -0500

    refactor

--- src/hooks/useDeployDAO.ts
+++ src/hooks/useDeployDAO.ts
@@ -1,15 +1,9 @@
 import { useCallback } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useTransaction } from '../contexts/web3Data/transactions';
-import {
-  GnosisDAO,
-  GovernanceTypes,
-  TokenGovernanceDAO,
-} from './../components/DaoCreator/provider/types/index';
+import { GnosisDAO, TokenGovernanceDAO } from './../components/DaoCreator/provider/types/index';
 import useBuildDAOTx from './useBuildDAOTx';
 import useSafeContracts from './useSafeContracts';
-
-type DeployDAOSuccessCallback = (daoAddress: string) => void;
 
 const useDeployDAO = () => {
   const { multiSendContract } = useSafeContracts();
@@ -19,71 +13,32 @@ const useDeployDAO = () => {
 
   const { t } = useTranslation('transaction');
 
-  const deployGnosisSafeWithUsul = useCallback(
-    (daoData: GnosisDAO | TokenGovernanceDAO, successCallback: DeployDAOSuccessCallback) => {
-      const deploy = async () => {
-        if (!multiSendContract) {
-          return;
-        }
-
-        const builtSafeTx = await build(daoData);
-        if (!builtSafeTx) {
-          return;
-        }
-
-        const { predictedGnosisSafeAddress, safeTx } = builtSafeTx;
-
-        contractCallDeploy({
-          contractFn: () => multiSendContract.multiSend(safeTx),
-          pendingMessage: t('pendingDeployGnosis'),
-          failedMessage: t('failedDeployGnosis'),
-          successMessage: t('successDeployGnosis'),
-          successCallback: () => successCallback(predictedGnosisSafeAddress),
-        });
-      };
-
-      deploy();
-    },
-    [multiSendContract, build, contractCallDeploy, t]
-  );
-
-  const deployGnosisSafe = useCallback(
-    (daoData: GnosisDAO | TokenGovernanceDAO, successCallback: DeployDAOSuccessCallback) => {
-      const deploy = async () => {
-        if (!multiSendContract) {
-          return;
-        }
-
-        const builtSafeTx = await build(daoData);
-        if (!builtSafeTx) {
-          return;
-        }
-
-        const { predictedGnosisSafeAddress, safeTx } = builtSafeTx;
-
-        contractCallDeploy({
-          contractFn: () => multiSendContract.multiSend(safeTx),
-          pendingMessage: t('pendingDeployGnosis'),
-          failedMessage: t('failedDeployGnosis'),
-          successMessage: t('successDeployGnosis'),
-          successCallback: () => successCallback(predictedGnosisSafeAddress),
-        });
-      };
-      deploy();
-    },
-    [multiSendContract, build, contractCallDeploy, t]
-  );
-
   const deployDao = useCallback(
     (daoData: TokenGovernanceDAO | GnosisDAO, successCallback: (daoAddress: string) => void) => {
-      switch (daoData.governance) {
-        case GovernanceTypes.GNOSIS_SAFE_USUL:
-          return deployGnosisSafeWithUsul(daoData, successCallback);
-        case GovernanceTypes.GNOSIS_SAFE:
-          return deployGnosisSafe(daoData, successCallback);
-      }
+      const deploy = async () => {
+        if (!multiSendContract) {
+          return;
+        }
+
+        const builtSafeTx = await build(daoData);
+        if (!builtSafeTx) {
+          return;
+        }
+
+        const { predictedGnosisSafeAddress, safeTx } = builtSafeTx;
+
+        contractCallDeploy({
+          contractFn: () => multiSendContract.multiSend(safeTx),
+          pendingMessage: t('pendingDeployGnosis'),
+          failedMessage: t('failedDeployGnosis'),
+          successMessage: t('successDeployGnosis'),
+          successCallback: () => successCallback(predictedGnosisSafeAddress),
+        });
+      };
+
+      deploy();
     },
-    [deployGnosisSafe, deployGnosisSafeWithUsul]
+    [build, contractCallDeploy, multiSendContract, t]
   );
 
   return [deployDao, contractCallPending] as const;
