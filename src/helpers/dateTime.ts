2afd1c69d0be7d31b767f5c46509951605bcc774
Kirill Klimenko <klimenkomud@gmail.com>
2022-11-15 00:42:11 +0200

    Add missing namespace

--- src/helpers/dateTime.ts
+++ src/helpers/dateTime.ts
@@ -4,6 +4,7 @@ import differenceInMinutes from 'date-fns/differenceInMinutes';
 import differenceInMonths from 'date-fns/differenceInMonths';
 import differenceInYears from 'date-fns/differenceInYears';
 import { TFunction } from 'react-i18next';
+import { common } from '../assets/typechain-types/fractal-contracts/@gnosis.pm/safe-contracts/contracts';
 
 export function formatDatesDiffReadable(
   dateStart: Date,
@@ -17,26 +18,30 @@ export function formatDatesDiffReadable(
 
   let formattedString = '';
   if (diffInMinutes < 5) {
-    formattedString = t('labelDateJustNow');
+    formattedString = t('labelDateJustNow', { ns: 'common' });
   } else if (diffInMinutes < 60) {
     formattedString = t('labelDateMinutes', { ns: 'common', count: Math.floor(diffInMinutes) });
   } else if (diffInMinutes < 60 * 24) {
     const diffInHours = Math.abs(differenceInHours(dateStart, dateEnd));
     formattedString = t(diffInHours === 1 ? 'labelDateHour_single' : 'labelDateHour_plural', {
       count: diffInHours,
+      ns: 'common',
     });
   } else if (diffInMonths < 1) {
     formattedString = t(diffInDays === 1 ? 'labelDateDay_single' : 'labelDateDay_plural', {
       count: diffInDays,
+      ns: 'common',
     });
   } else if (diffInMonths < 12) {
     formattedString = t(diffInMonths === 1 ? 'labelDateMonth_single' : 'labelDateMonth_plural', {
       count: diffInMonths,
+      ns: 'common',
     });
   } else {
     const diffInYears = Math.abs(differenceInYears(dateStart, dateEnd));
     formattedString = t(diffInYears === 1 ? 'labelDateYear_single' : 'labelDateYear_plural', {
       count: diffInYears,
+      ns: 'common',
     });
   }
 
