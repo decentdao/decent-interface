bc6e2aaf523b2c5dad8b08b87799612b6c1335a9
herbig <mikeherbig@gmail.com>
2022-11-17 18:38:03 -0500

    fix import issues

--- src/hooks/useBuildDAOTx.ts
+++ src/hooks/useBuildDAOTx.ts
@@ -1,6 +1,6 @@
+import { VotesToken__factory } from '@fractal-framework/fractal-contracts';
 import { BigNumber, ethers } from 'ethers';
 import { useCallback } from 'react';
-import { VotesToken__factory } from '../assets/typechain-types/fractal-contracts';
 import { GnosisSafe__factory } from '../assets/typechain-types/gnosis-safe';
 import { OZLinearVoting__factory, Usul__factory } from '../assets/typechain-types/usul';
 import { useWeb3Provider } from '../contexts/web3Data/hooks/useWeb3Provider';
