1f3cf84255328e58242daad1d037c1750f2238de
Kirill Klimenko <klimenkomud@gmail.com>
2022-11-22 04:09:04 +0200

    Remove console.log from ProposalVotes

--- src/components/Proposals/ProposalVotes.tsx
+++ src/components/Proposals/ProposalVotes.tsx
@@ -146,8 +146,6 @@ function ProposalVotes({
   const noVotesPercentage = no.div(govTokenTotalSupply).mul(100).toNumber();
   const abstainVotesPercentage = abstain.div(govTokenTotalSupply).mul(100).toNumber();
 
-  console.log(yesVotesPercentage);
-
   return (
     <>
       <ContentBox bg="black.900-semi-transparent">
