025c1f09661861fc2269ac28b174377d63042e86
David Colon <38386583+Da-Colon@users.noreply.github.com>
2022-11-30 14:13:36 -0500

    remove unused hook from being used for now

--- src/providers/Fractal/hooks/useSubDAODeploy.ts
+++ src/providers/Fractal/hooks/useSubDAODeploy.ts
@@ -5,9 +5,16 @@ import { DeployDAOSuccessCallback, GnosisDAO, GovernanceTypes } from '../types';
 
 export const useSubDAODeploy = () => {
   const [contractCallDeploy, deploySubDAOPending] = useTransaction();
-
+  console.log(
+    '🚀 ~ REMOVE WHEN IMPLEMENTED, This prevents warning for netlify',
+    contractCallDeploy
+  );
   const deploySubDao = useCallback(
     (daoData: GnosisDAO, successCallback: DeployDAOSuccessCallback) => {
+      console.log(
+        '🚀 ~ REMOVE WHEN IMPLEMENTED, This prevents warning for netlify',
+        successCallback
+      );
       switch (daoData.governance) {
         case GovernanceTypes.GNOSIS_SAFE_USUL:
         // return deployGnosisSafeWithUsul(daoData, successCallback);
