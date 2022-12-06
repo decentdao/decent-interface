e147da41d38c979248919c635d0dd9b33fe378af
Kirill Klimenko <klimenkomud@gmail.com>
2022-12-07 01:02:17 +0200

    Remove Mocked transactions

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -2,28 +2,11 @@ import { DecodedTransaction, MetaTransaction } from '../types';
 
 // eslint-disable-next-line
 export const decodeTransactions = (transactions: MetaTransaction[]): DecodedTransaction[] => {
-  // @todo - implement decoding transactions
-
-  const MOCK_DECODED_TRANSACTIONS: DecodedTransaction[] = [
-    {
-      target: '0x6a0db4cef1ce2a5f81c8e6322862439f71aca29d',
-      function: 'firstFunction',
-      parameterTypes: ['uint256', 'string'],
-      parameterValues: ['420', 'hello world'],
-    },
-    {
-      target: '0x6a0db4cef1ce2a5f81c8e6322862439f71aca29d',
-      function: 'secondFunction',
-      parameterTypes: ['uint256', 'string'],
-      parameterValues: ['420', 'hello world'],
-    },
-    {
-      target: '0x6a0db4cef1ce2a5f81c8e6322862439f71aca29d',
-      function: 'thirdFunction',
-      parameterTypes: ['uint256', 'string'],
-      parameterValues: ['420', 'hello world'],
-    },
-  ];
-
-  return MOCK_DECODED_TRANSACTIONS;
+  // @todo: Use data decoder
+  return transactions.map(tx => ({
+    target: tx.to,
+    function: 'transfer',
+    parameterTypes: ['uint256', 'address'],
+    parameterValues: ['1000000', '0x'],
+  }));
 };
