517f2224aaa31709c7a70941c28f1dee53ff87e4
Christopher Dancy <chris.dancy@decentlabs.io>
2022-12-07 13:22:40 -0500

    created card

---
+++ src/pages/DaoDashboard/Activities/ActionCard.tsx
@@ -0,0 +1,42 @@
+import { Box, Flex, Text } from '@chakra-ui/react';
+import { Calendar } from '@decent-org/fractal-ui';
+import { ReactNode } from 'react';
+import { ActivityBox } from '../../../components/ui/containers/AcitivityBox';
+
+interface IAcitivityCard {
+  eventDate: string;
+  description: ReactNode;
+  RightElement?: ReactNode;
+  Badge?: ReactNode;
+}
+
+export function ActionCard({ Badge, eventDate, description, RightElement }: IAcitivityCard) {
+  return (
+    <ActivityBox>
+      <Flex
+        justifyContent="space-between"
+        alignItems="center"
+      >
+        <Flex flexDirection="column">
+          <Flex
+            color="sand.700"
+            alignItems="center"
+            gap="1rem"
+            mb="1rem"
+          >
+            {Badge}
+            <Flex
+              alignItems="center"
+              gap="0.5rem"
+            >
+              <Calendar />
+              <Text textStyle="text-base-sans-regular">{eventDate}</Text>
+            </Flex>
+          </Flex>
+          <Box>{description}</Box>
+        </Flex>
+        {RightElement}
+      </Flex>
+    </ActivityBox>
+  );
+}
