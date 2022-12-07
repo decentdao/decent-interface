0f7c51902c46ccc5e963d0871a396afe190608ea
Christopher Dancy <chris.dancy@decentlabs.io>
2022-12-07 18:52:01 -0500

    created freeze component

--- src/pages/DaoDashboard/Freeze/index.tsx
+++ src/pages/DaoDashboard/Freeze/index.tsx
@@ -1,19 +1,9 @@
 import { Box, Button, Flex } from '@chakra-ui/react';
-import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Sort } from '../../../components/ui/Sort';
-import { EmptyBox } from '../../../components/ui/containers/EmptyBox';
-import { InfoBoxLoader } from '../../../components/ui/loaders/InfoBoxLoader';
-import { ActivityEventType, SortBy } from '../../../types';
+import Alert from '../../../components/ui/svg/Alert';
 import { ActionCard } from '../Activities/ActionCard';
-import { ActivityGovernance } from '../Activities/ActivityGovernance';
-import { ActivityTreasury } from '../Activities/ActivityTreasury';
-import { useActivities } from '../Activities/hooks/useActivities';
 
 export function Freeze() {
-  const [sortBy, setSortBy] = useState<SortBy>(SortBy.Newest);
-
-  const { sortedActivities, isActivitiesLoading } = useActivities(sortBy);
   const { t } = useTranslation('dashboard');
   return (
     <Box>
@@ -23,18 +13,19 @@ export function Freeze() {
         my="1rem"
       >
         <ActionCard
-          Badge={''}
-          description={''}
+          Badge={'Freeze Initiated'}
+          LeftElement={<Alert bgColor="#0085FF" />}
           RightElement={
             <Button
               variant="text"
               size="lg"
               px="0px"
             >
-              {t('labelEtherscan')}
+              {'Freeze DAO'}
             </Button>
           }
-          eventDate={''}
+          eventDate={'4 days remaining'}
+          borderColor="#0085FF"
         />
       </Flex>
     </Box>
