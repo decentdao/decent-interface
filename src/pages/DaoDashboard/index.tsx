6c451c7fbec403b77cd91b3aec7c6f1c83b991db
Christopher Dancy <chris.dancy@decentlabs.io>
2022-12-08 14:34:37 -0500

    removed unused component

--- src/pages/DaoDashboard/index.tsx
+++ src/pages/DaoDashboard/index.tsx
@@ -1,12 +1,10 @@
 import { Box } from '@chakra-ui/react';
 import { Activities } from './Activities';
-import { Freeze } from './Freeze';
 import { Info } from './Info';
 
 export function DaoDashboard() {
   return (
     <Box mt={12}>
-      <Freeze />
       <Info />
       <Activities />
     </Box>
