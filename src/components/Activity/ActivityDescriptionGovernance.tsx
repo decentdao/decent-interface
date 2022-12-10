94fe1640134e3f580b5f075f2b2e451770879b9e
David Colon <38386583+Da-Colon@users.noreply.github.com>
2022-12-10 00:31:56 -0500

    cleans up activity description for readability

--- src/components/Activity/ActivityDescriptionGovernance.tsx
+++ src/components/Activity/ActivityDescriptionGovernance.tsx
@@ -9,8 +9,55 @@ import {
 } from '../../providers/Fractal/types';
 import { ActivityAddress } from './ActivityAddress';
 
-export function ActivityDescriptionGovernance({ activity }: { activity: Activity }) {
-  const { t } = useTranslation(['common', 'dashboard', 'treasury']);
+interface IActivityDescription {
+  activity: Activity;
+}
+interface IActivityProposal {
+  proposalNumber: string;
+}
+
+function ActivityProposalNumber({ proposalNumber }: IActivityProposal) {
+  return <Text>#{createAccountSubstring(proposalNumber)}</Text>;
+}
+
+function ActivityAddresses({ activity }: IActivityDescription) {
+  const { t } = useTranslation('treasury');
+  const governanceActivity = activity as GovernanceActivity;
+  const txCount = governanceActivity.txHashes.length;
+
+  if (txCount > 1) {
+    return <Text>{t('addresses', { ns: 'treasury', numOfAddresses: txCount })}</Text>;
+  }
+  return (
+    <>
+      {governanceActivity.targets.map((address, i, arr) => (
+        <ActivityAddress
+          key={address + i}
+          address={address}
+          addComma={i !== arr.length - 1}
+        />
+      ))}
+    </>
+  );
+}
+
+function OnChainRejectionMessage({ activity }: IActivityDescription) {
+  const { t } = useTranslation('dashboard');
+  const governanceActivity = activity as GovernanceActivity;
+  if (!governanceActivity.multisigRejectedProposalNumber) {
+    return null;
+  }
+  return (
+    <Text>
+      {t('proposalOnChainRejection', {
+        proposalNumber: createAccountSubstring(governanceActivity.multisigRejectedProposalNumber),
+      })}
+    </Text>
+  );
+}
+
+export function ActivityDescriptionGovernance({ activity }: IActivityDescription) {
+  const { t } = useTranslation(['common', 'dashboard']);
 
   if (activity.eventType !== ActivityEventType.Governance) {
     return null;
@@ -20,40 +67,19 @@ export function ActivityDescriptionGovernance({ activity }: { activity: Activity
 
   const hasTransfers =
     treasuryActivity.transferAddresses && !!treasuryActivity.transferAddresses.length;
-  const txCount = governanceActivity.txHashes.length;
+
+  const transactionDescription = t('proposalDescription', {
+    ns: 'dashboard',
+    txCount: governanceActivity.txHashes.length,
+  });
 
   return (
     <>
-      <Text>#{createAccountSubstring(governanceActivity.proposalNumber)}</Text>
-      <Text>
-        {t('proposalDescription', {
-          ns: 'dashboard',
-          txCount: governanceActivity.txHashes.length,
-        })}{' '}
-      </Text>
-      {txCount > 1 ? (
-        <Text>{t('addresses', { ns: 'treasury', numOfAddresses: txCount })}</Text>
-      ) : (
-        governanceActivity.targets.map((address, i, arr) => (
-          <ActivityAddress
-            key={address + i}
-            address={address}
-            addComma={i !== arr.length - 1}
-          />
-        ))
-      )}
+      <ActivityProposalNumber proposalNumber={governanceActivity.proposalNumber} />
+      <Text>{transactionDescription}</Text>
+      <ActivityAddresses activity={activity} />
       {hasTransfers && <Text> {t('proposalDescriptionCont', { ns: 'dashboard' })} </Text>}
-      {governanceActivity.multisigRejectedProposalNumber && (
-        <Text>
-          {' '}
-          {t('proposalOnChainRejection', {
-            ns: 'dashboard',
-            proposalNumber: createAccountSubstring(
-              governanceActivity.multisigRejectedProposalNumber
-            ),
-          })}{' '}
-        </Text>
-      )}
+      <OnChainRejectionMessage activity={activity} />
     </>
   );
 }
