efb5a89179e4144cf570d5f0f55d21ec41ecf6bc
David Colon <38386583+Da-Colon@users.noreply.github.com>
2022-12-10 20:22:42 -0500

    executes approved transaction

--- src/components/Proposals/MultisigDetails/TxActions.tsx
+++ src/components/Proposals/MultisigDetails/TxActions.tsx
@@ -2,13 +2,18 @@ import { Box, Button, Text } from '@chakra-ui/react';
 import { Check } from '@decent-org/fractal-ui';
 import { TypedDataSigner } from '@ethersproject/abstract-signer';
 import { GnosisSafe__factory } from '@fractal-framework/fractal-contracts';
-import { SafeMultisigTransactionWithTransfersResponse } from '@gnosis.pm/safe-service-client';
+import { SafeMultisigTransactionWithTransfersResponse } from '@safe-global/safe-service-client';
 import { Signer } from 'ethers';
 import { useTranslation } from 'react-i18next';
-import { buildSafeTransaction, SafeSignature, safeSignTypedData } from '../../../helpers';
+import {
+  buildSafeTransaction,
+  buildSignatureBytes,
+  SafeSignature,
+  safeSignTypedData,
+} from '../../../helpers';
 import { logError } from '../../../helpers/errorLogging';
 import { useFractal } from '../../../providers/Fractal/hooks/useFractal';
-import { MultisigProposal } from '../../../providers/Fractal/types';
+import { MultisigProposal, TxProposalState } from '../../../providers/Fractal/types';
 import { useWeb3Provider } from '../../../providers/Web3Data/hooks/useWeb3Provider';
 import ContentBox from '../../ui/ContentBox';
 
@@ -23,6 +28,8 @@ export function TxActions({ proposal }: { proposal: MultisigProposal }) {
   const isOwner = safe.owners?.includes(account || '');
   const hasSigned = proposal.confirmations.find(confirm => confirm.owner === account);
   const multisigTx = proposal.transaction as SafeMultisigTransactionWithTransfersResponse;
+  const isExecuted = proposal.state === TxProposalState.Executed;
+
   const confirmTransaction = async () => {
     try {
       if (!safeService || !signerOrProvider || !multisigTx || !safe.address) {
@@ -43,17 +50,66 @@ export function TxActions({ proposal }: { proposal: MultisigProposal }) {
       logError(e, 'Error occured during transaction confirmation');
     }
   };
+
+  const executeTransaction = async () => {
+    try {
+      if (!safeService || !signerOrProvider || !multisigTx || !safe.address) {
+        return;
+      }
+
+      if (!multisigTx.confirmations) {
+        console.log("this shouldn't ever be the case");
+        return;
+      }
+      const gnosisContract = GnosisSafe__factory.connect(safe.address, signerOrProvider);
+      const safeTx = buildSafeTransaction({
+        ...multisigTx,
+      });
+      const signatures = buildSignatureBytes(
+        multisigTx.confirmations.map(confirmation => ({
+          signer: confirmation.owner,
+          data: confirmation.signature,
+        }))
+      );
+      await gnosisContract.execTransaction(
+        safeTx.to,
+        safeTx.value,
+        safeTx.data,
+        safeTx.operation,
+        safeTx.safeTxGas,
+        safeTx.baseGas,
+        safeTx.gasPrice,
+        safeTx.gasToken,
+        safeTx.refundReceiver,
+        signatures
+      );
+    } catch (e) {
+      logError(e, 'Error occured during transaction execution');
+    }
+  };
+
+  if (isExecuted) {
+    return null;
+  }
+  const hasReachedThreshold = proposal.confirmations.length >= (safe.threshold || 0);
+  const isExecutable = hasReachedThreshold && proposal.state === TxProposalState.Pending;
+
+  const buttonText = isExecutable ? t('execute') : t('approve');
+  const buttonAction = isExecutable ? executeTransaction : confirmTransaction;
+  const buttonIcon = isExecutable ? undefined : <Check boxSize="1.5rem" />;
+  const isButtonActive = isOwner && (!hasSigned || hasReachedThreshold);
+
   return (
     <ContentBox bg="black.900-semi-transparent">
       <Text textStyle="text-lg-mono-medium">Proposal Details</Text>
       <Box marginTop={4}>
         <Button
           w="full"
-          rightIcon={<Check boxSize="1.5rem" />}
-          disabled={!isOwner || !!hasSigned}
-          onClick={confirmTransaction}
+          rightIcon={buttonIcon}
+          disabled={!isButtonActive}
+          onClick={buttonAction}
         >
-          {t('approve')}
+          {buttonText}
         </Button>
       </Box>
     </ContentBox>
