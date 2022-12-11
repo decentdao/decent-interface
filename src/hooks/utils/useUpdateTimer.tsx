08f72090ff3a8e43dea0c52a830c81243b9fda7c
David Colon <38386583+Da-Colon@users.noreply.github.com>
2022-12-11 11:23:45 -0500

    updates polling variable name to match value

--- src/hooks/utils/useUpdateTimer.tsx
+++ src/hooks/utils/useUpdateTimer.tsx
@@ -2,10 +2,10 @@ import { useCallback, useEffect, useState } from 'react';
 
 export const useUpdateTimer = (safeAddress?: string) => {
   const [timers, setTimers] = useState<NodeJS.Timer[]>([]);
-  const thirtySeconds = 20000; // in milliseconds
+  const twentySeconds = 20000; // in milliseconds
 
   const setMethodonInterval = useCallback(
-    (getMethod: () => Promise<void>, milliseconds: number = thirtySeconds) => {
+    (getMethod: () => Promise<void>, milliseconds: number = twentySeconds) => {
       Promise.resolve(getMethod());
       const intervalId = setInterval(() => {
         Promise.resolve(getMethod());
