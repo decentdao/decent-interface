a4dc360d40f91530edc82a962f6bb20bde9f5646
David Colon <38386583+Da-Colon@users.noreply.github.com>
2022-12-12 16:29:57 -0500

    fix conditional

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,7 +1,7 @@
 import { createAccountSubstring } from './../hooks/utils/useDisplayName';
 
 export const createProposalNumberSubstring = (proposalNumber: string) => {
-  if (proposalNumber.length > 8) {
+  if (proposalNumber.length < 8) {
     return proposalNumber;
   }
   return `#${createAccountSubstring(proposalNumber)}`;
