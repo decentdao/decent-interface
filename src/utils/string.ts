e0a74f464342dca341d14fbaf1fffb69f1d7e46b
David Colon <38386583+Da-Colon@users.noreply.github.com>
2022-12-12 16:36:53 -0500

    adds # to small proposal number

--- src/utils/string.ts
+++ src/utils/string.ts
@@ -1,8 +1,8 @@
 import { createAccountSubstring } from './../hooks/utils/useDisplayName';
 
 export const createProposalNumberSubstring = (proposalNumber: string) => {
-  if (proposalNumber.length < 8) {
-    return proposalNumber;
+  if (proposalNumber.length < 10) {
+    return `#${proposalNumber}`;
   }
   return `#${createAccountSubstring(proposalNumber)}`;
 };
