d5a07cea22935d71ef2eb9fcb8bf7750d2880f7e
David Colon <38386583+Da-Colon@users.noreply.github.com>
2022-12-12 23:32:42 -0500

    fixes typo

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,3 +1,3 @@
 export const HEADER_HEIGHT = '4rem';
 export const CONTENT_HEIGHT = `calc(100vh - ${HEADER_HEIGHT})`;
-export const BACKGROUND_SEMI_TRANSPARENT = 'BACKGROUND_SEMI_TRANSPARENT';
+export const BACKGROUND_SEMI_TRANSPARENT = 'black.900-semi-transparent';
