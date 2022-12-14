b65cba4f54a53d94c2fcb6067c6eed196ddafa41
David Colon <38386583+Da-Colon@users.noreply.github.com>
2022-12-14 15:58:02 -0500

    adds some documentation

--- src/hooks/utils/useAsyncRetry.ts
+++ src/hooks/utils/useAsyncRetry.ts
@@ -1,8 +1,19 @@
 import { useCallback } from 'react';
 export function useAsyncRetry() {
+  /**
+   * Use to async request that will retry on failure
+   * Current setup relies on response status 404 for rejection and retry.
+   *
+   * @param func,function that returns a promise
+   * @param retries number of retries
+   *
+   */
   const requestWithRetries = useCallback(async (func: () => Promise<any>, retries: number) => {
     let currentRetries = retries;
     const request = async () => {
+      /**
+       * the Promise won't resolve until the method is resolved. rejections are caught and return null.
+       */
       const funcResponse = await new Promise((resolve, reject) => {
         setTimeout(() => resolve(func().catch(reject)), 3000);
       }).catch(() => null);
