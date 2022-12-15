1a3dd1da575396e5b6f48c507071ce6849a0c114
Christopher Dancy <chris.dancy@decentlabs.io>
2022-12-15 10:06:20 -0500

    freeze button component

---
+++ src/components/Activity/FreezeButton.tsx
@@ -0,0 +1,38 @@
+import { Button } from '@chakra-ui/react';
+import { VetoERC20Voting, VetoMultisigVoting } from '@fractal-framework/fractal-contracts';
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import useCastFreezeVote from '../../hooks/DAO/proposal/useCastFreezeVote';
+
+export function FreezeButton({
+  isFrozen,
+  userHasFreezeVoted,
+  vetoVotingContract,
+}: {
+  isFrozen: boolean;
+  userHasFreezeVoted: boolean;
+  vetoVotingContract: VetoERC20Voting | VetoMultisigVoting;
+}) {
+  const [pending, setPending] = useState<boolean>(false);
+  const { t } = useTranslation(['proposal', 'common']);
+  const castFreezeVote = useCastFreezeVote({
+    vetoVotingContract: vetoVotingContract,
+    setPending: setPending,
+  });
+
+  const disabled = isFrozen || userHasFreezeVoted || pending;
+
+  return (
+    <Button
+      variant="ghost"
+      bgColor={'black.900'}
+      border="1px"
+      borderColor={'blue.500'}
+      textColor={'blue.500'}
+      onClick={() => castFreezeVote}
+      disabled={disabled}
+    >
+      {userHasFreezeVoted ? t('freezeVotedButton') : t('freezeButton')}
+    </Button>
+  );
+}
