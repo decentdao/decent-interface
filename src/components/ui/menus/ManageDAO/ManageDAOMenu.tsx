40a903f27fae728757c28f2914254f515326e249
Christopher Dancy <chris.dancy@decentlabs.io>
2022-12-16 12:47:08 -0500

    added tx for dao menu

--- src/components/ui/menus/ManageDAO/ManageDAOMenu.tsx
+++ src/components/ui/menus/ManageDAO/ManageDAOMenu.tsx
@@ -1,22 +1,31 @@
 import { VEllipsis } from '@decent-org/fractal-ui';
 import { useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useFractal } from '../../../../providers/Fractal/hooks/useFractal';
 import { DAO_ROUTES } from '../../../../routes/constants';
 import { OptionMenu } from '../OptionMenu';
 
 export function ManageDAOMenu({ safeAddress }: { safeAddress: string }) {
   const navigate = useNavigate();
+  const {
+    gnosis: { guardContracts },
+  } = useFractal();
 
-  const options = useMemo(
-    () => [
-      {
-        optionKey: 'optionCreateSubDAO',
-        onClick: () => navigate(DAO_ROUTES.newSubDao.relative(safeAddress)),
-      },
-      { optionKey: 'optionInitiateFreeze', onClick: () => {} }, // TODO freeze hook (if parent voting holder)
-    ],
-    [safeAddress, navigate]
-  );
+  const options = useMemo(() => {
+    const createSubDAOOption = {
+      optionKey: 'optionCreateSubDAO',
+      onClick: () => navigate(DAO_ROUTES.newSubDao.relative(safeAddress)),
+    };
+    if (guardContracts.vetoVotingContract) {
+      const freezeOption = {
+        optionKey: 'optionInitiateFreeze',
+        onClick: () => guardContracts.vetoVotingContract?.castFreezeVote(),
+      };
+      return [createSubDAOOption, freezeOption];
+    } else {
+      return [createSubDAOOption];
+    }
+  }, [safeAddress, navigate, guardContracts]);
 
   return (
     <OptionMenu
