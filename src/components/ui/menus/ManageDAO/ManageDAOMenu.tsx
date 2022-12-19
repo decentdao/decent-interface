1cfe94d29998392822b2ef4fe0a94b72502df88c
Christopher Dancy <chris.dancy@decentlabs.io>
2022-12-19 11:13:11 -0500

    checks if frozen or within freeze init period

--- src/components/ui/menus/ManageDAO/ManageDAOMenu.tsx
+++ src/components/ui/menus/ManageDAO/ManageDAOMenu.tsx
@@ -1,6 +1,7 @@
 import { VEllipsis } from '@decent-org/fractal-ui';
 import { useMemo } from 'react';
 import { useNavigate } from 'react-router-dom';
+import useWithinFreezePeriod from '../../../../hooks/utils/useWithinFreezePeriod';
 import { useFractal } from '../../../../providers/Fractal/hooks/useFractal';
 import { DAO_ROUTES } from '../../../../routes/constants';
 import { OptionMenu } from '../OptionMenu';
@@ -8,15 +9,20 @@ import { OptionMenu } from '../OptionMenu';
 export function ManageDAOMenu({ safeAddress }: { safeAddress: string }) {
   const navigate = useNavigate();
   const {
-    gnosis: { guardContracts },
+    gnosis: { guardContracts, freezeData },
   } = useFractal();
+  const withinFreezeProposalPeriod = useWithinFreezePeriod(freezeData);
 
   const options = useMemo(() => {
     const createSubDAOOption = {
       optionKey: 'optionCreateSubDAO',
       onClick: () => navigate(DAO_ROUTES.newSubDao.relative(safeAddress)),
     };
-    if (guardContracts.vetoVotingContract) {
+    if (
+      guardContracts.vetoVotingContract &&
+      !freezeData.isFrozen &&
+      !withinFreezeProposalPeriod.withinPeriod
+    ) {
       const freezeOption = {
         optionKey: 'optionInitiateFreeze',
         onClick: () => guardContracts.vetoVotingContract?.castFreezeVote(),
@@ -25,7 +31,13 @@ export function ManageDAOMenu({ safeAddress }: { safeAddress: string }) {
     } else {
       return [createSubDAOOption];
     }
-  }, [safeAddress, navigate, guardContracts]);
+  }, [
+    safeAddress,
+    navigate,
+    guardContracts,
+    freezeData.isFrozen,
+    withinFreezeProposalPeriod.withinPeriod,
+  ]);
 
   return (
     <OptionMenu
