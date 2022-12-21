aa24b831c93e62dbbcc7be4ace72e55086b76249
tbwebb22 <tbwebb22@gmail.com>
2022-12-21 15:36:31 -0500

    Handle txProposal type null

--- src/components/ui/proposal/ProposalStateBox.tsx
+++ src/components/ui/proposal/ProposalStateBox.tsx
@@ -2,10 +2,12 @@ import { TxProposalState } from '../../../providers/Fractal/types';
 import { Badge } from '../badges/Badge';
 
 interface ProposalStateBoxProps {
-  state: TxProposalState;
+  state: TxProposalState | null;
 }
 
 export default function TxProposalStateBox({ state }: ProposalStateBoxProps) {
+  if (!state) return null;
+
   return (
     <Badge
       size="base"
