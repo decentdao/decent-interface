896faefde7ac76cbabee4c54fdbc79c4cc6f8276
herbig <mikeherbig@gmail.com>
2022-12-21 16:28:57 -0500

    documentation and typing

--- src/hooks/safe/useIsSafe.ts
+++ src/hooks/safe/useIsSafe.ts
@@ -4,9 +4,20 @@ import { useEffect, useState } from 'react';
 import { buildGnosisApiUrl } from '../../providers/Fractal/utils/api';
 import { useWeb3Provider } from '../../providers/Web3Data/hooks/useWeb3Provider';
 
+/**
+ * A hook which determines whether the provided Ethereum address is a Gnosis
+ * Safe smart contract address on the currently connected chain (chainId).
+ *
+ * The state can be either true/false or undefined, if a network call is currently
+ * being performed to determine that status.
+ *
+ * @param address the address to check
+ * @returns isSafe: whether the address is a Safe,
+ *  isSafeLoading: true/false whether the isSafe status is still being determined
+ */
 export const useIsGnosisSafe = (address: string | undefined) => {
   const [isSafeLoading, setSafeLoading] = useState<boolean>(false);
-  const [isSafe, setIsSafe] = useState<boolean>();
+  const [isSafe, setIsSafe] = useState<boolean | undefined>();
   const {
     state: { chainId },
   } = useWeb3Provider();
