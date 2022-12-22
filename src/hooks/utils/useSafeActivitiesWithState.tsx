c8a589aba5043e06be2378db51d1adc4f2c989ed
tbwebb22 <tbwebb22@gmail.com>
2022-12-22 10:29:58 -0500

    Move timelockperiod and executionperiod calls out of array mapping, only need to be called once

--- src/hooks/utils/useSafeActivitiesWithState.tsx
+++ src/hooks/utils/useSafeActivitiesWithState.tsx
@@ -16,95 +16,99 @@ export function useSafeActivitiesWithState(
 
   useEffect(() => {
     if (vetoGuard) {
-      Promise.all(
-        activities.map(async (activity, _, activityArr) => {
-          if (activity.eventType !== ActivityEventType.Governance || !activity.transaction) {
-            return activity;
-          }
-
-          const isMultiSigTransaction = activity.transaction.txType === 'MULTISIG_TRANSACTION';
-          const isModuleTransaction = activity.transaction.txType === 'MODULE_TRANSACTION';
-          const multiSigTransaction =
-            activity.transaction as SafeMultisigTransactionWithTransfersResponse;
-
-          const eventNonce = multiSigTransaction.nonce;
-
-          const isRejected =
-            isMultiSigTransaction &&
-            activityArr.find(_activity => {
-              const multiSigTx =
-                _activity.transaction as SafeMultisigTransactionWithTransfersResponse;
-              return (
-                multiSigTx.nonce === eventNonce &&
-                multiSigTx.safeTxHash !== multiSigTransaction.safeTxHash &&
-                multiSigTx.isExecuted
-              );
-            });
-
-          const isQueuable = multiSigTransaction.confirmations
-            ? multiSigTransaction.confirmations?.length >= multiSigTransaction.confirmationsRequired
-            : false;
-
-          // todo: move timelockPeriod and executionPeriod outside of array map
-          const timelockPeriod = (await vetoGuard.timelockPeriod()).toNumber();
-          const executionPeriod = (await vetoGuard.executionPeriod()).toNumber();
-
-          // Get the txHash used by the VetoGuard since it is different than the one used by Gnosis Safe
-          const vetoGuardTransactionHash = await vetoGuard.getTransactionHash(
-            multiSigTransaction.to,
-            multiSigTransaction.value,
-            multiSigTransaction.data as string,
-            multiSigTransaction.operation,
-            multiSigTransaction.safeTxGas,
-            multiSigTransaction.baseGas,
-            multiSigTransaction.gasPrice,
-            multiSigTransaction.gasToken,
-            multiSigTransaction.refundReceiver as string
-          );
-
-          const queuedTimestamp = (
-            await vetoGuard.getTransactionQueuedTimestamp(vetoGuardTransactionHash)
-          ).toNumber();
-
-          const lastBlockTimestamp = provider
-            ? (await provider.getBlock(await provider.getBlockNumber())).timestamp
-            : 0;
-
-          let state: TxProposalState;
-
-          if (isModuleTransaction) {
-            state = TxProposalState.Module;
-          } else if (multiSigTransaction.isExecuted) {
-            state = TxProposalState.Executed;
-          } else if (queuedTimestamp === 0) {
-            // Has not been queued
-            if (isRejected) {
-              state = TxProposalState.Rejected;
-            } else if (isQueuable) {
-              state = TxProposalState.Queuable;
-            } else {
-              state = TxProposalState.Active;
-            }
-          } else {
-            // Has been Queued
-            if (lastBlockTimestamp > queuedTimestamp + timelockPeriod) {
-              // Timelock has ended
-              if (lastBlockTimestamp < queuedTimestamp + timelockPeriod + executionPeriod) {
-                // Within execution period
-                state = TxProposalState.Executing;
-              } else {
-                // Execution period has ended
-                state = TxProposalState.Expired;
+      Promise.all([vetoGuard.timelockPeriod(), vetoGuard.executionPeriod()]).then(
+        ([timelockPeriod, executionPeriod]) => {
+          Promise.all(
+            activities.map(async (activity, _, activityArr) => {
+              if (activity.eventType !== ActivityEventType.Governance || !activity.transaction) {
+                return activity;
               }
-            } else {
-              // Within timelock period
-              state = TxProposalState.Queued;
-            }
-          }
 
-          return { ...activity, state };
-        })
-      ).then(setActivitiesWithState);
+              const isMultiSigTransaction = activity.transaction.txType === 'MULTISIG_TRANSACTION';
+              const isModuleTransaction = activity.transaction.txType === 'MODULE_TRANSACTION';
+              const multiSigTransaction =
+                activity.transaction as SafeMultisigTransactionWithTransfersResponse;
+
+              const eventNonce = multiSigTransaction.nonce;
+
+              const isRejected =
+                isMultiSigTransaction &&
+                activityArr.find(_activity => {
+                  const multiSigTx =
+                    _activity.transaction as SafeMultisigTransactionWithTransfersResponse;
+                  return (
+                    multiSigTx.nonce === eventNonce &&
+                    multiSigTx.safeTxHash !== multiSigTransaction.safeTxHash &&
+                    multiSigTx.isExecuted
+                  );
+                });
+
+              const isQueuable = multiSigTransaction.confirmations
+                ? multiSigTransaction.confirmations?.length >=
+                  multiSigTransaction.confirmationsRequired
+                : false;
+
+              // Get the txHash used by the VetoGuard since it is different than the one used by Gnosis Safe
+              const vetoGuardTransactionHash = await vetoGuard.getTransactionHash(
+                multiSigTransaction.to,
+                multiSigTransaction.value,
+                multiSigTransaction.data as string,
+                multiSigTransaction.operation,
+                multiSigTransaction.safeTxGas,
+                multiSigTransaction.baseGas,
+                multiSigTransaction.gasPrice,
+                multiSigTransaction.gasToken,
+                multiSigTransaction.refundReceiver as string
+              );
+
+              const queuedTimestamp = (
+                await vetoGuard.getTransactionQueuedTimestamp(vetoGuardTransactionHash)
+              ).toNumber();
+
+              const lastBlockTimestamp = provider
+                ? (await provider.getBlock(await provider.getBlockNumber())).timestamp
+                : 0;
+
+              let state: TxProposalState;
+
+              if (isModuleTransaction) {
+                state = TxProposalState.Module;
+              } else if (multiSigTransaction.isExecuted) {
+                state = TxProposalState.Executed;
+              } else if (queuedTimestamp === 0) {
+                // Has not been queued
+                if (isRejected) {
+                  state = TxProposalState.Rejected;
+                } else if (isQueuable) {
+                  state = TxProposalState.Queuable;
+                } else {
+                  state = TxProposalState.Active;
+                }
+              } else {
+                // Has been Queued
+                if (lastBlockTimestamp > queuedTimestamp + timelockPeriod.toNumber()) {
+                  // Timelock has ended
+                  if (
+                    lastBlockTimestamp <
+                    queuedTimestamp + timelockPeriod.toNumber() + executionPeriod.toNumber()
+                  ) {
+                    // Within execution period
+                    state = TxProposalState.Executing;
+                  } else {
+                    // Execution period has ended
+                    state = TxProposalState.Expired;
+                  }
+                } else {
+                  // Within timelock period
+                  state = TxProposalState.Queued;
+                }
+              }
+
+              return { ...activity, state };
+            })
+          ).then(setActivitiesWithState);
+        }
+      );
     } else {
       // DAO does not have a VetoGuard
       setActivitiesWithState(
