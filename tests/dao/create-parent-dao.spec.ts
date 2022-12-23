c1f4b8fb83dabc442f851c70cf53669c138a47c9
David Colon <38386583+Da-Colon@users.noreply.github.com>
2022-12-23 01:07:25 -0500

    mocks Initial API requests and removes skip on test

--- tests/dao/create-parent-dao.spec.ts
+++ tests/dao/create-parent-dao.spec.ts
@@ -1,42 +1,32 @@
-import { test } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 import { DAOCreate } from '../models/DAOCreate';
 import { HomePage } from '../models/HomePage';
+import { NewDAOMockRequests } from '../models/mock/NewDAOMockRequests';
 
-test.describe.skip('DAO Creation', () => {
-  let create: DAOCreate;
+let create: DAOCreate;
 
-  test.beforeEach(async ({ page }) => {
-    const home = await new HomePage(page).visit();
-    create = await home
-      .dismissAuditMessage()
-      .then(() => home.connectToWallet())
-      .then(() => home.dismissConnectedMessage())
-      .then(() => home.clickCreateAFractal());
-  });
+test.beforeEach(async ({ page }) => {
+  const home = await new HomePage(page).visit();
+  create = await home
+    .dismissAuditMessage()
+    .then(() => home.connectToWallet())
+    .then(() => home.dismissConnectedMessage())
+    .then(() => home.clickCreateAFractal());
+});
 
-  test('Create MVD Gnosis DAO', async ({ page }) => {
-    await create
-      .fillFractalName('Test Fractal')
-      .then(() => create.clickNextButton())
-      .then(() => create.clickMVDGnosisSafe())
-      .then(() => create.clickNextButton())
-      .then(() => create.fillWalletAddress('0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'))
-      .then(() => create.clickDeployButton());
+test('Create Pure Gnosis DAO', async ({ page }) => {
+  await new NewDAOMockRequests(page).newDAORoutes();
+  await create
+    .fillFractalName('Test Fractal')
+    .then(() => create.clickNextButton())
+    .then(() => create.clickPureGnosisSafe())
+    .then(() => create.clickNextButton())
+    .then(() => create.fillWalletAddress('0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'))
+    .then(() => create.clickDeployButton());
 
-    await page.waitForURL(create.baseUrl + '/daos/0x*');
-  });
+  await page.waitForURL(create.baseUrl + '/daos/*');
 
-  test('Create Pure Gnosis DAO', async ({ page }) => {
-    await create
-      .fillFractalName('Test Fractal')
-      .then(() => create.clickNextButton())
-      .then(() => create.clickPureGnosisSafe())
-      .then(() => create.clickNextButton())
-      .then(() => create.fillWalletAddress('0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266'))
-      .then(() => create.clickDeployButton());
-
-    await page.waitForURL(create.baseUrl + '/daos/0x*');
-  });
-
-  // TODO both DAO pure gnosis creation paths (token and multisig)
+  const daoNameEle = page.locator('[data-testid=DAOInfo-name]');
+  await page.waitForSelector('[data-testid=DAOInfo-name]', { timeout: 5000 });
+  expect(daoNameEle).toContainText('Test Fractal');
 });
