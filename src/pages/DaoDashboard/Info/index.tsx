6daa83892bd1b5733f4d47c95a7f6bd7cf6606a6
herbig <mikeherbig@gmail.com>
2022-12-23 14:12:16 -0500

    better InfoBox linking

--- src/pages/DaoDashboard/Info/index.tsx
+++ src/pages/DaoDashboard/Info/index.tsx
@@ -1,11 +1,18 @@
 import { Flex } from '@chakra-ui/react';
 import { InfoBox } from '../../../components/ui/containers/InfoBox';
+import { useFractal } from '../../../providers/Fractal/hooks/useFractal';
+import { DAO_ROUTES } from '../../../routes/constants';
 import { InfoDAO } from './InfoDAO';
 import { InfoGovernance } from './InfoGovernance';
 import { InfoProposals } from './InfoProposals';
 import { InfoTreasury } from './InfoTreasury';
 
 export function Info() {
+  const {
+    gnosis: {
+      safe: { address },
+    },
+  } = useFractal();
   return (
     <Flex
       flexDirection="column"
@@ -23,10 +30,16 @@ export function Info() {
         <InfoBox minWidth={{ sm: '100%', lg: '34.5%', xl: '23.5%', '2xl': '14rem' }}>
           <InfoGovernance />
         </InfoBox>
-        <InfoBox minWidth={{ sm: '100%', lg: '32%', xl: '21%', '2xl': '10rem' }}>
+        <InfoBox
+          to={DAO_ROUTES.proposals.relative(address)}
+          minWidth={{ sm: '100%', lg: '32%', xl: '21%', '2xl': '10rem' }}
+        >
           <InfoProposals />
         </InfoBox>
-        <InfoBox minWidth={{ sm: '100%', lg: '30%', xl: '15%', '2xl': '9rem' }}>
+        <InfoBox
+          to={DAO_ROUTES.treasury.relative(address)}
+          minWidth={{ sm: '100%', lg: '30%', xl: '15%', '2xl': '9rem' }}
+        >
           <InfoTreasury />
         </InfoBox>
       </Flex>
