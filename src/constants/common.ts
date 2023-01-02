fcc6e951f8890c7df18e1c9552a78ec35ec7b07e
herbig <mikeherbig@gmail.com>
2023-01-02 10:03:08 -0500

    use truncated 00s for placesholder

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,4 +1,4 @@
 export const HEADER_HEIGHT = '4rem';
 export const CONTENT_HEIGHT = `calc(100vh - ${HEADER_HEIGHT})`;
 export const BACKGROUND_SEMI_TRANSPARENT = 'black.900-semi-transparent';
-export const ETH_ADDRESS_PLACEHOLDER = '0x1234';
+export const ETH_ADDRESS_PLACEHOLDER = '0x0000...0000';
