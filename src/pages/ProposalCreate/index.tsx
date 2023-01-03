3531fbdee194c71d929f70f655e1f4dcaa544d27
tbwebb22 <tbwebb22@gmail.com>
2023-01-03 17:00:39 -0500

    Use actual ETH values instead of hardcoded zero

--- src/pages/ProposalCreate/index.tsx
+++ src/pages/ProposalCreate/index.tsx
@@ -25,6 +25,7 @@ import { notProd, useProposeStuff } from '../../utils/dev';
 
 const defaultTransaction = {
   targetAddress: '',
+  ethValue: { value: '0', bigNumberValue: BigNumber.from('0') },
   functionName: '',
   functionSignature: '',
   parameters: '',
@@ -83,7 +84,7 @@ function ProposalCreate() {
     }
     const proposal = {
       targets: transactions.map(transaction => transaction.targetAddress),
-      values: transactions.map(() => BigNumber.from('0')),
+      values: transactions.map(transaction => transaction.ethValue.bigNumberValue),
       calldatas: transactions.map(transaction => transaction.encodedFunctionData || ''),
       title: '',
       description: proposalDescription,
