5f6427a05054fa14bc5b867901d6500d7bc08ac7
tbwebb22 <tbwebb22@gmail.com>
2023-01-13 15:44:46 -0500

    Remove regex URL validation

--- src/components/ProposalCreate/UsulMetadata.tsx
+++ src/components/ProposalCreate/UsulMetadata.tsx
@@ -30,16 +30,11 @@ function UsulMetadata({
   const isValidUrl = (url: string) => {
     if (!url) return true;
 
-    let pattern = new RegExp(
-      '^(https?:\\/\\/)?' + // protocol
-        '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
-        '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-        '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-        '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-        '(\\#[-a-z\\d_]*)?$',
-      'i'
-    ); // fragment locator
-    return !!pattern.test(url);
+    try {
+      return Boolean(new URL(url));
+    } catch (e) {
+      return false;
+    }
   };
 
   const updateTitle = (title: string) => {
