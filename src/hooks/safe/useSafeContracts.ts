d3f0f6300a369bbde29a7bbf18477d3e40156a23
sethhrbek <seth.hrbek@gmail.com>
2023-01-30 20:37:21 -0500

    revert contract init comments

--- src/hooks/safe/useSafeContracts.ts
+++ src/hooks/safe/useSafeContracts.ts
@@ -30,51 +30,26 @@ import { MultiSend, MultiSend__factory } from '../../assets/typechain-types/usul
 import { useNetworkConfg } from '../../providers/NetworkConfig/NetworkConfigProvider';
 
 export default function useSafeContracts() {
-  // Used to batch transactions
   const [multiSendContract, setMultisendContract] = useState<MultiSend>();
-
-  // Safe Proxy Factory - Allows to create new safe proxy contact and execute a message call to the new proxy within one transaction.
   const [gnosisSafeFactoryContract, setGnosisSafeFactoryContract] =
     useState<GnosisSafeProxyFactory>();
-
-  // Singleton safe contract which is sent as an argument to the safe factory contract
   const [gnosisSafeSingletonContract, setGnosisSafeSingletonContract] = useState<GnosisSafe>();
-
-  // Module Proxy Factory - used to deploy modules on the safe proxy
   const [zodiacModuleProxyFactoryContract, setZodiacModuleProxyFactoryContract] =
     useState<ModuleProxyFactory>();
-
-  // Fractal Usul master copy contract which extends base Usul contract
   const [fractalUsulMasterCopyContract, setFractalUsulMasterCopyContract] = useState<FractalUsul>();
-
-  // Linear Voting strategy contract -- defines the token voting strategy used by the Usul contract
   const [linearVotingMasterCopyContract, setLinearVotingMasterCopyContract] =
     useState<OZLinearVoting>(); // 1:1 Token Voting contract
-
-  // Fractal Module master copy contract -- this is the module which gets attached to Usul contracts
   const [fractalModuleMasterCopyContract, setFractalModuleMasterCopyContract] =
     useState<FractalModule>();
-
-  // Fractal Name Registry -- used to emit DAO Name events to "store" Fractal DAO names
   const [fractalRegistryContract, setFractalRegistryContract] = useState<FractalRegistry>();
-
-  // A guard contract that prevents transactions that have been vetoed from being executed on the Gnosis Safe
   const [gnosisVetoGuardMasterCopyContract, setGnosisVetoGuardMasterCopyContract] =
     useState<VetoGuard>();
-
-  // Veto Guard used for Usul DAOs
   const [usulVetoGuardMasterCopyContract, setUsulVetoGuardMasterCopyContract] =
     useState<UsulVetoGuard>();
-
-  // Veto Guard used for Multisig DAOs
   const [vetoMultisigVotingMasterCopyContract, setVetoMultisigVotingMasterCopyContract] =
     useState<VetoMultisigVoting>();
-
-  // A contract for casting veto votes with an ERC20 votes token
   const [vetoERC20VotingMasterCopyContract, setVetoERC20VotingMasterCopyContract] =
     useState<VetoERC20Voting>();
-
-  // Creates ERC20 voting token for Usul DAOs
   const [votesTokenMasterCopyContract, setVotesTokenMasterCopyContract] = useState<VotesToken>();
 
   const provider = useProvider();
