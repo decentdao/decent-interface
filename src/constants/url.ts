818875fe3f64a18ea630a30a73b0086ea00f2fd7
herbig <mikeherbig@gmail.com>
2023-02-01 10:50:01 -0500

    switch to permanent discord invite link

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,3 +1,3 @@
 export const URL_FAQ = 'https://docs.fractalframework.xyz/welcome-to-fractal/user-guides/faq';
-export const URL_DISCORD = 'https://discord.com/invite/decent-dao';
+export const URL_DISCORD = 'https://discord.gg/Zh2emKspVF';
 export const URL_DOCS = 'https://docs.fractalframework.xyz/welcome-to-fractal/';
