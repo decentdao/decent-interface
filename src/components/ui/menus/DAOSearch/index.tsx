35703e5177e2b7a5825f1a6ac45e97a2f6dc935d
herbig <mikeherbig@gmail.com>
2023-02-01 22:49:47 -0500

    close search and clear text when clicking to view the result or outside the component

--- src/components/ui/menus/DAOSearch/index.tsx
+++ src/components/ui/menus/DAOSearch/index.tsx
@@ -5,9 +5,11 @@ import {
   InputLeftElement,
   Popover,
   PopoverTrigger,
+  useDisclosure,
+  useOutsideClick,
 } from '@chakra-ui/react';
 import { Search } from '@decent-org/fractal-ui';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useSearchDao } from '../../../../hooks/DAO/useSearchDao';
 import { SearchDisplay } from './SearchDisplay';
@@ -16,17 +18,27 @@ export function DAOSearch() {
   const { t } = useTranslation(['dashboard']);
   const [localInput, setLocalInput] = useState('');
   const { errorMessage, isLoading, address, isSafe, setSearchString } = useSearchDao();
+  const { onClose } = useDisclosure(); // popover close function
+  const ref = useRef() as React.MutableRefObject<HTMLInputElement>;
 
   useEffect(() => {
     setSearchString(localInput);
   }, [localInput, setSearchString]);
 
   const resetSearch = () => {
+    onClose();
+    setLocalInput('');
     setSearchString('');
   };
 
+  useOutsideClick({
+    ref: ref,
+    handler: () => resetSearch(),
+  });
+
   return (
     <Box
+      ref={ref}
       width="full"
       maxW="31.125rem"
       height="full"
