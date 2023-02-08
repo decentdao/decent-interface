d673164ee33027d2b5088d9049282d864685a718
Kirill Klimenko <klimenkomud@gmail.com>
2023-02-08 21:06:50 +0200

    Remove ActivityAction component as it is too small for composition

--- src/components/Activity/ActivityGovernance.tsx
+++ src/components/Activity/ActivityGovernance.tsx
@@ -1,11 +1,13 @@
+import { Flex } from '@chakra-ui/react';
 import { format } from 'date-fns';
 import { useTranslation } from 'react-i18next';
 import { useFractal } from '../../providers/Fractal/hooks/useFractal';
 import { TxProposal, ActivityEventType } from '../../providers/Fractal/types';
 import { DEFAULT_DATE_FORMAT } from '../../utils/numberFormats';
+import { ProposalAction } from '../Proposals/ProposalActions/ProposalAction';
 import { Badge } from '../ui/badges/Badge';
-import ActivityAction from './ActivityAction';
 
+import ProposalTime from '../ui/proposal/ProposalTime';
 import { ActivityCard } from './ActivityCard';
 import { ActivityDescription } from './ActivityDescription';
 
@@ -34,7 +36,15 @@ export function ActivityGovernance({ activity }: { activity: TxProposal }) {
         )
       }
       description={<ActivityDescription activity={activity} />}
-      RightElement={<ActivityAction activity={activity} />}
+      RightElement={
+        <Flex
+          gap={4}
+          alignItems="center"
+        >
+          <ProposalTime proposal={activity} />
+          <ProposalAction proposal={activity} />
+        </Flex>
+      }
       eventDate={format(activity.eventDate, DEFAULT_DATE_FORMAT)}
       eventDateLabel={eventDateLabel}
     />
