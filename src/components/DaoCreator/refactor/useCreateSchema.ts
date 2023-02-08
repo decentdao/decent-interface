ccb8c30488a66c4be87f820caa408f98a2e3744d
David Colon <38386583+Da-Colon@users.noreply.github.com>
2023-02-08 14:01:58 -0500

    in progress schema

--- src/components/DaoCreator/refactor/useCreateSchema.ts
+++ src/components/DaoCreator/refactor/useCreateSchema.ts
@@ -15,7 +15,6 @@ export const useCreateSchema = () => {
         gnosis: Yup.object().shape({
           trustedAddresses: Yup.array()
             .min(1)
-            .ensure()
             .of(Yup.string().test(addressValidationTest))
             .when({
               is: (array: string[]) => array && array.length > 1,
@@ -23,6 +22,39 @@ export const useCreateSchema = () => {
             }),
           signatureThreshold: Yup.number().min(1).required(),
           numOfSigners: Yup.number().min(1),
+        }),
+        govToken: Yup.object().shape({
+          tokenName: Yup.string().required(),
+          tokenSupply: Yup.number(),
+          tokenSymbol: Yup.string().max(5, 'Limited to 5 chars'),
+          parentAllocationAmount: Yup.number()
+            .notRequired()
+            .test({
+              name: 'manage parent token amount',
+              message: 'incorrect',
+              test: function (value, context) {
+                return false;
+              },
+            }),
+          tokenAllocations: Yup.array().of(
+            Yup.object()
+              .shape({
+                address: Yup.string().test(addressValidationTest),
+                amount: Yup.number().test({
+                  name: 'manage token amount',
+                  message: 'incorrect',
+                  test: function (value, context) {
+                    return false;
+                  },
+                }),
+              })
+              .when({
+                is: (array: string[]) => array && array.length > 1,
+                then: Yup.array().of(
+                  Yup.object().shape({ address: Yup.string().test(uniqueAddressValidationTest) })
+                ),
+              })
+          ),
         }),
       }),
     [addressValidationTest, uniqueAddressValidationTest]
