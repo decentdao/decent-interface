85c1e6685e97591ced1c83057e54e3f2986008f0
David Colon <38386583+Da-Colon@users.noreply.github.com>
2023-03-16 01:09:07 -0400

    refactors UsulMetaData to use Yup values/validation

--- src/hooks/schemas/proposalCreate/useCreateProposalSchema.ts
+++ src/hooks/schemas/proposalCreate/useCreateProposalSchema.ts
@@ -1,4 +1,5 @@
 import { useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
 import * as Yup from 'yup';
 import { useValidationAddress } from '../common/useValidationAddress';
 
@@ -9,12 +10,11 @@ import { useValidationAddress } from '../common/useValidationAddress';
 export const useCreateProposalSchema = () => {
   const { addressValidationTest } = useValidationAddress();
 
-  // const { t } = useTranslation(['createProposal']);
+  const { t } = useTranslation(['createProposal']);
 
-  const createDAOValidation = useMemo(
+  const createProposalValidation = useMemo(
     () =>
       Yup.object().shape({
-        proposalDescription: Yup.string(),
         transactions: Yup.array()
           .min(1)
           .of(
@@ -27,19 +27,33 @@ export const useCreateProposalSchema = () => {
               // @add function error handling
               functionSignature: Yup.string(),
               parameters: Yup.string(),
-              isExpanded: Yup.boolean(),
               // @todo use Yup to update this value
               encodedFunctionData: Yup.string(),
             })
           ),
-        proposalData: Yup.object().shape({
-          title: Yup.string(),
-          description: Yup.string(),
-          documentationUrl: Yup.string(),
+        proposalMetadata: Yup.object().shape({
+          title: Yup.string().notRequired(),
+          description: Yup.string().notRequired(),
+          documentationUrl: Yup.string()
+            .notRequired()
+            .when({
+              is: (value?: string) => !!value,
+              then: schema =>
+                schema.test({
+                  message: t('Invalid URL'),
+                  test: value => {
+                    try {
+                      return Boolean(new URL(value || ''));
+                    } catch (e) {
+                      return false;
+                    }
+                  },
+                }),
+            }),
         }),
         nonce: Yup.number(),
       }),
-    [addressValidationTest]
+    [addressValidationTest, t]
   );
-  return { createDAOValidation };
+  return { createProposalValidation };
 };
--- src/types/createProposal.ts
+++ src/types/createProposal.ts
@@ -1,61 +1,60 @@
 import { Dispatch, SetStateAction } from 'react';
+import { BigNumberValuePair } from './common';
 import { ProposalExecuteData } from './daoProposal';
-import { TransactionData } from './transaction';
 
-export interface TransactionsAndSubmitProps {
-  show: boolean | undefined;
-  showBackButton: boolean;
-  onGoBack: () => void;
-  addTransaction: () => void;
-  pendingCreateTx: boolean;
-  submitProposal: ({
-    proposalData,
-    nonce,
-    pendingToastMessage,
-    failedToastMessage,
-    successToastMessage,
-    successCallback,
-  }: {
-    proposalData: ProposalExecuteData | undefined;
-    nonce: number | undefined;
-    pendingToastMessage: string;
-    failedToastMessage: string;
-    successToastMessage: string;
-    successCallback?: ((daoAddress: string) => void) | undefined;
-  }) => Promise<void>;
-  proposalData: ProposalExecuteData | undefined;
-  nonce: number | undefined;
-  successCallback: () => void;
-  isCreateDisabled: boolean;
-  transactions: TransactionData[];
-  setTransactions: Dispatch<SetStateAction<TransactionData[]>>;
-  removeTransaction: (transactionNumber: number) => void;
+// export interface TransactionsAndSubmitProps {
+//   show: boolean | undefined;
+//   showBackButton: boolean;
+//   onGoBack: () => void;
+//   addTransaction: () => void;
+//   pendingCreateTx: boolean;
+//   submitProposal: ({
+//     proposalData,
+//     nonce,
+//     pendingToastMessage,
+//     failedToastMessage,
+//     successToastMessage,
+//     successCallback,
+//   }: {
+//     proposalData: ProposalExecuteData | undefined;
+//     nonce: number | undefined;
+//     pendingToastMessage: string;
+//     failedToastMessage: string;
+//     successToastMessage: string;
+//     successCallback?: ((daoAddress: string) => void) | undefined;
+//   }) => Promise<void>;
+//   proposalData: ProposalExecuteData | undefined;
+//   nonce: number | undefined;
+//   successCallback: () => void;
+//   isCreateDisabled: boolean;
+//   transactions: TransactionData[];
+//   setTransactions: Dispatch<SetStateAction<TransactionData[]>>;
+//   removeTransaction: (transactionNumber: number) => void;
+// }
+
+export enum CreateProposalState {
+  METADATA_FORM,
+  TRANSACTIONS_FORM,
+  LOADING,
 }
 
-export type ProposalFormState = {
-  proposalDescription: string;
-  transactions: TransactionData[];
-  proposalData?: {
-    title: string;
-    description: string;
-    documentationUrl: string;
-  };
-  nonce: Number | undefined;
+export interface CreateProposalTransaction<T = BigNumberValuePair> {
+  targetAddress: string;
+  ethValue: T;
+  functionName: string;
+  functionSignature?: string;
+  parameters?: string;
+  encodedFunctionData?: string;
+}
+
+export type CreateProposalMetadata = {
+  title: string;
+  description: string;
+  documentationUrl: string;
 };
 
-export interface UsulMetadataProps {
-  show: boolean;
-  setInputtedMetadata: Dispatch<SetStateAction<boolean>>;
-  metadata: {
-    title: string;
-    description: string;
-    documentationUrl: string;
-  };
-  setMetadata: Dispatch<
-    SetStateAction<{
-      title: string;
-      description: string;
-      documentationUrl: string;
-    }>
-  >;
-}
+export type CreateProposalForm = {
+  transactions: CreateProposalTransaction[];
+  proposalMetadata: CreateProposalMetadata;
+  nonce?: Number;
+};
