016f5145319e35d6b4e7c2f297da8216c569c23f
David Colon <38386583+Da-Colon@users.noreply.github.com>
2023-03-16 15:53:33 -0400

    remove unused code

--- src/types/createProposal.ts
+++ src/types/createProposal.ts
@@ -1,36 +1,4 @@
-import { Dispatch, SetStateAction } from 'react';
 import { BigNumberValuePair } from './common';
-import { ProposalExecuteData } from './daoProposal';
-
-// export interface TransactionsAndSubmitProps {
-//   show: boolean | undefined;
-//   showBackButton: boolean;
-//   onGoBack: () => void;
-//   addTransaction: () => void;
-//   pendingCreateTx: boolean;
-//   submitProposal: ({
-//     proposalData,
-//     nonce,
-//     pendingToastMessage,
-//     failedToastMessage,
-//     successToastMessage,
-//     successCallback,
-//   }: {
-//     proposalData: ProposalExecuteData | undefined;
-//     nonce: number | undefined;
-//     pendingToastMessage: string;
-//     failedToastMessage: string;
-//     successToastMessage: string;
-//     successCallback?: ((daoAddress: string) => void) | undefined;
-//   }) => Promise<void>;
-//   proposalData: ProposalExecuteData | undefined;
-//   nonce: number | undefined;
-//   successCallback: () => void;
-//   isCreateDisabled: boolean;
-//   transactions: TransactionData[];
-//   setTransactions: Dispatch<SetStateAction<TransactionData[]>>;
-//   removeTransaction: (transactionNumber: number) => void;
-// }
 
 export enum CreateProposalState {
   METADATA_FORM,
