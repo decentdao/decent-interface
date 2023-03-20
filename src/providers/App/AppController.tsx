2f4fb7f147cbf8942d68e66ccbd13901b2b84d16
David Colon <38386583+Da-Colon@users.noreply.github.com>
2023-03-19 22:38:20 -0400

    remove unneeded component

--- src/providers/App/AppController.tsx
+++
@@ -1,8 +0,0 @@
-import { Box } from '@chakra-ui/react';
-import { ReactNode } from 'react';
-
-export function AppController({ children }: { children: ReactNode }) {
-  // This new layer will be used to control the app state without re-rendering the whole app.
-  // I believe that by extracts this part below the useMemo of the provider will allow for better more intentional loading
-  return children;
-}
