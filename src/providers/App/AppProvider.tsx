65dea1769e9c9a247924ee6fa00ee5120f5cccbc
David Colon <38386583+Da-Colon@users.noreply.github.com>
2023-03-19 23:43:37 -0400

    loads safeclient into state

--- src/providers/App/AppProvider.tsx
+++ src/providers/App/AppProvider.tsx
@@ -1,6 +1,7 @@
 import { Context, createContext, useContext, useMemo, useReducer } from 'react';
 import useSafeContracts from '../../hooks/safe/useSafeContracts';
 import { FractalStore } from '../../types';
+import { useSafeService } from './hooks/useSafeService';
 import { initialNodeState, nodeReducer } from './node/reducer';
 
 export const FractalContext = createContext<FractalStore | null>(null);
@@ -11,7 +12,8 @@ export const useFractal = (): FractalStore => useContext(FractalContext as Conte
 export function AppProvider() {
   // loads base Fractal contracts with provider into state
   const baseContracts = useSafeContracts();
-
+  // loads safe service into state;
+  const safeService = useSafeService();
   // handles current viewing node (DAO) state
   const [node, nodeDispatch] = useReducer(nodeReducer, initialNodeState);
 
@@ -22,9 +24,12 @@ export function AppProvider() {
       dispatch: {
         node: nodeDispatch,
       },
+      clients: {
+        safeService,
+      },
       baseContracts,
     };
-  }, [node, baseContracts]);
+  }, [node, safeService, baseContracts]);
 
   return <FractalContext.Provider value={fractalStore}></FractalContext.Provider>;
 }
