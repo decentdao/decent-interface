8ba0bcc69cdbf315ad030a774181d23e7d134db9
David Colon <38386583+Da-Colon@users.noreply.github.com>
2023-04-23 22:46:46 -0400

    happy path, wrap token functionality

--- src/components/ui/modals/WrapToken.tsx
+++ src/components/ui/modals/WrapToken.tsx
@@ -1,4 +1,4 @@
-import { Button, Checkbox, Flex, Input } from '@chakra-ui/react';
+import { Button, Flex, Input } from '@chakra-ui/react';
 import { LabelWrapper } from '@decent-org/fractal-ui';
 import { BigNumber, Contract } from 'ethers';
 import { Formik, FormikProps } from 'formik';
@@ -6,14 +6,18 @@ import { useCallback, useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { erc20ABI, useAccount, useSigner } from 'wagmi';
 import * as Yup from 'yup';
+import { VotesERC20Wrapper } from '../../../assets/typechain-types/VotesERC20Wrapper';
+import { logError } from '../../../helpers/errorLogging';
+import useApproval from '../../../hooks/utils/useApproval';
 import { useFormHelpers } from '../../../hooks/utils/useFormHelpers';
+import { useTransaction } from '../../../hooks/utils/useTransaction';
 import { useFractal } from '../../../providers/App/AppProvider';
 import { AzoriusGovernance, BigNumberValuePair } from '../../../types';
 import { formatCoin } from '../../../utils';
 import { BigNumberInput } from '../forms/BigNumberInput';
 
 export function WrapToken({ close }: { close: () => void }) {
-  const { governance } = useFractal();
+  const { governance, governanceContracts } = useFractal();
   const azoriusGovernance = governance as AzoriusGovernance;
   const { data: signer } = useSigner();
   const { address: account } = useAccount();
@@ -23,9 +27,16 @@ export function WrapToken({ close }: { close: () => void }) {
     bigNumberValue: BigNumber.from(0),
   });
 
-  const { t } = useTranslation('modals');
+  const [contractCall, pending] = useTransaction();
+  const [approved, approveTransaction, approvalPending] = useApproval(
+    governanceContracts.tokenContract?.asSigner.attach(governanceContracts.underlyingTokenAddress!),
+    azoriusGovernance.votesToken.address
+  );
+
+  const { t } = useTranslation(['modals', 'treasury']);
   const { restrictChars } = useFormHelpers();
-  const getUserUderlyingTokenBalance = useCallback(async () => {
+
+  const getUserUnderlyingTokenBalance = useCallback(async () => {
     if (
       !azoriusGovernance.votesToken.decimals ||
       !azoriusGovernance.votesToken.underlyingTokenData ||
@@ -38,34 +49,49 @@ export function WrapToken({ close }: { close: () => void }) {
       erc20ABI,
       signer
     );
-    const [balance, decimals]: [BigNumber, number] = await Promise.all([
-      baseTokenContract.balanceOf(account),
-      baseTokenContract.decimals(),
-    ]);
-    // formatCoin(
-    //   balance,
-    //   false,
-    //   decimals,
-    //   azoriusGovernance.votesToken.underlyingTokenData?.symbol
-    // )
-    setUserBalance({
-      value: formatCoin(
-        balance,
-        false,
-        decimals,
-        azoriusGovernance.votesToken.underlyingTokenData?.symbol
-      ),
-      bigNumberValue: balance,
-    });
+    try {
+      const [balance, decimals]: [BigNumber, number] = await Promise.all([
+        baseTokenContract.balanceOf(account),
+        baseTokenContract.decimals(),
+      ]);
+      setUserBalance({
+        value: formatCoin(
+          balance,
+          false,
+          decimals,
+          azoriusGovernance.votesToken.underlyingTokenData?.symbol
+        ),
+        bigNumberValue: balance,
+      });
+    } catch (e) {
+      logError(e);
+      return;
+    }
   }, [account, azoriusGovernance.votesToken, signer]);
 
   useEffect(() => {
-    getUserUderlyingTokenBalance();
-  }, [getUserUderlyingTokenBalance]);
+    getUserUnderlyingTokenBalance();
+  }, [getUserUnderlyingTokenBalance]);
 
-  // @todo submit callback
-  const handleFormSubmit = useCallback(() => {}, []);
-  // @todo add delegate to self checkbox to form values
+  const handleFormSubmit = useCallback(
+    (amount: BigNumberValuePair, delegateToSelf: boolean) => {
+      const { tokenContract } = governanceContracts;
+      if (!tokenContract || !signer || !account) return;
+      const wrapperTokenContract = tokenContract.asSigner as VotesERC20Wrapper;
+      contractCall({
+        contractFn: () => wrapperTokenContract.depositFor(account, amount.bigNumberValue!),
+        pendingMessage: t('wrapTokenPendingMessage'),
+        failedMessage: t('wrapTokenFailedMessage'),
+        successMessage: t('wrapTokenSuccessMessage'),
+        successCallback: () => {},
+        completedCallback: () => {
+          close();
+        },
+      });
+    },
+    [account, contractCall, governanceContracts, signer, close, t]
+  );
+
   if (
     !azoriusGovernance.votesToken.decimals ||
     !azoriusGovernance.votesToken.underlyingTokenData ||
@@ -73,20 +99,24 @@ export function WrapToken({ close }: { close: () => void }) {
   ) {
     return null;
   }
+
   return (
     <Formik
       initialValues={{
         amount: { value: '', bigNumberValue: BigNumber.from(0) },
         delegateToSelf: false,
       }}
-      onSubmit={() => {}}
+      onSubmit={values => {
+        const { amount, delegateToSelf } = values;
+        handleFormSubmit(amount, delegateToSelf);
+      }}
       validationSchema={Yup.object().shape({
         amount: Yup.object({
           value: Yup.string().required(),
           bigNumberValue: Yup.mixed().required(),
         }).test({
           name: 'Wrap Token Validation',
-          message: 'Invalid amount',
+          message: t('wrapTokenError'),
           test: amount => {
             const amountBN = amount.bigNumberValue as BigNumber;
             if (!amount) return false;
@@ -126,18 +156,35 @@ export function WrapToken({ close }: { close: () => void }) {
                   onChange={valuePair => setFieldValue('amount', valuePair)}
                   data-testid="wrapToken-amount"
                   onKeyDown={restrictChars}
+                  isDisabled={!approved}
                 />
               </LabelWrapper>
 
-              <Checkbox
+              {/* @todo re-enable/delete after discussion */}
+              {/* <Checkbox
                 color="gold.500"
                 isChecked={values.delegateToSelf}
                 onChange={() => setFieldValue('linkSelfDelegate', !values.delegateToSelf)}
               >
                 {t('linkSelfDelegate')}
-              </Checkbox>
+              </Checkbox> */}
 
-              <Button isDisabled={!!errors.amount}>Wrap</Button>
+              {approved ? (
+                <Button
+                  type="submit"
+                  isDisabled={!!errors.amount || pending}
+                >
+                  {t('wrapTokenButton')}
+                </Button>
+              ) : (
+                <Button
+                  type="button"
+                  onClick={approveTransaction}
+                  isDisabled={approvalPending}
+                >
+                  {t('approveToken', { ns: 'treasury' })}
+                </Button>
+              )}
             </Flex>
           </form>
         );
