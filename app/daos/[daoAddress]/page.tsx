cbbb54e9ef4aee7bf18e1f2e02b3e34faa85e00e
Kirill Klimenko <klimenkomud@gmail.com>
2023-05-25 12:31:08 +0300

    Revert ClientOnly removal, that doesn't help. Move FractalErrorBoundary up the tree to catch errors from above providers

--- app/daos/[daoAddress]/page.tsx
+++ app/daos/[daoAddress]/page.tsx
@@ -4,13 +4,16 @@ import { Box } from '@chakra-ui/react';
 import { Activities } from '../../../src/components/pages/DaoDashboard/Activities';
 import { ERCO20Claim } from '../../../src/components/pages/DaoDashboard/ERC20Claim';
 import { Info } from '../../../src/components/pages/DaoDashboard/Info';
+import ClientOnly from '../../../src/components/ui/utils/ClientOnly';
 
 export default function DaoDashboardPage() {
   return (
-    <Box mt={12}>
-      <Info />
-      <ERCO20Claim />
-      <Activities />
-    </Box>
+    <ClientOnly>
+      <Box mt={12}>
+        <Info />
+        <ERCO20Claim />
+        <Activities />
+      </Box>
+    </ClientOnly>
   );
 }
