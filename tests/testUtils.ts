6776336b845f930560cd9f7947ae82d108da812a
Kirill Klimenko <klimenkomud@gmail.com>
2023-06-06 22:57:49 +0200

    Mock only requests to hardhat network in tests

--- tests/testUtils.ts
+++ tests/testUtils.ts
@@ -4,3 +4,6 @@ export const SUBGRAPH_GOERLI_API_URL =
   'https://api.studio.thegraph.com/query/43215/fractal-goerli/version/latest';
 export const SUBGRAPH_MAINNET_API_URL =
   'https://api.studio.thegraph.com/query/43215/fractal-mainnet/version/latest';
+
+export const SUBGRAPH_TEST_API_URL =
+  'https://api.studio.thegraph.com/query/43215/fractal-hardhat/version/latest';
