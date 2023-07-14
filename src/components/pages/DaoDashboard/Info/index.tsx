43b609ee07a1185af91ed544a49e3724aef64b54
herbig <mikeherbig@gmail.com>
2023-07-14 12:50:04 -0400

    fix gap weirdness causing strange info card breaking

--- src/components/pages/DaoDashboard/Info/index.tsx
+++ src/components/pages/DaoDashboard/Info/index.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Flex } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 import { DAO_ROUTES } from '../../../../constants/routes';
 import { useFractal } from '../../../../providers/App/AppProvider';
 import { InfoBox } from '../../../ui/containers/InfoBox';
@@ -14,6 +14,12 @@ export function Info() {
   const {
     node: { daoAddress },
   } = useFractal();
+
+  // using this gap method instead of 'gap' to make width percentages more precise, since they
+  // can now add up to 100%, as well as prevent variable gap space as you widen the screen
+  const PAD = '0.5rem'; // half the intended gap of 1rem
+  const NONE = '0rem';
+
   return (
     <Flex
       flexDirection="column"
@@ -22,28 +28,49 @@ export function Info() {
       <ParentLink />
       <Flex
         flexWrap="wrap"
-        gap="1rem"
         justifyContent="space-between"
         mb="1rem"
       >
-        <InfoBox minWidth={{ base: '100%', md: '48.75%', lg: '32%', xl: '37%', '2xl': '47%' }}>
-          <InfoDAO />
-        </InfoBox>
-        <InfoBox minWidth={{ base: '100%', md: '48.75%', lg: '13rem', xl: '15rem' }}>
-          <InfoGovernance />
-        </InfoBox>
-        <InfoBox
-          to={DAO_ROUTES.proposals.relative(daoAddress)}
-          minWidth={{ base: '100%', md: '48.75%', lg: '10rem', xl: '12rem' }}
+        <Box
+          minWidth={{ base: '100%', md: '100%', lg: '33%', xl: '40%' }}
+          pe={{ base: NONE, lg: PAD }}
+          pb={{ sm: PAD, lg: NONE }}
         >
-          <InfoProposals />
-        </InfoBox>
-        <InfoBox
-          to={DAO_ROUTES.treasury.relative(daoAddress)}
-          minWidth={{ base: '100%', md: '48.75%', lg: '12rem' }} // 12 rem
+          <InfoBox>
+            <InfoDAO />
+          </InfoBox>
+        </Box>
+        <Box
+          minWidth={{ base: '100%', md: '33.3%', lg: '25%', xl: '20%' }}
+          ps={{ base: NONE, lg: PAD }}
+          pe={{ base: NONE, md: PAD }}
+          pb={{ sm: PAD, md: NONE }}
+          pt={{ sm: PAD, lg: NONE }}
         >
-          <InfoTreasury />
-        </InfoBox>
+          <InfoBox>
+            <InfoGovernance />
+          </InfoBox>
+        </Box>
+        <Box
+          minWidth={{ base: '100%', md: '33.3%', lg: '19%', xl: '20%' }}
+          ps={{ base: NONE, md: PAD }}
+          pe={{ base: NONE, md: PAD }}
+          pb={{ sm: PAD, md: NONE }}
+          pt={{ sm: PAD, lg: NONE }}
+        >
+          <InfoBox to={DAO_ROUTES.proposals.relative(daoAddress)}>
+            <InfoProposals />
+          </InfoBox>
+        </Box>
+        <Box
+          minWidth={{ base: '100%', md: '33.3%', lg: '23%', xl: '20%' }}
+          ps={{ base: NONE, md: PAD }}
+          pt={{ sm: PAD, lg: NONE }}
+        >
+          <InfoBox to={DAO_ROUTES.treasury.relative(daoAddress)}>
+            <InfoTreasury />
+          </InfoBox>
+        </Box>
       </Flex>
     </Flex>
   );
