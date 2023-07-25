ecb2be3584dcca9c6076249bb466967e7698d6af
Kirill Klimenko <klimenkomud@gmail.com>
2023-07-26 00:59:33 +0200

    Proper calculation for user voting weight on NFT DAO

--- src/providers/App/useReadOnlyValues.ts
+++ src/providers/App/useReadOnlyValues.ts
@@ -1,47 +1,72 @@
+import { ERC721__factory } from '@fractal-framework/fractal-contracts';
 import { BigNumber, utils } from 'ethers';
-import { useCallback } from 'react';
+import { useEffect, useState } from 'react';
+import useSignerOrProvider from '../../hooks/utils/useSignerOrProvider';
 import { Fractal, AzoriusGovernance, GovernanceSelectionType, ReadOnlyState } from '../../types';
 
+const INITIAL_READ_ONLY_VALUES: ReadOnlyState = {
+  user: {
+    address: undefined,
+    votingWeight: BigNumber.from(0),
+  },
+  dao: null,
+};
 /**
  * Sets "read only" values which are passed on to the FractalProvider.
  *
  * These are useful dao or user specific values calculated from other stateful
  * values.
  */
-export const useReadOnlyValues = () => {
-  const readOnlyValues = useCallback((_state: Fractal, _account: string | undefined) => {
-    const { node, governance } = _state;
+export const useReadOnlyValues = ({ node, governance }: Fractal, _account?: string) => {
+  const [readOnlyValues, setReadOnlyValues] = useState<ReadOnlyState>(INITIAL_READ_ONLY_VALUES);
+  const signerOrProvider = useSignerOrProvider();
 
-    const isSigner = _account && node.safe?.owners.includes(_account);
-    const tokenWeight =
-      (governance as AzoriusGovernance).votesToken?.votingWeight || BigNumber.from(0);
+  useEffect(() => {
+    const loadReadOnlyValues = async () => {
+      const getVotingWeight = async () => {
+        const azoriusGovernance = governance as AzoriusGovernance;
+        switch (governance.type) {
+          case GovernanceSelectionType.MULTISIG:
+            const isSigner = _account && node.safe?.owners.includes(_account);
+            return isSigner ? BigNumber.from(1) : BigNumber.from(0);
+          case GovernanceSelectionType.AZORIUS_ERC20:
+            const tokenWeight = azoriusGovernance.votesToken?.votingWeight || BigNumber.from(0);
+            return tokenWeight;
+          case GovernanceSelectionType.AZORIUS_ERC721:
+            if (!_account || !azoriusGovernance.erc721Tokens) {
+              return BigNumber.from(0);
+            }
+            const userVotingWeight = (
+              await Promise.all(
+                azoriusGovernance.erc721Tokens.map(async ({ address, votingWeight }) => {
+                  const tokenContract = ERC721__factory.connect(address, signerOrProvider);
+                  const userBalance = await tokenContract.balanceOf(_account);
+                  return userBalance.mul(votingWeight);
+                })
+              )
+            ).reduce((prev, curr) => prev.add(curr), BigNumber.from(0));
+            return userVotingWeight;
+          default:
+            return BigNumber.from(0);
+        }
+      };
 
-    const votingWeight = () => {
-      switch (governance.type) {
-        case GovernanceSelectionType.MULTISIG:
-          return isSigner ? BigNumber.from(1) : BigNumber.from(0);
-        case GovernanceSelectionType.AZORIUS_ERC20:
-          return tokenWeight;
-        case GovernanceSelectionType.AZORIUS_ERC721:
-          return BigNumber.from(0);
-        default:
-          return BigNumber.from(0);
-      }
+      setReadOnlyValues({
+        user: {
+          address: _account ? utils.getAddress(_account) : undefined,
+          votingWeight: await getVotingWeight(),
+        },
+        dao: !node.daoAddress
+          ? null // if there is no DAO connected, we return null for this
+          : {
+              isAzorius:
+                governance.type === GovernanceSelectionType.AZORIUS_ERC20 ||
+                governance.type === GovernanceSelectionType.AZORIUS_ERC721,
+            },
+      });
     };
 
-    return {
-      user: {
-        address: _account ? utils.getAddress(_account) : undefined,
-        votingWeight: votingWeight(),
-      },
-      dao: !node.daoAddress
-        ? null // if there is no DAO connected, we return null for this
-        : {
-            isAzorius:
-              governance.type === GovernanceSelectionType.AZORIUS_ERC20 ||
-              governance.type === GovernanceSelectionType.AZORIUS_ERC721,
-          },
-    } as ReadOnlyState;
-  }, []);
+    loadReadOnlyValues();
+  }, [node, governance, _account, signerOrProvider]);
   return { readOnlyValues };
 };
