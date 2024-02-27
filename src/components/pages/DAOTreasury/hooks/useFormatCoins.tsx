0b037598db3740521532c37ab116b3b4ea258dc6
Kirill Klimenko <klimenkomud@gmail.com>
2024-02-28 00:56:13 +0100

    Implement fetching coin prices from CoinGecko

--- src/components/pages/DAOTreasury/hooks/useFormatCoins.tsx
+++ src/components/pages/DAOTreasury/hooks/useFormatCoins.tsx
@@ -1,5 +1,7 @@
 import { SafeBalanceUsdResponse } from '@safe-global/safe-service-client';
-import { ethers } from 'ethers';
+import { BigNumber, ethers } from 'ethers';
+import { useEffect, useState } from 'react';
+import useCoinGeckoAPI from '../../../../providers/App/hooks/useCoinGeckoAPI';
 import { useNetworkConfig } from '../../../../providers/NetworkConfig/NetworkConfigProvider';
 import { formatCoin, formatUSD } from '../../../../utils/numberFormats';
 
@@ -17,27 +19,55 @@ export interface TokenDisplayData {
 
 export function useFormatCoins(assets: SafeBalanceUsdResponse[]) {
   const { nativeTokenSymbol, nativeTokenIcon } = useNetworkConfig();
-  let totalFiatValue = 0;
-  let displayData: TokenDisplayData[] = [];
-  for (let i = 0; i < assets.length; i++) {
-    let asset = assets[i];
-    if (asset.balance === '0') continue;
-    totalFiatValue += Number(asset.fiatBalance);
-    let symbol = asset.token === null ? nativeTokenSymbol : asset.token.symbol;
-    const formatted: TokenDisplayData = {
-      iconUri: asset.token === null ? nativeTokenIcon : asset.token.logoUri,
-      address: asset.tokenAddress === null ? ethers.constants.AddressZero : asset.tokenAddress,
-      truncatedCoinTotal: formatCoin(asset.balance, true, asset?.token?.decimals, symbol),
-      fiatValue: Number(asset.fiatBalance),
-      symbol: symbol,
-      fiatConversion: `1 ${symbol} = ${formatUSD(Number(asset.fiatConversion))}`,
-      fullCoinTotal: formatCoin(asset.balance, false, asset?.token?.decimals, symbol),
-      fiatDisplayValue: formatUSD(Number(asset.fiatBalance)),
-      rawValue: asset.balance,
-    };
-    displayData.push(formatted);
-  }
-  displayData.sort((a, b) => b.fiatValue - a.fiatValue); // sort by USD value
+  const [totalFiatValue, setTotalFiatValue] = useState(0);
+  const [displayData, setDisplayData] = useState<TokenDisplayData[]>([]);
+  const { getTokenPrices } = useCoinGeckoAPI();
+
+  useEffect(() => {
+    async function loadDisplayData() {
+      let newTotalFiatValue = 0;
+      let newDisplayData = [];
+      const tokenPrices = await getTokenPrices(assets);
+      for (let i = 0; i < assets.length; i++) {
+        let asset = assets[i];
+        if (asset.balance === '0') continue;
+        const tokenPrice = asset.tokenAddress
+          ? tokenPrices[asset.tokenAddress.toLowerCase()]?.usd
+          : tokenPrices.ethereum?.usd;
+
+        let tokenFiatBalance = 0;
+        if (tokenPrice && asset.balance) {
+          const numerator = 1000000000;
+          tokenFiatBalance =
+            BigNumber.from(asset.balance)
+              .mul(Math.round(tokenPrice * numerator)) // We'll be loosing precision with super small prices like for meme coins. But that shouldn't be awfully off - maybe up to 1%
+              .div(BigNumber.from(10).pow(asset.token?.decimals || 18))
+              .toNumber() / numerator;
+          newTotalFiatValue += tokenFiatBalance;
+        }
+
+        let symbol = asset.token === null ? nativeTokenSymbol : asset.token.symbol;
+        const formatted: TokenDisplayData = {
+          iconUri: asset.token === null ? nativeTokenIcon : asset.token.logoUri,
+          address: asset.tokenAddress === null ? ethers.constants.AddressZero : asset.tokenAddress,
+          truncatedCoinTotal: formatCoin(asset.balance, true, asset?.token?.decimals, symbol),
+          fiatValue: tokenFiatBalance,
+          symbol: symbol,
+          fiatConversion: tokenPrice ? `1 ${symbol} = ${formatUSD(tokenPrice)}` : 'N/A',
+          fullCoinTotal: formatCoin(asset.balance, false, asset?.token?.decimals, symbol),
+          fiatDisplayValue: formatUSD(tokenFiatBalance),
+          rawValue: asset.balance,
+        };
+        newDisplayData.push(formatted);
+      }
+      newDisplayData.sort((a, b) => b.fiatValue - a.fiatValue); // sort by USD value
+      setTotalFiatValue(newTotalFiatValue);
+      setDisplayData(newDisplayData);
+    }
+
+    loadDisplayData();
+  }, [assets, nativeTokenIcon, nativeTokenSymbol, getTokenPrices]);
+
   return {
     totalFiatValue,
     displayData,
