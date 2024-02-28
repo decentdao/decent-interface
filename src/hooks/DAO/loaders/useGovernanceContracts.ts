09833514449193a2b9b60e4dfd5244577fb01879
David Colon <38386583+Da-Colon@users.noreply.github.com>
2024-02-27 21:43:06 -0500

    loading key no longer needed - The key and other additional dependencies was causing additional renders

--- src/hooks/DAO/loaders/useGovernanceContracts.ts
+++ src/hooks/DAO/loaders/useGovernanceContracts.ts
@@ -6,7 +6,7 @@ import {
   LinearERC721Voting,
 } from '@fractal-framework/fractal-contracts';
 import { ethers } from 'ethers';
-import { useCallback, useEffect, useRef, useState } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
 import { LockRelease, LockRelease__factory } from '../../../assets/typechain-types/dcnt';
 import { useFractal } from '../../../providers/App/AppProvider';
 import { GovernanceContractAction } from '../../../providers/App/governanceContracts/action';
@@ -31,179 +31,163 @@ export const useGovernanceContracts = () => {
     action,
   } = useFractal();
   const { getZodiacModuleProxyMasterCopyData } = useMasterCopy();
-
   const provider = useEthersProvider();
   const signerOrProvider = useSignerOrProvider();
-  const {
-    network: { chainId },
-  } = provider;
 
-  const [lastChainId, setLastChainId] = useState<number>();
+  const { fractalModules, isModulesLoaded, daoAddress } = node;
 
   const loadGovernanceContracts = useCallback(async () => {
-    const { fractalModules } = node;
-
     const azoriusModule = getAzoriusModuleFromModules(fractalModules);
     const azoriusModuleContract = azoriusModule?.moduleContract as Azorius;
 
-    if (node.isModulesLoaded) {
-      if (!!azoriusModuleContract) {
-        const azoriusContract = {
-          asProvider: fractalAzoriusMasterCopyContract.asProvider.attach(
-            azoriusModuleContract.address
-          ),
-          asSigner: fractalAzoriusMasterCopyContract.asSigner.attach(azoriusModuleContract.address),
+    if (!!azoriusModuleContract) {
+      const azoriusContract = {
+        asProvider: fractalAzoriusMasterCopyContract.asProvider.attach(
+          azoriusModuleContract.address
+        ),
+        asSigner: fractalAzoriusMasterCopyContract.asSigner.attach(azoriusModuleContract.address),
+      };
+
+      let votingContractMasterCopyAddress: string | undefined;
+      let govTokenAddress: string | undefined;
+
+      let ozLinearVotingContract: ContractConnection<LinearERC20Voting> | undefined;
+      let erc721LinearVotingContract: ContractConnection<LinearERC721Voting> | undefined;
+      let tokenContract: ContractConnection<VotesERC20 | VotesERC20Wrapper> | undefined;
+      let underlyingTokenAddress: string | undefined;
+      let lockReleaseContract: ContractConnection<LockRelease> | null = null;
+
+      // @dev assumes the first strategy is the voting contract
+      const votingContractAddress = (
+        await azoriusContract.asProvider.getStrategies(
+          '0x0000000000000000000000000000000000000001',
+          0
+        )
+      )[1];
+
+      let isOzLinearVoting,
+        isOzLinearVotingERC721 = false;
+      if (!votingContractMasterCopyAddress) {
+        const masterCopyData = await getZodiacModuleProxyMasterCopyData(votingContractAddress);
+        isOzLinearVoting = masterCopyData.isOzLinearVoting;
+        isOzLinearVotingERC721 = masterCopyData.isOzLinearVotingERC721;
+      }
+
+      if (isOzLinearVoting) {
+        ozLinearVotingContract = {
+          asSigner: linearVotingMasterCopyContract.asSigner.attach(votingContractAddress!),
+          asProvider: linearVotingMasterCopyContract.asProvider.attach(votingContractAddress!),
         };
+      } else if (isOzLinearVotingERC721) {
+        erc721LinearVotingContract = {
+          asSigner: linearVotingERC721MasterCopyContract.asSigner.attach(votingContractAddress!),
+          asProvider: linearVotingERC721MasterCopyContract.asProvider.attach(
+            votingContractAddress!
+          ),
+        };
+      }
 
-        let votingContractMasterCopyAddress: string | undefined;
-        let govTokenAddress: string | undefined;
+      if (ozLinearVotingContract) {
+        govTokenAddress = await ozLinearVotingContract.asProvider.governanceToken();
+        const possibleERC20Wrapper =
+          votesERC20WrapperMasterCopyContract.asProvider.attach(govTokenAddress);
+        underlyingTokenAddress = await possibleERC20Wrapper.underlying().catch(() => {
+          // if the underlying token is not an ERC20Wrapper, this will throw an error,
+          // so we catch it and return undefined
+          return undefined;
+        });
+        const possibleLockRelease = new ethers.Contract(
+          govTokenAddress,
+          LockRelease__factory.abi,
+          provider
+        ) as LockRelease;
 
-        let ozLinearVotingContract: ContractConnection<LinearERC20Voting> | undefined;
-        let erc721LinearVotingContract: ContractConnection<LinearERC721Voting> | undefined;
-        let tokenContract: ContractConnection<VotesERC20 | VotesERC20Wrapper> | undefined;
-        let underlyingTokenAddress: string | undefined;
-        let lockReleaseContract: ContractConnection<LockRelease> | null = null;
+        const lockedToken = await possibleLockRelease.token().catch(() => {
+          // if the underlying token is not an ERC20Wrapper, this will throw an error,
+          // so we catch it and return undefined
+          return undefined;
+        });
 
-        // @dev assumes the first strategy is the voting contract
-        const votingContractAddress = (
-          await azoriusContract.asProvider.getStrategies(
-            '0x0000000000000000000000000000000000000001',
-            0
-          )
-        )[1];
-
-        let isOzLinearVoting,
-          isOzLinearVotingERC721 = false;
-        if (!votingContractMasterCopyAddress) {
-          const masterCopyData = await getZodiacModuleProxyMasterCopyData(votingContractAddress);
-          isOzLinearVoting = masterCopyData.isOzLinearVoting;
-          isOzLinearVotingERC721 = masterCopyData.isOzLinearVotingERC721;
-        }
-
-        if (isOzLinearVoting) {
-          ozLinearVotingContract = {
-            asSigner: linearVotingMasterCopyContract.asSigner.attach(votingContractAddress!),
-            asProvider: linearVotingMasterCopyContract.asProvider.attach(votingContractAddress!),
+        if (lockedToken) {
+          lockReleaseContract = {
+            asSigner: LockRelease__factory.connect(govTokenAddress, signerOrProvider),
+            asProvider: LockRelease__factory.connect(govTokenAddress, provider),
           };
-        } else if (isOzLinearVotingERC721) {
-          erc721LinearVotingContract = {
-            asSigner: linearVotingERC721MasterCopyContract.asSigner.attach(votingContractAddress!),
-            asProvider: linearVotingERC721MasterCopyContract.asProvider.attach(
-              votingContractAddress!
-            ),
+          tokenContract = {
+            asSigner: votesERC20WrapperMasterCopyContract.asSigner.attach(lockedToken),
+            asProvider: votesERC20WrapperMasterCopyContract.asProvider.attach(lockedToken),
           };
-        }
-
-        if (ozLinearVotingContract) {
-          if (!govTokenAddress) {
-            govTokenAddress = await ozLinearVotingContract.asProvider.governanceToken();
-          }
-          const possibleERC20Wrapper =
-            votesERC20WrapperMasterCopyContract.asProvider.attach(govTokenAddress);
-          underlyingTokenAddress = await possibleERC20Wrapper.underlying().catch(() => {
-            // if the underlying token is not an ERC20Wrapper, this will throw an error,
-            // so we catch it and return undefined
-            return undefined;
-          });
-          if (underlyingTokenAddress) {
-            const possibleLockRelease = new ethers.Contract(
-              govTokenAddress,
-              LockRelease__factory.abi,
-              signerOrProvider
-            ) as LockRelease;
-
-            const lockedToken = await possibleLockRelease.token().catch(() => {
-              // if the underlying token is not an ERC20Wrapper, this will throw an error,
-              // so we catch it and return undefined
-              return undefined;
-            });
-
-            if (lockedToken) {
-              lockReleaseContract = {
-                asSigner: LockRelease__factory.connect(govTokenAddress, signerOrProvider),
-                asProvider: LockRelease__factory.connect(govTokenAddress, provider),
-              };
-              tokenContract = {
-                asSigner: votesERC20WrapperMasterCopyContract.asSigner.attach(lockedToken),
-                asProvider: votesERC20WrapperMasterCopyContract.asProvider.attach(lockedToken),
-              };
-            }
-          } else if (!underlyingTokenAddress) {
-            tokenContract = {
-              asSigner: votesTokenMasterCopyContract.asSigner.attach(govTokenAddress),
-              asProvider: votesTokenMasterCopyContract.asProvider.attach(govTokenAddress),
-            };
-          } else {
-            tokenContract = {
-              asSigner: votesERC20WrapperMasterCopyContract.asSigner.attach(govTokenAddress),
-              asProvider: votesERC20WrapperMasterCopyContract.asProvider.attach(govTokenAddress),
-            };
-          }
-        }
-        if (!!ozLinearVotingContract && !!tokenContract) {
-          action.dispatch({
-            type: GovernanceContractAction.SET_GOVERNANCE_CONTRACT,
-            payload: {
-              ozLinearVotingContract,
-              erc721LinearVotingContract: null,
-              azoriusContract,
-              tokenContract,
-              underlyingTokenAddress,
-              lockReleaseContract,
-            },
-          });
-        } else if (!!erc721LinearVotingContract) {
-          currentValidAddress.current = node.daoAddress;
-          action.dispatch({
-            type: GovernanceContractAction.SET_GOVERNANCE_CONTRACT,
-            payload: {
-              ozLinearVotingContract: null,
-              lockReleaseContract: null,
-              erc721LinearVotingContract,
-              azoriusContract,
-              tokenContract: null,
-              underlyingTokenAddress,
-            },
-          });
-        } else if (!!erc721LinearVotingContract) {
-          currentValidAddress.current = node.daoAddress;
-          action.dispatch({
-            type: GovernanceContractAction.SET_GOVERNANCE_CONTRACT,
-            payload: {
-              ozLinearVotingContract: null,
-              erc721LinearVotingContract,
-              azoriusContract,
-              tokenContract: null,
-              underlyingTokenAddress,
-              lockReleaseContract: null,
-            },
-          });
+        } else if (!underlyingTokenAddress) {
+          tokenContract = {
+            asSigner: votesTokenMasterCopyContract.asSigner.attach(govTokenAddress),
+            asProvider: votesTokenMasterCopyContract.asProvider.attach(govTokenAddress),
+          };
         } else {
-          currentValidAddress.current = node.daoAddress;
-          action.dispatch({
-            type: GovernanceContractAction.SET_GOVERNANCE_CONTRACT,
-            payload: {
-              ozLinearVotingContract: null,
-              erc721LinearVotingContract: null,
-              azoriusContract: null,
-              tokenContract: null,
-              lockReleaseContract: null,
-            },
-          });
+          tokenContract = {
+            asSigner: votesERC20WrapperMasterCopyContract.asSigner.attach(govTokenAddress),
+            asProvider: votesERC20WrapperMasterCopyContract.asProvider.attach(govTokenAddress),
+          };
         }
-      } else {
-        currentValidAddress.current = node.daoAddress;
+      }
+      if (!!ozLinearVotingContract && !!tokenContract) {
+        action.dispatch({
+          type: GovernanceContractAction.SET_GOVERNANCE_CONTRACT,
+          payload: {
+            ozLinearVotingContract,
+            erc721LinearVotingContract: null,
+            azoriusContract,
+            tokenContract,
+            underlyingTokenAddress,
+            lockReleaseContract,
+          },
+        });
+      } else if (!!erc721LinearVotingContract) {
         action.dispatch({
           type: GovernanceContractAction.SET_GOVERNANCE_CONTRACT,
           payload: {
             ozLinearVotingContract: null,
-            azoriusContract: null,
+            lockReleaseContract: null,
+            erc721LinearVotingContract,
+            azoriusContract,
+            tokenContract: null,
+            underlyingTokenAddress,
+          },
+        });
+      } else if (!!erc721LinearVotingContract) {
+        action.dispatch({
+          type: GovernanceContractAction.SET_GOVERNANCE_CONTRACT,
+          payload: {
+            ozLinearVotingContract: null,
+            erc721LinearVotingContract,
+            azoriusContract,
+            tokenContract: null,
+            underlyingTokenAddress,
+            lockReleaseContract: null,
+          },
+        });
+      } else {
+        action.dispatch({
+          type: GovernanceContractAction.SET_GOVERNANCE_CONTRACT,
+          payload: {
+            ozLinearVotingContract: null,
             erc721LinearVotingContract: null,
+            azoriusContract: null,
             tokenContract: null,
             lockReleaseContract: null,
           },
         });
       }
+    } else {
+      action.dispatch({
+        type: GovernanceContractAction.SET_GOVERNANCE_CONTRACT,
+        payload: {
+          ozLinearVotingContract: null,
+          azoriusContract: null,
+          erc721LinearVotingContract: null,
+          tokenContract: null,
+          lockReleaseContract: null,
+        },
+      });
     }
   }, [
     action,
@@ -212,21 +196,16 @@ export const useGovernanceContracts = () => {
     votesTokenMasterCopyContract,
     fractalAzoriusMasterCopyContract,
     votesERC20WrapperMasterCopyContract,
-    node,
     getZodiacModuleProxyMasterCopyData,
     linearVotingMasterCopyContract,
     linearVotingERC721MasterCopyContract,
+    fractalModules,
   ]);
 
   useEffect(() => {
-    if (
-      (!!node.daoAddress &&
-        node.isModulesLoaded &&
-        node.daoAddress !== currentValidAddress.current) ||
-      lastChainId !== chainId
-    ) {
-      setLastChainId(chainId);
+    if (currentValidAddress.current !== daoAddress && isModulesLoaded) {
       loadGovernanceContracts();
+      currentValidAddress.current = daoAddress;
     }
-  }, [node, loadGovernanceContracts, lastChainId, chainId]);
+  }, [isModulesLoaded, loadGovernanceContracts, daoAddress]);
 };
