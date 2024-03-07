215d2be30cb4372a0ad62e3cbb103f2616aa889a
David Colon <38386583+Da-Colon@users.noreply.github.com>
2024-03-07 15:32:46 -0500

    updates to next config

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ module.exports = {
 
     return config;
   },
+  output: 'export',
   images: {
     dangerouslyAllowSVG: true,
     contentDispositionType: 'attachment',
@@ -15,6 +16,5 @@ module.exports = {
   },
   experimental: {
     esmExternals: false,
-    appDir: true,
   },
 };
