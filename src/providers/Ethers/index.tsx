ed46ee252b72015c3555786c574b99ae4e6887a8
Adam Gall <galladamj@gmail.com>
2024-03-07 16:13:27 -0500

    Pretty fix

--- src/providers/Ethers/index.tsx
+++ src/providers/Ethers/index.tsx
@@ -33,8 +33,8 @@ export default function EthersContextProvider({ children }: { children: ReactNod
     if (transport.type === 'fallback') {
       return new providers.FallbackProvider(
         (transport.transports as ReturnType<Transport>[]).map(
-          ({ value }) => new providers.JsonRpcProvider(value?.url, network)
-        )
+          ({ value }) => new providers.JsonRpcProvider(value?.url, network),
+        ),
       );
     }
     return new providers.JsonRpcProvider(transport.url, network);
