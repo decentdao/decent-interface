fa5512140353ce12805330bb41322c337c8733bf
David Colon <38386583+Da-Colon@users.noreply.github.com>
2024-03-11 13:11:05 -0400

    add env typing

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,7 +2,18 @@
 /// <reference types="vite/client" />
 
 interface ImportMetaEnv {
+  readonly VITE_APP_NAME: string;
+  readonly VITE_APP_ALCHEMY_MAINNET_API_KEY: string;
+  readonly VITE_APP_ALCHEMY_SEPOLIA_API_KEY: string;
+  readonly VITE_APP_ETHERSCAN_API_KEY: string;
 
+  readonly VITE_APP_INFURA_IPFS_API_KEY: string;
+  readonly VITE_APP_INFURA_IPFS_API_SECRET: string;
+  readonly VITE_APP_SENTRY_DSN_URL: string;
+  readonly VITE_APP_SHUTTER_EON_PUBKEY: string;
+
+  readonly VITE_APP_SITE_URL: string;
+  readonly VITE_APP_WALLET_CONNECT_PROJECT_ID: string;
 }
 
 interface ImportMeta {
