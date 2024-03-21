107446b00cfa217ec500ac116e1dd5b272f3d298
Kirill Klimenko <klimenkomud@gmail.com>
2024-03-21 16:30:10 +0100

    Refactor DAOController to not display InvalidChain

--- src/pages/DAOController.tsx
+++ src/pages/DAOController.tsx
@@ -9,7 +9,6 @@ import useDAOMetadata from '../hooks/DAO/useDAOMetadata';
 import { useFractal } from '../providers/App/AppProvider';
 import {
   disconnectedChain,
-  supportedChains,
 } from '../providers/NetworkConfig/NetworkConfigProvider';
 
 function InvalidSafe() {
@@ -35,35 +34,10 @@ function InvalidSafe() {
   );
 }
 
-function InvalidChain() {
-  const { t } = useTranslation(['common', 'menu']);
-  const supportedChainNames = supportedChains.map(c => c.name).join(', ');
-  return (
-    <Center
-      padding="3rem"
-      textColor="grayscale.100"
-    >
-      <VStack>
-        <Text
-          paddingTop="3rem"
-          textStyle="text-6xl-mono-regular"
-        >
-          {t('errorSentryFallbackTitle')}
-        </Text>
-        <Text paddingBottom="1rem">{t('invalidChain')}</Text>
-        <Text paddingBottom="1rem">
-          {t('toastSwitchChain', { ns: 'menu', chainNames: supportedChainNames })}
-        </Text>
-      </VStack>
-    </Center>
-  );
-}
-
 export default function DAOController() {
   const { node } = useFractal();
   const { nodeLoading, errorLoading } = useDAOController();
   const daoMetadata = useDAOMetadata();
-  const { chain } = useAccount();
   const activeTheme = useMemo(() => {
     if (daoMetadata && daoMetadata.bodyBackground) {
       return extendTheme({
@@ -89,40 +63,18 @@ export default function DAOController() {
 
   const validSafe = node.safe;
   let display;
-  const childrenDisplay = (
-    <ChakraProvider theme={activeTheme}>
-      <Outlet />
-    </ChakraProvider>
-  );
 
   if (import.meta.env.VITE_APP_TESTING_ENVIRONMENT) {
-    display = childrenDisplay;
-  } else if (!chain) {
-    // if we're disconnected
-    if (nodeLoading || validSafe || !errorLoading) {
-      display = <Outlet />;
-    } else {
-      display = <InvalidSafe />;
-    }
+    display = (
+      <ChakraProvider theme={activeTheme}>
+        <Outlet />
+      </ChakraProvider>
+    );
+  } else if (nodeLoading || validSafe || !errorLoading) {
+    display = <Outlet />;
   } else {
-    // if we're connected
-    const invalidChain = !supportedChains.map(c => c.chainId).includes(chain.id);
-    if (invalidChain) {
-      display = <InvalidChain />;
-    } else if (nodeLoading || validSafe || !errorLoading) {
-      display = <Outlet />;
-    } else {
-      display = <InvalidSafe />;
-    }
+    display = <InvalidSafe />;
   }
-  return (
-    <>
-      <title>
-        {node?.daoName
-          ? `${node.daoName} | ${import.meta.env.VITE_APP_NAME}`
-          : import.meta.env.VITE_APP_NAME}
-      </title>
-      {display}
-    </>
-  );
+
+  return display
 }
