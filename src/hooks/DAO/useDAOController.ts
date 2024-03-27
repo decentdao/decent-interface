69cdc16b5efffef995fda9a17a86922d955ccba8
David Colon <38386583+Da-Colon@users.noreply.github.com>
2024-03-27 13:10:00 -0400

    add check if DAOAddress has changed

--- src/hooks/DAO/useDAOController.ts
+++ src/hooks/DAO/useDAOController.ts
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { useParams } from 'react-router-dom';
 import { useFractal } from '../../providers/App/AppProvider';
 import { useERC20Claim } from './loaders/governance/useERC20Claim';
@@ -11,6 +11,7 @@ import { useFractalTreasury } from './loaders/useFractalTreasury';
 import { useGovernanceContracts } from './loaders/useGovernanceContracts';
 
 export default function useDAOController() {
+  const currentDAOAddress = useRef<string | null>(null);
   const { daoAddress } = useParams();
   const {
     node: {
@@ -19,12 +20,16 @@ export default function useDAOController() {
     action,
   } = useFractal();
   useEffect(() => {
-    if (!daoAddress) {
+    if (!daoAddress || daoAddress !== currentDAOAddress.current) {
       action.resetDAO();
+      currentDAOAddress.current = null;
+    }
+    if (daoAddress && !currentDAOAddress.current) {
+      currentDAOAddress.current = daoAddress;
     }
   }, [action, daoAddress]);
 
-  const { nodeLoading, errorLoading } = useFractalNode({ daoAddress });
+  const { nodeLoading, errorLoading } = useFractalNode({ daoAddress: currentDAOAddress.current || '' });
   useGovernanceContracts();
   useFractalGuardContracts({});
   useFractalFreeze({ parentSafeAddress: parentAddress });
