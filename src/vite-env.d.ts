f6feb2ba8622031afce4c38ff83e2db2d85e54dd
Adam Gall <galladamj@gmail.com>
2024-03-29 16:31:25 -0400

    Reorder and clean up existing .env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -4,17 +4,22 @@
 interface ImportMetaEnv {
   readonly VITE_APP_NAME: string;
   readonly VITE_APP_GIT_HASH: string;
-  readonly VITE_APP_ALCHEMY_POLYGON_API_KEY: string;
+
   readonly VITE_APP_ALCHEMY_MAINNET_API_KEY: string;
+  readonly VITE_APP_ALCHEMY_POLYGON_API_KEY: string;
   readonly VITE_APP_ALCHEMY_SEPOLIA_API_KEY: string;
+
   readonly VITE_APP_ETHERSCAN_API_KEY: string;
 
   readonly VITE_APP_INFURA_IPFS_API_KEY: string;
   readonly VITE_APP_INFURA_IPFS_API_SECRET: string;
+
   readonly VITE_APP_SENTRY_DSN_URL: string;
+
   readonly VITE_APP_SHUTTER_EON_PUBKEY: string;
 
   readonly VITE_APP_SITE_URL: string;
+
   readonly VITE_APP_WALLET_CONNECT_PROJECT_ID: string;
 }
 
