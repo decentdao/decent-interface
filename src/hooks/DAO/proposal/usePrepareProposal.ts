4fa08c6a7f5c0a701d291254a08d6c4179fc9b99
Kirill Klimenko <klimenkomud@gmail.com>
2024-04-09 13:21:19 +0200

    If signature is empty - do not call encodeFunction but simply return 0x as calldata

--- src/hooks/DAO/proposal/usePrepareProposal.ts
+++ src/hooks/DAO/proposal/usePrepareProposal.ts
@@ -10,19 +10,22 @@ export function usePrepareProposal() {
     async (values: CreateProposalForm) => {
       const { transactions, proposalMetadata } = values;
       const transactionsWithEncoding = transactions.map(tx => {
+        const signature = tx.parameters.map(parameter => parameter.signature.trim()).join(', ');
+        const parameters = tx.parameters
+        .map(parameter =>
+          isValidUrl(parameter.value!.trim())
+            ? encodeURIComponent(parameter.value!.trim()) // If parameter.value is valid URL with special symbols like ":" or "?" - decoding might fail, thus we need to encode URL
+            : parameter.value!.trim(),
+        )
+        .join(', ')
+
         return {
           ...tx,
-          encodedFunctionData: encodeFunction(
+          calldata: signature ? encodeFunction(
             tx.functionName,
-            tx.parameters.map(parameter => parameter.signature.trim()).join(', '),
-            tx.parameters
-              .map(parameter =>
-                isValidUrl(parameter.value!.trim())
-                  ? encodeURIComponent(parameter.value!.trim()) // If parameter.value is valid URL with special symbols like ":" or "?" - decoding might fail, thus we need to encode URL
-                  : parameter.value!.trim(),
-              )
-              .join(', '),
-          ),
+            signature,
+            parameters,
+          ) : '0x',
         };
       });
       const targets = await Promise.all(
@@ -33,11 +36,12 @@ export function usePrepareProposal() {
           return tx.targetAddress;
         }),
       );
+
       return {
         targets,
         values: transactionsWithEncoding.map(transaction => transaction.ethValue.bigNumberValue!),
         calldatas: transactionsWithEncoding.map(
-          transaction => transaction.encodedFunctionData || '',
+          transaction => transaction.calldata || '',
         ),
         metaData: {
           title: proposalMetadata.title,
