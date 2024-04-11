aed1c47d0899cdb2dae9d505b2b606f2ecc6774a
Adam Gall <galladamj@gmail.com>
2024-04-11 13:58:18 -0400

    Render the zero address token link

--- src/components/ui/links/EtherscanLinkERC20.tsx
+++ src/components/ui/links/EtherscanLinkERC20.tsx
@@ -1,5 +1,4 @@
 import { LinkProps } from '@chakra-ui/react';
-import { zeroAddress } from 'viem';
 import { useNetworkConfig } from '../../../providers/NetworkConfig/NetworkConfigProvider';
 import EtherscanLinkBase from './EtherscanLinkBase';
 
@@ -11,7 +10,7 @@ interface Props extends LinkProps {
 function EtherscanLinkERC20({ address, children, ...rest }: Props) {
   const { etherscanBaseURL } = useNetworkConfig();
 
-  if (!address || address === zeroAddress) {
+  if (!address) {
     return null;
   }
 
