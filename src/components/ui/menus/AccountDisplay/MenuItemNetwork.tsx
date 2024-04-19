0de9cadd6fdd0b4cdcef2784778b17d0d50bf8a8
David Colon <38386583+Da-Colon@users.noreply.github.com>
2024-04-18 22:32:30 -0400

    Update Wallet Menu with new styles

--- src/components/ui/menus/AccountDisplay/MenuItemNetwork.tsx
+++ src/components/ui/menus/AccountDisplay/MenuItemNetwork.tsx
@@ -1,4 +1,5 @@
 import { Box, Flex, Select, Text } from '@chakra-ui/react';
+import { CaretDown } from '@phosphor-icons/react';
 import { useTranslation } from 'react-i18next';
 import { useSwitchChain } from 'wagmi';
 import {
@@ -17,26 +18,52 @@ export function MenuItemNetwork() {
   return (
     <Box
       cursor="default"
-      p="1rem 1.5rem"
+      bg="neutral-3"
+      pt="0.5rem"
     >
       <Flex
         direction="column"
         gap="2"
+        // @dev Not my favorite way to do this, but it's the only way to target the icon and adjust the margin without using a custom theme
+        sx={{
+          '.chakra-select__icon-wrapper': {
+            'margin-right': '0.5rem',
+          },
+          '*:hover': {
+            'color': 'lilac--1',
+          },
+        }}
       >
         <Text
-          textStyle="text-sm-sans-regular"
-          color="chocolate.100"
+          px="0.5rem"
+          textStyle="helper-text-small"
+          color="neutral-7"
         >
           {t('network')}
         </Text>
         <Select
-          mt={4}
-          width="100%"
-          bgColor="#2c2c2c"
-          borderColor="#4d4d4d"
+          h="3rem"
+          bgColor="transparent"
+          border="none"
+          _hover={{
+            bg: 'white-alpha-04',
+          }}
+          _focus={{
+            border: 'none',
+            bg: 'white-alpha-08',
+            color: 'lilac--2',
+            boxShadow: 'none',
+          }}
+          _active={{
+            border: 'none',
+            bg: 'white-alpha-08',
+            color: 'lilac--2',
+          }}
           data-testid="accountMenu-network"
-          rounded="sm"
           cursor="pointer"
+          iconSize="1.5rem"
+          icon={<CaretDown />}
+          borderRadius="4px"
           onChange={async e => {
             e.preventDefault();
             switchChain({ chainId: Number(e.target.value) });
