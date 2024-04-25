1ae81bb3b5159f46fe92aaf306bc4968d3996243
Kellar <kellar.base09@gmail.com>
2024-04-25 16:52:31 +0100

    reskin DisplayAddress

--- src/components/ui/links/DisplayAddress.tsx
+++ src/components/ui/links/DisplayAddress.tsx
@@ -1,5 +1,5 @@
 import { HStack, Text } from '@chakra-ui/react';
-import { ArrowAngleUp } from '@decent-org/fractal-ui';
+import { ArrowUpRight } from '@phosphor-icons/react';
 import { ReactNode } from 'react';
 import useDisplayName from '../../../hooks/utils/useDisplayName';
 import EtherscanLinkAddress from './EtherscanLinkAddress';
@@ -15,14 +15,22 @@ export function DisplayAddress({
 }) {
   const displayAddress = useDisplayName(address, truncate);
   return (
-    <EtherscanLinkAddress address={address}>
-      <HStack
-        color="gold.500"
-        _hover={{ color: 'gold.500-hover' }}
-        textStyle="text-base-sm-regular"
-      >
-        <Text>{children || displayAddress.displayName}</Text>
-        <ArrowAngleUp />
+    <EtherscanLinkAddress
+      border="1px"
+      borderColor="neutral-3"
+      bg="neutral-3"
+      px="0.75rem"
+      borderRadius="625rem"
+      _hover={{
+        bg: 'neutral-4',
+        borderColor: 'lilac-0',
+      }}
+      address={address}
+      textAlign="center"
+    >
+      <HStack color="lilac-0">
+        <Text pb="1px">{children || displayAddress.displayName}</Text>
+        <ArrowUpRight />
       </HStack>
     </EtherscanLinkAddress>
   );
