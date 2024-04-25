5f11113a5c8a922b5b8a7384aff80456792a6f5f
Kellar <kellar.base09@gmail.com>
2024-04-25 17:57:27 +0100

    Reskin modal base

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,6 +1,6 @@
 export const HEADER_HEIGHT = '4.5rem';
 export const CONTENT_HEIGHT = `calc(100vh - ${HEADER_HEIGHT})`;
-export const BACKGROUND_SEMI_TRANSPARENT = 'green'; // This will make it obvious anywhere this happens to be used that a reskin is in order. After all usages are replaced, this constant should be removed.
+export const BACKGROUND_SEMI_TRANSPARENT = '#10041480';
 /**
  * Max width for most informational Tooltips. However we don't add max width
  * to some Tooltips that shouldn't wrap no matter how long, such as token amounts.
