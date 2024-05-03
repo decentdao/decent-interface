b31eb9a3dad9854e8b7eb9cbb207406876749bd4
Kellar <kellar.base09@gmail.com>
2024-05-03 12:11:24 +0100

    Throw reskin colours at error page. Actual reskin TBD.

--- src/components/ui/utils/ErrorFallback.tsx
+++ src/components/ui/utils/ErrorFallback.tsx
@@ -1,8 +1,7 @@
-import { Box, Button, Center, Flex, Text, Link } from '@chakra-ui/react';
-import { Alert } from '@decent-org/fractal-ui';
-import { Trans, useTranslation } from 'react-i18next';
+import { Box, Button, Center, Flex, Icon, Text } from '@chakra-ui/react';
+import { Warning } from "@phosphor-icons/react";
+import { useTranslation } from 'react-i18next';
 import { CONTENT_HEIGHT } from '../../../constants/common';
-import { URL_DISCORD } from '../../../constants/url';
 import { InfoBox } from '../containers/InfoBox';
 
 export function ErrorFallback() {
@@ -15,24 +14,17 @@ export function ErrorFallback() {
             alignItems="center"
             direction="column"
             padding="1rem"
+            gap="1.5rem"
           >
-            <Alert />
+            <Icon as={Warning} color="red-0" boxSize="4rem" />
             <Text
-              textStyle="text-2xl-mono-regular"
-              marginTop="1.5rem"
-              marginBottom="1.5rem"
+              textStyle="display-xl"
+              color="lilac-0"
             >
               {t('errorSentryFallbackTitle')}
             </Text>
-            <Text marginBottom="1.5rem">
-              <Trans i18nKey="errorSentryFallbackMessage">
-                placeholder
-                <Link
-                  color="celery-0"
-                  href={URL_DISCORD}
-                  target="_blank"
-                />
-              </Trans>
+            <Text textStyle="body-base">
+              {t('errorSentryFallbackMessage')}
             </Text>
             <Button onClick={() => window.location.reload()}>{t('reload')}</Button>
           </Flex>
