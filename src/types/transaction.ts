2e2fc23bc9ba20479bdaee419ff5554ba7045011
Kirill Klimenko <klimenkomud@gmail.com>
2024-05-04 01:31:57 +0200

    Remove SafeAPITransaction interface

--- src/types/transaction.ts
+++ src/types/transaction.ts
@@ -42,16 +42,3 @@ export type DecodedTxParam = {
   type: string;
   value: string;
 };
-
-export interface SafeAPITransaction {
-  to: Address; //'<checksummed address>'
-  value: bigint; // Value in wei
-  data: Hex; // '<0x prefixed hex string>'
-  operation: 0 | 1; // 0 CALL, 1 DELEGATE_CALL
-  gasToken: string; // Token address (hold by the Safe) to be used as a refund to the sender, if `null` is Ether
-  safeTxGas: bigint; // Max gas to use in the transaction
-  baseGas: bigint; // Gas costs not related to the transaction execution (signature check, refund payment...)
-  gasPrice: bigint; // Gas price used for the refund calculation
-  refundReceiver: string; //Address of receiver of gas payment (or `null` if tx.origin)
-  nonce: number; // Nonce of the Safe, transaction cannot be executed until Safe's nonce is not equal to this nonce
-}
