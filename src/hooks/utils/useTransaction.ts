d942e119820f22031645ca900763582e3f87d01d
Adam Gall <galladamj@gmail.com>
2024-05-04 22:37:28 -0400

    Create a second useTransaction hook for these viem-style contract calls

--- src/hooks/utils/useTransaction.ts
+++ src/hooks/utils/useTransaction.ts
@@ -2,12 +2,24 @@ import { ContractReceipt, ethers } from 'ethers';
 import React, { useCallback, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { toast } from 'react-toastify';
+import { Hash, TransactionReceipt } from 'viem';
+import { usePublicClient } from 'wagmi';
 import { logError } from '../../helpers/errorLogging';
 
 interface ProviderRpcError extends Error {
   message: string;
   code: number;
   data?: any;
+}
+
+interface ContractCallParamsViem {
+  contractFn: () => Promise<Hash>;
+  pendingMessage: string;
+  failedMessage: string;
+  successMessage: string;
+  failedCallback?: () => void;
+  successCallback?: (txReceipt: TransactionReceipt) => void;
+  completedCallback?: () => void;
 }
 
 interface ContractCallParams {
@@ -23,6 +35,8 @@ interface ContractCallParams {
 const useTransaction = () => {
   const [pending, setPending] = useState(false);
   const { t } = useTranslation(['transaction', 'common']);
+  const publicClient = usePublicClient();
+
   const contractCall = useCallback(
     (params: ContractCallParams) => {
       let toastId: React.ReactText;
@@ -36,7 +50,7 @@ const useTransaction = () => {
       setPending(true);
       params
         .contractFn()
-        .then((txResponse: ethers.ContractTransaction) => {
+        .then(txResponse => {
           return Promise.all([txResponse.wait(), toastId]);
         })
         .then(([txReceipt, toastID]) => {
@@ -76,7 +90,65 @@ const useTransaction = () => {
     [t],
   );
 
-  return [contractCall, pending] as const;
+  const contractCallViem = useCallback(
+    (params: ContractCallParamsViem) => {
+      if (!publicClient) return;
+
+      let toastId: React.ReactText;
+      toastId = toast(params.pendingMessage, {
+        autoClose: false,
+        closeOnClick: false,
+        draggable: false,
+        closeButton: false,
+        progress: 1,
+      });
+      setPending(true);
+      params
+        .contractFn()
+        .then(txReceipt => {
+          return Promise.all([
+            publicClient.waitForTransactionReceipt({ hash: txReceipt }),
+            toastId,
+          ]);
+        })
+        .then(([txReceipt, toastID]) => {
+          toast.dismiss(toastID);
+          if (txReceipt.status === 'reverted') {
+            toast.error(params.failedMessage);
+            if (params.failedCallback) params.failedCallback();
+          } else if (txReceipt.status === 'success') {
+            toast(params.successMessage);
+            if (params.successCallback) params.successCallback(txReceipt);
+          } else {
+            toast.error(t('errorTransactionUnknown'));
+            if (params.failedCallback) params.failedCallback();
+          }
+          if (params.completedCallback) params.completedCallback();
+
+          // Give the block event emitter a couple seconds to play the latest
+          // block on the app state, before informing app that the transaction
+          // is completed.
+          setTimeout(() => {
+            setPending(false);
+          }, 2000);
+        })
+        .catch((error: ProviderRpcError) => {
+          logError(error);
+          toast.dismiss(toastId);
+          setPending(false);
+
+          if (error.code === 4001) {
+            toast.error(t('errorUserDeniedTransaction'));
+            return;
+          }
+
+          toast.error(t('errorGeneral', { ns: 'common' }));
+        });
+    },
+    [t, publicClient],
+  );
+
+  return [contractCall, pending, contractCallViem] as const;
 };
 
 export { useTransaction };
