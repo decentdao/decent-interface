855ae270d30a1d65ccc1a596667bcd313a09ff40
Adam Gall <galladamj@gmail.com>
2024-05-10 13:51:19 -0400

    Offset fixed-positioned nav bar from top

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -1,5 +1,5 @@
 export const HEADER_HEIGHT = '4.5rem';
-export const FOOTER_HEIGHT = '4.125rem';
+const FOOTER_HEIGHT = '4.125rem';
 export const CONTENT_HEIGHT = `calc(100vh - ${HEADER_HEIGHT} - ${FOOTER_HEIGHT})`;
 
 // TODO get these into `decent-ui` repo
