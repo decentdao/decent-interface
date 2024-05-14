c7742111e061d25d607ec31e05ac862993e81022
David Colon <38386583+Da-Colon@users.noreply.github.com>
2024-05-14 18:19:08 -0400

    remove external link from AddressCopier - prevent a nested link situation on hierarchy

--- src/components/ui/links/AddressCopier.tsx
+++ src/components/ui/links/AddressCopier.tsx
@@ -1,9 +1,7 @@
-import { Text, Icon, LinkProps } from '@chakra-ui/react';
+import { Text, Icon, LinkProps, Box } from '@chakra-ui/react';
 import { CopySimple } from '@phosphor-icons/react';
 import { useCopyText } from '../../../hooks/utils/useCopyText';
 import useDisplayName from '../../../hooks/utils/useDisplayName';
-import { useNetworkConfig } from '../../../providers/NetworkConfig/NetworkConfigProvider';
-import ExternalLink from './ExternalLink';
 
 interface AddressCopierProps extends LinkProps {
   address: string;
@@ -14,15 +12,22 @@ interface AddressCopierProps extends LinkProps {
  * icon to the right of it.
  */
 export default function AddressCopier({ address, ...rest }: AddressCopierProps) {
-  const { etherscanBaseURL } = useNetworkConfig();
   const { accountSubstring } = useDisplayName(address);
   const copyToClipboard = useCopyText();
 
   return (
-    <ExternalLink
-      href={`${etherscanBaseURL}/address/${address}`}
+    <Box
+      color="celery-0"
+      padding="0.25rem 0.75rem"
+      gap="0.25rem"
+      borderRadius="625rem"
+      borderColor="transparent"
+      borderWidth="1px"
+      _hover={{ bg: 'celery--6', borderColor: 'celery--6' }}
+      _active={{ bg: 'celery--6', borderWidth: '1px', borderColor: 'celery--5' }}
       onClick={e => {
         e.preventDefault();
+        e.stopPropagation();
         copyToClipboard(address);
       }}
       width="fit-content"
@@ -38,6 +43,6 @@ export default function AddressCopier({ address, ...rest }: AddressCopierProps) 
           as={CopySimple}
         />
       </Text>
-    </ExternalLink>
+    </Box>
   );
 }
