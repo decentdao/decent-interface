4c3d83bbda7d60bb85a5a46a1b89d64538236682
Adam Gall <galladamj@gmail.com>
2024-05-14 16:04:09 -0400

    Remove git hash from app and env var creation on run/build

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -5,7 +5,6 @@ interface ImportMetaEnv {
   readonly PACKAGE_VERSION: string;
 
   readonly VITE_APP_NAME: string;
-  readonly VITE_APP_GIT_HASH: string;
 
   readonly VITE_APP_ALCHEMY_MAINNET_API_KEY: string;
   readonly VITE_APP_ALCHEMY_POLYGON_API_KEY: string;
