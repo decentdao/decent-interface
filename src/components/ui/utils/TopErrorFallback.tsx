c0827347cd03a56f557c6fb0076a0b92e718d36e
Kellar <kellar.base09@gmail.com>
2024-05-15 14:44:49 +0100

    Widescreen design error page

--- src/components/ui/utils/TopErrorFallback.tsx
+++ src/components/ui/utils/TopErrorFallback.tsx
@@ -1,5 +1,4 @@
-import { Box, Button, Center, Flex, Text, Icon } from '@chakra-ui/react';
-import { Warning } from '@phosphor-icons/react';
+import { Box, Button, Center, Flex, Text, Image } from '@chakra-ui/react';
 import { useTranslation } from 'react-i18next';
 import { CONTENT_HEIGHT } from '../../../constants/common';
 
@@ -7,38 +6,40 @@ export function TopErrorFallback() {
   const { t } = useTranslation();
   return (
     <Center h={CONTENT_HEIGHT}>
-      <Box maxWidth="fit-content">
-        <Box
-          minWidth="100%"
-          h="100%"
-          minHeight="10.6rem"
-          bg="black.900-semi-transparent"
-          p="1rem"
-          borderRadius="0.5rem"
-        >
+      <Box
+        borderRadius="0.5rem"
+        overflow="hidden"
+        bg="neutral-2"
+        my="12.8rem"
+        ml="9.44rem"
+        mr="9.5rem"
+      >
+        <Flex ml="3.06rem">
           <Flex
-            alignItems="center"
-            direction="column"
-            padding="1rem"
-            gap="1.5rem"
+            mt="5.37rem"
+            mr="7.16rem"
+            flexDir="column"
+            gap="0.5rem"
           >
-            <Icon
-              as={Warning}
-              boxSize="2rem"
-            />
-            <Text
-              textStyle="display-2xl"
-              marginTop="1.5rem"
-              marginBottom="1.5rem"
+            <Text textStyle="display-4xl">{t('errorSentryFallbackTitle')}</Text>
+            <Text textStyle="display-xl">{t('errorSentryFallbackMessage')}</Text>
+            <Button
+              mr="5.84rem"
+              mt="1rem"
+              onClick={() => window.location.reload()}
             >
-              {t('errorSentryFallbackTitle')}
-            </Text>
-            <Text textStyle="body-base">
-              {t('errorSentryFallbackMessage')}
-            </Text>
-            <Button onClick={() => window.location.reload()}>{t('reload')}</Button>
+              {t('goHome')}
+            </Button>
           </Flex>
-        </Box>
+          <Image
+            mt="1.56rem"
+            mr="3.19rem"
+            p="0"
+            fit="cover"
+            src="/images/tools.svg"
+            alt="Something went wrong"
+          />
+        </Flex>
       </Box>
     </Center>
   );
