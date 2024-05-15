cf5ee6d29a82418298f29e73c1295d554b61d8fe
Kellar <kellar.base09@gmail.com>
2024-05-15 18:44:33 +0100

    Add responsiveness to error page

--- src/components/ui/utils/TopErrorFallback.tsx
+++ src/components/ui/utils/TopErrorFallback.tsx
@@ -1,46 +1,141 @@
-import { Box, Button, Center, Flex, Text, Image } from '@chakra-ui/react';
+import { Button, Center, Flex, Text, Image } from '@chakra-ui/react';
+import * as Sentry from '@sentry/react';
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { CONTENT_HEIGHT } from '../../../constants/common';
+import { useNavigate } from 'react-router-dom';
+import { BASE_ROUTES } from '../../../constants/routes';
+import CeleryButtonWithIcon from './CeleryButtonWithIcon';
+
+// Hook to get and react to the current window width. Using this because the default breakpoints
+// are not enough for the design of the error page.
+//
+// This hook could potentially be moved to a separate file and used in other components during
+// responsive design push on pages that require pixel-level logic.
+const useWindowWidth = () => {
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowWidth(window.innerWidth);
+    };
+
+    window.addEventListener('resize', handleResize);
+
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  return windowWidth;
+};
 
 export function TopErrorFallback() {
   const { t } = useTranslation();
+  const windowWidth = useWindowWidth();
+  const navigate = useNavigate();
+
+  // These numbers are not arbitrary.
+  const largeWidth = 1324;
+  const smallWidth = 1060;
+
+  // Default large, medium, and small breakpoints for the error page.
+  const isWidthLarge = windowWidth >= largeWidth;
+  const isWidthMedium = windowWidth < largeWidth;
+  const isWidthSmall = windowWidth < smallWidth;
+
+  const actionButtons = (
+    <Flex
+      mx={isWidthSmall ? '4rem' : 0}
+      flexDir="column"
+    >
+      <Button
+        mr={isWidthSmall ? 0 : '5.84rem'}
+        mt="1rem"
+        onClick={() => navigate(BASE_ROUTES.landing)}
+      >
+        {t('goHome')}
+      </Button>
+      <CeleryButtonWithIcon
+        mr={isWidthSmall ? 0 : '5.84rem'}
+        text="Submit Crash Report"
+        mt="1rem"
+        onClick={() => {
+          // todo: doesn't seem to work, not sure what to do
+          Sentry.showReportDialog({
+            eventId: '1234',
+            title: "It looks like we're having issues",
+            subtitle:
+              "Decent devs have been notified. If you'd like to help, tell us what happened below.",
+            labelName: 'NAME',
+            labelEmail: 'EMAIL',
+            labelComments: 'WHAT HAPPENED?',
+            labelSubmit: 'Submit Crash Report',
+            successMessage: 'Thank you for your feedback!',
+          });
+        }}
+      />
+    </Flex>
+  );
+
+  const useMainImageFullsize = windowWidth > 1170;
+
+  const errorImageMain = (
+    <Image
+      mt={useMainImageFullsize ? '1.56rem' : 0}
+      mb={useMainImageFullsize ? 0 : '4rem'}
+      mr="3.19rem"
+      width={useMainImageFullsize ? 'auto' : '20rem'}
+      p="0"
+      fit="fill"
+      src="/images/tools.svg"
+      alt="Something went wrong"
+    />
+  );
+
+  const errorImageSmall = (
+    <Image
+      w="auto"
+      my="1.5rem"
+      mx="6rem"
+      fit="fill"
+      src="/images/tools.svg"
+      alt="Something went wrong"
+    />
+  );
+
   return (
-    <Center h={CONTENT_HEIGHT}>
-      <Box
+    <Center>
+      <Flex
         borderRadius="0.5rem"
         overflow="hidden"
         bg="neutral-2"
-        my="12.8rem"
-        ml="9.44rem"
-        mr="9.5rem"
+        w="auto"
+        mt={isWidthLarge ? '12.81rem' : isWidthMedium ? '6rem' : '3.38rem'}
+        mb={isWidthSmall ? '3.38rem' : '12.81rem'}
+        ml={isWidthSmall ? 0 : '9.44rem'}
+        mr={isWidthSmall ? 0 : '9.5rem'}
       >
-        <Flex ml="3.06rem">
-          <Flex
-            mt="5.37rem"
-            mr="7.16rem"
-            flexDir="column"
-            gap="0.5rem"
-          >
-            <Text textStyle="display-4xl">{t('errorSentryFallbackTitle')}</Text>
-            <Text textStyle="display-xl">{t('errorSentryFallbackMessage')}</Text>
-            <Button
-              mr="5.84rem"
-              mt="1rem"
-              onClick={() => window.location.reload()}
-            >
-              {t('goHome')}
-            </Button>
-          </Flex>
-          <Image
-            mt="1.56rem"
-            mr="3.19rem"
-            p="0"
-            fit="cover"
-            src="/images/tools.svg"
-            alt="Something went wrong"
-          />
+        {/* ERROR TEXT */}
+        <Flex
+          mt={isWidthLarge ? '5.37rem' : isWidthMedium ? '3.37rem' : 0}
+          mb={isWidthSmall ? '5.37rem' : isWidthMedium ? '3rem' : 0}
+          ml={isWidthSmall ? 0 : '3.06rem'}
+          mr={isWidthSmall ? 0 : '7.16rem'}
+          p={isWidthSmall ? '1.25rem' : 0}
+          flexDir="column"
+          gap="0.5rem"
+        >
+          <Text textStyle="display-4xl">{t('errorSentryFallbackTitle')}</Text>
+          <Text textStyle="display-xl">{t('errorSentryFallbackMessage')}</Text>
+
+          {isWidthSmall && errorImageSmall}
+
+          {actionButtons}
         </Flex>
-      </Box>
+
+        {/* RIGHT IMAGE */}
+        {!isWidthSmall && errorImageMain}
+      </Flex>
     </Center>
   );
 }
