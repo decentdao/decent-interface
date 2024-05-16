1e1b7f047c9cd6193a2c5b6f6a5a572d68c7fee4
Adam Gall <galladamj@gmail.com>
2024-05-16 14:05:01 -0400

    Add the DAO Search component to new Home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
 import { Flex } from '@chakra-ui/react';
 import { DecentSignature } from '@decent-org/fractal-ui';
 import { useEffect } from 'react';
+import { DAOSearch } from '../components/ui/menus/DAOSearch';
 import { useFractal } from '../providers/App/AppProvider';
 
 export default function HomePage() {
@@ -27,6 +28,7 @@ export default function HomePage() {
         height="auto"
         width="8rem"
       />
+      <DAOSearch />
     </Flex>
   );
 }
