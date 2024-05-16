0c684033c01063e2093662a3c072c06eb11526ff
Adam Gall <galladamj@gmail.com>
2024-05-16 14:03:09 -0400

    Remove everything else from Home page, except for Decent Signature logo

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,15 +1,9 @@
-import { VStack, Text, Flex } from '@chakra-ui/react';
+import { Flex } from '@chakra-ui/react';
+import { DecentSignature } from '@decent-org/fractal-ui';
 import { useEffect } from 'react';
-import { useTranslation } from 'react-i18next';
-import CTABox from '../components/pages/AppHome/CTABox';
-import { CreateDAOIllustration, DocsIllustration } from '../components/ui/icons/Icons';
-import { CONTENT_MAXW } from '../constants/common';
-import { BASE_ROUTES } from '../constants/routes';
-import { URL_DOCS } from '../constants/url';
 import { useFractal } from '../providers/App/AppProvider';
 
 export default function HomePage() {
-  const { t } = useTranslation('home');
   const {
     node: { daoAddress },
     action,
@@ -22,41 +16,16 @@ export default function HomePage() {
   }, [daoAddress, action]);
 
   return (
-    <Flex flexWrap="wrap">
-      <VStack
-        maxW={CONTENT_MAXW}
-        flex="1"
-        alignItems="start"
-        pt="3rem"
-        px="1.5rem"
-      >
-        <Text textStyle="display-xl">{t('homeTitle')}</Text>
-        <Flex
-          mt="1.5rem"
-          gap="1.5rem"
-          w="full"
-          flexWrap="wrap"
-        >
-          <CTABox
-            titleKey="createCTA"
-            descKey="createDesc"
-            to={BASE_ROUTES.create}
-            Icon={<CreateDAOIllustration />}
-            titleColor="cosmic-nebula-0"
-            descColor="cosmic-nebula-0"
-            bg="lilac-0"
-          />
-          <CTABox
-            to={URL_DOCS}
-            target="_blank"
-            titleKey="docsCTA"
-            descKey="docsDesc"
-            Icon={<DocsIllustration />}
-            pr={0}
-            iconContainerJustify="flex-end"
-          />
-        </Flex>
-      </VStack>
+    <Flex
+      direction="column"
+      gap="1rem"
+      alignItems="center"
+    >
+      <DecentSignature
+        marginTop="2rem"
+        height="auto"
+        width="8rem"
+      />
     </Flex>
   );
 }
