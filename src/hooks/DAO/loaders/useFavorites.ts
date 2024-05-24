8800715b0465f0d389f2ec20d94c1090f8ab5749
David Colon <38386583+Da-Colon@users.noreply.github.com>
2024-05-24 10:26:00 -0400

    clean up

--- src/hooks/DAO/loaders/useFavorites.ts
+++ src/hooks/DAO/loaders/useFavorites.ts
@@ -1,53 +1,26 @@
-import { useCallback, useEffect, useMemo, useState } from 'react';
+import { useState } from 'react';
 import { getAddress } from 'viem';
-import { useFractal } from '../../../providers/App/AppProvider';
 import { CacheKeys, CacheExpiry } from '../../utils/cache/cacheDefaults';
 import { useLocalStorage } from '../../utils/cache/useLocalStorage';
 
-/**
- * handles loading favorites data into Fractal state
- */
 export const useAccountFavorites = () => {
-  const {
-    node: { daoAddress },
-  } = useFractal();
-
   const { setValue, getValue } = useLocalStorage();
-  const [favoritesList, setFavoritesList] = useState<string[]>([]);
+  const [favoritesList, setFavoritesList] = useState<string[]>(getValue(CacheKeys.FAVORITES));
 
-  useEffect(() => {
-    setFavoritesList(getValue(CacheKeys.FAVORITES));
-  }, [getValue]);
+  const toggleFavorite = (address: string) => {
+    const normalizedAddress = getAddress(address);
+    const favorites: string[] = getValue(CacheKeys.FAVORITES);
+    let updatedFavorites: string[] = [];
 
-  /**
-   * @returns favorited status of loaded safe
-   */
-  const isConnectedFavorited = useMemo(
-    () => (!daoAddress ? false : favoritesList.includes(daoAddress)),
-    [daoAddress, favoritesList],
-  );
+    if (favorites.includes(normalizedAddress)) {
+      updatedFavorites = favorites.filter(favorite => favorite !== normalizedAddress);
+    } else {
+      updatedFavorites = favorites.concat([normalizedAddress]);
+    }
 
-  /**
-   * toggles the given address's favorited status
-   *
-   * if IS favorited it will remove from the address from local storage and state favorite lists
-   * if NOT favorited, the address will be saved to local storage and state favorites
-   */
-  const toggleFavorite = useCallback(
-    (address: string) => {
-      const normalizedAddress = getAddress(address);
-      const favorites: string[] = getValue(CacheKeys.FAVORITES);
-      let updatedFavorites: string[] = [];
-      if (favorites.includes(normalizedAddress)) {
-        updatedFavorites = favorites.filter(favorite => favorite !== normalizedAddress);
-      } else {
-        updatedFavorites = favorites.concat([normalizedAddress]);
-      }
-      setValue(CacheKeys.FAVORITES, updatedFavorites, CacheExpiry.NEVER);
-      setFavoritesList(updatedFavorites);
-    },
-    [getValue, setValue],
-  );
+    setValue(CacheKeys.FAVORITES, updatedFavorites, CacheExpiry.NEVER);
+    setFavoritesList(updatedFavorites);
+  };
 
-  return { favoritesList, isConnectedFavorited, toggleFavorite };
+  return { favoritesList, toggleFavorite };
 };
