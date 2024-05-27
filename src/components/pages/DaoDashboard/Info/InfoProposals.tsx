979d84a9ca80b6804870c84c3b698fe5c58c8c57
Adam Gall <galladamj@gmail.com>
2024-05-27 16:51:35 -0400

    Move code into a regular function

--- src/components/pages/DaoDashboard/Info/InfoProposals.tsx
+++ src/components/pages/DaoDashboard/Info/InfoProposals.tsx
@@ -1,6 +1,5 @@
 import { Box, Flex, Text } from '@chakra-ui/react';
 import { Scroll } from '@phosphor-icons/react';
-import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useFractal } from '../../../../providers/App/AppProvider';
 import {
@@ -16,40 +15,40 @@ const isSnapshotProposal = (proposal: FractalProposal) => {
   return !!snapshotProposal.snapshotProposalId;
 };
 
+const totalProposals = (proposals: FractalProposal[] | null, type: GovernanceType | undefined) => {
+  if (!proposals) {
+    return '...';
+  }
+
+  switch (type) {
+    case GovernanceType.MULTISIG: {
+      return proposals.length.toString();
+    }
+    case GovernanceType.AZORIUS_ERC20:
+    case GovernanceType.AZORIUS_ERC721: {
+      const nonSnapshotProposals = proposals.filter(proposal => !isSnapshotProposal(proposal));
+      const highestNonSnapshotProposalId = nonSnapshotProposals.reduce((p, c) => {
+        const propId = Number(c.proposalId);
+        if (propId > p) {
+          return propId;
+        }
+        return p;
+      }, 0);
+      const snapshotProposalCount = proposals.length - nonSnapshotProposals.length;
+      return (highestNonSnapshotProposalId + snapshotProposalCount).toString();
+    }
+    default: {
+      return '0';
+    }
+  }
+};
+
 export function InfoProposals() {
   const { t } = useTranslation('dashboard');
   const {
     node: { daoAddress },
     governance: { proposals, type },
   } = useFractal();
-
-  const totalProposals = useMemo(() => {
-    if (!proposals) {
-      return '...';
-    }
-
-    switch (type) {
-      case GovernanceType.MULTISIG: {
-        return proposals.length.toString();
-      }
-      case GovernanceType.AZORIUS_ERC20:
-      case GovernanceType.AZORIUS_ERC721: {
-        const nonSnapshotProposals = proposals.filter(proposal => !isSnapshotProposal(proposal));
-        const highestNonSnapshotProposalId = nonSnapshotProposals.reduce((p, c) => {
-          const propId = Number(c.proposalId);
-          if (propId > p) {
-            return propId;
-          }
-          return p;
-        }, 0);
-        const snapshotProposalCount = proposals.length - nonSnapshotProposals.length;
-        return (highestNonSnapshotProposalId + snapshotProposalCount).toString();
-      }
-      default: {
-        return '0';
-      }
-    }
-  }, [proposals, type]);
 
   if (!daoAddress || !type) {
     return (
@@ -90,7 +89,7 @@ export function InfoProposals() {
         gap="0.5rem"
       >
         <Text color="neutral-7">{t('titleTotal')}</Text>
-        <Text>{totalProposals}</Text>
+        <Text>{totalProposals(proposals, type)}</Text>
       </Flex>
       <Flex
         alignItems="center"
