47aeefe75777955669a4f95b7bfeea9f09bd7ab2
Adam Gall <galladamj@gmail.com>
2024-05-29 10:06:14 -0400

    Remove unused imports

--- src/providers/NetworkConfig/networks/mainnet.ts
+++ src/providers/NetworkConfig/networks/mainnet.ts
@@ -19,7 +19,7 @@ import {
   getSafeL2SingletonDeployment,
   getCompatibilityFallbackHandlerDeployment,
 } from '@safe-global/safe-deployments';
-import { getAddress, zeroAddress } from 'viem';
+import { getAddress } from 'viem';
 import { mainnet } from 'wagmi/chains';
 import { GovernanceType } from '../../../types';
 import { NetworkConfig } from '../../../types/network';
