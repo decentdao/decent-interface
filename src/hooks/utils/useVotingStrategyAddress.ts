45045dd7a9c053c412562511cc8abe07f0114207
Adam Gall <galladamj@gmail.com>
2024-05-29 10:12:52 -0400

    Wrap `getVotingStrategyAddress` in a `useCallback`

--- src/hooks/utils/useVotingStrategyAddress.ts
+++ src/hooks/utils/useVotingStrategyAddress.ts
@@ -1,3 +1,4 @@
+import { useCallback } from 'react';
 import { Address, getAddress, getContract } from 'viem';
 import { usePublicClient } from 'wagmi';
 import AzoriusAbi from '../../assets/abi/Azorius';
@@ -14,39 +15,42 @@ const useVotingStrategyAddress = () => {
   const safeAPI = useSafeAPI();
   const lookupModules = useFractalModules();
 
-  const getVotingStrategyAddress = async (safeAddress?: Address) => {
-    let azoriusModule: FractalModuleData | undefined;
+  const getVotingStrategyAddress = useCallback(
+    async (safeAddress?: Address) => {
+      let azoriusModule: FractalModuleData | undefined;
 
-    if (safeAddress) {
-      if (!safeAPI) {
-        throw new Error('Safe API not ready');
+      if (safeAddress) {
+        if (!safeAPI) {
+          throw new Error('Safe API not ready');
+        }
+        const safeInfo = await safeAPI.getSafeInfo(getAddress(safeAddress));
+        const safeModules = await lookupModules(safeInfo.modules);
+        azoriusModule = getAzoriusModuleFromModules(safeModules);
+        if (!azoriusModule) return;
+      } else {
+        azoriusModule = getAzoriusModuleFromModules(node.fractalModules);
       }
-      const safeInfo = await safeAPI.getSafeInfo(getAddress(safeAddress));
-      const safeModules = await lookupModules(safeInfo.modules);
-      azoriusModule = getAzoriusModuleFromModules(safeModules);
-      if (!azoriusModule) return;
-    } else {
-      azoriusModule = getAzoriusModuleFromModules(node.fractalModules);
-    }
 
-    if (!azoriusModule) {
-      throw new Error('Azorius module not found');
-    }
+      if (!azoriusModule) {
+        throw new Error('Azorius module not found');
+      }
 
-    if (!publicClient) {
-      throw new Error('Public client undefined');
-    }
+      if (!publicClient) {
+        throw new Error('Public client undefined');
+      }
 
-    const azoriusContract = getContract({
-      abi: AzoriusAbi,
-      address: getAddress(azoriusModule.moduleAddress),
-      client: publicClient,
-    });
+      const azoriusContract = getContract({
+        abi: AzoriusAbi,
+        address: getAddress(azoriusModule.moduleAddress),
+        client: publicClient,
+      });
 
-    // @dev assumes the first strategy is the voting contract
-    const strategies = await azoriusContract.read.getStrategies([SENTINEL_ADDRESS, 0n]);
-    return strategies[1];
-  };
+      // @dev assumes the first strategy is the voting contract
+      const strategies = await azoriusContract.read.getStrategies([SENTINEL_ADDRESS, 0n]);
+      return strategies[1];
+    },
+    [lookupModules, node.fractalModules, publicClient, safeAPI],
+  );
 
   return { getVotingStrategyAddress };
 };
