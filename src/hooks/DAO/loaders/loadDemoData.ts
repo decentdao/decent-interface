d9cbd2f333f7b61e967a0f3f385da3aa2a0e2b29
Adam Gall <galladamj@gmail.com>
2024-05-30 15:05:33 -0400

    Refactor to support correct behavior when setting hierarchy

--- src/hooks/DAO/loaders/loadDemoData.ts
+++ src/hooks/DAO/loaders/loadDemoData.ts
@@ -10,47 +10,66 @@ type DemoData = {
   [networkId: number]: NetworkData;
 };
 
-const decentDAO = '0xB98d45F9021D71E6Fc30b43FD37FB3b1Bf12c064';
-const coordinapeDAO = '0xd2786bEc22F4F2500E0dd4E42cE7c9dceBB5Ff93';
+const decentDAOAddress = '0xB98d45F9021D71E6Fc30b43FD37FB3b1Bf12c064';
+const coordinapeDAOAddress = '0xd2786bEc22F4F2500E0dd4E42cE7c9dceBB5Ff93';
+
+const coordinapeDAO = {
+  id: coordinapeDAOAddress,
+  address: coordinapeDAOAddress,
+  parentAddress: decentDAOAddress,
+  name: 'Coordinape DAO',
+  snapshotENS: 'the-co-dao.eth',
+  proposalTemplatesHash: '',
+  hierarchy: [],
+};
+
+const decentDAO = {
+  id: decentDAOAddress,
+  address: decentDAOAddress,
+  parentAddress: '',
+  name: 'Decent DAO',
+  snapshotENS: 'decent-dao.eth',
+  proposalTemplatesHash: '',
+  hierarchy: [coordinapeDAO],
+};
 
 export const demoData: DemoData = {
   [mainnet.id]: {
-    [decentDAO]: {
-      id: decentDAO,
-      address: decentDAO,
-      parentAddress: '',
-      name: 'Decent DAO',
-      snapshotENS: 'decent-dao.eth',
-      proposalTemplatesHash: '',
-      hierarchy: [],
-    },
-    [coordinapeDAO]: {
-      id: coordinapeDAO,
-      address: coordinapeDAO,
-      parentAddress: '',
-      name: 'Coordinape DAO',
-      snapshotENS: 'the-co-dao.eth',
-      proposalTemplatesHash: '',
-      hierarchy: [],
-    },
+    [decentDAOAddress]: decentDAO,
+    [coordinapeDAOAddress]: coordinapeDAO,
   },
 };
 
 export const loadDemoData = (
   chain: Chain,
   daoAddress: Address,
-  result: { data?: DAOQueryQuery | undefined },
-): { data: DAOQueryQuery | undefined } => {
-  let demo = demoData[chain.id][daoAddress];
-  if (result.data && result.data.daos[0]) {
-    demo = { ...demo, ...result.data.daos[0] };
+  result: { data?: DAOQueryQuery },
+) => {
+  const demo = demoData[chain.id][daoAddress];
+
+  if (demo === undefined) {
+    return result;
   }
 
-  const data: DAOQueryQuery = { daos: [] };
-
-  if (demo) {
-    data.daos = [demo];
+  if (result.data === undefined || result.data.daos[0] === undefined) {
+    return { data: { daos: [demo] } };
   }
 
-  return { data };
+  const onchain = result.data.daos[0];
+
+  return {
+    data: {
+      daos: [
+        {
+          id: daoAddress,
+          address: daoAddress,
+          parentAddress: onchain.parentAddress ?? demo.parentAddress,
+          name: onchain.name ?? demo.name,
+          snapshotENS: onchain.snapshotENS ?? demo.snapshotENS,
+          proposalTemplatesHash: onchain.proposalTemplatesHash ?? demo.proposalTemplatesHash,
+          hierarchy: onchain.hierarchy.length === 0 ? demo.hierarchy : onchain.hierarchy,
+        },
+      ],
+    },
+  };
 };
