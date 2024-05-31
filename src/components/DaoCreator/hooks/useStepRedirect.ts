46c97444c6cc51e7de6e4b98911f278868b689a8
Kirill Klimenko <klimenkomud@gmail.com>
2024-05-31 09:46:07 +0200

    Prettier

--- src/components/DaoCreator/hooks/useStepRedirect.ts
+++ src/components/DaoCreator/hooks/useStepRedirect.ts
@@ -1,17 +1,17 @@
-import { useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import { CreatorFormState } from "../../../types";
-import { scrollToTop } from "../../../utils/ui";
+import { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { CreatorFormState } from '../../../types';
+import { scrollToTop } from '../../../utils/ui';
 
 export default function useStepRedirect(values: CreatorFormState) {
-    const navigate = useNavigate();
-    useEffect(() => {
-        // @dev we might be also checking for other required values at given step
-        // However, only imaginable way how consequent step might be missing required data is from page reload
-        // Thus, we can actually rely on just missing daoName and governance
-        if (!values.essentials.daoName || !values.essentials.governance) {
-          navigate('/create/essentials', { replace: true });
-          scrollToTop();
-        }
-      }, [values.essentials.daoName, values.essentials.governance, navigate]);
+  const navigate = useNavigate();
+  useEffect(() => {
+    // @dev we might be also checking for other required values at given step
+    // However, only imaginable way how consequent step might be missing required data is from page reload
+    // Thus, we can actually rely on just missing daoName and governance
+    if (!values.essentials.daoName || !values.essentials.governance) {
+      navigate('/create/essentials', { replace: true });
+      scrollToTop();
+    }
+  }, [values.essentials.daoName, values.essentials.governance, navigate]);
 }
