7423cd716fb1233b44249adfd102da24f50c1f55
Kirill Klimenko <klimenkomud@gmail.com>
2024-05-31 09:45:48 +0200

    Add comment, explaining that I'm not lazy but just avoiding over-engineering

--- src/components/DaoCreator/hooks/useStepRedirect.ts
+++ src/components/DaoCreator/hooks/useStepRedirect.ts
@@ -6,6 +6,9 @@ import { scrollToTop } from "../../../utils/ui";
 export default function useStepRedirect(values: CreatorFormState) {
     const navigate = useNavigate();
     useEffect(() => {
+        // @dev we might be also checking for other required values at given step
+        // However, only imaginable way how consequent step might be missing required data is from page reload
+        // Thus, we can actually rely on just missing daoName and governance
         if (!values.essentials.daoName || !values.essentials.governance) {
           navigate('/create/essentials', { replace: true });
           scrollToTop();
