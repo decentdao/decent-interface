3bd48d9c3fd51063af53b932635f59ea606aa691
David Colon <38386583+Da-Colon@users.noreply.github.com>
2024-06-02 23:47:08 -0400

    update setValue and getValue functions to new key version - add versioning

--- src/hooks/utils/cache/useLocalStorage.ts
+++ src/hooks/utils/cache/useLocalStorage.ts
@@ -1,76 +1,47 @@
-import { useCallback } from 'react';
-import { useNetworkConfig } from '../../../providers/NetworkConfig/NetworkConfigProvider';
-import { CacheExpiry, IStorageValue, CACHE_DEFAULTS, keyInternal } from './cacheDefaults';
+import {
+  CacheExpiry,
+  IStorageValue,
+  FavoritesCacheKey,
+  MasterCacheKey,
+  CacheKey,
+  CURRENT_CACHE_VERSION,
+} from './cacheDefaults';
+type Key = FavoritesCacheKey | MasterCacheKey | Omit<CacheKey, 'version'>;
 
 export const setValue = (
-  key: string,
+  key: Key,
   value: any,
-  chainId: number,
   expirationMinutes: number = CacheExpiry.ONE_WEEK,
 ): void => {
   if (typeof window !== 'undefined') {
     const val: IStorageValue = {
       v: value,
       e:
-        expirationMinutes === CacheExpiry.NEVER
-          ? CacheExpiry.NEVER
-          : Date.now() + expirationMinutes * 60000,
+      expirationMinutes === CacheExpiry.NEVER
+      ? CacheExpiry.NEVER
+      : Date.now() + expirationMinutes * 60000,
     };
-    localStorage.setItem(keyInternal(chainId, key), JSON.stringify(val));
+    localStorage.setItem(JSON.stringify({ ...key, version: CURRENT_CACHE_VERSION }), JSON.stringify(val));
   }
 };
 
-export const getValue = (key: string, chainId: number): any => {
+export const getValue = (key: Key, version = CURRENT_CACHE_VERSION): any => {
   if (typeof window !== 'undefined') {
-    const rawVal = localStorage.getItem(keyInternal(chainId, key));
+    const rawVal = localStorage.getItem(JSON.stringify({ ...key, version }));
     if (rawVal) {
       const parsed: IStorageValue = JSON.parse(rawVal);
       if (parsed.e === CacheExpiry.NEVER) {
         return parsed.v;
       } else {
         if (parsed.e < Date.now()) {
-          localStorage.removeItem(keyInternal(chainId, key));
+          localStorage.removeItem(JSON.stringify(key));
           return null;
         } else {
           return parsed.v;
         }
       }
-    } else if (CACHE_DEFAULTS[key]) {
-      return CACHE_DEFAULTS[key];
     } else {
       return null;
     }
   }
-};
-
-/**
- * A hook which returns a getter and setter for local storage cache,
- * with an optional expiration (in minutes) param.
- *
- * Each value set/get is specific to the currently connected chainId.
- *
- * The default expiration is 1 week. Use CacheExpiry.NEVER to keep
- * the value cached indefinitely.
- *
- * All JSON parsing is done internally, you should only need to pass
- * the value, array, or object you would like to cache.
- */
-export const useLocalStorage = () => {
-  const { chain } = useNetworkConfig();
-
-  const set = useCallback(
-    (key: string, value: any, expirationMinutes: number = CacheExpiry.ONE_WEEK) => {
-      setValue(key, value, chain.id, expirationMinutes);
-    },
-    [chain],
-  );
-
-  const get = useCallback(
-    (key: string) => {
-      return getValue(key, chain.id);
-    },
-    [chain],
-  );
-
-  return { setValue: set, getValue: get };
 };
