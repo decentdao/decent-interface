614368397f253b6ad2c2b660f8cd9cf7cb6b8c0c
Kirill Klimenko <klimenkomud@gmail.com>
2024-06-10 15:48:53 +0200

    Prettier

--- src/hooks/DAO/useClawBack.ts
+++ src/hooks/DAO/useClawBack.ts
@@ -24,7 +24,9 @@ export default function useClawBack({ childSafeInfo, parentAddress }: IUseClawBa
 
   const handleClawBack = useCallback(async () => {
     if (childSafeInfo.daoAddress && parentAddress && safeAPI && provider) {
-      const childSafeBalance: { tokens: TokenBalance[] } = await getTokenBalances(childSafeInfo.daoAddress)
+      const childSafeBalance: { tokens: TokenBalance[] } = await getTokenBalances(
+        childSafeInfo.daoAddress,
+      );
 
       const santitizedParentAddress = getAddress(parentAddress);
       const parentSafeInfo = await safeAPI.getSafeData(santitizedParentAddress);
@@ -35,56 +37,58 @@ export default function useClawBack({ childSafeInfo, parentAddress }: IUseClawBa
         );
         const fractalModuleContract = fractalModule?.moduleContract as FractalModule;
         if (fractalModule) {
-          const transactions = childSafeBalance.tokens.filter(tokenBalance => !tokenBalance.possibleSpam).map(asset => {
-            if (!asset.tokenAddress) {
-              // Seems like we're operating with native coin i.e ETH
-              const txData = encodeAbiParameters(
-                parseAbiParameters('address, uint256, bytes, uint8'),
-                [parentAddress, BigInt(asset.balance), '0x', 0],
-              );
+          const transactions = childSafeBalance.tokens
+            .filter(tokenBalance => !tokenBalance.possibleSpam)
+            .map(asset => {
+              if (!asset.tokenAddress) {
+                // Seems like we're operating with native coin i.e ETH
+                const txData = encodeAbiParameters(
+                  parseAbiParameters('address, uint256, bytes, uint8'),
+                  [parentAddress, BigInt(asset.balance), '0x', 0],
+                );
 
-              const fractalModuleCalldata = fractalModuleContract.interface.encodeFunctionData(
-                'execTx',
-                [txData],
-              );
-              if (!isHex(fractalModuleCalldata)) {
-                throw new Error('Error encoding clawback call data');
+                const fractalModuleCalldata = fractalModuleContract.interface.encodeFunctionData(
+                  'execTx',
+                  [txData],
+                );
+                if (!isHex(fractalModuleCalldata)) {
+                  throw new Error('Error encoding clawback call data');
+                }
+                return {
+                  target: getAddress(fractalModuleContract.address),
+                  value: 0,
+                  calldata: fractalModuleCalldata,
+                };
+              } else {
+                const tokenContract = ERC20__factory.connect(asset.tokenAddress, provider);
+                const clawBackCalldata = tokenContract.interface.encodeFunctionData('transfer', [
+                  parentAddress,
+                  asset.balance,
+                ]);
+                if (!isHex(clawBackCalldata)) {
+                  throw new Error('Error encoding clawback call data');
+                }
+                const txData = encodeAbiParameters(
+                  parseAbiParameters('address, uint256, bytes, uint8'),
+                  [getAddress(asset.tokenAddress), 0n, clawBackCalldata, 0],
+                );
+
+                const fractalModuleCalldata = fractalModuleContract.interface.encodeFunctionData(
+                  'execTx',
+                  [txData],
+                );
+
+                if (!isHex(fractalModuleCalldata)) {
+                  throw new Error('Error encoding clawback call data');
+                }
+
+                return {
+                  target: getAddress(fractalModuleContract.address),
+                  value: 0,
+                  calldata: fractalModuleCalldata,
+                };
               }
-              return {
-                target: getAddress(fractalModuleContract.address),
-                value: 0,
-                calldata: fractalModuleCalldata,
-              };
-            } else {
-              const tokenContract = ERC20__factory.connect(asset.tokenAddress, provider);
-              const clawBackCalldata = tokenContract.interface.encodeFunctionData('transfer', [
-                parentAddress,
-                asset.balance,
-              ]);
-              if (!isHex(clawBackCalldata)) {
-                throw new Error('Error encoding clawback call data');
-              }
-              const txData = encodeAbiParameters(
-                parseAbiParameters('address, uint256, bytes, uint8'),
-                [getAddress(asset.tokenAddress), 0n, clawBackCalldata, 0],
-              );
-
-              const fractalModuleCalldata = fractalModuleContract.interface.encodeFunctionData(
-                'execTx',
-                [txData],
-              );
-
-              if (!isHex(fractalModuleCalldata)) {
-                throw new Error('Error encoding clawback call data');
-              }
-
-              return {
-                target: getAddress(fractalModuleContract.address),
-                value: 0,
-                calldata: fractalModuleCalldata,
-              };
-            }
-          });
+            });
 
           submitProposal({
             proposalData: {
@@ -108,7 +112,16 @@ export default function useClawBack({ childSafeInfo, parentAddress }: IUseClawBa
         }
       }
     }
-  }, [canUserCreateProposal, childSafeInfo, parentAddress, provider, submitProposal, t, safeAPI, getTokenBalances]);
+  }, [
+    canUserCreateProposal,
+    childSafeInfo,
+    parentAddress,
+    provider,
+    submitProposal,
+    t,
+    safeAPI,
+    getTokenBalances,
+  ]);
 
   return { handleClawBack };
 }
