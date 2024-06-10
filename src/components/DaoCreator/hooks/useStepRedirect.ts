6c293ed718ec611380400cc24d50a5c383cf2efb
Kirill Klimenko <klimenkomud@gmail.com>
2024-06-10 13:06:10 +0200

    Use current location in redirectToInitialStep instead of logic gates

--- src/components/DaoCreator/hooks/useStepRedirect.ts
+++ src/components/DaoCreator/hooks/useStepRedirect.ts
@@ -1,14 +1,10 @@
 import { useCallback, useEffect } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { BASE_ROUTES, DAO_ROUTES } from '../../../constants/routes';
-import { useFractal } from '../../../providers/App/AppProvider';
-import { useNetworkConfig } from '../../../providers/NetworkConfig/NetworkConfigProvider';
-import { CreatorFormState } from '../../../types';
+import { useNavigate, useLocation } from 'react-router-dom';
+import { CreatorFormState, CreatorSteps } from '../../../types';
 import { DAOCreateMode } from '../formComponents/EstablishEssentials';
 
 export default function useStepRedirect({
   values,
-  mode,
   redirectOnMount = true,
 }: {
   values: CreatorFormState;
@@ -16,20 +12,14 @@ export default function useStepRedirect({
   redirectOnMount?: boolean;
 }) {
   const navigate = useNavigate();
-  const { addressPrefix } = useNetworkConfig();
-  const {
-    node: { daoAddress },
-  } = useFractal();
+  const location = useLocation();
 
   const redirectToInitialStep = useCallback(() => {
-    if (mode === DAOCreateMode.ROOTDAO) {
-      navigate(BASE_ROUTES.create, { replace: true });
-    } else if (mode === DAOCreateMode.SUBDAO && daoAddress) {
-      navigate(DAO_ROUTES.newSubDao.relative(addressPrefix, daoAddress), { replace: true });
-    } else if (mode === DAOCreateMode.EDIT && daoAddress) {
-      navigate(DAO_ROUTES.modifyGovernance.relative(addressPrefix, daoAddress), { replace: true });
-    }
-  }, [navigate, mode, addressPrefix, daoAddress]);
+    const step = location.pathname.split('/').pop();
+    const redirectPath = `${location.pathname.replace(`${step}`, CreatorSteps.ESSENTIALS)}${location.search}`;
+    navigate(redirectPath, { replace: true });
+  }, [navigate, location]);
+
   useEffect(() => {
     // @dev we might be also checking for other required values at given step
     // However, only imaginable way how consequent step might be missing required data is from page reload
