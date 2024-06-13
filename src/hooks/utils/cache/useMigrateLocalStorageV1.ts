5f1d82eb15fb490122c0a74e47d54c617f930234
David Colon <38386583+Da-Colon@users.noreply.github.com>
2024-06-12 21:35:37 -0400

    extract to a testable function; - added conditionals to ensure proper handling of cache

--- src/hooks/utils/cache/useMigrateLocalStorageV1.ts
+++ src/hooks/utils/cache/useMigrateLocalStorageV1.ts
@@ -6,6 +6,49 @@ import { addNetworkPrefix } from './../../../utils/url';
 import { CacheKeys } from './cacheDefaults';
 import { setValue } from './useLocalStorage';
 
+//@dev for testing seperated the function from the hook and export
+export function migrateCacheToV1(): boolean {
+  // Migrate old cache keys to new format
+  const keys = Object.keys(localStorage);
+  const fractKeys = keys.filter(key => key.includes('fract'));
+  if (!fractKeys.length) {
+    return true;
+  }
+  // Get All Network Favorites
+  const favoritesCache = fractKeys.filter(key => key.endsWith('favorites'));
+  const newFavorites: string[] = [];
+
+  if (!favoritesCache.length) {
+    return true;
+  }
+  // loop through all favorites
+  for (const favorite of favoritesCache) {
+    // Get ChainId from favorite key
+    const [, chainId] = favorite.split('_');
+    if (Number.isNaN(Number(chainId))) {
+      continue;
+    }
+    const favoritesValue = localStorage.getItem(favorite);
+    if (favoritesValue) {
+      // Parse favorites value and add network prefix
+      const parsedValue: { v: string[] } = JSON.parse(favoritesValue);
+      parsedValue.v.forEach((value: string) => {
+        newFavorites.push(addNetworkPrefix(value, Number(chainId)));
+      });
+    }
+  }
+  if (newFavorites.length) {
+    // Set new Favorites cache
+    setValue({ cacheName: CacheKeys.FAVORITES }, newFavorites);
+  }
+
+  // delete old cache
+  fractKeys.forEach(key => {
+    localStorage.removeItem(key);
+  });
+  return true;
+}
+
 export const useMigrateLocalStorageV1 = () => {
   const isMounted = useRef(false);
   const [isMigrated, setIsMigrated] = useState(false);
@@ -14,45 +57,7 @@ export const useMigrateLocalStorageV1 = () => {
     // prevent multiple calls
     if (isMounted.current) return;
     if (isMigrated) return;
-
-    // Migrate old cache keys to new format
-    const keys = Object.keys(localStorage);
-    const fractKeys = keys.filter(key => key.includes('fract'));
-
-    if (!fractKeys.length) {
-      setIsMigrated(true);
-      return;
-    }
-    // Get All Network Favorites
-    const favoritesCache = fractKeys.filter(key => key.includes('favorites'));
-    const newFavorites: string[] = [];
-
-    if (!favoritesCache.length) {
-      setIsMigrated(true);
-      return;
-    }
-    // loop through all favorites
-    favoritesCache.forEach(favorite => {
-      // Get ChainId from favorite key
-      const [, chainId] = favorite.split('_');
-      const favoritesValue = localStorage.getItem(favorite);
-      if (favoritesValue) {
-        // Parse favorites value and add network prefix
-        const parsedValue: string[] = JSON.parse(favoritesValue);
-        parsedValue.forEach((value: string) => {
-          newFavorites.push(addNetworkPrefix(value, Number(chainId)));
-        });
-      }
-    });
-    // Set new Favorites cache
-    setValue({ cacheName: CacheKeys.FAVORITES }, newFavorites);
-
-    // delete old cache
-    fractKeys.forEach(key => {
-      localStorage.removeItem(key);
-    });
-
-    setIsMigrated(true);
+    setIsMigrated(migrateCacheToV1());
     isMounted.current = true;
   }, [isMigrated]);
 
