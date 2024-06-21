3fb9fa50cb9a75fe9e246c1251be14a0643dac8f
Adam Gall <galladamj@gmail.com>
2024-06-21 13:32:00 -0400

    Create some structure for New Hats Tree data

--- src/pages/daos/[daoAddress]/roles/index.tsx
+++ src/pages/daos/[daoAddress]/roles/index.tsx
@@ -1,18 +1,30 @@
 import { Box } from '@chakra-ui/react';
 import { useFractal } from '../../../../providers/App/AppProvider';
 
+function CreateNewHatsTree() {
+  return (
+    <Box mb="1rem">
+      <Box fontSize={'larger'}>Create New Hats Tree</Box>
+    </Box>
+  );
+}
+
 function Roles() {
   const { roles } = useFractal();
 
   return (
-    <Box
-      borderColor={'red'}
-      borderWidth={1}
-    >
-      {!roles.hatsTree && <Box>no hat tree :(</Box>}
+    <Box my="1rem">
+      <Box fontSize={'larger'}>Hat Tree</Box>
+      {roles.hatsTree === undefined && <Box>Searching for Hats Tree...</Box>}
+      {roles.hatsTree === null && (
+        <>
+          <Box mb="1rem">No Hats Tree exists for this Safe yet :(</Box>
+          <CreateNewHatsTree />
+        </>
+      )}
       {roles.hatsTree && (
         <Box>
-          <Box>we have hat tree! {roles.hatsTree.id}</Box>
+          <Box>We have a Hats Tree! {roles.hatsTree.id}</Box>
           <Box>it has {roles.hatsTree.hats ? roles.hatsTree.hats.length : 0} hats</Box>
         </Box>
       )}
