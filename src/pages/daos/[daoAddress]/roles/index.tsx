d22c59074e3f512cb6d2158d64fc3c4eb99c8dac
David Colon <38386583+Da-Colon@users.noreply.github.com>
2024-06-27 10:54:44 -0400

    add translation copy, add navigation to edit role

--- src/pages/daos/[daoAddress]/roles/index.tsx
+++ src/pages/daos/[daoAddress]/roles/index.tsx
@@ -1,17 +1,27 @@
 import { Box, Show, Text } from '@chakra-ui/react';
 import { Pencil } from '@phosphor-icons/react';
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
 import { zeroAddress } from 'viem';
 import { RoleCard } from '../../../../components/pages/Roles/RoleCard';
 import { RolesTable } from '../../../../components/pages/Roles/RolesTable';
 import { Card } from '../../../../components/ui/cards/Card';
 import { BarLoader } from '../../../../components/ui/loaders/BarLoader';
 import PageHeader from '../../../../components/ui/page/Header/PageHeader';
+import { DAO_ROUTES } from '../../../../constants/routes';
+import { useFractal } from '../../../../providers/App/AppProvider';
+import { useNetworkConfig } from '../../../../providers/NetworkConfig/NetworkConfigProvider';
 import { useRolesState } from '../../../../state/useRolesState';
 function Roles() {
   const { hatsTree } = useRolesState();
+  const { addressPrefix } = useNetworkConfig();
   const { t } = useTranslation(['roles', 'navigation', 'breadcrumbs', 'dashboard']);
+  const {
+    node: { daoAddress },
+  } = useFractal();
+  const navigate = useNavigate();
 
+  if (!daoAddress) return null;
   return (
     <Box>
       <PageHeader
@@ -30,7 +40,7 @@ function Roles() {
           leftIcon: <Pencil />,
         }}
         // @todo navigate to edit roles page
-        buttonClick={() => {}}
+        buttonClick={() => navigate(DAO_ROUTES.rolesEdit.relative(addressPrefix, daoAddress))}
       />
       {hatsTree === undefined && (
         <Card
