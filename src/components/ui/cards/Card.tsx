9ed3aca8d7e5adf99c946137b79fe9d359bb506f
David Colon <38386583+Da-Colon@users.noreply.github.com>
2024-06-27 01:37:28 -0400

    remove minHeight

--- src/components/ui/cards/Card.tsx
+++ src/components/ui/cards/Card.tsx
@@ -3,7 +3,6 @@ import { Box, BoxProps } from '@chakra-ui/react';
 export function Card({ children, ...rest }: BoxProps) {
   return (
     <Box
-      minHeight="6.25rem"
       bg="neutral-2"
       _hover={{ bg: 'neutral-3' }}
       _active={{ bg: 'neutral-2', border: '1px solid', borderColor: 'neutral-3' }}
