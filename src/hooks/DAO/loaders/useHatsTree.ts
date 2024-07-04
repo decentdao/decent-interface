54ed653795c8614b9a7d1e8c51a540dbc07c49d8
Kirill Klimenko <klimenkomud@gmail.com>
2024-07-04 19:42:28 +0200

    Read hat details from IPFS and cache it, then read it from the cache

--- src/hooks/DAO/loaders/useHatsTree.ts
+++ src/hooks/DAO/loaders/useHatsTree.ts
@@ -1,7 +1,11 @@
+import { Tree } from '@hatsprotocol/sdk-v1-subgraph';
 import { useEffect } from 'react';
 import { toast } from 'react-toastify';
+import useIPFSClient from '../../../providers/App/hooks/useIPFSClient';
 import { useNetworkConfig } from '../../../providers/NetworkConfig/NetworkConfigProvider';
 import { DecentHatsError, useRolesState } from '../../../state/useRolesState';
+import { CacheExpiry, CacheKeys } from '../../utils/cache/cacheDefaults';
+import { getValue, setValue } from '../../utils/cache/useLocalStorage';
 import { useHatsSubgraphClient } from './useHatsSubgraphClient';
 
 const useHatsTree = () => {
@@ -9,48 +13,81 @@ const useHatsTree = () => {
   const { chain } = useNetworkConfig();
   const hatsSubgraphClient = useHatsSubgraphClient();
   const { setHatsTree } = useRolesState();
+  const ipfsClient = useIPFSClient();
 
   useEffect(() => {
-    if (hatsTreeId === undefined || hatsTreeId === null) {
-      return;
-    }
-    hatsSubgraphClient
-      .getTree({
-        chainId: chain.id,
-        treeId: hatsTreeId,
-        props: {
-          hats: {
-            props: {
-              prettyId: true,
-              status: true,
-              createdAt: true,
-              details: true,
-              maxSupply: true,
-              eligibility: true,
-              toggle: true,
-              mutable: true,
-              levelAtLocalTree: true,
-              currentSupply: true,
-              wearers: {
-                props: {},
-                filters: {
-                  first: 1,
+    async function getHatsTree() {
+      if (hatsTreeId === undefined || hatsTreeId === null) {
+        return;
+      }
+      try {
+        const tree = await hatsSubgraphClient.getTree({
+          chainId: chain.id,
+          treeId: hatsTreeId,
+          props: {
+            hats: {
+              props: {
+                prettyId: true,
+                status: true,
+                createdAt: true,
+                details: true,
+                maxSupply: true,
+                eligibility: true,
+                toggle: true,
+                mutable: true,
+                levelAtLocalTree: true,
+                currentSupply: true,
+                wearers: {
+                  props: {},
+                  filters: {
+                    first: 1,
+                  },
                 },
               },
             },
           },
-        },
-      })
-      .then(tree => {
+        });
+        const hatsWithFetchedDetails = tree.hats
+          ? await Promise.all(
+              tree.hats.map(async hat => {
+                const ipfsPrefix = 'ipfs://';
+                if (hat.details && hat.details.includes(ipfsPrefix)) {
+                  const hash = hat.details.split(ipfsPrefix)[1];
+                  if (hash) {
+                    const cacheKey = {
+                      cacheName: CacheKeys.IPFS_HASH,
+                      hash,
+                      chainId: chain.id,
+                    } as const;
+                    const cachedDetails = getValue(cacheKey);
+                    if (cachedDetails) {
+                      return { ...hat, details: cachedDetails };
+                    } else {
+                      try {
+                        const detailsFromIpfs = await ipfsClient.cat(hash);
+                        const jsonStringDetails = JSON.stringify(detailsFromIpfs);
+                        setValue(cacheKey, jsonStringDetails, CacheExpiry.NEVER);
+                        return { ...hat, details: jsonStringDetails };
+                      } catch (e) {
+                        // Fuck it =/
+                      }
+                    }
+                  }
+                }
+                return hat;
+              }),
+            )
+          : tree.hats;
+
+        const treeWithFetchedDetails: Tree = { ...tree, hats: hatsWithFetchedDetails };
         try {
-          setHatsTree(tree);
+          setHatsTree(treeWithFetchedDetails);
         } catch (e) {
           if (e instanceof DecentHatsError) {
             toast(e.message);
           }
         }
-      })
-      .catch(() => {
+      } catch (e) {
         setHatsTree(undefined);
         const message = 'Hats Tree ID is not valid';
         toast(message);
@@ -58,11 +95,14 @@ const useHatsTree = () => {
           message,
           args: {
             network: chain.id,
-            hatsTreeId: hatsTreeId,
+            hatsTreeId,
           },
         });
-      });
-  }, [chain.id, hatsSubgraphClient, hatsTreeId, setHatsTree]);
+      }
+    }
+
+    getHatsTree();
+  }, [chain.id, hatsSubgraphClient, hatsTreeId, setHatsTree, ipfsClient]);
 };
 
 export { useHatsTree };
