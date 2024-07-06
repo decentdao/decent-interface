082bdc4fd899d305ec1eb39a6c206f0367820125
David Colon <38386583+Da-Colon@users.noreply.github.com>
2024-07-05 22:19:38 -0400

    run pretty/lint

--- src/components/pages/Roles/forms/RoleFormCreateProposal.tsx
+++ src/components/pages/Roles/forms/RoleFormCreateProposal.tsx
@@ -16,7 +16,7 @@ import { RoleFormValues } from '../types';
 
 export default function RoleFormCreateProposal({ close }: { close: () => void }) {
   const [drawerRoleIndex, setDrawerRoleIndex] = useState<number>();
-  console.log("🚀 ~ drawerRoleIndex:", drawerRoleIndex)
+  console.log('🚀 ~ drawerRoleIndex:', drawerRoleIndex);
   const { t } = useTranslation(['modals', 'common', 'proposal']);
   const { values, handleSubmit } = useFormikContext<RoleFormValues>();
   const editedRoles = useMemo(() => {
