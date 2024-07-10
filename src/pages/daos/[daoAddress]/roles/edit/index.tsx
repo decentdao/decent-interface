afbf8a2135da4b91527b1fa5a22848663b8af312
Adam Gall <galladamj@gmail.com>
2024-07-09 22:46:39 -0400

    Revert "Add useHatsListeners"
    This reverts commit f1528d72a1a9cea6e651e1a919477e64ab51eaad.

--- src/pages/daos/[daoAddress]/roles/edit/index.tsx
+++ src/pages/daos/[daoAddress]/roles/edit/index.tsx
@@ -20,7 +20,6 @@ import {
 import PageHeader from '../../../../../components/ui/page/Header/PageHeader';
 import { DAO_ROUTES } from '../../../../../constants/routes';
 import useSubmitProposal from '../../../../../hooks/DAO/proposal/useSubmitProposal';
-import { useHatsListeners } from '../../../../../hooks/DAO/useHatsListeners';
 import { useRolesSchema } from '../../../../../hooks/schemas/roles/useRolesSchema';
 import {
   parseEditedHatsFormValues,
@@ -46,8 +45,6 @@ function RolesEdit() {
 
   const { rolesSchema } = useRolesSchema();
   const { hatsTree, hatsTreeId, getHat } = useRolesState();
-
-  useHatsListeners();
 
   const navigate = useNavigate();
 
