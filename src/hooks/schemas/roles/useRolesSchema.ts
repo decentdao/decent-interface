852a2c3aa61cb2fcc6c9b92c19e09114ad3aa400
Kellar <kellar.base09@gmail.com>
2024-07-15 12:32:16 +0100

    Move role validation feedback text into translation

--- src/hooks/schemas/roles/useRolesSchema.ts
+++ src/hooks/schemas/roles/useRolesSchema.ts
@@ -1,10 +1,13 @@
 import { useMemo } from 'react';
+import { useTranslation } from 'react-i18next';
 import * as Yup from 'yup';
 import { RoleValue } from '../../../components/pages/Roles/types';
 import { useValidationAddress } from '../common/useValidationAddress';
 
 export const useRolesSchema = () => {
   const { addressValidationTest } = useValidationAddress();
+  const { t } = useTranslation(['roles']);
+
   const rolesSchema = useMemo(
     () =>
       Yup.object().shape({
@@ -15,13 +18,13 @@ export const useRolesSchema = () => {
             is: (roleEditing: RoleValue) => roleEditing !== undefined,
             then: _schema =>
               _schema.shape({
-                name: Yup.string().required('Role name is required'),
-                description: Yup.string().required('Role description is required'),
-                wearer: Yup.string().required('Member is required').test(addressValidationTest),
+                name: Yup.string().required(t('roleNameRequired')),
+                description: Yup.string().required(t('roleDescriptionRequired')),
+                wearer: Yup.string().required(t('roleMemberRequired')).test(addressValidationTest),
               }),
           }),
       }),
-    [addressValidationTest],
+    [addressValidationTest, t],
   );
   return { rolesSchema };
 };
