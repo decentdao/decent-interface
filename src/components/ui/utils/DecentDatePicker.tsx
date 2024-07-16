19716854cc0fcf91522f1ea87842ba500cc489af
Kellar <kellar.base09@gmail.com>
2024-07-16 15:48:25 +0100

    Tighten up calendar styles, cleanup unused

--- src/components/ui/utils/DecentDatePicker.tsx
+++ src/components/ui/utils/DecentDatePicker.tsx
@@ -1,14 +1,19 @@
 import { Box, Icon } from '@chakra-ui/react';
 import { CaretLeft, CaretRight } from '@phosphor-icons/react';
-
+import { useState } from 'react';
 import { Calendar } from 'react-calendar';
+
 import '../../../assets/css/Calendar.css';
 
 interface DecentDatePickerProps {
   minDate?: Date;
+  onChange?: (date: Date) => void;
 }
 
-export function DecentDatePicker({ minDate }: DecentDatePickerProps) {
+type DateOrNull = Date | null;
+type OnDateChangeValue = DateOrNull | [DateOrNull, DateOrNull];
+
+export function DecentDatePicker({ minDate, onChange }: DecentDatePickerProps) {
   const isToday = (someDate: Date) => {
     const today = new Date();
     return (
@@ -18,6 +23,8 @@ export function DecentDatePicker({ minDate }: DecentDatePickerProps) {
     );
   };
 
+  const [selectedDate, setSelectedDate] = useState<DateOrNull>();
+
   return (
     <Box
       display="flex"
@@ -26,33 +33,27 @@ export function DecentDatePicker({ minDate }: DecentDatePickerProps) {
       <Calendar
         formatShortWeekday={(_, date) => date.toString().slice(0, 2)}
         minDate={minDate || new Date()}
-        prevLabel={
-          <Icon
-            as={CaretLeft}
-            color="lilac-0"
-          />
-        }
-        nextLabel={
-          <Icon
-            as={CaretRight}
-            color="lilac-0"
-          />
-        }
+        prevLabel={<Icon as={CaretLeft} />}
+        nextLabel={<Icon as={CaretRight} />}
         next2Label={null}
         prev2Label={null}
         tileContent={({ date }) =>
           isToday(date) ? (
             <Box
               ml="1rem"
-              bg="white-1"
+              bg={selectedDate && isToday(selectedDate) ? 'cosmic-nebula-0' : 'white-1'}
               borderRadius="50%"
               w="4px"
               h="4px"
             />
           ) : null
         }
-        onChange={e => {
+        onChange={(e: OnDateChangeValue) => {
           console.log(e);
+          if (e instanceof Date) {
+            setSelectedDate(e);
+            onChange?.(e);
+          }
         }}
       />
     </Box>
