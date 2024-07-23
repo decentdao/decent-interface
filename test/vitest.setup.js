7027a058e9d33635ac3ec9967c44ff0676766678
David Colon <38386583+Da-Colon@users.noreply.github.com>
2024-07-23 15:17:43 -0400

    run pretty/lint

--- test/vitest.setup.js
+++ test/vitest.setup.js
@@ -3,12 +3,12 @@ import { vi } from 'vitest';
 
 vi.mock('viem', () => {
   return {
-    getAddress: (address) => {
+    getAddress: address => {
       if (!/^0x[a-fA-F0-9]{40}$/.test(address)) {
         throw new Error('Invalid Ethereum address');
       }
       return address;
     },
-    isAddress: (address) => /^0x[a-fA-F0-9]{40}$/.test(address),
+    isAddress: address => /^0x[a-fA-F0-9]{40}$/.test(address),
   };
 });
