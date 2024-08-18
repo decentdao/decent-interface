59c5dff7021f4e32d496f5e1c03fee6ada31a854
David Colon <38386583+Da-Colon@users.noreply.github.com>
2024-08-18 14:35:58 -0400

    update amount errors - add translations for amount error and amount required - adds test that amount entered is less than treasury balance.

--- src/hooks/schemas/roles/useRolesSchema.ts
+++ src/hooks/schemas/roles/useRolesSchema.ts
@@ -2,20 +2,35 @@ import { useMemo } from 'react';
 import { useTranslation } from 'react-i18next';
 import * as Yup from 'yup';
 import { SablierPayment, RoleValue, RoleFormValues } from '../../../components/pages/Roles/types';
+import { useFractal } from '../../../providers/App/AppProvider';
 import { useValidationAddress } from '../common/useValidationAddress';
 
 export const useRolesSchema = () => {
   const { t } = useTranslation(['roles']);
+  const {
+    treasury: { assetsFungible },
+  } = useFractal();
   const { addressValidationTest } = useValidationAddress();
 
-  const bigIntValidationSchema = Yup.object()
-    .shape({
-      value: Yup.string(),
-      bigintValue: Yup.mixed().nullable(),
-      // @todo - add validation for balance bigger than entered amount
-      // It's problematic at the moment due to how streams are passed into Zustand store
-    })
-    .required();
+  const bigIntValidationSchema = Yup.object().shape({
+    value: Yup.string().required(t('roleInfoErrorPaymentAmountRequired')),
+    bigintValue: Yup.mixed<bigint>()
+      .nullable()
+      .test({
+        name: 'bigInt',
+        message: t('roleInfoErrorPaymentInvalidAmount'),
+        test: (value, cxt) => {
+          if (!value || !cxt.from) return false;
+          // @dev finds the parent asset address from the formik context `from` array
+          const parentAssetAddress: string | undefined = cxt.from[1].value.asset.address;
+          if (!parentAssetAddress) return false;
+          const asset = assetsFungible.find(_asset => _asset.tokenAddress === parentAssetAddress);
+          if (!asset) return false;
+
+          return !isNaN(Number(value)) && value >= 0 && value <= BigInt(asset.balance);
+        },
+      }),
+  });
 
   const assetValidationSchema = Yup.object().shape({
     address: Yup.string(),
