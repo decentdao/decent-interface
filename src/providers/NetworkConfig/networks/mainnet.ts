466214de50fc5821e41b73b1cea4456aa83f1477
David Colon <38386583+Da-Colon@users.noreply.github.com>
2024-08-19 14:39:57 -0400

    add official module factory contract address

--- src/providers/NetworkConfig/networks/mainnet.ts
+++ src/providers/NetworkConfig/networks/mainnet.ts
@@ -62,7 +62,8 @@ export const mainnetConfig: NetworkConfig = {
       version: SAFE_VERSION,
       network: mainnet.id.toString(),
     })?.networkAddresses[mainnet.id.toString()]!,
-    zodiacModuleProxyFactory: ModuleProxyFactory.address,
+    zodiacModuleProxyFactory: '0x000000000000aDdB49795b0f9bA5BC298cDda236',
+    zodiacModuleProxyFactoryOld: ModuleProxyFactory.address,
     linearVotingMasterCopy: LinearERC20Voting.address,
     multisend: getMultiSendCallOnlyDeployment({
       version: SAFE_VERSION,
--- src/providers/NetworkConfig/networks/sepolia.ts
+++ src/providers/NetworkConfig/networks/sepolia.ts
@@ -62,7 +62,8 @@ export const sepoliaConfig: NetworkConfig = {
       version: SAFE_VERSION,
       network: sepolia.id.toString(),
     })?.networkAddresses[sepolia.id.toString()]!,
-    zodiacModuleProxyFactory: ModuleProxyFactory.address,
+    zodiacModuleProxyFactory: '0x000000000000aDdB49795b0f9bA5BC298cDda236',
+    zodiacModuleProxyFactoryOld: ModuleProxyFactory.address,
     linearVotingMasterCopy: LinearERC20Voting.address,
     multisend: getMultiSendCallOnlyDeployment({
       version: SAFE_VERSION,
