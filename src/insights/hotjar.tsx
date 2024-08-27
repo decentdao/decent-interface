ab54abf84ec97529bd7abfa176c845d198d688a2
Kellar <kellar.base09@gmail.com>
2024-08-27 14:58:57 +0100

    remove console log

--- src/insights/hotjar.tsx
+++ src/insights/hotjar.tsx
@@ -4,6 +4,5 @@ const hotjarSiteId = import.meta.env.VITE_APP_HOTJAR_SITE_ID;
 const hotjarVersion = import.meta.env.VITE_APP_HOTJAR_VERSION;
 
 if (hotjarSiteId !== undefined && hotjarVersion !== undefined) {
-  console.count('initHotjar from hotjar file');
   Hotjar.init(hotjarSiteId, hotjarVersion);
 }
