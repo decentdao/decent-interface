e5c4591148da0164dbcf764b0e6b9b60246a4a3f
Kyrylo Klymenko <klimenkomud@gmail.com>
2024-10-03 19:29:50 +0200

    Fix redirect loaders

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,24 @@
-import './insights';
+import '@fontsource/space-mono';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import { useNetworkConfig } from './providers/NetworkConfig/NetworkConfigProvider';
-import Providers from './providers/Providers';
-import { router } from './router';
-import '@fontsource/space-mono';
 import 'react-toastify/dist/ReactToastify.min.css';
 import './assets/css/Markdown.css';
 import './assets/css/sentry.css';
+import './insights';
+import { useNetworkConfig } from './providers/NetworkConfig/NetworkConfigProvider';
+import Providers from './providers/Providers';
+import { router } from './router';
 
-function FractalRouterProvider() {
+function DecentRouterProvider() {
   const { addressPrefix } = useNetworkConfig();
-  return <RouterProvider router={router(addressPrefix)} />;
+  const urlParams = new URLSearchParams(window.location.search);
+  const addressWithPrefix = urlParams.get('dao');
+
+  const prefixAndAddress = addressWithPrefix?.split(':');
+  const daoAddressStr = prefixAndAddress?.[1];
+
+  return <RouterProvider router={router(addressPrefix, daoAddressStr)} />;
 }
 
 const root = document.getElementById('root');
@@ -20,7 +26,7 @@ if (root !== null) {
   ReactDOM.createRoot(root).render(
     <React.StrictMode>
       <Providers>
-        <FractalRouterProvider />
+        <DecentRouterProvider />
       </Providers>
     </React.StrictMode>,
   );
