e747990959c25eed35fc6082ec21ce8d4058cd0d
David Colon <38386583+Da-Colon@users.noreply.github.com>
2024-10-10 11:01:41 -0400

    add abis

---
+++ src/assets/abi/DecentAutonomousAdminTempAbi.ts
@@ -0,0 +1,87 @@
+export default [
+  {
+    "inputs": [],
+    "name": "NAME",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "setUp",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "components": [
+          {
+            "internalType": "address",
+            "name": "currentWearer",
+            "type": "address"
+          },
+          {
+            "internalType": "contract IHats",
+            "name": "userHatProtocol",
+            "type": "address"
+          },
+          {
+            "internalType": "uint256",
+            "name": "userHatId",
+            "type": "uint256"
+          },
+          {
+            "internalType": "address",
+            "name": "nominatedWearer",
+            "type": "address"
+          },
+          {
+            "components": [
+              {
+                "internalType": "uint256",
+                "name": "streamId",
+                "type": "uint256"
+              },
+              {
+                "internalType": "contract ISablierV2LockupLinear",
+                "name": "sablierV2LockupLinear",
+                "type": "address"
+              }
+            ],
+            "internalType": "struct DecentAutonomousAdmin.SablierStreamInfo[]",
+            "name": "sablierStreamInfo",
+            "type": "tuple[]"
+          }
+        ],
+        "internalType": "struct DecentAutonomousAdmin.TriggerStartArgs",
+        "name": "args",
+        "type": "tuple"
+      }
+    ],
+    "name": "triggerStartNextTerm",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "version_",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+] as const;
