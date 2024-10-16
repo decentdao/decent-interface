72f034ae360f23f7c0cc1a2d6a4b3dae8204fc41
Adam Gall <galladamj@gmail.com>
2024-10-16 15:30:46 -0400

    Replace implementation of useEthersSigner to not use any provider context

--- src/providers/Ethers/hooks/useEthersSigner.ts
+++ src/providers/Ethers/hooks/useEthersSigner.ts
@@ -1,7 +1,19 @@
-import { useContext } from 'react';
-import { EthersContext } from '..';
+import { providers } from 'ethers';
+import { useMemo } from 'react';
+import { useWalletClient } from 'wagmi';
 
 export function useEthersSigner() {
-  const { signer } = useContext(EthersContext);
+  const { data: walletClient } = useWalletClient();
+
+  const signer = useMemo(() => {
+    if (!walletClient) {
+      return undefined;
+    }
+
+    const web3Provider = new providers.Web3Provider(walletClient.transport);
+    const jsonRpcSigner = web3Provider.getSigner(walletClient.account.address);
+    return jsonRpcSigner;
+  }, [walletClient]);
+
   return signer;
 }
