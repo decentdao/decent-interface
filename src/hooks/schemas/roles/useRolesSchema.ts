4d8e3a3fe6a6a6a80f85eac6b1d65a01ec5bf0e5
David Colon <38386583+Da-Colon@users.noreply.github.com>
2024-10-18 10:41:40 -0400

    update role form schema

--- src/hooks/schemas/roles/useRolesSchema.ts
+++ src/hooks/schemas/roles/useRolesSchema.ts
@@ -124,6 +124,34 @@ export const useRolesSchema = () => {
                           }),
                     }),
                 ),
+                roleTerms: Yup.array().of(
+                  Yup.object().shape({
+                    nominee: Yup.string()
+                      .required(t('roleInfoErrorMemberRequired'))
+                      .test(addressValidationTest),
+                    termEndDate: Yup.date()
+                      .required('roleInfoErrorTermEndDateRequired')
+                      .test({
+                        name: 'term-end-date-after-now',
+                        message: t('roleInfoErrorTermEndDateInvalid'),
+                        test: (termEndDate, cxt) => {
+                          if (!termEndDate || !cxt.from) return true;
+                          const [, { value: terms }] = cxt.from;
+                          // remove the last element from terms and create a new array with the rest of the elements
+                          const previousTerms = terms.slice(0, -1);
+                          if (
+                            previousTerms.every(
+                              (term: { termEndDate: Date }) =>
+                                term.termEndDate.getTime() < termEndDate.getTime(),
+                            )
+                          ) {
+                            return true;
+                          }
+                          return termEndDate > new Date();
+                        },
+                      }),
+                  }),
+                ),
               }),
           }),
       }),
