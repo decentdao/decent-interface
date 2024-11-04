63226faa3c5ba4a0dc28240111c7e41829e4aa71
Kyrylo Klymenko <klimenkomud@gmail.com>
2024-11-04 10:53:34 +0100

    Fix lint & prettier, add feature flag for whitelisting

--- src/components/pages/Roles/forms/RoleFormInfo.tsx
+++ src/components/pages/Roles/forms/RoleFormInfo.tsx
@@ -35,109 +35,38 @@ export default function RoleFormInfo() {
   const { displayName } = useGetAccountName(values.roleEditing?.resolvedWearer, false);
 
   return (
-    <Box
-      px={{ base: '1rem', md: 0 }}
-      py="1rem"
-      bg="neutral-2"
-      boxShadow={{
-        base: DETAILS_BOX_SHADOW,
-        md: 'unset',
-      }}
-      borderRadius="0.5rem"
-      display="flex"
-      flexDirection="column"
-      gap="1rem"
-    >
-      <FormControl>
-        <Field name="roleEditing.name">
-          {({
-            field,
-            form: { setFieldTouched },
-            meta,
-          }: {
-            field: FieldInputProps<string>;
-            form: FormikProps<RoleFormValues>;
-            meta: FieldMetaProps<string>;
-          }) => (
-            <InputComponent
-              value={field.value ?? ''}
-              onChange={e => {
-                setFieldValue('roleEditing.name', e.target.value);
-              }}
-              onBlur={() => {
-                setFieldTouched('roleEditing.name', true);
-              }}
-              testId="role-name"
-              placeholder={t('roleName')}
-              isRequired
-              gridContainerProps={{
-                gridTemplateColumns: { base: '1fr', md: '1fr' },
-              }}
-              inputContainerProps={{
-                p: 0,
-              }}
-              label={t('roleName')}
-              errorMessage={meta.touched && meta.error ? meta.error : undefined}
-            />
-          )}
-        </Field>
-      </FormControl>
-      <FormControl>
-        <Field name="roleEditing.description">
-          {({
-            field,
-            form: { setFieldTouched },
-            meta,
-          }: {
-            field: FieldInputProps<string>;
-            form: FormikProps<RoleFormValues>;
-            meta: FieldMetaProps<string>;
-          }) => (
-            <TextareaComponent
-              value={field.value ?? ''}
-              onChange={e => {
-                setFieldValue('roleEditing.description', e.target.value);
-              }}
-              isRequired
-              gridContainerProps={{
-                gridTemplateColumns: { base: '1fr', md: '1fr' },
-              }}
-              inputContainerProps={{
-                p: 0,
-              }}
-              textAreaProps={{
-                h: '12rem',
-                onBlur: () => {
-                  setFieldTouched('roleEditing.description', true);
-                },
-              }}
-              label={t('roleDescription')}
-              errorMessage={meta.touched && meta.error ? meta.error : undefined}
-            />
-          )}
-        </Field>
-      </FormControl>
-      <FormControl>
-        <Field name="roleEditing.wearer">
-          {({
-            field,
-            form: { setFieldTouched },
-            meta,
-          }: {
-            field: FieldInputProps<string>;
-            form: FormikProps<RoleFormValues>;
-            meta: FieldMetaProps<string>;
-          }) => (
-            <LabelWrapper
-              label={t('member')}
-              errorMessage={meta.touched && meta.error ? meta.error : undefined}
-              isRequired
-              labelColor="neutral-7"
-            >
-              <AddressInput
-                value={displayName ?? field.value}
+    <>
+      <Box
+        px={{ base: '1rem', md: 0 }}
+        py="1rem"
+        bg="neutral-2"
+        boxShadow={{
+          base: DETAILS_BOX_SHADOW,
+          md: 'unset',
+        }}
+        borderRadius="0.5rem"
+        display="flex"
+        flexDirection="column"
+        gap="1rem"
+      >
+        <FormControl>
+          <Field name="roleEditing.name">
+            {({
+              field,
+              form: { setFieldTouched },
+              meta,
+            }: {
+              field: FieldInputProps<string>;
+              form: FormikProps<RoleFormValues>;
+              meta: FieldMetaProps<string>;
+            }) => (
+              <InputComponent
+                value={field.value ?? ''}
+                onChange={e => {
+                  setFieldValue('roleEditing.name', e.target.value);
+                }}
                 onBlur={() => {
-                  setFieldTouched('roleEditing.wearer', true);
+                  setFieldTouched('roleEditing.name', true);
                 }}
                 testId="role-name"
                 placeholder={t('roleName')}
@@ -156,7 +85,79 @@ export default function RoleFormInfo() {
         </FormControl>
         <FormControl>
           <Field name="roleEditing.description">
-            {({ field, form: { setFieldTouched }, meta }) => (
+            {({
+              field,
+              form: { setFieldTouched },
+              meta,
+            }: {
+              field: FieldInputProps<string>;
+              form: FormikProps<RoleFormValues>;
+              meta: FieldMetaProps<string>;
+            }) => (
+              <TextareaComponent
+                value={field.value ?? ''}
+                onChange={e => {
+                  setFieldValue('roleEditing.description', e.target.value);
+                }}
+                isRequired
+                gridContainerProps={{
+                  gridTemplateColumns: { base: '1fr', md: '1fr' },
+                }}
+                inputContainerProps={{
+                  p: 0,
+                }}
+                textAreaProps={{
+                  h: '12rem',
+                  onBlur: () => {
+                    setFieldTouched('roleEditing.description', true);
+                  },
+                }}
+                label={t('roleDescription')}
+                errorMessage={meta.touched && meta.error ? meta.error : undefined}
+              />
+            )}
+          </Field>
+        </FormControl>
+        <FormControl>
+          <Field name="roleEditing.wearer">
+            {({
+              field,
+              form: { setFieldTouched },
+              meta,
+            }: {
+              field: FieldInputProps<string>;
+              form: FormikProps<RoleFormValues>;
+              meta: FieldMetaProps<string>;
+            }) => (
+              <LabelWrapper
+                label={t('member')}
+                errorMessage={meta.touched && meta.error ? meta.error : undefined}
+                isRequired
+                labelColor="neutral-7"
+              >
+                <AddressInput
+                  value={displayName ?? field.value}
+                  onBlur={() => {
+                    setFieldTouched('roleEditing.wearer', true);
+                  }}
+                  placeholder={t('roleName')}
+                  isRequired
+                />
+              </LabelWrapper>
+            )}
+          </Field>
+        </FormControl>
+        <FormControl>
+          <Field name="roleEditing.description">
+            {({
+              field,
+              form: { setFieldTouched },
+              meta,
+            }: {
+              field: FieldInputProps<string>;
+              form: FormikProps<RoleFormValues>;
+              meta: FieldMetaProps<string>;
+            }) => (
               <TextareaComponent
                 value={field.value || ''}
                 onChange={e => {
@@ -183,7 +184,15 @@ export default function RoleFormInfo() {
         </FormControl>
         <FormControl>
           <Field name="roleEditing.wearer">
-            {({ field, form: { setFieldTouched }, meta }) => (
+            {({
+              field,
+              form: { setFieldTouched },
+              meta,
+            }: {
+              field: FieldInputProps<string>;
+              form: FormikProps<RoleFormValues>;
+              meta: FieldMetaProps<string>;
+            }) => (
               <LabelWrapper
                 label={t('member')}
                 errorMessage={meta.touched && meta.error ? meta.error : undefined}
@@ -207,7 +216,7 @@ export default function RoleFormInfo() {
         </FormControl>
       </Box>
       {/* @dev - deploying whitelisting voting strategy is feasible from UI/UX standpoint only when Safe has Azorius module AND hatsTree been created already */}
-      {type !== GovernanceType.MULTISIG && !!hatsTree && !!hatsTreeId && (
+      {type !== GovernanceType.MULTISIG && !!hatsTree && !!hatsTreeId && import.meta.env.VITE_APP_FLAG_WHITELISTING === 'ON' && (
         <FormControl mt={4}>
           <Field name="roleEditing.canCreateProposals">
             {() => (
