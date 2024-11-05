d9cb2ecf11c5d104fe892aff61d01132ebaa95f4
Kellar <kellar.base09@gmail.com>
2024-11-05 14:54:00 +0000

    If it turns out this was an unsafe removal, revert

--- src/types/daoTreasury.ts
+++ src/types/daoTreasury.ts
@@ -7,7 +7,6 @@ export enum TokenEventType {
 
 export interface TokenEvent {
   transactionHash: string;
-  blockNumber: number; // TODO: PROBABLY not being used. Remove?
   eventType: TokenEventType;
   blockTimestamp: number;
 }
