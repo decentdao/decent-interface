2b2a3e92ab5324b4820dbfda03101d7d7eaccfa4
Kellar <kellar.base09@gmail.com>
2024-11-11 18:16:04 +0000

    Ignore unconnected wallet in demo mode

--- src/constants/common.ts
+++ src/constants/common.ts
@@ -56,6 +56,7 @@ export const MAX_CONTENT_WIDTH = '80rem';
 const features = {
   developmentMode: 'DEVELOPMENT_MODE',
   termedRoles: 'TERMED_ROLES',
+  demoMode: 'DEMO_MODE',
 } as const;
 
 type FeatureFlag = (typeof features)[keyof typeof features];
@@ -71,6 +72,7 @@ export const isFeatureEnabled = (feature: FeatureFlag) => {
 
 export const isDevMode = () => isFeatureEnabled(features.developmentMode);
 export const isTermedRolesEnabled = () => isFeatureEnabled(features.termedRoles);
+export const isDemoMode = () => isFeatureEnabled(features.demoMode);
 
 /**
  * @dev DO NOT CHANGE THE SALT
