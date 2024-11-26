14e2233ac55a40319f02cecbd2495b8730f5cb35
Kellar <kellar.base09@gmail.com>
2024-11-26 15:54:45 +0000

    Move create subdao to organisations page

--- src/components/ui/menus/ManageDAO/ManageDAOMenu.tsx
+++ src/components/ui/menus/ManageDAO/ManageDAOMenu.tsx
@@ -109,15 +109,6 @@ export function ManageDAOMenu() {
   );
 
   const options = useMemo(() => {
-    const createSubDAOOption = {
-      optionKey: 'optionCreateSubDAO',
-
-      onClick: () => {
-        if (safeAddress) {
-          navigate(DAO_ROUTES.newSubDao.relative(addressPrefix, safeAddress));
-        }
-      },
-    };
     const clawBackOption = {
       optionKey: 'optionInitiateClawback',
       onClick: handleClawBack,
@@ -146,9 +137,9 @@ export function ManageDAOMenu() {
       guard.userHasVotes
     ) {
       if (type === GovernanceType.MULTISIG) {
-        return [createSubDAOOption, freezeOption, modifyGovernanceOption, settingsOption];
+        return [freezeOption, modifyGovernanceOption, settingsOption];
       } else {
-        return [createSubDAOOption, freezeOption, settingsOption];
+        return [freezeOption, settingsOption];
       }
     } else if (
       guard.freezeProposalCreatedTime !== null &&
@@ -168,7 +159,6 @@ export function ManageDAOMenu() {
     } else {
       const optionsArr = [];
       if (canUserCreateProposal) {
-        optionsArr.push(createSubDAOOption);
         if (type === GovernanceType.MULTISIG) {
           optionsArr.push(modifyGovernanceOption);
         }
@@ -179,14 +169,11 @@ export function ManageDAOMenu() {
   }, [
     guard,
     currentTime,
-    navigate,
-    safeAddress,
     type,
     handleClawBack,
     canUserCreateProposal,
     handleModifyGovernance,
     handleNavigateToSettings,
-    addressPrefix,
     freezeOption,
     node.fractalModules,
   ]);
--- src/pages/dao/hierarchy/SafeHierarchyPage.tsx
+++ src/pages/dao/hierarchy/SafeHierarchyPage.tsx
@@ -2,11 +2,15 @@ import * as amplitude from '@amplitude/analytics-browser';
 import { Box, Center } from '@chakra-ui/react';
 import { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
+import { useNavigate } from 'react-router-dom';
 import { DaoHierarchyNode } from '../../../components/DaoHierarchy/DaoHierarchyNode';
 import { BarLoader } from '../../../components/ui/loaders/BarLoader';
 import PageHeader from '../../../components/ui/page/Header/PageHeader';
 import { useHeaderHeight } from '../../../constants/common';
+import { DAO_ROUTES } from '../../../constants/routes';
+import { useCanUserCreateProposal } from '../../../hooks/utils/useCanUserSubmitProposal';
 import { analyticsEvents } from '../../../insights/analyticsEvents';
+import { useNetworkConfig } from '../../../providers/NetworkConfig/NetworkConfigProvider';
 import { useDaoInfoStore } from '../../../store/daoInfo/useDaoInfoStore';
 
 export function SafeHierarchyPage() {
@@ -24,6 +28,10 @@ export function SafeHierarchyPage() {
   const { t } = useTranslation('breadcrumbs');
 
   const HEADER_HEIGHT = useHeaderHeight();
+  const navigate = useNavigate();
+  const { addressPrefix } = useNetworkConfig();
+
+  const { canUserCreateProposal } = useCanUserCreateProposal();
 
   const safeAddress = safe?.address;
 
@@ -48,6 +56,19 @@ export function SafeHierarchyPage() {
             path: '',
           },
         ]}
+        buttonProps={
+          canUserCreateProposal
+            ? {
+                children: t('optionCreateSubDAO', { ns: 'menu' }),
+                onClick: () => {
+                  if (safeAddress) {
+                    navigate(DAO_ROUTES.newSubDao.relative(addressPrefix, safeAddress));
+                  }
+                },
+                size: 'sm',
+              }
+            : undefined
+        }
       />
       <DaoHierarchyNode
         safeAddress={parentAddress || safeAddress}
