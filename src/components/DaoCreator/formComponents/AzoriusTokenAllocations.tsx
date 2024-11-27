ef499410b1dc9529505d1e0de34cb51d6f9bb1f2
Kellar <kellar.base09@gmail.com>
2024-11-27 20:39:18 +0000

    Fix allocation input spacing

--- src/components/DaoCreator/formComponents/AzoriusTokenAllocations.tsx
+++ src/components/DaoCreator/formComponents/AzoriusTokenAllocations.tsx
@@ -40,7 +40,7 @@ export function AzoriusTokenAllocations(props: ICreationStepProps) {
             <Grid
               gridTemplateColumns="1fr 35% 2rem"
               columnGap={4}
-              rowGap={2}
+              rowGap={8}
               data-testid="tokenVoting-tokenAllocations"
             >
               <Text>{t('titleAddress')}</Text>
@@ -80,9 +80,11 @@ export function AzoriusTokenAllocations(props: ICreationStepProps) {
                 );
               })}
             </Grid>
+
             <Text
               color="neutral-7"
               textStyle="helper-text-base"
+              mt={4}
             >
               {t('helperAllocations')}
             </Text>
