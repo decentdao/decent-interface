129912fb0c88f56516da798396ac8a4a09757d79
David Colon <38386583+Da-Colon@users.noreply.github.com>
2024-11-30 05:57:24 -0500

    add navigation icons for social media links in footer

--- src/components/ui/page/Footer.tsx
+++ src/components/ui/page/Footer.tsx
@@ -1,10 +1,49 @@
-import { Flex } from '@chakra-ui/react';
+import { Box, ComponentWithAs, Flex, Icon as ChakraIcon, IconProps } from '@chakra-ui/react';
+import { DiscordLogo, Icon, TelegramLogo, XLogo } from '@phosphor-icons/react';
 import { useTranslation } from 'react-i18next';
-import { URL_DECENT } from '../../../constants/url';
+import { Link } from 'react-router-dom';
+import { WarpcastIcon } from '../../../assets/theme/custom/icons/WarpcastIcon';
+import {
+  URL_DECENT,
+  URL_DISCORD,
+  URL_TELEGRAM,
+  URL_TWITTER,
+  URL_WARPCAST,
+} from '../../../constants/url';
 import ExternalLink from '../links/ExternalLink';
 
+function NavigationIconLink(props: {
+  DisplayIcon: Icon | ComponentWithAs<'svg', IconProps>;
+  to: string;
+  ariaLabel: string;
+}) {
+  const { t } = useTranslation('navigation');
+  const { DisplayIcon, to, ariaLabel } = props;
+  return (
+    <Link
+      aria-label={t(ariaLabel)}
+      to={to}
+    >
+      <Box p="0.25rem">
+        <Flex
+          py="6px"
+          px="6px"
+          borderRadius={{ md: 4 }}
+          transition="all ease-out 300ms"
+          _hover={{ bgColor: 'neutral-3' }}
+        >
+          <ChakraIcon
+            as={DisplayIcon}
+            boxSize="1rem"
+          />
+        </Flex>
+      </Box>
+    </Link>
+  );
+}
+
 export function Footer() {
-  const { t } = useTranslation('home');
+  const { t } = useTranslation('navigation');
   return (
     <Flex
       w="100%"
@@ -29,6 +68,32 @@ export function Footer() {
       >
         {t('madeWithLove')}
       </ExternalLink>
+
+      <Flex
+        gap={4}
+        alignItems="center"
+      >
+        <NavigationIconLink
+          to={URL_TELEGRAM}
+          ariaLabel="navigationExternalTelegram"
+          DisplayIcon={TelegramLogo}
+        />
+        <NavigationIconLink
+          to={URL_TWITTER}
+          ariaLabel="navigationExternalX"
+          DisplayIcon={XLogo}
+        />
+        <NavigationIconLink
+          to={URL_WARPCAST}
+          ariaLabel="navigationExternalWarpcast"
+          DisplayIcon={WarpcastIcon}
+        />
+        <NavigationIconLink
+          to={URL_DISCORD}
+          ariaLabel="navigationExternalDiscord"
+          DisplayIcon={DiscordLogo}
+        />
+      </Flex>
     </Flex>
   );
 }
--- src/constants/url.ts
+++ src/constants/url.ts
@@ -1,5 +1,5 @@
 export const URL_FAQ = 'https://docs.decentdao.org/app/learn-more/faq';
-export const URL_CHAT = 'https://warpcast.com/~/channel/decentdao';
+export const URL_WARPCAST = 'https://warpcast.com/~/channel/decentdao';
 export const URL_DOCS = 'https://docs.decentdao.org/app';
 export const URL_DOCS_GOV_TYPES = 'https://docs.decentdao.org/app/user-guide/types';
 export const URL_DECENT = 'https://www.decentdao.org/';
