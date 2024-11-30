f918c39434e292ebe3fc3dda1f7ada50d4b9a3ec
David Colon <38386583+Da-Colon@users.noreply.github.com>
2024-11-30 05:55:35 -0500

    add social media URLs for Twitter, Telegram, and Discord

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -3,3 +3,6 @@ export const URL_CHAT = 'https://warpcast.com/~/channel/decentdao';
 export const URL_DOCS = 'https://docs.decentdao.org/app';
 export const URL_DOCS_GOV_TYPES = 'https://docs.decentdao.org/app/user-guide/types';
 export const URL_DECENT = 'https://www.decentdao.org/';
+export const URL_TWITTER = 'https://twitter.com/'; // @todo - Add Twitter handle;
+export const URL_TELEGRAM = 'https://t.me/'; // @todo - Add Telegram handle;
+export const URL_DISCORD = 'https://discord.gg/'; // @todo - Add Discord handle;
