819a2b2ffe2ec9e30dec22dfa406e29d07aa9070
David Colon <38386583+Da-Colon@users.noreply.github.com>
2024-12-02 15:46:41 -0500

    fix: update Telegram URL with the correct handle

--- src/constants/url.ts
+++ src/constants/url.ts
@@ -4,5 +4,5 @@ export const URL_DOCS = 'https://docs.decentdao.org/app';
 export const URL_DOCS_GOV_TYPES = 'https://docs.decentdao.org/app/user-guide/types';
 export const URL_DECENT = 'https://www.decentdao.org/';
 export const URL_TWITTER = 'https://x.com/decentdao';
-export const URL_TELEGRAM = 'https://t.me/'; // @todo - Add Telegram handle;
+export const URL_TELEGRAM = 'https://t.me/+ZmKrTaL3pQcyOGRh';
 export const URL_DISCORD = 'https://discord.gg/Zh2emKspVF';
