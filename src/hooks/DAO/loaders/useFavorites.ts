5f8c02bac7d6ef5e460910749c8070d8cab98e23
John Huang <john.huang@decentdao.org>
2024-12-12 11:28:48 -0800

    ENG-1 Fixed "My DAOs" menu update issue when DAOs are favorited/unfavorited.

--- src/hooks/DAO/loaders/useFavorites.ts
+++ src/hooks/DAO/loaders/useFavorites.ts
@@ -1,13 +1,22 @@
 import { useState } from 'react';
+import { useBetween } from 'use-between';
 import { Address } from 'viem';
 import { useNetworkConfig } from '../../../providers/NetworkConfig/NetworkConfigProvider';
 import { CacheKeys, CacheExpiry, FavoritesCacheValue } from '../../utils/cache/cacheDefaults';
 import { getValue, setValue } from '../../utils/cache/useLocalStorage';
 
-export const useAccountFavorites = () => {
+const useSharedAccountFavorites = () => {
   const [favoritesList, setFavoritesList] = useState<FavoritesCacheValue[]>(
     getValue({ cacheName: CacheKeys.FAVORITES }) || [],
   );
+  return {
+    favoritesList,
+    setFavoritesList
+  };
+};
+
+export const useAccountFavorites = () => {
+  const { favoritesList, setFavoritesList } = useBetween(useSharedAccountFavorites)
   const { addressPrefix } = useNetworkConfig();
 
   const toggleFavorite = (address: Address, name: string) => {
