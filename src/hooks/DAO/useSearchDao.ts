d2319e573f360248b283f9edc8df393916c3075d
David Colon <38386583+Da-Colon@users.noreply.github.com>
2024-12-13 00:11:29 -0500

    Enhance useIsSafe hook to search across all supported networks and return found network prefixes

--- src/hooks/DAO/useSearchDao.ts
+++ src/hooks/DAO/useSearchDao.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
 import { useNetworkConfigStore } from '../../providers/NetworkConfig/useNetworkConfigStore';
 import { useIsSafe } from '../safe/useIsSafe';
@@ -7,9 +7,10 @@ import useAddress from '../utils/useAddress';
 export const useSearchDao = () => {
   const [searchString, setSearchString] = useState<string>('');
   const [errorMessage, setErrorMessage] = useState<string>();
+  // This hook needs to search all supoorted chains for the address
 
   const { address, isValid, isLoading: isAddressLoading } = useAddress(searchString);
-  const { isSafe, isSafeLoading } = useIsSafe(address);
+  const { isSafe, isSafeLoading, safeFoundNetworkPrefixes } = useIsSafe(address);
   const { t } = useTranslation('dashboard');
   const { chain } = useNetworkConfigStore();
 
@@ -21,15 +22,15 @@ export const useSearchDao = () => {
     if (searchString === '' || isLoading || isSafe || isValid === undefined) {
       return;
     }
-
     if (isValid === true) {
-      setErrorMessage(t('errorFailedSearch', { chain: chain.name }));
+      setErrorMessage(t('errorFailedSearch'));
     } else {
       setErrorMessage(t('errorInvalidSearch'));
     }
   }, [chain.name, isLoading, isSafe, isValid, searchString, t]);
 
   return {
+    safeFoundNetworkPrefixes,
     errorMessage,
     isLoading,
     address,
--- src/hooks/safe/useIsSafe.ts
+++ src/hooks/safe/useIsSafe.ts
@@ -1,6 +1,7 @@
-import { useEffect, useState } from 'react';
+import SafeApiKit from '@safe-global/api-kit';
+import { useCallback, useEffect, useState } from 'react';
 import { isAddress } from 'viem';
-import { useSafeAPI } from '../../providers/App/hooks/useSafeAPI';
+import { supportedNetworks } from '../../providers/NetworkConfig/useNetworkConfigStore';
 
 /**
  * A hook which determines whether the provided Ethereum address is a Safe
@@ -15,25 +16,43 @@ import { useSafeAPI } from '../../providers/App/hooks/useSafeAPI';
  */
 export const useIsSafe = (address: string | undefined) => {
   const [isSafeLoading, setSafeLoading] = useState<boolean>(false);
-  const [isSafe, setIsSafe] = useState<boolean | undefined>();
-  const safeAPI = useSafeAPI();
+  const [isSafe, setIsSafe] = useState<boolean | undefined>(undefined);
+  const [safeFoundNetworkPrefixes, setNetworkPrefixes] = useState<string[]>([]);
+
+  const findSafes = useCallback(async (_address: string) => {
+    const networkPrefixes = []; // address prefixes
+    for await (const network of supportedNetworks) {
+      const safeAPI = new SafeApiKit({ chainId: BigInt(network.chain.id) });
+      safeAPI.getSafeCreationInfo(_address);
+      try {
+        await safeAPI.getSafeCreationInfo(_address);
+        networkPrefixes.push(network.addressPrefix);
+      } catch (e) {
+        // Safe not found
+        continue;
+      }
+    }
+    return [networkPrefixes, networkPrefixes.length > 0] as const; // [networks, isSafe]
+  }, []);
 
   useEffect(() => {
     setSafeLoading(true);
     setIsSafe(undefined);
 
-    if (!address || !isAddress(address) || !safeAPI) {
+    if (!address || !isAddress(address)) {
       setIsSafe(false);
       setSafeLoading(false);
       return;
     }
 
-    safeAPI
-      .getSafeCreationInfo(address)
-      .then(() => setIsSafe(true))
+    findSafes(address)
+      .then(([_safeFoundNetworkPrefixes, _isSafe]) => {
+        setNetworkPrefixes(_safeFoundNetworkPrefixes);
+        setIsSafe(_isSafe);
+      })
       .catch(() => setIsSafe(false))
       .finally(() => setSafeLoading(false));
-  }, [address, safeAPI]);
+  }, [address, findSafes]);
 
-  return { isSafe, isSafeLoading };
+  return { isSafe, isSafeLoading, safeFoundNetworkPrefixes };
 };
