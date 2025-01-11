8cd6ce820e39002663312367c1e1757944fde3b6
Kyrylo Klymenko <klimenkomud@gmail.com>
2025-01-11 13:23:05 +0100

    Remove commented out code

--- src/components/ProposalTemplates/ExampleTemplateCard.tsx
+++ src/components/ProposalTemplates/ExampleTemplateCard.tsx
@@ -1,5 +1,4 @@
 import { Avatar, Flex, Text } from '@chakra-ui/react';
-// import { useCanUserCreateProposal } from '../../hooks/utils/useCanUserSubmitProposal';
 import ContentBox from '../ui/containers/ContentBox';
 import Markdown from '../ui/proposal/Markdown';
 
@@ -14,8 +13,6 @@ export default function ExampleTemplateCard({
   description,
   onProposalTemplateClick,
 }: ExampleTemplateCardProps) {
-  // const { canUserCreateProposal } = useCanUserCreateProposal();
-
   return (
     <ContentBox
       containerBoxProps={{ flex: '0 0 calc(33.333333% - 0.6666666rem)', my: '0' }}
