202af38be3650535c73ba28898860f45821074e5
David Colon <38386583+Da-Colon@users.noreply.github.com>
2025-01-16 23:33:19 -0500

    Update multicall configuration in useNetworkPublicClient to include a wait time

--- src/hooks/useNetworkPublicClient.ts
+++ src/hooks/useNetworkPublicClient.ts
@@ -9,7 +9,9 @@ export default function useNetworkPublicClient() {
       createPublicClient({
         chain,
         batch: {
-          multicall: true,
+          multicall: {
+            wait: 500,
+          },
         },
         transport: http(rpcEndpoint),
       }),
