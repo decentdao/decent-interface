430bafa66ef90f7ec7dfb5e5bf9d6113d5e04cb8
David Colon <38386583+Da-Colon@users.noreply.github.com>
2025-01-23 18:45:07 -0500

    Refactor useNetworkEnsAddressAsync to improve network handling and remove public client dependency

--- src/hooks/useNetworkEnsAddress.ts
+++ src/hooks/useNetworkEnsAddress.ts
@@ -1,15 +1,15 @@
 import { useCallback } from 'react';
+import { createPublicClient, http } from 'viem';
 import { mainnet } from 'viem/chains';
 import { useEnsAddress } from 'wagmi';
 import {
   supportedEnsNetworks,
   useNetworkConfigStore,
 } from '../providers/NetworkConfig/useNetworkConfigStore';
-import useNetworkPublicClient from './useNetworkPublicClient';
 
 interface UseNetworkEnsAddressProps {
-  name?: string;
   chainId?: number;
+  name?: string;
 }
 
 export function useNetworkEnsAddress(props?: UseNetworkEnsAddressProps) {
@@ -21,21 +21,23 @@ export function useNetworkEnsAddress(props?: UseNetworkEnsAddressProps) {
   return useEnsAddress({ name: props?.name, chainId: ensNetworkOrMainnet });
 }
 
-export function useNetworkEnsAddressAsync(props?: UseNetworkEnsAddressProps) {
-  const { chain } = useNetworkConfigStore();
-  const ensNetworkOrMainnet = supportedEnsNetworks.includes(props?.chainId ?? chain.id)
-    ? chain.id
-    : mainnet.id;
-
-  const publicClient = useNetworkPublicClient({
-    chainId: ensNetworkOrMainnet,
-  });
+export function useNetworkEnsAddressAsync() {
+  const { chain, getConfigByChainId } = useNetworkConfigStore();
 
   const getEnsAddress = useCallback(
-    (args: { name: string }) => {
+    (args: { name: string; chainId?: number }) => {
+      const ensNetworkOrMainnet = supportedEnsNetworks.includes(args?.chainId ?? chain.id)
+        ? chain.id
+        : mainnet.id;
+
+      const networkConfig = getConfigByChainId(ensNetworkOrMainnet);
+      const publicClient = createPublicClient({
+        chain: networkConfig.chain,
+        transport: http(networkConfig.rpcEndpoint),
+      });
       return publicClient.getEnsAddress({ name: args.name });
     },
-    [publicClient],
+    [chain, getConfigByChainId],
   );
 
   return { getEnsAddress };
