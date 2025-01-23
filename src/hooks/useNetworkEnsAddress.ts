6a6de0a5b6406bd9c847957743c2e97de6ea7831
David Colon <38386583+Da-Colon@users.noreply.github.com>
2025-01-23 07:29:01 -0500

    Refactor ENS address retrieval to use custom hook for network-specific handling

---
+++ src/hooks/useNetworkEnsAddress.ts
@@ -0,0 +1,20 @@
+import { mainnet } from 'viem/chains';
+import { useEnsAddress } from 'wagmi';
+import {
+  supportedEnsNetworks,
+  useNetworkConfigStore,
+} from '../providers/NetworkConfig/useNetworkConfigStore';
+
+interface UseNetworkEnsAddressProps {
+  name?: string;
+  chainId?: number;
+}
+
+export function useNetworkEnsAddress(props: UseNetworkEnsAddressProps | undefined) {
+  const { chain } = useNetworkConfigStore();
+  const ensNetworkOrMainnet = supportedEnsNetworks.includes(props?.chainId ?? chain.id)
+    ? chain.id
+    : mainnet.id;
+
+  return useEnsAddress({ name: props?.name, chainId: ensNetworkOrMainnet });
+}
