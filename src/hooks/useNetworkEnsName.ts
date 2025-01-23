352b8117e4061a2ea03655b980dc31751e6b9aef
David Colon <38386583+Da-Colon@users.noreply.github.com>
2025-01-23 08:05:12 -0500

    Refactor ENS name retrieval to use useNetworkEnsName hook for improved network handling

---
+++ src/hooks/useNetworkEnsName.ts
@@ -0,0 +1,21 @@
+import { Address } from 'viem';
+import { mainnet } from 'viem/chains';
+import { useEnsName } from 'wagmi';
+import {
+  supportedEnsNetworks,
+  useNetworkConfigStore,
+} from '../providers/NetworkConfig/useNetworkConfigStore';
+
+interface UseNetworkEnsNameProps {
+  address?: Address;
+  chainId?: number;
+}
+
+export function useNetworkEnsName(props: UseNetworkEnsNameProps | undefined) {
+  const { chain } = useNetworkConfigStore();
+  const ensNetworkOrMainnet = supportedEnsNetworks.includes(props?.chainId ?? chain.id)
+    ? chain.id
+    : mainnet.id;
+
+  return useEnsName({ address: props?.address, chainId: ensNetworkOrMainnet });
+}
